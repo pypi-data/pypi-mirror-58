c7c7b8dae07e52fce126f01243574524ac55d5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package mypy-boto3-elbv2/1.10.42.1/mypy-boto3-elbv2-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-elbv2/mypy-boto3-elbv2-1.10.42.1.tar.gz/mypy-boto3-elbv2-1.10.42.1/mypy_boto3_elbv2/__init__.py
@@ -0,0 +1,39 @@
+"Main interface for elbv2 service"
+from mypy_boto3_elbv2.client import (
+    ElasticLoadBalancingv2Client as Client,
+    ElasticLoadBalancingv2Client,
+)
+from mypy_boto3_elbv2.paginator import (
+    DescribeAccountLimitsPaginator,
+    DescribeListenerCertificatesPaginator,
+    DescribeListenersPaginator,
+    DescribeLoadBalancersPaginator,
+    DescribeRulesPaginator,
+    DescribeSSLPoliciesPaginator,
+    DescribeTargetGroupsPaginator,
+)
+from mypy_boto3_elbv2.waiter import (
+    LoadBalancerAvailableWaiter,
+    LoadBalancerExistsWaiter,
+    LoadBalancersDeletedWaiter,
+    TargetDeregisteredWaiter,
+    TargetInServiceWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeAccountLimitsPaginator",
+    "DescribeListenerCertificatesPaginator",
+    "DescribeListenersPaginator",
+    "DescribeLoadBalancersPaginator",
+    "DescribeRulesPaginator",
+    "DescribeSSLPoliciesPaginator",
+    "DescribeTargetGroupsPaginator",
+    "ElasticLoadBalancingv2Client",
+    "LoadBalancerAvailableWaiter",
+    "LoadBalancerExistsWaiter",
+    "LoadBalancersDeletedWaiter",
+    "TargetDeregisteredWaiter",
+    "TargetInServiceWaiter",
+)
