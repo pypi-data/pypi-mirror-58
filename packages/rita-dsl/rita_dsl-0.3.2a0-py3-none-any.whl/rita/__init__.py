4f638713308fb6f4c3d20d8d707e8066a1b77a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package rita-dsl/0.3.2a0/rita_dsl-0.3.2a0-py3-none-any.whl

---
+++ packages/rita-dsl/rita_dsl-0.3.2a0-py3-none-any.whl/rita/__init__.py
@@ -0,0 +1,40 @@
+import logging
+import types
+
+from rita.config import with_config
+from rita.preprocess import preprocess_rules
+
+
+logger = logging.getLogger(__name__)
+
+__version__ = (0, 3, 2, "a")
+
+
+def get_version():
+    return "{0}.{1}.{2}-{3}".format(*__version__)
+
+
+@with_config
+def compile_string(raw, config, use_engine=None):
+    from rita.parser import RitaParser
+    parser = RitaParser(config)
+    parser.build()
+    root = parser.parse(raw)
+    logger.debug(root)
+    if use_engine:
+        compile_rules = config.get_engine(use_engine)
+    else:
+        compile_rules = config.default_engine
+    rules = list(preprocess_rules(root, config))
+    result = compile_rules(rules, config)
+    if isinstance(result, types.GeneratorType):
+        return list(result)
+    else:
+        return result
+
+
+def compile(fname, compile_fn=None):
+    with open(fname, "r") as f:
+        raw = f.read()
+
+    return compile_string(raw)
