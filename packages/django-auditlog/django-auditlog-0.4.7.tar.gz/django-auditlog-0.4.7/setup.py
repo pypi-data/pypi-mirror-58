64e6dcd37eb02df43c32ef41b5cab3a49b75c265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:21 +0000

    Add package django-auditlog/0.4.7/django-auditlog-0.4.7.tar.gz

---
+++ packages/django-auditlog/django-auditlog-0.4.7.tar.gz/django-auditlog-0.4.7/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='django-auditlog',
+    version='0.4.7',
+    packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
+    package_dir={'': 'src'},
+    url='https://github.com/jjkester/django-auditlog',
+    license='MIT',
+    author='Jan-Jelle Kester',
+    description='Audit log app for Django',
+    install_requires=[
+        'django-jsonfield>=1.0.0',
+        'python-dateutil==2.6.0'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],        
+)
