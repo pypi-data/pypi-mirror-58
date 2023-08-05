27c0e01ab224b16c07f46c79e52d14cfc15979da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package mypy-boto3-meteringmarketplace/1.10.41.0/mypy-boto3-meteringmarketplace-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-meteringmarketplace/mypy-boto3-meteringmarketplace-1.10.41.0.tar.gz/mypy-boto3-meteringmarketplace-1.10.41.0/mypy_boto3_meteringmarketplace/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for meteringmarketplace service"
+from mypy_boto3_meteringmarketplace.client import (
+    MarketplaceMeteringClient as Client,
+    MarketplaceMeteringClient,
+)
+
+
+__all__ = ("Client", "MarketplaceMeteringClient")
