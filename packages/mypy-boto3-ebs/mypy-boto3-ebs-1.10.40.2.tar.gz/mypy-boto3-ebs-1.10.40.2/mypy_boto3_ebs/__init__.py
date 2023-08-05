4bfcdce5a7e2ef92071090494be56f64409f1350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package mypy-boto3-ebs/1.10.40.2/mypy-boto3-ebs-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-ebs/mypy-boto3-ebs-1.10.40.2.tar.gz/mypy-boto3-ebs-1.10.40.2/mypy_boto3_ebs/__init__.py
@@ -0,0 +1,5 @@
+"Main interface for ebs service"
+from mypy_boto3_ebs.client import EBSClient as Client, EBSClient
+
+
+__all__ = ("Client", "EBSClient")
---
+++ packages/mypy-boto3-ebs/mypy-boto3-ebs-1.10.40.2.tar.gz/mypy-boto3-ebs-1.10.40.2/mypy_boto3_ebs/client.py
@@ -0,0 +1,82 @@
+"Main interface for ebs service Client"
+from __future__ import annotations
+
+from typing import Any, Dict
+from botocore.client import BaseClient
+from botocore.exceptions import ClientError as Boto3ClientError
+
+# pylint: disable=import-self
+import mypy_boto3_ebs.client as client_scope
+from mypy_boto3_ebs.type_defs import (
+    GetSnapshotBlockResponseTypeDef,
+    ListChangedBlocksResponseTypeDef,
+    ListSnapshotBlocksResponseTypeDef,
+)
+
+
+__all__ = ("EBSClient",)
+
+
+class EBSClient(BaseClient):
+    """
+    [EBS.Client documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ebs.html#EBS.Client)
+    """
+
+    exceptions: client_scope.Exceptions
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Client.can_paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ebs.html#EBS.Client.can_paginate)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> None:
+        """
+        [Client.generate_presigned_url documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ebs.html#EBS.Client.generate_presigned_url)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def get_snapshot_block(
+        self, SnapshotId: str, BlockIndex: int, BlockToken: str
+    ) -> GetSnapshotBlockResponseTypeDef:
+        """
+        [Client.get_snapshot_block documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ebs.html#EBS.Client.get_snapshot_block)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def list_changed_blocks(
+        self,
+        SecondSnapshotId: str,
+        FirstSnapshotId: str = None,
+        NextToken: str = None,
+        MaxResults: int = None,
+        StartingBlockIndex: int = None,
+    ) -> ListChangedBlocksResponseTypeDef:
+        """
+        [Client.list_changed_blocks documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ebs.html#EBS.Client.list_changed_blocks)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def list_snapshot_blocks(
+        self,
+        SnapshotId: str,
+        NextToken: str = None,
+        MaxResults: int = None,
+        StartingBlockIndex: int = None,
+    ) -> ListSnapshotBlocksResponseTypeDef:
+        """
+        [Client.list_snapshot_blocks documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ebs.html#EBS.Client.list_snapshot_blocks)
+        """
+
+
+class Exceptions:
+    ClientError: Boto3ClientError
+    ResourceNotFoundException: Boto3ClientError
+    ValidationException: Boto3ClientError
