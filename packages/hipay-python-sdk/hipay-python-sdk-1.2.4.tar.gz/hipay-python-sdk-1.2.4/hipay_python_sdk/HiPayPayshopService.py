00f5d639d133bc22357c6895d1dae77625cdd359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package hipay-python-sdk/1.2.4/hipay-python-sdk-1.2.4.tar.gz

---
+++ packages/hipay-python-sdk/hipay-python-sdk-1.2.4.tar.gz/hipay-python-sdk-1.2.4/hipay_python_sdk/HiPayPayshopService.py
@@ -0,0 +1,37 @@
+import zeep
+
+
+class HiPayPayshopService:
+
+    def __init__(self, username, password, wsdl='https://comprafacil2.hipay.pt/webservice/comprafacilWS.asmx?WSDL'):
+        self.client = zeep.Client(wsdl=wsdl)
+        self.username = username
+        self.password = password
+
+    def get_reference_payshop(self, email, amount, origin="", additionalInfo="", name="", address="", postCode="",
+                              city="", NIC="", externalReference="", contactPhone="", IDUserBackoffice=-1,
+                              sendEmailBuyer=True):
+        generated_reference = self.client.service.getReferencePS(
+            username=self.username,
+            password=self.password,
+            email=email,
+            amount=amount,
+            origin=origin,
+            additionalInfo=additionalInfo,
+            name=name,
+            address=address,
+            postCode=postCode,
+            city=city,
+            NIC=NIC,
+            externalReference=externalReference,
+            contactPhone=contactPhone,
+            IDUserBackoffice=IDUserBackoffice,
+            sendEmailBuyer=sendEmailBuyer
+        )
+
+        return generated_reference
+
+    def get_info_reference(self, reference):
+        reference_info = self.client.service.getInfoReference(reference=reference, username=self.username, password=self.password)
+
+        return reference_info
