9233fa2d021b847be3ada8ab999e09543e2924f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package mypy-boto3-sns/1.10.41.0/mypy-boto3-sns-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-sns/mypy-boto3-sns-1.10.41.0.tar.gz/mypy-boto3-sns-1.10.41.0/mypy_boto3_sns/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for sns service"
+from mypy_boto3_sns.client import SNSClient as Client, SNSClient
+from mypy_boto3_sns.paginator import (
+    ListEndpointsByPlatformApplicationPaginator,
+    ListPhoneNumbersOptedOutPaginator,
+    ListPlatformApplicationsPaginator,
+    ListSubscriptionsByTopicPaginator,
+    ListSubscriptionsPaginator,
+    ListTopicsPaginator,
+)
+from mypy_boto3_sns.service_resource import (
+    SNSServiceResource as ServiceResource,
+    SNSServiceResource,
+)
+
+
+__all__ = (
+    "Client",
+    "ListEndpointsByPlatformApplicationPaginator",
+    "ListPhoneNumbersOptedOutPaginator",
+    "ListPlatformApplicationsPaginator",
+    "ListSubscriptionsByTopicPaginator",
+    "ListSubscriptionsPaginator",
+    "ListTopicsPaginator",
+    "SNSClient",
+    "SNSServiceResource",
+    "ServiceResource",
+)
