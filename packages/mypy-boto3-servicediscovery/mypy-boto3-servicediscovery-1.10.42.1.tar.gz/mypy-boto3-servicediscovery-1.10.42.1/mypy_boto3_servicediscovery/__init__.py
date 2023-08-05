3d64cd0ae00db8a6bcc5306d5aaf5cb52a05d0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package mypy-boto3-servicediscovery/1.10.42.1/mypy-boto3-servicediscovery-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-servicediscovery/mypy-boto3-servicediscovery-1.10.42.1.tar.gz/mypy-boto3-servicediscovery-1.10.42.1/mypy_boto3_servicediscovery/__init__.py
@@ -0,0 +1,21 @@
+"Main interface for servicediscovery service"
+from mypy_boto3_servicediscovery.client import (
+    ServiceDiscoveryClient as Client,
+    ServiceDiscoveryClient,
+)
+from mypy_boto3_servicediscovery.paginator import (
+    ListInstancesPaginator,
+    ListNamespacesPaginator,
+    ListOperationsPaginator,
+    ListServicesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListInstancesPaginator",
+    "ListNamespacesPaginator",
+    "ListOperationsPaginator",
+    "ListServicesPaginator",
+    "ServiceDiscoveryClient",
+)
