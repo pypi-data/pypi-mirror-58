d856476bbacaa68692c5fc76631fe7e28e887f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:33 +0000

    Add package unplugged/0.1.5/unplugged-0.1.5.tar.gz

---
+++ packages/unplugged/unplugged-0.1.5.tar.gz/unplugged-0.1.5/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')) as f:
+    requirements = f.read().strip().split('\n')
+
+
+setup(
+    name="unplugged",
+    version="0.1.5",
+    packages=find_packages(),
+    package_data={
+        '': [
+            'unplugged/fixtures/unplugged-initial.json',
+        ],
+    },
+    include_package_data=True,
+
+    install_requires=requirements,
+    author="Anders Jensen",
+    author_email="johndoee@tidalstream.org",
+    description="Django based plugin system",
+    license="MIT",
+    url="https://github.com/JohnDoee/unplugged",
+
+)
---
+++ packages/unplugged/unplugged-0.1.5.tar.gz/unplugged-0.1.5/unplugged/schema.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema as OriginalSchema
+from marshmallow import INCLUDE, SchemaOpts, ValidationError, fields
+
+__all__ = ["INCLUDE", "Schema", "fields", "ValidationError"]
+
+
+class UISchemaOpts(SchemaOpts):
+    def __init__(self, meta, **kwargs):
+        SchemaOpts.__init__(self, meta, **kwargs)
+        self.ui_schema = getattr(meta, "ui_schema", {})
+
+
+class Schema(OriginalSchema):
+    OPTIONS_CLASS = UISchemaOpts
+
+
+Schema.Meta.unknown = INCLUDE
