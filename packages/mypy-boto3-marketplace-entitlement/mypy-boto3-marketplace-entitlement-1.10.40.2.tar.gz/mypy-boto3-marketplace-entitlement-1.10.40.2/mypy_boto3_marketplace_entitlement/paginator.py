0d7a422926d4595c062e4f98b778d1681993b220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package mypy-boto3-marketplace-entitlement/1.10.40.2/mypy-boto3-marketplace-entitlement-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-marketplace-entitlement/mypy-boto3-marketplace-entitlement-1.10.40.2.tar.gz/mypy-boto3-marketplace-entitlement-1.10.40.2/mypy_boto3_marketplace_entitlement/paginator.py
@@ -0,0 +1,35 @@
+"Main interface for marketplace-entitlement service Paginators"
+from __future__ import annotations
+
+import sys
+from typing import Dict, Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_marketplace_entitlement.type_defs import (
+    GetEntitlementsResultTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("GetEntitlementsPaginator",)
+
+
+class GetEntitlementsPaginator(Boto3Paginator):
+    """
+    [Paginator.GetEntitlements documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/marketplace-entitlement.html#MarketplaceEntitlementService.Paginator.GetEntitlements)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        ProductCode: str,
+        Filter: Dict[Literal["CUSTOMER_IDENTIFIER", "DIMENSION"], List[str]] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[GetEntitlementsResultTypeDef, None, None]:
+        """
+        [GetEntitlements.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/marketplace-entitlement.html#MarketplaceEntitlementService.Paginator.GetEntitlements.paginate)
+        """
