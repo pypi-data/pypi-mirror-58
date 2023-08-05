d239c1b6a1d022b7029c7c724fc84739d7e23a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package mypy-boto3-acm/1.10.40.2/mypy-boto3-acm-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-acm/mypy-boto3-acm-1.10.40.2.tar.gz/mypy-boto3-acm-1.10.40.2/mypy_boto3_acm/waiter.py
@@ -0,0 +1,20 @@
+"Main interface for acm service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_acm.type_defs import WaiterConfigTypeDef
+
+
+__all__ = ("CertificateValidatedWaiter",)
+
+
+class CertificateValidatedWaiter(Boto3Waiter):
+    """
+    [Waiter.CertificateValidated documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/acm.html#ACM.Waiter.CertificateValidated)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, CertificateArn: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [CertificateValidated.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/acm.html#ACM.Waiter.CertificateValidated.wait)
+        """
