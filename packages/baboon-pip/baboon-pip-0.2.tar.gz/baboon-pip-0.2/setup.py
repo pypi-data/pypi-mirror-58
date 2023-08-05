0e96e10f09f52c9fc5da71d2515df3e6d3a9b2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package baboon-pip/0.2/baboon-pip-0.2.tar.gz

---
+++ packages/baboon-pip/baboon-pip-0.2.tar.gz/baboon-pip-0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='baboon-pip',  
+     version='0.2',
+     scripts=['baboon'] ,
+     author="Kishore",
+     author_email="kishore@tealpod.com",
+     description="A Simple File Copy Tool",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/prakis/baboon",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 2.7",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
