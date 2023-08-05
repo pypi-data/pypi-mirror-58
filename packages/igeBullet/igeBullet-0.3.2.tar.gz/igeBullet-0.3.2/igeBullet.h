89e562932ab5f4e61373bdfa18e99516108a8dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:48 +0000

    Add package igeBullet/0.3.2/igeBullet-0.3.2.tar.gz

---
+++ packages/igeBullet/igeBullet-0.3.2.tar.gz/igeBullet-0.3.2/igeBullet.h
@@ -0,0 +1,33 @@
+#include <Python.h>
+
+typedef struct {
+	PyObject_HEAD
+		void* btconstraint;
+} constraint_obj;
+
+typedef struct {
+	PyObject_HEAD
+		void* btshape;
+} shape_obj;
+
+typedef struct {
+	PyObject_HEAD
+		void* btbody;
+		PyObject* world;
+		short collisionGroup;
+		short collisionMask;
+} rigidbody_obj;
+
+typedef struct {
+	PyObject_HEAD
+		void* broadphase;
+		void* dispatcher;
+		void* solver;
+		void* ghostPairCallback;
+		void* collisionConfiguration;
+		void* btworld;
+#if defined _WIN32
+		__int64 freq;
+#endif
+		double lasttime;
+} world_obj;
