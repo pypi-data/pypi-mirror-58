3cf60845862d80a437c54c38010a960f5fa29d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package mypy-boto3-ram/1.10.43.0/mypy-boto3-ram-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-ram/mypy-boto3-ram-1.10.43.0.tar.gz/mypy-boto3-ram-1.10.43.0/mypy_boto3_ram/__init__.py
@@ -0,0 +1,22 @@
+"Main interface for ram service"
+from mypy_boto3_ram.client import RAMClient, RAMClient as Client
+from mypy_boto3_ram.paginator import (
+    GetResourcePoliciesPaginator,
+    GetResourceShareAssociationsPaginator,
+    GetResourceShareInvitationsPaginator,
+    GetResourceSharesPaginator,
+    ListPrincipalsPaginator,
+    ListResourcesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "GetResourcePoliciesPaginator",
+    "GetResourceShareAssociationsPaginator",
+    "GetResourceShareInvitationsPaginator",
+    "GetResourceSharesPaginator",
+    "ListPrincipalsPaginator",
+    "ListResourcesPaginator",
+    "RAMClient",
+)
