fdc269403418aa7f2e8ee01e29f9e27d4b3d063d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package protaccel/0.300/protaccel-0.300.tar.gz

---
+++ packages/protaccel/protaccel-0.300.tar.gz/protaccel-0.300/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="protaccel",
+    version="0.300",
+    author="Gennady Gorin",
+    author_email="ggorin@caltech.edu",
+    description="Package to infer kinetics from scRNA-seq/feature barcoding data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pachterlab/GSP_2019",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
+
