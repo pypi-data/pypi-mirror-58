8140fd42e3007deb1a2b3dfeb5e006313b06f8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:08 +0000

    Add package richie/1.16.2/richie-1.16.2-py2.py3-none-any.whl

---
+++ packages/richie/richie-1.16.2-py2.py3-none-any.whl/richie/apps/courses/forms.py
@@ -0,0 +1,48 @@
+"""
+Courses model forms
+"""
+
+from django import forms
+from django.forms import widgets
+
+from djangocms_text_ckeditor.widgets import TextEditorWidget
+
+from .models import Category, Licence, LicencePluginModel
+
+
+class AdminCategoryForm(forms.ModelForm):
+    """
+    Category model form used within Django admin.
+    """
+
+    class Meta:
+
+        fields = ["color"]
+        model = Category
+        widgets = {
+            "color": widgets.TextInput(attrs={"type": "color", "style": "width: 5rem;"})
+        }
+
+
+class AdminLicenceForm(forms.ModelForm):
+    """
+    Licence model form used within Django admin
+    """
+
+    class Meta:
+
+        model = Licence
+        widgets = {"content": TextEditorWidget}
+        fields = ["name", "logo", "url", "content"]
+
+
+class LicencePluginForm(forms.ModelForm):
+    """
+    Licence plugin form used to fill its content from frontend admin.
+    """
+
+    class Meta:
+
+        model = LicencePluginModel
+        widgets = {"description": TextEditorWidget}
+        fields = ["licence", "description"]
---
+++ packages/richie/richie-1.16.2-py2.py3-none-any.whl/richie/apps/courses/models/blog.py
@@ -0,0 +1,58 @@
+"""
+Declare and configure the models for the blog part
+"""
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from cms.api import Page
+from cms.extensions.extension_pool import extension_pool
+from cms.models.pluginmodel import CMSPlugin
+
+from ...core.models import BasePageExtension, PagePluginMixin
+from ..defaults import BLOGPOSTS_PAGE
+
+
+class BlogPost(BasePageExtension):
+    """
+    The blogpost extension represents and records a blog article.
+    """
+
+    PAGE = BLOGPOSTS_PAGE
+
+    class Meta:
+        db_table = "richie_blog_post"
+        ordering = ["-pk"]
+        verbose_name = _("blog post")
+
+    def __str__(self):
+        """Human representation of a blogpost"""
+        return "{model}: {name}".format(
+            name=self.extended_object.get_title(), model=self._meta.verbose_name.title()
+        )
+
+
+class BlogPostPluginModel(PagePluginMixin, CMSPlugin):
+    """
+    BlogPost plugin model handles the relation between the BlogPostPluginModel
+    and its blogpost instance
+    """
+
+    page = models.ForeignKey(
+        Page,
+        on_delete=models.CASCADE,
+        related_name="blogpost_plugins",
+        limit_choices_to={"publisher_is_draft": True, "blogpost__isnull": False},
+    )
+
+    class Meta:
+        db_table = "richie_blog_post_plugin"
+        verbose_name = _("blog post plugin")
+
+    def __str__(self):
+        """Human representation of a blogpost plugin"""
+        return "{model:s}: {id:d}".format(
+            model=self._meta.verbose_name.title(), id=self.id
+        )
+
+
+extension_pool.register(BlogPost)
