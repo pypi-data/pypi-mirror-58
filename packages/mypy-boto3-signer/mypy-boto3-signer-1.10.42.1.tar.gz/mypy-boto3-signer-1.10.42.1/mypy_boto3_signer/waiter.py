46a852402dc60629ef9f7e0fd324058204e5a76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package mypy-boto3-signer/1.10.42.1/mypy-boto3-signer-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-signer/mypy-boto3-signer-1.10.42.1.tar.gz/mypy-boto3-signer-1.10.42.1/mypy_boto3_signer/waiter.py
@@ -0,0 +1,20 @@
+"Main interface for signer service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_signer.type_defs import WaiterConfigTypeDef
+
+
+__all__ = ("SuccessfulSigningJobWaiter",)
+
+
+class SuccessfulSigningJobWaiter(Boto3Waiter):
+    """
+    [Waiter.SuccessfulSigningJob documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/signer.html#Signer.Waiter.SuccessfulSigningJob)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, jobId: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [SuccessfulSigningJob.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/signer.html#Signer.Waiter.SuccessfulSigningJob.wait)
+        """
