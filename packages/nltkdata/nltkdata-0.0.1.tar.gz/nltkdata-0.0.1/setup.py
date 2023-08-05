808b1167cfb4222899707694c7079b33a5c51750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:05 +0000

    Add package nltkdata/0.0.1/nltkdata-0.0.1.tar.gz

---
+++ packages/nltkdata/nltkdata-0.0.1.tar.gz/nltkdata-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# Setup script for the Natural Language Toolkit Data.
+#
+# Copyright (C) 2001-2020 NLTK Project
+#
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+
+from distutils.core import setup
+
+setup(
+    name='nltkdata',
+    version='0.0.1',
+    packages=['nltkdata'],
+    description='NLTK Data',
+    long_description='',
+    url = 'https://github.com/nltk/nltk_data',
+    package_data={},
+    license="Apache License, Version 2.0"
+)
