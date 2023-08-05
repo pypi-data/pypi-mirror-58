835c1c2496cf120114a1716c67cad26de78ccd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:50 +0000

    Add package mypy-boto3-forecastquery/1.10.42.1/mypy-boto3-forecastquery-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-forecastquery/mypy-boto3-forecastquery-1.10.42.1.tar.gz/mypy-boto3-forecastquery-1.10.42.1/mypy_boto3_forecastquery/type_defs.py
@@ -0,0 +1,23 @@
+"Main interface for forecastquery service type defs"
+from __future__ import annotations
+
+import sys
+from typing import Dict, List
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("DataPointTypeDef", "ForecastTypeDef", "QueryForecastResponseTypeDef")
+
+DataPointTypeDef = TypedDict("DataPointTypeDef", {"Timestamp": str, "Value": float}, total=False)
+
+ForecastTypeDef = TypedDict(
+    "ForecastTypeDef", {"Predictions": Dict[str, List[DataPointTypeDef]]}, total=False
+)
+
+QueryForecastResponseTypeDef = TypedDict(
+    "QueryForecastResponseTypeDef", {"Forecast": ForecastTypeDef}, total=False
+)
