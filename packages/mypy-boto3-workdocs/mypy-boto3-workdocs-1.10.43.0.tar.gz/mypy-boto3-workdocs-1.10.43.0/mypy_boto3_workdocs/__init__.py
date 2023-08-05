e9ad239dbcfcca1f78fdda635be57caf7687a9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package mypy-boto3-workdocs/1.10.43.0/mypy-boto3-workdocs-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-workdocs/mypy-boto3-workdocs-1.10.43.0.tar.gz/mypy-boto3-workdocs-1.10.43.0/mypy_boto3_workdocs/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for workdocs service"
+from mypy_boto3_workdocs.client import WorkDocsClient, WorkDocsClient as Client
+from mypy_boto3_workdocs.paginator import (
+    DescribeActivitiesPaginator,
+    DescribeCommentsPaginator,
+    DescribeDocumentVersionsPaginator,
+    DescribeFolderContentsPaginator,
+    DescribeGroupsPaginator,
+    DescribeNotificationSubscriptionsPaginator,
+    DescribeResourcePermissionsPaginator,
+    DescribeRootFoldersPaginator,
+    DescribeUsersPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeActivitiesPaginator",
+    "DescribeCommentsPaginator",
+    "DescribeDocumentVersionsPaginator",
+    "DescribeFolderContentsPaginator",
+    "DescribeGroupsPaginator",
+    "DescribeNotificationSubscriptionsPaginator",
+    "DescribeResourcePermissionsPaginator",
+    "DescribeRootFoldersPaginator",
+    "DescribeUsersPaginator",
+    "WorkDocsClient",
+)
