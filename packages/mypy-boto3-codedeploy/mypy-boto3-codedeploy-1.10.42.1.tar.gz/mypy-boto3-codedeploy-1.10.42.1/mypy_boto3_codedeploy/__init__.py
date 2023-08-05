1bbcb1dde644b57f8dc080556bd0b91583d2a35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:40 +0000

    Add package mypy-boto3-codedeploy/1.10.42.1/mypy-boto3-codedeploy-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-codedeploy/mypy-boto3-codedeploy-1.10.42.1.tar.gz/mypy-boto3-codedeploy-1.10.42.1/mypy_boto3_codedeploy/__init__.py
@@ -0,0 +1,30 @@
+"Main interface for codedeploy service"
+from mypy_boto3_codedeploy.client import CodeDeployClient, CodeDeployClient as Client
+from mypy_boto3_codedeploy.paginator import (
+    ListApplicationRevisionsPaginator,
+    ListApplicationsPaginator,
+    ListDeploymentConfigsPaginator,
+    ListDeploymentGroupsPaginator,
+    ListDeploymentInstancesPaginator,
+    ListDeploymentTargetsPaginator,
+    ListDeploymentsPaginator,
+    ListGitHubAccountTokenNamesPaginator,
+    ListOnPremisesInstancesPaginator,
+)
+from mypy_boto3_codedeploy.waiter import DeploymentSuccessfulWaiter
+
+
+__all__ = (
+    "Client",
+    "CodeDeployClient",
+    "DeploymentSuccessfulWaiter",
+    "ListApplicationRevisionsPaginator",
+    "ListApplicationsPaginator",
+    "ListDeploymentConfigsPaginator",
+    "ListDeploymentGroupsPaginator",
+    "ListDeploymentInstancesPaginator",
+    "ListDeploymentTargetsPaginator",
+    "ListDeploymentsPaginator",
+    "ListGitHubAccountTokenNamesPaginator",
+    "ListOnPremisesInstancesPaginator",
+)
