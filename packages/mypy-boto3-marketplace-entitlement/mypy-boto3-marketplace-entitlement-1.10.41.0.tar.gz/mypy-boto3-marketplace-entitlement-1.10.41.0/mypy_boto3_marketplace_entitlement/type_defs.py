2a974bbaeb783f75a47b4243354a9700633a0e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package mypy-boto3-marketplace-entitlement/1.10.41.0/mypy-boto3-marketplace-entitlement-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-marketplace-entitlement/mypy-boto3-marketplace-entitlement-1.10.41.0.tar.gz/mypy-boto3-marketplace-entitlement-1.10.41.0/mypy_boto3_marketplace_entitlement/type_defs.py
@@ -0,0 +1,40 @@
+"Main interface for marketplace-entitlement service type defs"
+from __future__ import annotations
+
+from datetime import datetime
+import sys
+from typing import List
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+EntitlementValueTypeDef = TypedDict(
+    "EntitlementValueTypeDef",
+    {"IntegerValue": int, "DoubleValue": float, "BooleanValue": bool, "StringValue": str},
+    total=False,
+)
+
+EntitlementTypeDef = TypedDict(
+    "EntitlementTypeDef",
+    {
+        "ProductCode": str,
+        "Dimension": str,
+        "CustomerIdentifier": str,
+        "Value": EntitlementValueTypeDef,
+        "ExpirationDate": datetime,
+    },
+    total=False,
+)
+
+GetEntitlementsResultTypeDef = TypedDict(
+    "GetEntitlementsResultTypeDef",
+    {"Entitlements": List[EntitlementTypeDef], "NextToken": str},
+    total=False,
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef", {"MaxItems": int, "PageSize": int, "StartingToken": str}, total=False
+)
