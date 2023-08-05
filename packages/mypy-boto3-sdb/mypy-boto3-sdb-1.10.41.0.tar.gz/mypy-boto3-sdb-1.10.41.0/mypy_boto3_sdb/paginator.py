f41fa3a04da8636d968641eb7fb86f328e581e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package mypy-boto3-sdb/1.10.41.0/mypy-boto3-sdb-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-sdb/mypy-boto3-sdb-1.10.41.0.tar.gz/mypy-boto3-sdb-1.10.41.0/mypy_boto3_sdb/paginator.py
@@ -0,0 +1,44 @@
+"Main interface for sdb service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_sdb.type_defs import (
+    ListDomainsResultTypeDef,
+    PaginatorConfigTypeDef,
+    SelectResultTypeDef,
+)
+
+
+__all__ = ("ListDomainsPaginator", "SelectPaginator")
+
+
+class ListDomainsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListDomains documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/sdb.html#SimpleDB.Paginator.ListDomains)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListDomainsResultTypeDef, None, None]:
+        """
+        [ListDomains.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/sdb.html#SimpleDB.Paginator.ListDomains.paginate)
+        """
+
+
+class SelectPaginator(Boto3Paginator):
+    """
+    [Paginator.Select documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/sdb.html#SimpleDB.Paginator.Select)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        SelectExpression: str,
+        ConsistentRead: bool = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[SelectResultTypeDef, None, None]:
+        """
+        [Select.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/sdb.html#SimpleDB.Paginator.Select.paginate)
+        """
