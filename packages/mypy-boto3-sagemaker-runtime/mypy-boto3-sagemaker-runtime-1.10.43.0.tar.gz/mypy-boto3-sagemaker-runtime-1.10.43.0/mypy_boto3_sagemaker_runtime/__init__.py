e39a055c2851138e548bfa9322152ea44d44ec5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package mypy-boto3-sagemaker-runtime/1.10.43.0/mypy-boto3-sagemaker-runtime-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-sagemaker-runtime/mypy-boto3-sagemaker-runtime-1.10.43.0.tar.gz/mypy-boto3-sagemaker-runtime-1.10.43.0/mypy_boto3_sagemaker_runtime/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for sagemaker-runtime service"
+from mypy_boto3_sagemaker_runtime.client import (
+    SageMakerRuntimeClient,
+    SageMakerRuntimeClient as Client,
+)
+
+
+__all__ = ("Client", "SageMakerRuntimeClient")
