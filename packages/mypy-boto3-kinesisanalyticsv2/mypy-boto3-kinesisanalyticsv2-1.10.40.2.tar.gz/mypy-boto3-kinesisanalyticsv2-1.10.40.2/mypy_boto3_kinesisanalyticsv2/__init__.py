607e8c3fa73613653c55bce9db085ebc8e4c474c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:41 +0000

    Add package mypy-boto3-kinesisanalyticsv2/1.10.40.2/mypy-boto3-kinesisanalyticsv2-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-kinesisanalyticsv2/mypy-boto3-kinesisanalyticsv2-1.10.40.2.tar.gz/mypy-boto3-kinesisanalyticsv2-1.10.40.2/mypy_boto3_kinesisanalyticsv2/__init__.py
@@ -0,0 +1,17 @@
+"Main interface for kinesisanalyticsv2 service"
+from mypy_boto3_kinesisanalyticsv2.client import (
+    KinesisAnalyticsV2Client,
+    KinesisAnalyticsV2Client as Client,
+)
+from mypy_boto3_kinesisanalyticsv2.paginator import (
+    ListApplicationSnapshotsPaginator,
+    ListApplicationsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "KinesisAnalyticsV2Client",
+    "ListApplicationSnapshotsPaginator",
+    "ListApplicationsPaginator",
+)
