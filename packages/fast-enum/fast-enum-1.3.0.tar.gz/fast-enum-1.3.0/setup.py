c4710f14b261e51c84b536aa10402c3f62dc63f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package fast-enum/1.3.0/fast-enum-1.3.0.tar.gz

---
+++ packages/fast-enum/fast-enum-1.3.0.tar.gz/fast-enum-1.3.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as rfd:
+    long_description = rfd.read()
+
+setuptools.setup(
+    name='fast-enum',
+    version='1.3.0',
+    license='MIT',
+    platforms=['any'],
+    author='Andrey Semenov',
+    author_email='gatekeeper.mail@gmail.com',
+    description='A fast pure-python implementation of Enum',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/QratorLabs/fastenum',
+    packages=['fast_enum'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
