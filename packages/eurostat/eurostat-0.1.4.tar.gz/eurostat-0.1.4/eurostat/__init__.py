31a99d2af62a68d9a0385a2fa89900df0f363343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:19 +0000

    Add package eurostat/0.1.4/eurostat-0.1.4.tar.gz

---
+++ packages/eurostat/eurostat-0.1.4.tar.gz/eurostat-0.1.4/eurostat/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Noemi E. Cazzaniga - 2019
+@email: noemi.cazzaniga@polimi.it
+"""
+
+from eurostat.eurostat import get_data,get_data_df,get_dic,get_sdmx_data,\
+                              get_sdmx_data_df,get_sdmx_dic,get_sdmx_dims,\
+                              get_toc,get_toc_df,subset_toc_df,setproxy
