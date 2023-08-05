19c3ec495de2966f2e9f9fcca4a3729b53f834cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package mitoscripts/1.0.9/mitoscripts-1.0.9.tar.gz

---
+++ packages/mitoscripts/mitoscripts-1.0.9.tar.gz/mitoscripts-1.0.9/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mitoscripts",  # Replace with your own username
+    version="1.0.9",
+    author="Grant Hussey",
+    author_email="grant.hussey@nyulangone.org",
+    description="To assist in quantifying mitochondrial morphology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/granthussey/MitoScripts",
+    packages=['mitoscripts'],#setuptools.find_packages(),
+    install_requires=['numpy', 'pandas', 'scikit-learn', 'igraph', 'matplotlib', 'seaborn'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"mitoscripts": "src/mitoscripts"},
+    python_requires=">=3.6",
+)
