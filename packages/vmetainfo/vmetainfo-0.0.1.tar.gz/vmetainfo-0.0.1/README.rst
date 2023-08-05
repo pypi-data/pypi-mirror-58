dba66bff63afefcd31c051eec4aa01d7ecbf39c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package vmetainfo/0.0.1/vmetainfo-0.0.1.tar.gz

---
+++ packages/vmetainfo/vmetainfo-0.0.1.tar.gz/vmetainfo-0.0.1/README.rst
@@ -0,0 +1,8 @@
+=========
+VmetaInfo
+=========
+
+VideoMetaInfo:
+
+Get video/audio information using avconv.
+Based on pydub project: https://github.com/jiaaro/pydub/
---
+++ packages/vmetainfo/vmetainfo-0.0.1.tar.gz/vmetainfo-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vmetainfo',
+      version='0.0.1',
+      description='Get video meta information from video/audio file using ffprobe/avprobe',
+      url='https://bitbucket.org/pi11/vmetainfo',
+      author="pi11",
+      author_email="webii@pm.me"
+      )
