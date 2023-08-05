bb9b09bacafde00b1b5ffd2be28a2338bcff97ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:04 +0000

    Add package skymapping/0.0.2/skymapping-0.0.2-py3-none-any.whl

---
+++ packages/skymapping/skymapping-0.0.2-py3-none-any.whl/skymap/initialization.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+' initialize the raw sequence data '
+
+__author__ = 'Tansel, Zhao'
+
+
+class Initialization(object):
+    def __init__(self, r=1):
+        self.r = r
+
+    def greeting(self):
+
+        if self.r == 1:
+            print("This is the first stone.")
+
+        return
+
