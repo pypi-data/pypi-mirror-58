97fdb29874059ebcb1a1ebbf1914dec40211092f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package mypy-boto3-dynamodb/1.10.40.2/mypy-boto3-dynamodb-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-dynamodb/mypy-boto3-dynamodb-1.10.40.2.tar.gz/mypy-boto3-dynamodb-1.10.40.2/mypy_boto3_dynamodb/paginator.py
@@ -0,0 +1,141 @@
+"Main interface for dynamodb service Paginators"
+from __future__ import annotations
+
+from datetime import datetime
+import sys
+from typing import Dict, Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_dynamodb.type_defs import (
+    AttributeValueTypeDef,
+    ConditionTypeDef,
+    ListBackupsOutputTypeDef,
+    ListTablesOutputTypeDef,
+    ListTagsOfResourceOutputTypeDef,
+    PaginatorConfigTypeDef,
+    QueryOutputTypeDef,
+    ScanOutputTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "ListBackupsPaginator",
+    "ListTablesPaginator",
+    "ListTagsOfResourcePaginator",
+    "QueryPaginator",
+    "ScanPaginator",
+)
+
+
+class ListBackupsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListBackups documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.ListBackups)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        TableName: str = None,
+        TimeRangeLowerBound: datetime = None,
+        TimeRangeUpperBound: datetime = None,
+        BackupType: Literal["USER", "SYSTEM", "AWS_BACKUP", "ALL"] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[ListBackupsOutputTypeDef, None, None]:
+        """
+        [ListBackups.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.ListBackups.paginate)
+        """
+
+
+class ListTablesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListTables documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.ListTables)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListTablesOutputTypeDef, None, None]:
+        """
+        [ListTables.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.ListTables.paginate)
+        """
+
+
+class ListTagsOfResourcePaginator(Boto3Paginator):
+    """
+    [Paginator.ListTagsOfResource documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.ListTagsOfResource)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, ResourceArn: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListTagsOfResourceOutputTypeDef, None, None]:
+        """
+        [ListTagsOfResource.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.ListTagsOfResource.paginate)
+        """
+
+
+class QueryPaginator(Boto3Paginator):
+    """
+    [Paginator.Query documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.Query)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        TableName: str,
+        IndexName: str = None,
+        Select: Literal[
+            "ALL_ATTRIBUTES", "ALL_PROJECTED_ATTRIBUTES", "SPECIFIC_ATTRIBUTES", "COUNT"
+        ] = None,
+        AttributesToGet: List[str] = None,
+        ConsistentRead: bool = None,
+        KeyConditions: Dict[str, ConditionTypeDef] = None,
+        QueryFilter: Dict[str, ConditionTypeDef] = None,
+        ConditionalOperator: Literal["AND", "OR"] = None,
+        ScanIndexForward: bool = None,
+        ReturnConsumedCapacity: Literal["INDEXES", "TOTAL", "NONE"] = None,
+        ProjectionExpression: str = None,
+        FilterExpression: str = None,
+        KeyConditionExpression: str = None,
+        ExpressionAttributeNames: Dict[str, str] = None,
+        ExpressionAttributeValues: Dict[str, AttributeValueTypeDef] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[QueryOutputTypeDef, None, None]:
+        """
+        [Query.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.Query.paginate)
+        """
+
+
+class ScanPaginator(Boto3Paginator):
+    """
+    [Paginator.Scan documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.Scan)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        TableName: str,
+        IndexName: str = None,
+        AttributesToGet: List[str] = None,
+        Select: Literal[
+            "ALL_ATTRIBUTES", "ALL_PROJECTED_ATTRIBUTES", "SPECIFIC_ATTRIBUTES", "COUNT"
+        ] = None,
+        ScanFilter: Dict[str, ConditionTypeDef] = None,
+        ConditionalOperator: Literal["AND", "OR"] = None,
+        ReturnConsumedCapacity: Literal["INDEXES", "TOTAL", "NONE"] = None,
+        TotalSegments: int = None,
+        Segment: int = None,
+        ProjectionExpression: str = None,
+        FilterExpression: str = None,
+        ExpressionAttributeNames: Dict[str, str] = None,
+        ExpressionAttributeValues: Dict[str, AttributeValueTypeDef] = None,
+        ConsistentRead: bool = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[ScanOutputTypeDef, None, None]:
+        """
+        [Scan.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Paginator.Scan.paginate)
+        """
---
+++ packages/mypy-boto3-dynamodb/mypy-boto3-dynamodb-1.10.40.2.tar.gz/mypy-boto3-dynamodb-1.10.40.2/mypy_boto3_dynamodb/waiter.py
@@ -0,0 +1,32 @@
+"Main interface for dynamodb service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_dynamodb.type_defs import WaiterConfigTypeDef
+
+
+__all__ = ("TableExistsWaiter", "TableNotExistsWaiter")
+
+
+class TableExistsWaiter(Boto3Waiter):
+    """
+    [Waiter.TableExists documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Waiter.TableExists)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, TableName: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [TableExists.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Waiter.TableExists.wait)
+        """
+
+
+class TableNotExistsWaiter(Boto3Waiter):
+    """
+    [Waiter.TableNotExists documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Waiter.TableNotExists)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, TableName: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [TableNotExists.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/dynamodb.html#DynamoDB.Waiter.TableNotExists.wait)
+        """
