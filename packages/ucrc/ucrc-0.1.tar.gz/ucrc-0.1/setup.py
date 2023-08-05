2e1038892371145ba8b961aadfe36d7cfa26b396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:35 +0000

    Add package ucrc/0.1/ucrc-0.1.tar.gz

---
+++ packages/ucrc/ucrc-0.1.tar.gz/ucrc-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name='ucrc',
+    packages=['ucrc'],
+    version='0.1',
+    license='GPL',
+    description='',
+    author='Anton Sizov',
+    author_email='sizeofanton@gmail.com',
+    url='https://github.com/sizeofanton/ucrc',
+    download_url='https://github.com/sizeofanton/ucrc',
+    keywords=['CRC','CRC8','CRC16','CRC32','Cyclic redundancy check'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ],
+)
