6c9091531d53d62a495feadb727808edcabf6d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package muser/1.2.1.1/muser-1.2.1.1-py3-none-any.whl

---
+++ packages/muser/muser-1.2.1.1-py3-none-any.whl/game/muser.py
@@ -0,0 +1,22 @@
+import pyxel
+from sheet.reader import sheet_reader
+from game.constants import *
+from game.config import *
+from game.casts import *
+import traceback
+
+class Muser:
+    def __init__(self):
+        pyxel.run(self.update, self.draw)
+    def update(self):
+        try: 
+            if Config.CAST.is_finished():
+                Config.CAST = Config.CAST.next_cast()
+            Config.CAST.update()
+        except BaseException as e:
+            print(e)
+            traceback.print_exc()
+    def draw(self):
+        pyxel.cls(0)
+        Config.CAST.draw()
+Muser()
