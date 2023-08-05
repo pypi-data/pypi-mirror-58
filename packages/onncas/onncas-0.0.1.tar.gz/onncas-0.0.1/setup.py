f263a44abe9fe5595c348d3042f3bc632be6ddd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package onncas/0.0.1/onncas-0.0.1.tar.gz

---
+++ packages/onncas/onncas-0.0.1.tar.gz/onncas-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='onncas',
+    version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url='http://oznetnerd.com',
+    install_requires=[
+        'onnlogger==0.0.3',
+    ],
+    license='',
+    author='Will Robinson',
+    author_email='will@oznetnerd.com',
+    description='Convenience module for interacting with Trend Micro Cloud App Security (CAS)'
+)
