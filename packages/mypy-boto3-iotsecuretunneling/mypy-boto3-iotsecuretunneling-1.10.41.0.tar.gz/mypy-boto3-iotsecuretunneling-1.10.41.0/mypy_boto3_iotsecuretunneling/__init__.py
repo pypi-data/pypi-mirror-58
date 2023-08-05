e12ad000ce6a74c8912c25f0b100be18b700edd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package mypy-boto3-iotsecuretunneling/1.10.41.0/mypy-boto3-iotsecuretunneling-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-iotsecuretunneling/mypy-boto3-iotsecuretunneling-1.10.41.0.tar.gz/mypy-boto3-iotsecuretunneling-1.10.41.0/mypy_boto3_iotsecuretunneling/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for iotsecuretunneling service"
+from mypy_boto3_iotsecuretunneling.client import (
+    IoTSecureTunnelingClient,
+    IoTSecureTunnelingClient as Client,
+)
+
+
+__all__ = ("Client", "IoTSecureTunnelingClient")
