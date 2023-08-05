836e0a49055d79ee506d4b4f345efd7821b7b5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package mypy-boto3-ec2-instance-connect/1.10.43.0/mypy-boto3-ec2-instance-connect-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-ec2-instance-connect/mypy-boto3-ec2-instance-connect-1.10.43.0.tar.gz/mypy-boto3-ec2-instance-connect-1.10.43.0/mypy_boto3_ec2_instance_connect/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for ec2-instance-connect service"
+from mypy_boto3_ec2_instance_connect.client import (
+    EC2InstanceConnectClient,
+    EC2InstanceConnectClient as Client,
+)
+
+
+__all__ = ("Client", "EC2InstanceConnectClient")
