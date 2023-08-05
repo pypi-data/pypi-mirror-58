76cb238caba450aaf27b2a9280d73ecb7a7341d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package mypy-boto3-xray/1.10.42.1/mypy-boto3-xray-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-xray/mypy-boto3-xray-1.10.42.1.tar.gz/mypy-boto3-xray-1.10.42.1/mypy_boto3_xray/__init__.py
@@ -0,0 +1,26 @@
+"Main interface for xray service"
+from mypy_boto3_xray.client import XRayClient, XRayClient as Client
+from mypy_boto3_xray.paginator import (
+    BatchGetTracesPaginator,
+    GetGroupsPaginator,
+    GetSamplingRulesPaginator,
+    GetSamplingStatisticSummariesPaginator,
+    GetServiceGraphPaginator,
+    GetTimeSeriesServiceStatisticsPaginator,
+    GetTraceGraphPaginator,
+    GetTraceSummariesPaginator,
+)
+
+
+__all__ = (
+    "BatchGetTracesPaginator",
+    "Client",
+    "GetGroupsPaginator",
+    "GetSamplingRulesPaginator",
+    "GetSamplingStatisticSummariesPaginator",
+    "GetServiceGraphPaginator",
+    "GetTimeSeriesServiceStatisticsPaginator",
+    "GetTraceGraphPaginator",
+    "GetTraceSummariesPaginator",
+    "XRayClient",
+)
