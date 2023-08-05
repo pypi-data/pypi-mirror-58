723802ce81b7055366b8f3be13cd8fe742096e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package alpha-orm/1.2.24/alpha-orm-1.2.24.tar.gz

---
+++ packages/alpha-orm/alpha-orm-1.2.24.tar.gz/alpha-orm-1.2.24/alphaorm/generators/GeneratorInterface.py
@@ -0,0 +1,20 @@
+import interface
+class GeneratorInterface (interface.Interface):
+
+    @staticmethod
+    def checkColumnUpdates(columns_db, columns_record, alpha_record):
+        pass
+
+    @staticmethod
+    def creatNewColumns(dict_map, alpha_record, tablename):
+        pass
+
+    @staticmethod
+    def columns(columns_db, alpha_record, base = True):
+        pass
+
+    @staticmethod
+    def getGenerator(driver):
+        if driver == 'mysql':
+            from .MySQLGenerator import MySQLGenerator
+            return MySQLGenerator
