7d48e04df4810342adfb66c9bbd9a3bb2f02bc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package mypy-boto3-alexaforbusiness/1.10.40.2/mypy-boto3-alexaforbusiness-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-alexaforbusiness/mypy-boto3-alexaforbusiness-1.10.40.2.tar.gz/mypy-boto3-alexaforbusiness-1.10.40.2/mypy_boto3_alexaforbusiness/__init__.py
@@ -0,0 +1,39 @@
+"Main interface for alexaforbusiness service"
+from mypy_boto3_alexaforbusiness.client import (
+    AlexaForBusinessClient as Client,
+    AlexaForBusinessClient,
+)
+from mypy_boto3_alexaforbusiness.paginator import (
+    ListBusinessReportSchedulesPaginator,
+    ListConferenceProvidersPaginator,
+    ListDeviceEventsPaginator,
+    ListSkillsPaginator,
+    ListSkillsStoreCategoriesPaginator,
+    ListSkillsStoreSkillsByCategoryPaginator,
+    ListSmartHomeAppliancesPaginator,
+    ListTagsPaginator,
+    SearchDevicesPaginator,
+    SearchProfilesPaginator,
+    SearchRoomsPaginator,
+    SearchSkillGroupsPaginator,
+    SearchUsersPaginator,
+)
+
+
+__all__ = (
+    "AlexaForBusinessClient",
+    "Client",
+    "ListBusinessReportSchedulesPaginator",
+    "ListConferenceProvidersPaginator",
+    "ListDeviceEventsPaginator",
+    "ListSkillsPaginator",
+    "ListSkillsStoreCategoriesPaginator",
+    "ListSkillsStoreSkillsByCategoryPaginator",
+    "ListSmartHomeAppliancesPaginator",
+    "ListTagsPaginator",
+    "SearchDevicesPaginator",
+    "SearchProfilesPaginator",
+    "SearchRoomsPaginator",
+    "SearchSkillGroupsPaginator",
+    "SearchUsersPaginator",
+)
