0f3439f3ac637d4d381d8bbb518e8e0f6d6e0ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:38 +0000

    Add package gmic/2.8.1.dev0/gmic-2.8.1.dev0.tar.gz

---
+++ packages/gmic/gmic-2.8.1.dev0.tar.gz/gmic-2.8.1.dev0/gmicpy.cpp
@@ -0,0 +1,35 @@
+#include <Python.h>
+#include <iostream>
+#include "gmic.h"
+using namespace std;
+
+PyObject* run_impl(PyObject*, PyObject* commands_line) 
+{
+  const char* c_commands_line = PyUnicode_AsUTF8(commands_line);
+  try {
+    gmic(c_commands_line, 0, true);
+  } catch (gmic_exception& e) {
+    PyErr_SetString(PyExc_Exception, e.what());
+  } catch (std::exception& e) {
+    PyErr_SetString(PyExc_Exception, e.what());
+    return NULL;
+  }
+  return Py_True;
+}
+
+static PyMethodDef gmic_methods[] = {
+  {"run", (PyCFunction)run_impl, METH_O, nullptr },
+  {nullptr, nullptr, 0, nullptr }
+};
+
+PyModuleDef gmic_module = {
+  PyModuleDef_HEAD_INIT,
+  "gmic",
+  "Gmic Python binding",
+  0,
+  gmic_methods
+};
+
+PyMODINIT_FUNC PyInit_gmic() {
+  return PyModule_Create(&gmic_module);
+}
