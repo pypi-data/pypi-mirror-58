c2415fdb6b00724de2bfe94bfa9f7c7dd9741d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:20 +0000

    Add package census-data-downloader/0.0.25/census-data-downloader-0.0.25.tar.gz

---
+++ packages/census-data-downloader/census-data-downloader-0.0.25.tar.gz/census-data-downloader-0.0.25/census_data_downloader/tables/__init__.py
@@ -0,0 +1,168 @@
+from census_data_downloader.core.decorators import TABLE_LIST
+from .age import AgeDownloader
+from .ancestry import AncestryDownloader
+from .classofworker import ClassOfWorkerDownloader
+from .disability import (
+    DisabilityDownloader,
+    DisabilityPovertyDownloader,
+    CognitiveDifficultyDownloader
+)
+from .education import EducationDownloader
+from .employmentstatus import EmploymentStatusDownloader
+from .foreignborn import (
+    ForeignBornDownloader,
+    CitizenDownloader
+)
+from .gini import GiniIndexDownloader
+from .housing import (
+    HousingValueDownloader,
+    MedianMonthlyHousingCostsDownloader,
+    MedianHousingValueDownloader,
+    MedianGrossRentDownloader,
+    TenureDownloader,
+    TenurePopDownloader,
+    TenureLatinoDownloader,
+    TenureWhiteDownloader,
+    TenureBlackDownloader,
+    TenureAsianDownloader
+)
+from .householdincome import (
+    HouseholdIncomeDownloader,
+    HouseholdIncomeLatinoDownloader,
+    HouseholdIncomeWhiteDownloader,
+    HouseholdIncomeBlackDownloader,
+    HouseholdIncomeAsianDownloader
+)
+from .internet import InternetDownloader
+from .language import (
+    HouseholdLanguageDownloader,
+    LanguageShortFormDownloader,
+    LanguageLongFormDownloader
+)
+from .latino import LatinoDownloader
+from .medianage import MedianAgeDownloader
+from .medianhouseholdincome import (
+    MedianHouseholdIncomeDownloader,
+    MedianHouseholdIncomeLatinoDownloader,
+    MedianHouseholdIncomeWhiteDownloader,
+    MedianHouseholdIncomeBlackDownloader,
+    MedianHouseholdIncomeAsianDownloader
+)
+from .mobility import (
+    MobilityDownloader,
+    MobilityBySexDownloader,
+    MobilityWhiteDownloader,
+    MobilityBlackDownloader,
+    MobilityAsianDownloader,
+    MobilityLatinoDownloader,
+    MobilityByCitizenship
+)
+from .percapitaincome import (
+    PerCapitaIncomeDownloader,
+    PerCapitaIncomeLatinoDownloader,
+    PerCapitaIncomeWhiteDownloader,
+    PerCapitaIncomeBlackDownloader,
+    PerCapitaIncomeAsianDownloader
+)
+from .population import PopulationDownloader
+from .poverty import (
+    PovertyDownloader,
+    PovertyBySexDownloader,
+    PovertyAgeDownloader,
+    PovertyLatinoDownloader,
+    PovertyWhiteDownloader,
+    PovertyBlackDownloader,
+    PovertyAsianDownloader
+)
+from .race import (
+    RaceDownloader,
+    AmericanIndianAlaskaNativeAloneOrInComboDownloader,
+    AsianDownloader
+)
+from .snap import (
+    SnapbyPovertyDownloader,
+    SnapLatinoDownloader,
+    SnapWhiteDownloader,
+    SnapBlackDownloader,
+    SnapAsianDownloader,
+)
+from .veteran import VeteranDownloader
+from .yearstructurebuilt import (
+    YearStructureBuiltDownloader,
+    TenureByYearStructureBuiltDownloader,
+    AgeHouseholderByYearBuiltDownloader
+)
+
+
+__all__ = [
+    TABLE_LIST,
+    AgeDownloader,
+    AgeHouseholderByYearBuiltDownloader,
+    AncestryDownloader,
+    AsianDownloader,
+    CitizenDownloader,
+    ClassOfWorkerDownloader,
+    CognitiveDifficultyDownloader,
+    DisabilityDownloader,
+    DisabilityPovertyDownloader,
+    EducationDownloader,
+    EmploymentStatusDownloader,
+    ForeignBornDownloader,
+    GiniIndexDownloader,
+    HousingValueDownloader,
+    HouseholdIncomeDownloader,
+    HouseholdIncomeLatinoDownloader,
+    HouseholdIncomeWhiteDownloader,
+    HouseholdIncomeBlackDownloader,
+    HouseholdIncomeAsianDownloader,
+    InternetDownloader,
+    HouseholdLanguageDownloader,
+    LanguageShortFormDownloader,
+    LanguageLongFormDownloader,
+    LatinoDownloader,
+    MedianAgeDownloader,
+    MedianGrossRentDownloader,
+    MedianHouseholdIncomeDownloader,
+    MedianHouseholdIncomeLatinoDownloader,
+    MedianHouseholdIncomeWhiteDownloader,
+    MedianHouseholdIncomeBlackDownloader,
+    MedianHouseholdIncomeAsianDownloader,
+    MedianHousingValueDownloader,
+    MedianMonthlyHousingCostsDownloader,
+    MobilityDownloader,
+    MobilityBySexDownloader,
+    MobilityWhiteDownloader,
+    MobilityBlackDownloader,
+    MobilityAsianDownloader,
+    MobilityLatinoDownloader,
+    MobilityByCitizenship,
+    PerCapitaIncomeDownloader,
+    PerCapitaIncomeLatinoDownloader,
+    PerCapitaIncomeWhiteDownloader,
+    PerCapitaIncomeBlackDownloader,
+    PerCapitaIncomeAsianDownloader,
+    PopulationDownloader,
+    PovertyDownloader,
+    PovertyBySexDownloader,
+    PovertyAgeDownloader,
+    PovertyLatinoDownloader,
+    PovertyWhiteDownloader,
+    PovertyBlackDownloader,
+    PovertyAsianDownloader,
+    RaceDownloader,
+    SnapbyPovertyDownloader,
+    SnapLatinoDownloader,
+    SnapWhiteDownloader,
+    SnapBlackDownloader,
+    SnapAsianDownloader,
+    AmericanIndianAlaskaNativeAloneOrInComboDownloader,
+    TenureDownloader,
+    TenurePopDownloader,
+    TenureLatinoDownloader,
+    TenureWhiteDownloader,
+    TenureBlackDownloader,
+    TenureAsianDownloader,
+    TenureByYearStructureBuiltDownloader,
+    VeteranDownloader,
+    YearStructureBuiltDownloader
+]
---
+++ packages/census-data-downloader/census-data-downloader-0.0.25.tar.gz/census-data-downloader-0.0.25/census_data_downloader/tables/housing.py
@@ -0,0 +1,119 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*
+import collections
+from census_data_downloader.core.tables import BaseTableConfig
+from census_data_downloader.core.decorators import register
+
+
+@register
+class HousingValueDownloader(BaseTableConfig):
+    PROCESSED_TABLE_NAME = "housingvalue"
+    UNIVERSE = "owner occupied housing units"
+    RAW_TABLE_NAME = "B25075"
+    RAW_FIELD_CROSSWALK = collections.OrderedDict({
+        "01": "total",
+        "02": "0_10000",
+        "03": "10000_14999",
+        "04": "15000_19999",
+        "05": "20000_24999",
+        "06": "25000_29999",
+        "07": "30000_34999",
+        "08": "35000_39999",
+        "09": "40000_49999",
+        "10": "50000_59999",
+        "11": "60000_69999",
+        "12": "70000_79999",
+        "13": "80000_89999",
+        "14": "90000_99999",
+        "15": "100000_124999",
+        "16": "125000_149999",
+        "17": "150000_174999",
+        "18": "175000_199999",
+        "19": "200000_249999",
+        "20": "250000_299999",
+        "21": "300000_399999",
+        "22": "400000_499999",
+        "23": "500000_749999",
+        "24": "750000_999999",
+        "25": "1000000_1499999",
+        "26": "1500000_1999999",
+        "27": "2000000_more",
+    })
+
+
+@register
+class MedianMonthlyHousingCostsDownloader(BaseTableConfig):
+    PROCESSED_TABLE_NAME = "medianmonthlyhousingcosts"
+    UNIVERSE = "occupied housing units with monthly housing costs"
+    RAW_TABLE_NAME = "B25105"
+    RAW_FIELD_CROSSWALK = collections.OrderedDict({
+        "001": "median"
+    })
+
+
+@register
+class MedianHousingValueDownloader(BaseTableConfig):
+    PROCESSED_TABLE_NAME = "medianhousingvalue"
+    UNIVERSE = "owner-occupied housing units"
+    RAW_TABLE_NAME = 'B25077'
+    RAW_FIELD_CROSSWALK = collections.OrderedDict({
+        "001": "median"
+    })
+
+
+@register
+class MedianGrossRentDownloader(BaseTableConfig):
+    PROCESSED_TABLE_NAME = "mediangrossrent"
+    UNIVERSE = "renter-occupied housing units paying cash rent"
+    RAW_TABLE_NAME = 'B25064'
+    RAW_FIELD_CROSSWALK = collections.OrderedDict({
+        "001": "median"
+    })
+
+
+@register
+class TenurePopDownloader(BaseTableConfig):
+    PROCESSED_TABLE_NAME = "tenurepop"
+    UNIVERSE = "total population in occupied housing units"
+    RAW_TABLE_NAME = 'B25008'
+    RAW_FIELD_CROSSWALK = collections.OrderedDict({
+        '001': 'universe',
+        '002': 'owner_occupied',
+        '003': 'renter_occupied'
+    })
+
+
+@register
+class TenureDownloader(BaseTableConfig):
+    PROCESSED_TABLE_NAME = "tenure"
+    UNIVERSE = "occupied housing units"
+    RAW_TABLE_NAME = 'B25003'
+    RAW_FIELD_CROSSWALK = collections.OrderedDict({
+        '001': 'universe',
+        '002': 'owner_occupied',
+        '003': 'renter_occupied'
+    })
+
+
+@register
+class TenureLatinoDownloader(TenureDownloader):
+    PROCESSED_TABLE_NAME = "tenurelatino"
+    RAW_TABLE_NAME = 'B25003I'
+
+
+@register
+class TenureWhiteDownloader(TenureDownloader):
+    PROCESSED_TABLE_NAME = "tenurewhite"
+    RAW_TABLE_NAME = 'B25003H'
+
+
+@register
+class TenureBlackDownloader(TenureDownloader):
+    PROCESSED_TABLE_NAME = "tenureblack"
+    RAW_TABLE_NAME = 'B25003B'
+
+
+@register
+class TenureAsianDownloader(TenureDownloader):
+    PROCESSED_TABLE_NAME = "tenureasian"
+    RAW_TABLE_NAME = 'B25003D'
