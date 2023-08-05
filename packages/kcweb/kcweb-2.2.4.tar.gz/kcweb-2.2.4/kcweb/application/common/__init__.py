c747e2375be4d39a9ad7d17bf2f6f3bdaad1a437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:36 +0000

    Add package kcweb/2.2.4/kcweb-2.2.4.tar.gz

---
+++ packages/kcweb/kcweb-2.2.4.tar.gz/kcweb-2.2.4/kcweb/application/common/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from kcweb.common import *
+from ${appname} import config
+def returnjson(data=[],code=0,msg="成功",status='200 ok'):
+    """在浏览器输出包装过的json
+
+        参数 data 结果 默认[]
+
+        参数 code body状态码 默认0
+
+        参数 msg body状态描述 默认 成功
+
+        参数 status http状态码 默认 200
+
+        返回 json字符串结果集 
+        """
+    res={
+        "code":code,
+        "msg":msg,
+        "time":times(),
+        "data":data
+    }
+    return json_encode(res),status,{"Content-Type":"application/json; charset=utf-8"}
