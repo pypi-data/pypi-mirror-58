155c3487af2cf60101135afbca0c441eb962aec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:56 +0000

    Add package dynastes/0.4.7/dynastes-0.4.7.tar.gz

---
+++ packages/dynastes/dynastes-0.4.7.tar.gz/dynastes-0.4.7/dynastes/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from dynastes import blocks
+from dynastes import core
+from dynastes import helpers
+from dynastes import layers
+from dynastes import models
+from dynastes import ops
+from dynastes import probability
+from dynastes import regularizers
+from dynastes import util
+from dynastes import weight_normalizers
+from dynastes.core import backend
+from dynastes.probability import bijectors, bijector_partials, pseudoblocksparse_bijectors
+
+object_scope = {
+    **blocks.object_scope,
+    **layers.object_scope,
+    **weight_normalizers.object_scope,
+    **regularizers.object_scope,
+}
+
+__version__ = "0.4.7"
+# Cleanup symbols to avoid polluting namespace.
+del absolute_import
+del division
+del print_function
