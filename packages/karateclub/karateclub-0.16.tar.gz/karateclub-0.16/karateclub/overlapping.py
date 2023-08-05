860d30cd763a8021542c3e915a0b70eab836377e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:39 +0000

    Add package karateclub/0.16/karateclub-0.16.tar.gz

---
+++ packages/karateclub/karateclub-0.16.tar.gz/karateclub-0.16/karateclub/overlapping.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class ClassD(object):
+    def __init__(self):
+        self.x = "A"
+        self.y = np.ones(5)
+
+class ClassE(object):
+    def __init__(self):
+        self.x = "B"
+
+class ClassF(object):
+    def __init__(self):
+        self.x = "C"
