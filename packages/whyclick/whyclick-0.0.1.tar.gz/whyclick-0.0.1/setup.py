42ca4ba2e4c27eaa0748ccb684a49b054d844002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:51 +0000

    Add package whyclick/0.0.1/whyclick-0.0.1.tar.gz

---
+++ packages/whyclick/whyclick-0.0.1.tar.gz/whyclick-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Setup script for Standalone WordNet API
+#
+# Copyright (C) 2019-2020 NLTK Project
+# Author:
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+
+from distutils.core import setup
+
+setup(
+    name='whyclick',
+    version='0.0.1',
+    packages=['whyclick'],
+    description="Cos I don't like to click",
+    long_description='',
+    url = 'https://github.com/alvations/whyclick',
+    license="MIT"
+)
