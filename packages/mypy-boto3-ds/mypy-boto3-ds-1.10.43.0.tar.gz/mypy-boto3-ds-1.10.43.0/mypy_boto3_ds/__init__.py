6a532791104bcb170a790224f0c9fe54df1132dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:53 +0000

    Add package mypy-boto3-ds/1.10.43.0/mypy-boto3-ds-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-ds/mypy-boto3-ds-1.10.43.0.tar.gz/mypy-boto3-ds-1.10.43.0/mypy_boto3_ds/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for ds service"
+from mypy_boto3_ds.client import DirectoryServiceClient, DirectoryServiceClient as Client
+from mypy_boto3_ds.paginator import (
+    DescribeDirectoriesPaginator,
+    DescribeDomainControllersPaginator,
+    DescribeSharedDirectoriesPaginator,
+    DescribeSnapshotsPaginator,
+    DescribeTrustsPaginator,
+    ListIpRoutesPaginator,
+    ListLogSubscriptionsPaginator,
+    ListSchemaExtensionsPaginator,
+    ListTagsForResourcePaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeDirectoriesPaginator",
+    "DescribeDomainControllersPaginator",
+    "DescribeSharedDirectoriesPaginator",
+    "DescribeSnapshotsPaginator",
+    "DescribeTrustsPaginator",
+    "DirectoryServiceClient",
+    "ListIpRoutesPaginator",
+    "ListLogSubscriptionsPaginator",
+    "ListSchemaExtensionsPaginator",
+    "ListTagsForResourcePaginator",
+)
