4c9726bcf72aa9798f2080bd75164a4541e6b25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:08 +0000

    Add package phovea-data-hdf/3.0.0/phovea_data_hdf-3.0.0.tar.gz

---
+++ packages/phovea-data-hdf/phovea_data_hdf-3.0.0.tar.gz/phovea_data_hdf-3.0.0/phovea_data_hdf/__init__.py
@@ -0,0 +1,28 @@
+###############################################################################
+# Caleydo - Visualization for Molecular Biology - http://caleydo.org
+# Copyright (c) The Caleydo Team. All rights reserved.
+# Licensed under the new BSD license, available at http://caleydo.org/license
+###############################################################################
+
+
+def phovea(registry):
+  """
+  register extension points
+  :param registry:
+  """
+  # generator-phovea:begin
+  registry.append('dataset-provider', 'dataset-hdf', 'phovea_data_hdf.hdf', {})
+
+  registry.append('json-encoder', 'dataset-hdf', 'phovea_data_hdf.json_encoder', {})
+  # generator-phovea:end
+  pass
+
+
+def phovea_config():
+  """
+  :return: file pointer to config file
+  """
+  from os import path
+  here = path.abspath(path.dirname(__file__))
+  config_file = path.join(here, 'config.json')
+  return config_file if path.exists(config_file) else None
