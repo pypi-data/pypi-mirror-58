93891e86b6a8f2741cd41ea7b4ed75c6897e61af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package mypy-boto3-es/1.10.43.0/mypy-boto3-es-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-es/mypy-boto3-es-1.10.43.0.tar.gz/mypy-boto3-es-1.10.43.0/mypy_boto3_es/__init__.py
@@ -0,0 +1,20 @@
+"Main interface for es service"
+from mypy_boto3_es.client import ElasticsearchServiceClient, ElasticsearchServiceClient as Client
+from mypy_boto3_es.paginator import (
+    DescribeReservedElasticsearchInstanceOfferingsPaginator,
+    DescribeReservedElasticsearchInstancesPaginator,
+    GetUpgradeHistoryPaginator,
+    ListElasticsearchInstanceTypesPaginator,
+    ListElasticsearchVersionsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeReservedElasticsearchInstanceOfferingsPaginator",
+    "DescribeReservedElasticsearchInstancesPaginator",
+    "ElasticsearchServiceClient",
+    "GetUpgradeHistoryPaginator",
+    "ListElasticsearchInstanceTypesPaginator",
+    "ListElasticsearchVersionsPaginator",
+)
