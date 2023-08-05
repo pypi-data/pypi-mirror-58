e10cecfac92b2127516a5f4adb81c3c424088bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package mypy-boto3-elbv2/1.10.41.0/mypy-boto3-elbv2-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-elbv2/mypy-boto3-elbv2-1.10.41.0.tar.gz/mypy-boto3-elbv2-1.10.41.0/mypy_boto3_elbv2/paginator.py
@@ -0,0 +1,137 @@
+"Main interface for elbv2 service Paginators"
+from __future__ import annotations
+
+from typing import Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_elbv2.type_defs import (
+    DescribeAccountLimitsOutputTypeDef,
+    DescribeListenerCertificatesOutputTypeDef,
+    DescribeListenersOutputTypeDef,
+    DescribeLoadBalancersOutputTypeDef,
+    DescribeRulesOutputTypeDef,
+    DescribeSSLPoliciesOutputTypeDef,
+    DescribeTargetGroupsOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = (
+    "DescribeAccountLimitsPaginator",
+    "DescribeListenerCertificatesPaginator",
+    "DescribeListenersPaginator",
+    "DescribeLoadBalancersPaginator",
+    "DescribeRulesPaginator",
+    "DescribeSSLPoliciesPaginator",
+    "DescribeTargetGroupsPaginator",
+)
+
+
+class DescribeAccountLimitsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeAccountLimits documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeAccountLimits)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[DescribeAccountLimitsOutputTypeDef, None, None]:
+        """
+        [DescribeAccountLimits.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeAccountLimits.paginate)
+        """
+
+
+class DescribeListenerCertificatesPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeListenerCertificates documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeListenerCertificates)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, ListenerArn: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[DescribeListenerCertificatesOutputTypeDef, None, None]:
+        """
+        [DescribeListenerCertificates.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeListenerCertificates.paginate)
+        """
+
+
+class DescribeListenersPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeListeners documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeListeners)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        LoadBalancerArn: str = None,
+        ListenerArns: List[str] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeListenersOutputTypeDef, None, None]:
+        """
+        [DescribeListeners.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeListeners.paginate)
+        """
+
+
+class DescribeLoadBalancersPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeLoadBalancers documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeLoadBalancers)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        LoadBalancerArns: List[str] = None,
+        Names: List[str] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeLoadBalancersOutputTypeDef, None, None]:
+        """
+        [DescribeLoadBalancers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeLoadBalancers.paginate)
+        """
+
+
+class DescribeRulesPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeRules documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeRules)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        ListenerArn: str = None,
+        RuleArns: List[str] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeRulesOutputTypeDef, None, None]:
+        """
+        [DescribeRules.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeRules.paginate)
+        """
+
+
+class DescribeSSLPoliciesPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeSSLPolicies documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeSSLPolicies)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, Names: List[str] = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[DescribeSSLPoliciesOutputTypeDef, None, None]:
+        """
+        [DescribeSSLPolicies.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeSSLPolicies.paginate)
+        """
+
+
+class DescribeTargetGroupsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeTargetGroups documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeTargetGroups)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        LoadBalancerArn: str = None,
+        TargetGroupArns: List[str] = None,
+        Names: List[str] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeTargetGroupsOutputTypeDef, None, None]:
+        """
+        [DescribeTargetGroups.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elbv2.html#ElasticLoadBalancingv2.Paginator.DescribeTargetGroups.paginate)
+        """
