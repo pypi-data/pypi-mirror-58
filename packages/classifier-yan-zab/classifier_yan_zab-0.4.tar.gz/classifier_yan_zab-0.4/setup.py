c312c24f26488be0db130b92bba9704f89e9ca9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:25 +0000

    Add package classifier-yan-zab/0.4/classifier_yan_zab-0.4.tar.gz

---
+++ packages/classifier-yan-zab/classifier_yan_zab-0.4.tar.gz/classifier_yan_zab-0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='classifier_yan_zab',
+      version='0.4',
+      description='Example Package',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='intents',
+      url='http://github.com/theKirill/GensimSample',
+      author='Yanyushkin',
+      author_email='k.a.yanushkin@gmail.com',
+      license='MIT',
+      packages=['classifier_yan_zab'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['example-command=iclassifier_yan_zab.command_line:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
