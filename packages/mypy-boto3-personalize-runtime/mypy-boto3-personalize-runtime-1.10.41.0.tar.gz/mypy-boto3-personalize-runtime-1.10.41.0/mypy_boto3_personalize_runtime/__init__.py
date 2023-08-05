aed0f6489e7e0c7b0cdaf980ea03f10f266334ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package mypy-boto3-personalize-runtime/1.10.41.0/mypy-boto3-personalize-runtime-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-personalize-runtime/mypy-boto3-personalize-runtime-1.10.41.0.tar.gz/mypy-boto3-personalize-runtime-1.10.41.0/mypy_boto3_personalize_runtime/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for personalize-runtime service"
+from mypy_boto3_personalize_runtime.client import (
+    PersonalizeRuntimeClient as Client,
+    PersonalizeRuntimeClient,
+)
+
+
+__all__ = ("Client", "PersonalizeRuntimeClient")
