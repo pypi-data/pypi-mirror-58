3e3a4ad57e7497bbdd054d94aec8da4a4ae97a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:40 +0000

    Add package aiohttp-socks/0.3.1/aiohttp_socks-0.3.1.tar.gz

---
+++ packages/aiohttp-socks/aiohttp_socks-0.3.1.tar.gz/aiohttp_socks-0.3.1/aiohttp_socks/__init__.py
@@ -0,0 +1,18 @@
+from .proxy import SocksVer, ProxyType
+from .connector import SocksConnector, ProxyConnector
+from .utils import open_connection, create_connection
+from .errors import (
+    ProxyError, ProxyConnectionError, SocksError, SocksConnectionError,
+    NoAcceptableAuthMethods, UnknownAuthMethod,
+    LoginAuthenticationFailed, InvalidServerVersion, InvalidServerReply
+)
+
+__version__ = '0.3.1'
+
+__all__ = ('SocksConnector', 'ProxyConnector', 'SocksVer', 'ProxyType',
+           'ProxyError', 'ProxyConnectionError',
+           'SocksError', 'SocksConnectionError',
+           'NoAcceptableAuthMethods', 'UnknownAuthMethod',
+           'LoginAuthenticationFailed', 'InvalidServerVersion',
+           'InvalidServerReply', 'open_connection',
+           'create_connection')
