ab1432bdb7a3c06c189ebe2e0a1037750bce9f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:11 +0000

    Add package psyker/0.1.6/psyker-0.1.6.tar.gz

---
+++ packages/psyker/psyker-0.1.6.tar.gz/psyker-0.1.6/psyker/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2019 Jacopo Cascioli
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# -*- coding: utf-8 -*-
+from .Db import Db
+from .Model import Model
+from .ModelFactory import ModelFactory
+from .Psyker import Psyker
+from .columns import Column, Foreign
+
+__all__ = ['Column', 'Db', 'Foreign', 'Model', 'ModelFactory', 'Psyker']
---
+++ packages/psyker/psyker-0.1.6.tar.gz/psyker-0.1.6/psyker/columns/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019 Jacopo Cascioli
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# -*- coding: utf-8 -*-
+from .Column import Column
+from .Foreign import Foreign
+
+__all__ = ['Column', 'Foreign']
