b5003f09a944b71b8d89b12e8ed131a8fdb7fa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:27 +0000

    Add package django-admin-knows-password/0.2/django_admin_knows_password-0.2.tar.gz

---
+++ packages/django-admin-knows-password/django_admin_knows_password-0.2.tar.gz/django_admin_knows_password-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_admin_knows_password',
+    packages=find_packages(),
+    version='0.2',
+    license='MIT',
+    description='Django package that provides widget to display images.',
+    author='Serhii Beznisko',
+    author_email='beznisko.ss@gmail.com',
+    url='https://github.com/serhiibeznisko/django-admin-knows-password',
+    keywords=['django', 'admin', 'user', 'password', 'widget'],
+    install_requires=[
+        'django',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
