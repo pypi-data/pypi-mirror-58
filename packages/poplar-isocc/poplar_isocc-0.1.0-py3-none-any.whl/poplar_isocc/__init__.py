3fe33a7593e4eb7393d7f365f83407a3b8cdc8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package poplar-isocc/0.1.0/poplar_isocc-0.1.0-py3-none-any.whl

---
+++ packages/poplar-isocc/poplar_isocc-0.1.0-py3-none-any.whl/poplar_isocc/__init__.py
@@ -0,0 +1,23 @@
+import iso3166
+
+country_codes = [c.alpha2 for c in iso3166.countries]
+
+def is_valid_iso_cc(code):
+    """Is ``code`` a valid ISO-3166 code?
+
+    :param code: country code to validate
+    :type code: str
+    :rtype: bool
+    :returns: True if code is valid.
+
+    .. code-block:: python
+
+        from poplar_isocc import is_valid_iso_cc
+
+        # Check if a code is valid.
+        is_valid_iso_cc("CA")
+            # returns True
+        is_valid_iso_cc("ZZ")
+            # returns False
+    """
+    return code in country_codes
