ad2b0ba6aa52648dc22b96c4c6e200261d3a0a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:15 +0000

    Add package pytest-error-for-skips/2.0.2/pytest-error-for-skips-2.0.2.tar.gz

---
+++ packages/pytest-error-for-skips/pytest-error-for-skips-2.0.2.tar.gz/pytest-error-for-skips-2.0.2/pytest_error_for_skips.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if item.config.getoption('--error-for-skips'):
+        if rep.skipped and call.excinfo.errisinstance(pytest.skip.Exception):
+            rep.outcome = 'failed'
+            r = call.excinfo._getreprcrash()
+            rep.longrepr = 'Forbidden skipped test - {message}'.format(message=r.message)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--error-for-skips',
+        action='store_true',
+        default=False,
+        help='Treat skipped tests as errors'
+    )
