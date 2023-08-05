09b7a75ed4e3f8174de7cb7e807187eaca270874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package mypy-boto3-ecs/1.10.41.0/mypy-boto3-ecs-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-ecs/mypy-boto3-ecs-1.10.41.0.tar.gz/mypy-boto3-ecs-1.10.41.0/mypy_boto3_ecs/__init__.py
@@ -0,0 +1,36 @@
+"Main interface for ecs service"
+from mypy_boto3_ecs.client import ECSClient as Client, ECSClient
+from mypy_boto3_ecs.paginator import (
+    ListAccountSettingsPaginator,
+    ListAttributesPaginator,
+    ListClustersPaginator,
+    ListContainerInstancesPaginator,
+    ListServicesPaginator,
+    ListTaskDefinitionFamiliesPaginator,
+    ListTaskDefinitionsPaginator,
+    ListTasksPaginator,
+)
+from mypy_boto3_ecs.waiter import (
+    ServicesInactiveWaiter,
+    ServicesStableWaiter,
+    TasksRunningWaiter,
+    TasksStoppedWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "ECSClient",
+    "ListAccountSettingsPaginator",
+    "ListAttributesPaginator",
+    "ListClustersPaginator",
+    "ListContainerInstancesPaginator",
+    "ListServicesPaginator",
+    "ListTaskDefinitionFamiliesPaginator",
+    "ListTaskDefinitionsPaginator",
+    "ListTasksPaginator",
+    "ServicesInactiveWaiter",
+    "ServicesStableWaiter",
+    "TasksRunningWaiter",
+    "TasksStoppedWaiter",
+)
