a54d4dcbd5fb097fde9e1b428c2e22ed6de72dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:08 +0000

    Add package qcp/0.9/qcp-0.9.tar.gz

---
+++ packages/qcp/qcp-0.9.tar.gz/qcp-0.9/setup.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name = 'qcp',
+    version = '0.9',
+    author = 'togo',
+    author_email = "zhouquanjs@qq.com",
+    description = "quick scp in isomorphic enviroments",
+    license = "MIT",
+    url = "https://gitee.com/zzqq2199/qcp",
+    packages=['qcp'],
+    install_requires=[],
+    classifiers=[]
+)
