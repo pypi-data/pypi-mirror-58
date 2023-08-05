ec860ce3fe4c89909dc45db1b4164c012894391e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:22 +0000

    Add package weworkbot/0.3.0/weworkbot-0.3.0.tar.gz

---
+++ packages/weworkbot/weworkbot-0.3.0.tar.gz/weworkbot-0.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='weworkbot',
+    version='0.3.0',
+
+    description='A framework for creating bot for wechat work',
+    url='https://github.com/MakDon/we-work-bot',
+    author='Makdon',
+    author_email='makdon@makdon.me',
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='wechat bot',
+    packages=find_packages(exclude=("tests",)),
+    python_requires='>=3.5',
+    install_requires=['requests'],
+)
