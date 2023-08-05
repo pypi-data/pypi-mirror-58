6e81dac84668d984d584a83e981a73ecdcc77fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:13 +0000

    Add package torchline/0.1.4.1/torchline-0.1.4.1-py3-none-any.whl

---
+++ packages/torchline/torchline-0.1.4.1-py3-none-any.whl/torchline/losses/loss.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn.functional as F
+from .build import LOSS_FN_REGISTRY
+
+__all__ = [
+    'CrossEntropy'
+]
+
+@LOSS_FN_REGISTRY.register()
+def CrossEntropy(cfg):
+    weight = cfg.LOSS.CLASS_WEIGHT
+    if weight in ['', None, []]:
+        weight = None
+    else:
+        weight = torch.tensor(weight)
+        if torch.cuda.is_available(): weight=weight.cuda()
+    return torch.nn.CrossEntropyLoss(weight=weight)
