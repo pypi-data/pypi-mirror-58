048a48676ca6811ba2196a7fc68b99650b867a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package mypy-boto3-application-autoscaling/1.10.43.0/mypy-boto3-application-autoscaling-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-application-autoscaling/mypy-boto3-application-autoscaling-1.10.43.0.tar.gz/mypy-boto3-application-autoscaling-1.10.43.0/mypy_boto3_application_autoscaling/__init__.py
@@ -0,0 +1,21 @@
+"Main interface for application-autoscaling service"
+from mypy_boto3_application_autoscaling.client import (
+    ApplicationAutoScalingClient as Client,
+    ApplicationAutoScalingClient,
+)
+from mypy_boto3_application_autoscaling.paginator import (
+    DescribeScalableTargetsPaginator,
+    DescribeScalingActivitiesPaginator,
+    DescribeScalingPoliciesPaginator,
+    DescribeScheduledActionsPaginator,
+)
+
+
+__all__ = (
+    "ApplicationAutoScalingClient",
+    "Client",
+    "DescribeScalableTargetsPaginator",
+    "DescribeScalingActivitiesPaginator",
+    "DescribeScalingPoliciesPaginator",
+    "DescribeScheduledActionsPaginator",
+)
