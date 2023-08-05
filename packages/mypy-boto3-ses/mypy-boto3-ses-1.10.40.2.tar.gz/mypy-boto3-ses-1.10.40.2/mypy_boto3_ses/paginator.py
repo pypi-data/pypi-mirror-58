ad1ae6e8224d4d83bfd53ceb805f5157cc4486c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:57 +0000

    Add package mypy-boto3-ses/1.10.40.2/mypy-boto3-ses-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-ses/mypy-boto3-ses-1.10.40.2.tar.gz/mypy-boto3-ses-1.10.40.2/mypy_boto3_ses/paginator.py
@@ -0,0 +1,100 @@
+"Main interface for ses service Paginators"
+from __future__ import annotations
+
+import sys
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_ses.type_defs import (
+    ListConfigurationSetsResponseTypeDef,
+    ListCustomVerificationEmailTemplatesResponseTypeDef,
+    ListIdentitiesResponseTypeDef,
+    ListReceiptRuleSetsResponseTypeDef,
+    ListTemplatesResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "ListConfigurationSetsPaginator",
+    "ListCustomVerificationEmailTemplatesPaginator",
+    "ListIdentitiesPaginator",
+    "ListReceiptRuleSetsPaginator",
+    "ListTemplatesPaginator",
+)
+
+
+class ListConfigurationSetsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListConfigurationSets documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListConfigurationSets)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListConfigurationSetsResponseTypeDef, None, None]:
+        """
+        [ListConfigurationSets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListConfigurationSets.paginate)
+        """
+
+
+class ListCustomVerificationEmailTemplatesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListCustomVerificationEmailTemplates documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListCustomVerificationEmailTemplates)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListCustomVerificationEmailTemplatesResponseTypeDef, None, None]:
+        """
+        [ListCustomVerificationEmailTemplates.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListCustomVerificationEmailTemplates.paginate)
+        """
+
+
+class ListIdentitiesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListIdentities documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListIdentities)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        IdentityType: Literal["EmailAddress", "Domain"] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[ListIdentitiesResponseTypeDef, None, None]:
+        """
+        [ListIdentities.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListIdentities.paginate)
+        """
+
+
+class ListReceiptRuleSetsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListReceiptRuleSets documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListReceiptRuleSets)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListReceiptRuleSetsResponseTypeDef, None, None]:
+        """
+        [ListReceiptRuleSets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListReceiptRuleSets.paginate)
+        """
+
+
+class ListTemplatesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListTemplates documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListTemplates)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListTemplatesResponseTypeDef, None, None]:
+        """
+        [ListTemplates.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Paginator.ListTemplates.paginate)
+        """
---
+++ packages/mypy-boto3-ses/mypy-boto3-ses-1.10.40.2.tar.gz/mypy-boto3-ses-1.10.40.2/mypy_boto3_ses/waiter.py
@@ -0,0 +1,21 @@
+"Main interface for ses service Waiters"
+from __future__ import annotations
+
+from typing import List
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_ses.type_defs import WaiterConfigTypeDef
+
+
+__all__ = ("IdentityExistsWaiter",)
+
+
+class IdentityExistsWaiter(Boto3Waiter):
+    """
+    [Waiter.IdentityExists documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Waiter.IdentityExists)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, Identities: List[str], WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [IdentityExists.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/ses.html#SES.Waiter.IdentityExists.wait)
+        """
