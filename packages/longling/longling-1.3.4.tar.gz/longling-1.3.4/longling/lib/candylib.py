f00542a1a4b2ee958f27b3bcb09e04518f85a338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:21 +0000

    Add package longling/1.3.4/longling-1.3.4.tar.gz

---
+++ packages/longling/longling-1.3.4.tar.gz/longling-1.3.4/longling/lib/candylib.py
@@ -0,0 +1,63 @@
+__all__ = ['as_list']
+
+
+def as_list(obj):
+    r"""A utility function that converts the argument to a list
+    if it is not already.
+
+    Parameters
+    ----------
+    obj : object
+        argument to be converted to a list
+
+    Returns
+    -------
+    list_obj: list
+        If `obj` is a list or tuple, return it. Otherwise,
+        return `[obj]` as a single-element list.
+
+    Examples
+    --------
+    >>> as_list(1)
+    [1]
+    >>> as_list([1])
+    [1]
+    """
+    if isinstance(obj, (list, tuple)):
+        return obj
+    else:
+        return [obj]
+
+
+def get_all_subclass(cls):  # pragma: no cover
+    subclass = set()
+
+    def _get_all_subclass(cls, res_set):
+        res_set.add(cls)
+        for sub_cls in cls.__subclasses__():
+            _get_all_subclass(sub_cls, res_set)
+
+    _get_all_subclass(cls, res_set=subclass)
+    return subclass
+
+
+class RegisterNameExistedError(Exception):  # pragma: no cover
+    pass
+
+
+class Register(object):  # pragma: no cover
+    def __init__(self):
+        self.reg = set()
+
+    def register(self, name):
+        if name not in self.reg:
+            self.reg.add(name)
+            return True
+        else:
+            raise RegisterNameExistedError("name %s existed" % name)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
