944c0b9fac6f4fb09fbd0a1491c29dc8c587a605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:35 +0000

    Add package VyattaConfParser/0.5.5/VyattaConfParser-0.5.5.tar.gz

---
+++ packages/VyattaConfParser/VyattaConfParser-0.5.5.tar.gz/VyattaConfParser-0.5.5/setup.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import vyattaconfparser
+
+setup(
+    name='VyattaConfParser',
+    version=vyattaconfparser.__version__,
+    packages=['vyattaconfparser'],
+    url='https://github.com/hedin/vyatta-conf-parser',
+    author=vyattaconfparser.__author__,
+    author_email='a.m.mironov@gmail.com',
+    classifiers=(
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta'
+    ),
+    description='A python config parser for Vyatta',
+    extras_require={
+        'dev': ['datadiff', ],
+    },
+)
