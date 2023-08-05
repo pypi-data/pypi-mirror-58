0d2dcd7dd3a77e576add7f8dd47212040610e3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package TyphoonTest/1.10.1/TyphoonTest-1.10.1-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/TyphoonTest/TyphoonTest-1.10.1-cp36-cp36m-manylinux2010_x86_64.whl/typhoon/test/rms/__init__.py
@@ -0,0 +1,35 @@
+from . import impl as _impl
+
+
+def window_rms(signal, window_length=None, max_frequency=1000, match_num_of_cycles_with_samples=True):
+    '''Calculates RMS value of the signal using sliding window technique.
+
+    Parameters
+    ----------
+    signal : Series
+        A Pandas Series with the signal the RMS value is to be calculated.
+    window_length : float/int or Timedelta
+        Duration in seconds (if float/int) of the window used in the calculation. Defaults to None, in which case the period of the strongest frequency found on the signal will be used for window period.
+    max_frequency : float/int or Timedelta
+        If 'window_length' is not defined, sets the maximum frequency to automatically detect the 'window_length' as one period of the signal's strongest frequency. Default: 1000
+    match_num_of_cycles_with_samples: bool
+        If 'window_length' is not defined, tries to match the 'window_length' with an integer number of cycles of the strongest frequency. Default: True
+    Returns
+    -------
+    rms : Series
+        Time series with the same length as signal with the calculated RMS value.
+
+    Examples
+    --------
+    >>> from typhoon.test.signals import assert_is_constant, pandas_sine
+    >>> from typhoon.test.rms import window_rms
+    >>> from typhoon.test.ranges import around, after
+    >>> from typhoon.types.timedelta import Timedelta as td
+    >>> from numpy import sqrt
+
+    >>> sine = pandas_sine(amplitude=220*sqrt(2), frequency=60)
+    >>> period = td(1/60)
+    >>> rms = window_rms(sine, period)
+    >>> assert_is_constant(rms, at_value=around(220, tol_p=0.01), strictness=1, during=after(period))
+    '''
+    return _impl.window_rms(signal, window_length, max_frequency, match_num_of_cycles_with_samples)
