5637a5a0c6460f9d47c4af9cee42d30270549a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:20 +0000

    Add package django-datastore/0.1.1/django-datastore-0.1.1.tar.gz

---
+++ packages/django-datastore/django-datastore-0.1.1.tar.gz/django-datastore-0.1.1/datastore/serializers.py
@@ -0,0 +1,31 @@
+import logging
+
+from rest_framework import serializers
+
+from .fields import FileBase64Field
+from .models import DataStore, RelatedDocument
+
+logger = logging.getLogger(__name__)
+
+
+class DataStoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataStore
+        fields = '__all__'
+        read_only_fields = ('key', )
+
+
+class RelatedDocumentSerializer(serializers.ModelSerializer):
+    document = FileBase64Field()
+
+    class Meta:
+        model = RelatedDocument
+        fields = ('key', 'document', 'properties')
+
+
+class RelatedDocumentPDFSerializer(RelatedDocumentSerializer):
+    document = serializers.FileField(use_url=False)
+
+
+class RelatedDocumentFileSerializer(RelatedDocumentSerializer):
+    document = serializers.FileField()
