2e8ca03e99f1920c008fbd508366dc9c0f2b0d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:13 +0000

    Add package CADETMatch/0.5.7/CADETMatch-0.5.7-py3-none-any.whl

---
+++ packages/CADETMatch/CADETMatch-0.5.7-py3-none-any.whl/CADETMatch/kde_generator.py
@@ -0,0 +1,275 @@
+#This module simulates noise (systemic and random) to use for kernel density estimation based on experimental data
+
+#Currently the randomness is
+
+#Pump flow rate
+#Pump delay
+#Base noise
+#Signal noise
+
+import itertools
+import CADETMatch.score as score
+from cadet import Cadet, H5
+from pathlib import Path
+import numpy
+import multiprocessing
+from sklearn.neighbors.kde import KernelDensity
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+import copy
+
+bw_tol = 1e-4
+
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    import h5py
+
+import scipy.optimize
+
+import CADETMatch.util as util
+import CADETMatch.cache as cache
+
+import CADETMatch.synthetic_error as synthetic_error
+
+import joblib
+import subprocess
+import sys
+
+def bandwidth_score(bw, data, store):
+    bandwidth = 10**bw[0]
+    kde_bw = KernelDensity(kernel='gaussian', bandwidth=bandwidth, atol=bw_tol)
+    scores = cross_val_score(kde_bw, data, cv=3)
+    store.append( [bandwidth, -max(scores)] )
+    return -max(scores)
+
+def get_bandwidth(scores, cache):
+    store = []
+
+    result = scipy.optimize.differential_evolution(bandwidth_score, bounds = [(-4, 1),], 
+                    args = (scores,store,), updating='deferred', workers=cache.toolbox.map, disp=True,
+                    popsize=100)
+    bandwidth = 10**result.x[0]
+    multiprocessing.get_logger().info("selected bandwidth %s", bandwidth)
+
+    store = numpy.array(store)
+    return bandwidth, store
+
+def mirror(data):
+    data_min = numpy.max(data,0) - data
+    data_mask = numpy.ma.masked_equal(data_min, 0.0, copy=False)
+    min_value = data_mask.min(axis=0)
+    
+    data_mirror = 1 + 1 - numpy.copy(data) + min_value
+    full_data = numpy.vstack([data_mirror, data])
+    
+    return full_data
+
+def setupKDE(cache):
+    scores = generate_synthetic_error(cache)
+
+    mcmcDir = Path(cache.settings['resultsDirMCMC'])
+
+    scores_mirror = mirror(scores)
+
+    scaler = getScaler(scores_mirror)
+
+    scores_scaler = scaler.transform(scores_mirror)
+
+    bandwidth, store = get_bandwidth(scores_scaler, cache)
+
+    kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth, atol=bw_tol).fit(scores_scaler)
+
+    joblib.dump(scaler, mcmcDir / 'kde_scaler.joblib')
+
+    joblib.dump(kde, mcmcDir / 'kde_score.joblib')
+
+    h5_data = H5()
+    h5_data.filename = (mcmcDir / "kde_settings.h5").as_posix()
+    h5_data.root.bandwidth = bandwidth
+    h5_data.root.store = numpy.array(store)
+    h5_data.root.scores = scores
+    h5_data.root.scores_mirror = scores_mirror
+    h5_data.root.scores_mirror_scaled = scores_scaler
+    h5_data.save()
+
+    cwd = str(Path(__file__).parent)
+    ret = subprocess.run([sys.executable, 'graph_kde.py', str(cache.json_path), str(util.getCoreCounts())], 
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+    util.log_subprocess('graph_kde.py', ret)
+
+    return kde, scaler
+
+def getScaler(data):
+    scaler = preprocessing.StandardScaler().fit(data)
+    return scaler
+
+def getKDE(cache):
+    mcmcDir = Path(cache.settings['resultsDirMCMC'])
+
+    kde = joblib.load(mcmcDir / 'kde_score.joblib')
+
+    scaler = joblib.load(mcmcDir / 'kde_scaler.joblib')
+
+    return kde, scaler
+
+def getKDEPrevious(cache):
+    if 'mcmc_h5' in cache.settings:
+        mcmc_h5 = Path(cache.settings['mcmc_h5'])
+        mcmcDir = mcmc_h5.parent
+
+        if mcmcDir.exists():
+            kde = joblib.load(mcmcDir / 'kde_prior.joblib')
+
+            scaler = joblib.load(mcmcDir / 'kde_prior_scaler.joblib')
+
+            return kde, scaler
+    return None, None
+
+def generate_data(cache):
+    scores = generate_synthetic_error(cache)
+
+    mcmcDir = Path(cache.settings['resultsDirMCMC'])
+    save_scores = mcmcDir / 'scores_used.npy'
+
+    numpy.save(save_scores, scores)
+
+    scores_mirror = mirror(scores)
+
+    scaler = getScaler(scores_mirror)
+
+    scores_scaler = scaler.transform(scores_mirror)
+
+    bandwidth, store = get_bandwidth(scores_scaler, cache)
+
+    return scores, bandwidth
+
+def synthetic_error_simulation(json_path):
+    if json_path != cache.cache.json_path:
+        cache.cache.setup_dir(json_path)
+        util.setupLog(cache.cache.settings['resultsDirLog'], "main.log")
+        cache.cache.setup(json_path)
+
+    scores = []
+    outputs = {}
+    simulations = {}
+    experiment_failed = False
+
+    for experiment in cache.cache.settings['kde_synthetic']:
+        file_path = experiment['file_path']
+        delay_settings = experiment['delay']
+        flow_settings = experiment['flow']
+        load_settings = experiment['load']
+        count_settings = experiment['count']
+        experimental_csv = experiment['experimental_csv']
+        uv_noise = experiment['uv_noise']
+        units = experiment['units']
+        name = experiment['name']
+
+        dir_base = Path(cache.cache.settings.get('baseDir'))
+        file = dir_base / file_path
+
+        data = numpy.loadtxt(experimental_csv, delimiter=',')
+        times = data[:,0]
+
+        temp = Cadet()
+        temp.filename = file.as_posix()
+        temp.load()
+
+        util.setupSimulation(temp, times, cache.cache.target[name]['smallest_peak'], cache.cache)
+
+        nsec = temp.root.input.solver.sections.nsec
+
+        def post_function(simulation):
+            #baseline drift need to redo this
+            #error_slope = numpy.random.normal(error_slope_settings[0], error_slope_settings[1], 1)[0]
+
+            for unit in units:
+                unit_name = 'unit_%03d' % unit
+                for comp in range(simulation.root.input.model[unit_name].ncomp):
+                    comp_name = 'solution_outlet_comp_%03d' % comp
+                    error_uv = numpy.random.normal(uv_noise[0], uv_noise[1], len(simulation.root.output.solution[unit_name][comp_name]))
+                    simulation.root.output.solution[unit_name][comp_name] = simulation.root.output.solution[unit_name][comp_name] + error_uv
+    
+        error_delay = Cadet(temp.root)
+
+        delays = numpy.random.uniform(delay_settings[0], delay_settings[1], nsec)
+    
+        synthetic_error.pump_delay(error_delay, delays)
+
+        flow = numpy.random.normal(flow_settings[0], flow_settings[1], error_delay.root.input.solver.sections.nsec)
+    
+        synthetic_error.error_flow(error_delay, flow)
+    
+        load = numpy.random.normal(load_settings[0], load_settings[1], error_delay.root.input.solver.sections.nsec)
+    
+        synthetic_error.error_load(error_delay, load)
+
+
+        exp_info = None
+        for exp in cache.cache.settings['experiments']:
+            if exp['name'] == name:
+                exp_info = exp
+                break
+
+        result = util.runExperiment(None, exp_info, cache.cache.settings, cache.cache.target, error_delay, error_delay.root.timeout, cache.cache, post_function=post_function)
+
+        if result is not None:
+            scores.extend(result['scores'])
+
+            simulations[name] = result['simulation']
+
+            for unit in units:
+                unit_name = 'unit_%03d' % unit
+                for comp in range(result['simulation'].root.input.model[unit_name].ncomp):
+                    outputs['%s_unit_%03d_comp_%03d' % (name, int(unit), comp)] = result['simulation'].root.output.solution['unit_%03d' % int(unit)]['solution_outlet_comp_%03d' % comp]
+        else:
+            experiment_failed = True
+
+    if experiment_failed:
+        return None, None, None
+    return scores, simulations, outputs
+
+def generate_synthetic_error(cache):
+    if 'kde_synthetic' in cache.settings:
+        count_settings = int(cache.settings['kde_synthetic'][0]['count'])
+        
+        scores_all = []
+        times = {}
+        outputs_all = {}
+
+        for scores, simulations, outputs in cache.toolbox.map(synthetic_error_simulation, [cache.json_path] * count_settings):
+            if scores and simulations and outputs:
+
+                scores_all.append(scores)
+
+                for key,value in outputs.items():
+                    temp = outputs_all.get(key, [])
+                    temp.append(value)
+                    outputs_all[key] = temp
+
+                for key,sim in simulations.items():
+                    if key not in times:
+                        times[key] = sim.root.output.solution.solution_times
+                                        
+        scores = numpy.array(scores_all)
+
+        dir_base = cache.settings.get('resultsDirBase')
+        file = dir_base / 'kde_data.h5'
+
+        kde_data = H5()
+        kde_data.filename = file.as_posix()
+
+        kde_data.root.scores = scores
+
+        for output_name, output in outputs_all.items():
+            kde_data.root[output_name] = numpy.array(output)
+
+        for time_name, time in times.items():
+            kde_data.root['%s_time' % time_name] = time
+
+        kde_data.save()
+                               
+        return scores
+
+    return None, None
---
+++ packages/CADETMatch/CADETMatch-0.5.7-py3-none-any.whl/CADETMatch/scores/dextranShape.py
@@ -0,0 +1,89 @@
+import CADETMatch.util as util
+import CADETMatch.score as score
+import scipy.stats
+import numpy
+import numpy.linalg
+from addict import Dict
+import CADETMatch.smoothing as smoothing
+
+name = "DextranShape"
+settings = Dict()
+settings.adaptive = True
+settings.badScore = 0
+settings.meta_mask = True
+settings.count = 2
+settings.failure = [0.0] * settings.count, 1e6, 1, numpy.array([0.0]), numpy.array([0.0]), numpy.array([1e6]), [1.0] * settings.count
+
+def run(sim_data, feature):
+    "special score designed for dextran. This looks at only the front side of the peak up to the maximum slope and pins a value at the elbow in addition to the top"
+    exp_time_values = feature['time']
+    max_value = feature['max_value']
+
+    selected = feature['selected']
+        
+    sim_time_values, sim_data_values = util.get_times_values(sim_data['simulation'], feature)
+
+    if max(sim_data_values) < max_value: #the system has no point higher than the value we are looking for
+        #remove hard failure
+        max_value = max(sim_data_values)
+
+    exp_time_values = exp_time_values[selected]
+    exp_data_zero = feature['exp_data_zero']
+
+    min_index = numpy.argmax(sim_data_values >= 1e-3*max_value)
+    max_index = numpy.argmax(sim_data_values >= max_value)
+
+    sim_data_zero = numpy.zeros(len(sim_data_values))
+    sim_data_zero[min_index:max_index+1] = sim_data_values[min_index:max_index+1]
+
+    pearson, diff_time = score.pearson_spline(exp_time_values, sim_data_zero, exp_data_zero)
+
+    temp = [pearson,
+            feature['offsetTimeFunction'](numpy.abs(diff_time)),
+            ]
+
+    data = (temp, util.sse(sim_data_zero, exp_data_zero), len(sim_data_zero), 
+            sim_time_values, sim_data_zero, exp_data_zero, [1.0 - i for i in temp])
+
+    return data
+
+def setup(sim, feature, selectedTimes, selectedValues, CV_time, abstol, cache):
+    temp = {}
+    #change the stop point to be where the max positive slope is along the searched interval
+    name = '%s_%s' % (sim.root.experiment_name,   feature['name'])
+    s, crit_fs = smoothing.find_smoothing_factors(selectedTimes, selectedValues, name, cache)
+    values = smoothing.smooth_data_derivative(selectedTimes, selectedValues, crit_fs, s)
+    
+    max_index = numpy.argmax(values)
+    max_time = selectedTimes[max_index]
+    max_value = selectedValues[max_index]
+
+    min_index = numpy.argmax(selectedValues >= 1e-3*max_value)
+    min_time = selectedTimes[min_index]
+    min_value = selectedValues[min_index]
+
+    exp_data_zero = numpy.zeros(len(selectedValues))
+    exp_data_zero[min_index:max_index+1] = selectedValues[min_index:max_index+1]
+
+    temp['min_time'] = feature['start']
+    temp['max_time'] = feature['stop']
+    temp['max_value'] = max_value
+    temp['exp_data_zero'] = exp_data_zero
+    temp['offsetTimeFunction'] = score.time_function_decay_cv(CV_time, selectedTimes, max_time)
+    temp['peak_max'] = max_value
+    temp['smoothing_factor'] = s
+    temp['critical_frequency'] = crit_fs
+    return temp
+
+def headers(experimentName, feature):
+    name = "%s_%s" % (experimentName, feature['name'])
+    temp = ["%s_Shape" % name, 
+            "%s_Time" % name,
+            ]
+    return temp
+
+
+
+
+
+
