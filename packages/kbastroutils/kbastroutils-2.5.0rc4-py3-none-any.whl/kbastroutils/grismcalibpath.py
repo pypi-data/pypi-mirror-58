cc9a64aeb0fa3ac68a40b52642e322c3564558f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:17 +0000

    Add package kbastroutils/2.5.0rc4/kbastroutils-2.5.0rc4-py3-none-any.whl

---
+++ packages/kbastroutils/kbastroutils-2.5.0rc4-py3-none-any.whl/kbastroutils/grismcalibpath.py
@@ -0,0 +1,25 @@
+class GrismCalibPath:
+    def __init__(self):
+        CONF = {('HST','WFC3','IR'): {('G141','F140W'):None,
+                                      ('G141','F160W'):'/Users/kbhirombhakdi/_work/_calib_files/WFC3.IR.G141.cal.V4.32/G141.F160W.V4.32.conf'
+                                     },
+                ('HST','ACS','WFC'): None
+               }
+        SENS = {('HST','WFC3','IR'): {'G102':None,
+                                      'G141':'/Users/kbhirombhakdi/_work/_calib_files/WFC3.IR.G141.cal.V4.32/WFC3.IR.G141.1st.sens.2.fits'
+                                     },
+                ('HST','ACS','WFC'): None
+               }
+        BKG = {('HST','WFC3','IR'): {'G102':None,
+                                     'G141':'/Users/kbhirombhakdi/_work/_calib_files/WFC3.IR.G141.cal.V4.32/WFC3.IR.G141.sky.V1.0.fits'
+                                    },
+               ('HST','ACS','WFC'): None
+              }
+        FLAT = {('HST','WFC3','IR'): {'G102':None,
+                                      'G141':'/Users/kbhirombhakdi/_work/_calib_files/WFC3.IR.G141.cal.V4.32/WFC3.IR.G141.flat.2.fits'
+                                     },
+                ('HST','ACS','WFC'): None
+               }
+        TABLE = {'CONF':CONF, 'SENS':SENS, 'BKG':BKG, 'FLAT':FLAT}
+        self.table = TABLE
+        
