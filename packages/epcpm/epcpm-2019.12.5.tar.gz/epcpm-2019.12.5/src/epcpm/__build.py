e5cb1f9c8aec75d08c9c4c0c8492b2b82a96d9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:06 +0000

    Add package epcpm/2019.12.5/epcpm-2019.12.5.tar.gz

---
+++ packages/epcpm/epcpm-2019.12.5.tar.gz/epcpm-2019.12.5/src/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '29670643'
+build_number = '559'
+build_version = '1.0.559'
+job_id = 'vkbtowmalruayk2s'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/pm/build/job/vkbtowmalruayk2s'
