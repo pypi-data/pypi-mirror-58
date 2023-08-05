e2d7c0bdfa061a9e1ef70b6401583265610749be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:50 +0000

    Add package datamaestro-ml/2019.12.20/datamaestro_ml-2019.12.20-py3-none-any.whl

---
+++ packages/datamaestro-ml/datamaestro_ml-2019.12.20-py3-none-any.whl/datamaestro_ml/__init__.py
@@ -0,0 +1,6 @@
+import datamaestro
+
+class Repository(datamaestro.Repository):
+    AUTHOR = """Benjamin Piwowarski <benjamin@piwowarski.fr>"""
+    DESCRIPTION = """Machine learning datamaestro repository"""
+    NAMESPACE = """ml"""
---
+++ packages/datamaestro-ml/datamaestro_ml-2019.12.20-py3-none-any.whl/datamaestro_ml/test/test_datasets.py
@@ -0,0 +1,13 @@
+# --- Task and types definitions
+
+import unittest
+
+from datamaestro.test import DatasetTests
+from datamaestro import Context
+
+class MainTest(DatasetTests, unittest.TestCase):
+    pass        
+
+if __name__ == '__main__':
+    unittest.main()
+
