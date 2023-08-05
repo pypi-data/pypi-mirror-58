1f8e41be46ee1c7c3cb5b149a7e1e80c55624a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package team-mates/0.0.4/team_mates-0.0.4.tar.gz

---
+++ packages/team-mates/team_mates-0.0.4.tar.gz/team_mates-0.0.4/README.md
@@ -0,0 +1,31 @@
+# Team-Mates
+
+> A simple python library to analyze team-mates contributions to projects, using tools like Git and Trello. It embed a web interface to easily consult theses informations.
+
+## Sources
+
+* GitHub: https://github.com/HoshiyoSan/team-mates.git
+* PyPI: https://pypi.org/project/team-mates
+
+## Display the web interface
+
+```bash
+python3 -m team-mates web -p 8080 --browser
+```
+
+Run the dataviz web server on port 8080 (if omitted, default is 5000)
+
+## Helper commands
+
+A full list of commands can be retrieve by doing
+```
+$ make help
+```
+
+Example output
+```
+help                           display this help
+install                        install dev dependencies in a virtualenv
+test                           reinstall dev project and try to run web server
+upload                         build and upload package on pypi
+```
---
+++ packages/team-mates/team_mates-0.0.4.tar.gz/team_mates-0.0.4/setup.py
@@ -0,0 +1,39 @@
+import platform
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# select os specific dependencies
+operating_system = platform.system()
+if operating_system == 'Windows':
+    raise OSError("This module has no support for windows yet.")
+elif operating_system == 'Linux':
+    WSGI_SERVER = 'gunicorn'
+
+setuptools.setup(
+    name="team_mates",
+    version="0.0.4",
+    author="Sylvan LE DEUNFF",
+    author_email="sylvan.ledeunff@gmail.com",
+    description="A library to analyze team mates contributions to projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/team-mates",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'team-mates=team_mates.__main__:main'
+        ],
+    },
+    install_requires=[
+        'flask',
+        WSGI_SERVER
+    ],
+    python_requires='>=3.6',
+)
