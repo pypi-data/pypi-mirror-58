41e7cce3546a34c00b456d1e8cbf3c0ee7e0cc59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:38 +0000

    Add package pybump/1.1.1/pybump-1.1.1.tar.gz

---
+++ packages/pybump/pybump-1.1.1.tar.gz/pybump-1.1.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybump",
+    version="1.1.1",
+    author="Arie Lev",
+    author_email="levinsonarie@gmail.com",
+    description="Python version bumper",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/ArieLevs/PyBump",
+    license='Apache License 2.0',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyyaml==5.1',
+        'argparse==1.4.0',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            'pybump = pybump.pybump:main'
+        ],
+    },
+)
