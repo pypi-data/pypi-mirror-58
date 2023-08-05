17eef8f23b1e55f96b6247e299abd709e9918352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:15 +0000

    Add package channels/2.4.0/channels-2.4.0.tar.gz

---
+++ packages/channels/channels-2.4.0.tar.gz/channels-2.4.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+from channels import __version__
+
+setup(
+    name='channels',
+    version=__version__,
+    url='http://github.com/django/channels',
+    author='Django Software Foundation',
+    author_email='foundation@djangoproject.com',
+    description="Brings async, event-driven capabilities to Django. Django 2.2 and up only.",
+    license='BSD',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    python_requires='>=3.5',
+    install_requires=[
+        'Django>=2.2',
+        'asgiref~=3.2',
+        'daphne~=2.3',
+    ],
+    extras_require={
+        'tests': [
+            'pytest~=4.4',
+            "pytest-django~=3.4",
+            "pytest-asyncio~=0.10",
+            "async_generator~=1.10",
+            "async-timeout~=3.0",
+            'coverage~=4.5',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
