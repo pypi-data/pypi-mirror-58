8e7bdc42902c6d3bada5a06f56af4d5a886b9eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package burnham/0.1.0.dev2/burnham-0.1.0.dev2.tar.gz

---
+++ packages/burnham/burnham-0.1.0.dev2.tar.gz/burnham-0.1.0.dev2/src/burnham/__init__.py
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+__version__ = "0.1.0.dev2"
+__title__ = "burnham"
+__description__ = "Application for end-to-end testing Mozilla's Glean telemetry. 👩‍🚀"
+__url__ = "https://github.com/hackebrot/burnham"
+
+__author__ = "Raphael Pierzina"
+__email__ = "raphael@hackebrot.de"
+
+__license__ = "MPL 2.0"
+__copyright__ = "Copyright (c) 2019 Raphael Pierzina"
