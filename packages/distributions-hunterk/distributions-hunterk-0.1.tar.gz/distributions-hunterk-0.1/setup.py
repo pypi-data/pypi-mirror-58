c58ce9e9d89ef2e17dacb7672ba4d67e769a14ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:03 +0000

    Add package distributions-hunterk/0.1/distributions-hunterk-0.1.tar.gz

---
+++ packages/distributions-hunterk/distributions-hunterk-0.1.tar.gz/distributions-hunterk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-hunterk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-hunterk'],
+      zip_safe=False)
