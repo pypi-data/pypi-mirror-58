031f4eb20c1e8e6557b5e9cfdbb68fe3daf78f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:38 +0000

    Add package mypy-boto3-efs/1.10.40.2/mypy-boto3-efs-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-efs/mypy-boto3-efs-1.10.40.2.tar.gz/mypy-boto3-efs-1.10.40.2/mypy_boto3_efs/paginator.py
@@ -0,0 +1,62 @@
+"Main interface for efs service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_efs.type_defs import (
+    DescribeFileSystemsResponseTypeDef,
+    DescribeMountTargetsResponseTypeDef,
+    DescribeTagsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("DescribeFileSystemsPaginator", "DescribeMountTargetsPaginator", "DescribeTagsPaginator")
+
+
+class DescribeFileSystemsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeFileSystems documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/efs.html#EFS.Paginator.DescribeFileSystems)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        CreationToken: str = None,
+        FileSystemId: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeFileSystemsResponseTypeDef, None, None]:
+        """
+        [DescribeFileSystems.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/efs.html#EFS.Paginator.DescribeFileSystems.paginate)
+        """
+
+
+class DescribeMountTargetsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeMountTargets documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/efs.html#EFS.Paginator.DescribeMountTargets)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        FileSystemId: str = None,
+        MountTargetId: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeMountTargetsResponseTypeDef, None, None]:
+        """
+        [DescribeMountTargets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/efs.html#EFS.Paginator.DescribeMountTargets.paginate)
+        """
+
+
+class DescribeTagsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeTags documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/efs.html#EFS.Paginator.DescribeTags)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, FileSystemId: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[DescribeTagsResponseTypeDef, None, None]:
+        """
+        [DescribeTags.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/efs.html#EFS.Paginator.DescribeTags.paginate)
+        """
