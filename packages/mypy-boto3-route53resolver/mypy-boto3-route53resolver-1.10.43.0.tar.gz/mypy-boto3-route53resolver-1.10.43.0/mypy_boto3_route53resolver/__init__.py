5b9319568f1a8bffecce45bc6ec73383a1c295b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package mypy-boto3-route53resolver/1.10.43.0/mypy-boto3-route53resolver-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-route53resolver/mypy-boto3-route53resolver-1.10.43.0.tar.gz/mypy-boto3-route53resolver-1.10.43.0/mypy_boto3_route53resolver/__init__.py
@@ -0,0 +1,6 @@
+"Main interface for route53resolver service"
+from mypy_boto3_route53resolver.client import Route53ResolverClient, Route53ResolverClient as Client
+from mypy_boto3_route53resolver.paginator import ListTagsForResourcePaginator
+
+
+__all__ = ("Client", "ListTagsForResourcePaginator", "Route53ResolverClient")
