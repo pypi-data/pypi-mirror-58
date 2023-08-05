bc2413ba91805bd9ccdc57057eeddbd1561e7720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:41 +0000

    Add package django-assets/2.0/django-assets-2.0.tar.gz

---
+++ packages/django-assets/django-assets-2.0.tar.gz/django-assets-2.0/tests/__init__.py
@@ -0,0 +1,24 @@
+from nose import SkipTest
+try:
+    import django
+except ImportError:
+    raise SkipTest()
+
+
+# Setup a Django environment, before we do anything else.
+#
+# Most Django imports fail one way or another without an
+# environment.
+#
+# We can't even use setup_package() here, because then
+# module-global imports in our test submodules still run
+# first.
+
+from django.apps import apps
+from django.conf import settings
+
+settings.configure(
+    INSTALLED_APPS=['django_assets', 'django.contrib.staticfiles'],
+    TEMPLATES=[{'BACKEND': 'django.template.backends.django.DjangoTemplates'}]
+)
+apps.populate(settings.INSTALLED_APPS)
