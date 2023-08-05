d64e681a5178d30380a7147b0afb986e0c1c1a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:26 +0000

    Add package inprint/1.1.2/inprint-1.1.2-py2.py3-none-any.whl

---
+++ packages/inprint/inprint-1.1.2-py2.py3-none-any.whl/inprint/__init__.py
@@ -0,0 +1,10 @@
+from .functions import iprint
+
+__version__ = '1.1.2'
+
+
+def get_version():
+    """
+    Returns a PEP 386-compliant version number from __version__.
+    """
+    return __version__
