0d4d9de7a4520387851d9c7a6f8d7632d0fc83d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:56 +0000

    Add package mypy-boto3-rekognition/1.10.42.0/mypy-boto3-rekognition-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-rekognition/mypy-boto3-rekognition-1.10.42.0.tar.gz/mypy-boto3-rekognition-1.10.42.0/mypy_boto3_rekognition/__init__.py
@@ -0,0 +1,26 @@
+"Main interface for rekognition service"
+from mypy_boto3_rekognition.client import RekognitionClient as Client, RekognitionClient
+from mypy_boto3_rekognition.paginator import (
+    DescribeProjectVersionsPaginator,
+    DescribeProjectsPaginator,
+    ListCollectionsPaginator,
+    ListFacesPaginator,
+    ListStreamProcessorsPaginator,
+)
+from mypy_boto3_rekognition.waiter import (
+    ProjectVersionRunningWaiter,
+    ProjectVersionTrainingCompletedWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeProjectVersionsPaginator",
+    "DescribeProjectsPaginator",
+    "ListCollectionsPaginator",
+    "ListFacesPaginator",
+    "ListStreamProcessorsPaginator",
+    "ProjectVersionRunningWaiter",
+    "ProjectVersionTrainingCompletedWaiter",
+    "RekognitionClient",
+)
