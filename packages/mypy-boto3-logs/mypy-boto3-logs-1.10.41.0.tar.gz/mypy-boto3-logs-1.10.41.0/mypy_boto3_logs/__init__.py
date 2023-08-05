95f23ec02161f1d25bda90fcb42b3b7960a6ba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package mypy-boto3-logs/1.10.41.0/mypy-boto3-logs-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-logs/mypy-boto3-logs-1.10.41.0.tar.gz/mypy-boto3-logs-1.10.41.0/mypy_boto3_logs/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for logs service"
+from mypy_boto3_logs.client import CloudWatchLogsClient as Client, CloudWatchLogsClient
+from mypy_boto3_logs.paginator import (
+    DescribeDestinationsPaginator,
+    DescribeExportTasksPaginator,
+    DescribeLogGroupsPaginator,
+    DescribeLogStreamsPaginator,
+    DescribeMetricFiltersPaginator,
+    DescribeQueriesPaginator,
+    DescribeResourcePoliciesPaginator,
+    DescribeSubscriptionFiltersPaginator,
+    FilterLogEventsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "CloudWatchLogsClient",
+    "DescribeDestinationsPaginator",
+    "DescribeExportTasksPaginator",
+    "DescribeLogGroupsPaginator",
+    "DescribeLogStreamsPaginator",
+    "DescribeMetricFiltersPaginator",
+    "DescribeQueriesPaginator",
+    "DescribeResourcePoliciesPaginator",
+    "DescribeSubscriptionFiltersPaginator",
+    "FilterLogEventsPaginator",
+)
