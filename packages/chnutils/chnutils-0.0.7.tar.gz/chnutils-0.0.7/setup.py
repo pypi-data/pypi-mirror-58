d86012e38ea0c6bac179203883d32790918ffb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package chnutils/0.0.7/chnutils-0.0.7.tar.gz

---
+++ packages/chnutils/chnutils-0.0.7.tar.gz/chnutils-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name="chnutils",
+    version="0.0.7",
+    author="Drew Stinnett",
+    author_email="drew.stinnett@duke.edu",
+    description=("Helper scripts for CHN"),
+    install_requires=install_requirements,
+    license="LGPL 2.1",
+    keywords="chn",
+    scripts=['scripts/chn-register.py'],
+    long_description_content_type="text/markdown",
+    long_description=read('README.md'),
+)
