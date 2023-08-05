69a9c6a46a6d1ce9b4515bd3d73596f3eb851672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package hs-rpc/0.1.6/hs_rpc-0.1.6.tar.gz

---
+++ packages/hs-rpc/hs_rpc-0.1.6.tar.gz/hs_rpc-0.1.6/hs_rpc/__init__.py
@@ -0,0 +1,2 @@
+from hs_rpc.rpc import rpc
+from hs_rpc.request import rpc_request_invoke
---
+++ packages/hs-rpc/hs_rpc-0.1.6.tar.gz/hs_rpc-0.1.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hs_rpc",
+    version="0.1.6",
+    author="huansi_mes_team",
+    author_email="751995207@qq.com",
+    description="rpc服务打包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'Click',
+        'Flask',
+        'google',
+        'grpcio',
+        'protobuf',
+        'soupsieve',
+        'six',
+        'retrying'
+    ],
+)
