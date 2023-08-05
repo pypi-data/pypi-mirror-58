29f9064e52fa664647208bb2b35827b29beb4104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:54 +0000

    Add package mypy-boto3-mq/1.10.42.0/mypy-boto3-mq-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-mq/mypy-boto3-mq-1.10.42.0.tar.gz/mypy-boto3-mq-1.10.42.0/mypy_boto3_mq/paginator.py
@@ -0,0 +1,23 @@
+"Main interface for mq service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_mq.type_defs import ListBrokersResponseTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListBrokersPaginator",)
+
+
+class ListBrokersPaginator(Boto3Paginator):
+    """
+    [Paginator.ListBrokers documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/mq.html#MQ.Paginator.ListBrokers)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListBrokersResponseTypeDef, None, None]:
+        """
+        [ListBrokers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/mq.html#MQ.Paginator.ListBrokers.paginate)
+        """
