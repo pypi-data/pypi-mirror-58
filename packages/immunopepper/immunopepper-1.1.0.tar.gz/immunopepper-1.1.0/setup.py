52cb81371359ff4cce3db0608d340a436e3bf0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package immunopepper/1.1.0/immunopepper-1.1.0.tar.gz

---
+++ packages/immunopepper/immunopepper-1.1.0.tar.gz/immunopepper-1.1.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup_requirements = ['pytest-runner']
+
+with open('requirements.txt') as f:
+    requirements = list(f.readlines())
+
+test_requirements = ['pytest']
+
+setup(
+    author="Andre Kahles",
+    author_email='andre.kahles@inf.ethz.ch',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Software to translate splicing graphs into peptides",
+    entry_points = {
+        'console_scripts': ['immunopepper=immunopepper.main_immuno:cmd_entry'],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='immunopepper',
+    name='immunopepper',
+    packages=find_packages(include=['immunopepper']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ratschlab/immunopepper',
+    version='1.1.0',
+    zip_safe=False,
+)
