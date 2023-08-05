6c11b3ec62cabb4c609361fd9616d53769c539bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package mypy-boto3-importexport/1.10.41.0/mypy-boto3-importexport-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-importexport/mypy-boto3-importexport-1.10.41.0.tar.gz/mypy-boto3-importexport-1.10.41.0/mypy_boto3_importexport/paginator.py
@@ -0,0 +1,23 @@
+"Main interface for importexport service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_importexport.type_defs import ListJobsOutputTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListJobsPaginator",)
+
+
+class ListJobsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListJobs documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/importexport.html#ImportExport.Paginator.ListJobs)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, APIVersion: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListJobsOutputTypeDef, None, None]:
+        """
+        [ListJobs.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/importexport.html#ImportExport.Paginator.ListJobs.paginate)
+        """
