7cbdf6815102d6021418a7551ff12beca1bc7861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:00 +0000

    Add package mypy-boto3-resourcegroupstaggingapi/1.10.42.1/mypy-boto3-resourcegroupstaggingapi-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-resourcegroupstaggingapi/mypy-boto3-resourcegroupstaggingapi-1.10.42.1.tar.gz/mypy-boto3-resourcegroupstaggingapi-1.10.42.1/mypy_boto3_resourcegroupstaggingapi/__init__.py
@@ -0,0 +1,21 @@
+"Main interface for resourcegroupstaggingapi service"
+from mypy_boto3_resourcegroupstaggingapi.client import (
+    ResourceGroupsTaggingAPIClient as Client,
+    ResourceGroupsTaggingAPIClient,
+)
+from mypy_boto3_resourcegroupstaggingapi.paginator import (
+    GetComplianceSummaryPaginator,
+    GetResourcesPaginator,
+    GetTagKeysPaginator,
+    GetTagValuesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "GetComplianceSummaryPaginator",
+    "GetResourcesPaginator",
+    "GetTagKeysPaginator",
+    "GetTagValuesPaginator",
+    "ResourceGroupsTaggingAPIClient",
+)
