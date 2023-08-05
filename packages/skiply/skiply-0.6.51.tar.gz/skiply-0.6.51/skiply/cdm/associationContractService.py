464e5a6c867b3dd01665b2c13c313611fea11988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package skiply/0.6.51/skiply-0.6.51.tar.gz

---
+++ packages/skiply/skiply-0.6.51.tar.gz/skiply-0.6.51/skiply/cdm/associationContractService.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# coding: utf8
+
+# Copyright 2019 Skiply
+
+from __future__ import unicode_literals
+
+
+from .base import db_session, SkiplyBase
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class AssociationContractService(SkiplyBase):
+    ''' Device '''
+    __tablename__ = 'contract_service'
+    
+    contract_id = Column('contract_id', Integer, ForeignKey("so_contract.id"), nullable=False, primary_key=True)
+    service_id = Column('service_id', Integer, ForeignKey("so_service.id"), nullable=False, primary_key=True)
+
+    contract = relationship("Contract", back_populates="services")
+    service = relationship("Service", back_populates="contracts")
+
+    def __init__(self, contract_id, service_id):
+        self.contract_id = contract_id
+        self.service_id = service_id
+
+    def __repr__(self):
+        return '<Association Contract %r / Service %r>' % (self.contract_id, self.service_id)
+
+def get_contracts_for_services(service_ids):
+	session = db_session()
+	try:
+		results = session.query(AssociationContractService).filter(AssociationContractService.service_id._in(service_ids)).all()
+	except Exception as e:
+		print("DB Request get_contracts_for_services(service_ids) Failed with error : {}".format(e))
+		results = None
+	finally:
+		session.close()
+
+	return results
