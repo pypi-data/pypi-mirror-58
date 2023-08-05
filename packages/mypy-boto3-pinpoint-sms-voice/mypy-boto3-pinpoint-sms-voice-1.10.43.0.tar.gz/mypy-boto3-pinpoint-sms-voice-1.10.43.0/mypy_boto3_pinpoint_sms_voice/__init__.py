1c8abd4f32de189e9ecc5d9d308a638f0fab2797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package mypy-boto3-pinpoint-sms-voice/1.10.43.0/mypy-boto3-pinpoint-sms-voice-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-pinpoint-sms-voice/mypy-boto3-pinpoint-sms-voice-1.10.43.0.tar.gz/mypy-boto3-pinpoint-sms-voice-1.10.43.0/mypy_boto3_pinpoint_sms_voice/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for pinpoint-sms-voice service"
+from mypy_boto3_pinpoint_sms_voice.client import (
+    PinpointSMSVoiceClient,
+    PinpointSMSVoiceClient as Client,
+)
+
+
+__all__ = ("Client", "PinpointSMSVoiceClient")
