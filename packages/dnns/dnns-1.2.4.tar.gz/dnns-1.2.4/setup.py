02034fa6c28d8c06efa7e924a9fc40b6b94e173f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package dnns/1.2.4/dnns-1.2.4.tar.gz

---
+++ packages/dnns/dnns-1.2.4.tar.gz/dnns-1.2.4/setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='dnns',  
+     version='1.2.4',
+     scripts=['worker.py'] ,
+     author="Kevin Ryczko",
+     author_email="kryczko@uottawa.ca",
+     description="A deep learning package for using HDF5 and Pytorch (Distributed Data Parallel with NVIDIA mixed-precision) with ease.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/CLEANit/dnns.git",
+     packages=setuptools.find_packages(),
+     install_requires=[
+        'torch'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+     dependency_links=['https://github.com/NVIDIA/apex/archive/master.zip']
+ )
