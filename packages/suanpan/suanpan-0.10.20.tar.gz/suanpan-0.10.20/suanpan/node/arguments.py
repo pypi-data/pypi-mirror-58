c4e32c6a0868555c799da6445ad7d8cd27405d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:51 +0000

    Add package suanpan/0.10.20/suanpan-0.10.20.tar.gz

---
+++ packages/suanpan/suanpan-0.10.20.tar.gz/suanpan-0.10.20/suanpan/node/arguments.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan import error
+from suanpan.arguments.auto import ProxyArg
+from suanpan.node import node
+
+
+class DataFrame(ProxyArg):
+    MAPPING = {
+        "data": "suanpan.storage.arguments.Csv",
+        "table": "suanpan.dw.arguments.Table",
+    }
+
+    def __init__(self, key, table, partition, *args, **kwargs):
+        port = node.get(table) or node.get(key)
+        if not port:
+            raise error.NodeError(
+                "No such {}: {}".format(self.name, ",".join([key, table, partition]))
+            )
+        kwargs.setdefault(self.TYPE_KEY, port.type)
+        kwargs.update(key=key, table=table, partition=partition)
+        super(DataFrame, self).__init__(*args, **kwargs)
