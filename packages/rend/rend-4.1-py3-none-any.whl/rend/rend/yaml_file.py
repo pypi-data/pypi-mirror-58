af77c32f3795462457388f20a98800ad53455062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:10 +0000

    Add package rend/4.1/rend-4.1-py3-none-any.whl

---
+++ packages/rend/rend-4.1-py3-none-any.whl/rend/rend/yaml_file.py
@@ -0,0 +1,20 @@
+# Import third party libs
+import yaml
+
+# Import local libs
+import rend.exc
+
+__virtualname__ = 'yaml'
+
+
+async def render(hub, data):
+    '''
+    Given the data, attempt to render it as yaml
+    '''
+    try:
+        ret = yaml.safe_load(data)
+    except (yaml.parser.ParserError,
+            yaml.constructor.ConstructorError,
+            yaml.scanner.ScannerError) as exc:
+        raise rend.exc.RenderException(f'Yaml render error: {exc.problem}')
+    return ret
