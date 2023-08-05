fd2e7b7dfd5e027ddc739f66ffa6ee54f129a92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:22 +0000

    Add package mypy-boto3-codecommit/1.10.42.0/mypy-boto3-codecommit-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-codecommit/mypy-boto3-codecommit-1.10.42.0.tar.gz/mypy-boto3-codecommit-1.10.42.0/mypy_boto3_codecommit/__init__.py
@@ -0,0 +1,24 @@
+"Main interface for codecommit service"
+from mypy_boto3_codecommit.client import CodeCommitClient, CodeCommitClient as Client
+from mypy_boto3_codecommit.paginator import (
+    DescribePullRequestEventsPaginator,
+    GetCommentsForComparedCommitPaginator,
+    GetCommentsForPullRequestPaginator,
+    GetDifferencesPaginator,
+    ListBranchesPaginator,
+    ListPullRequestsPaginator,
+    ListRepositoriesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "CodeCommitClient",
+    "DescribePullRequestEventsPaginator",
+    "GetCommentsForComparedCommitPaginator",
+    "GetCommentsForPullRequestPaginator",
+    "GetDifferencesPaginator",
+    "ListBranchesPaginator",
+    "ListPullRequestsPaginator",
+    "ListRepositoriesPaginator",
+)
