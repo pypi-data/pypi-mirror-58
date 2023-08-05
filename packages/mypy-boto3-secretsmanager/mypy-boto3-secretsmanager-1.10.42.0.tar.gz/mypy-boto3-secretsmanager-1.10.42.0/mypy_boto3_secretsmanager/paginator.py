354d84e1ef6d74efa93b1c9574177981013ff8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package mypy-boto3-secretsmanager/1.10.42.0/mypy-boto3-secretsmanager-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-secretsmanager/mypy-boto3-secretsmanager-1.10.42.0.tar.gz/mypy-boto3-secretsmanager-1.10.42.0/mypy_boto3_secretsmanager/paginator.py
@@ -0,0 +1,23 @@
+"Main interface for secretsmanager service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_secretsmanager.type_defs import ListSecretsResponseTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListSecretsPaginator",)
+
+
+class ListSecretsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListSecrets documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/secretsmanager.html#SecretsManager.Paginator.ListSecrets)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListSecretsResponseTypeDef, None, None]:
+        """
+        [ListSecrets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/secretsmanager.html#SecretsManager.Paginator.ListSecrets.paginate)
+        """
