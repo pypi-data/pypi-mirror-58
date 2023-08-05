1be4e7b9233ebecba657405d02fb067ae13ce4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:05 +0000

    Add package mypy-boto3-sagemaker-a2i-runtime/1.10.42.1/mypy-boto3-sagemaker-a2i-runtime-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-sagemaker-a2i-runtime/mypy-boto3-sagemaker-a2i-runtime-1.10.42.1.tar.gz/mypy-boto3-sagemaker-a2i-runtime-1.10.42.1/mypy_boto3_sagemaker_a2i_runtime/__init__.py
@@ -0,0 +1,9 @@
+"Main interface for sagemaker-a2i-runtime service"
+from mypy_boto3_sagemaker_a2i_runtime.client import (
+    AugmentedAIRuntimeClient as Client,
+    AugmentedAIRuntimeClient,
+)
+from mypy_boto3_sagemaker_a2i_runtime.paginator import ListHumanLoopsPaginator
+
+
+__all__ = ("AugmentedAIRuntimeClient", "Client", "ListHumanLoopsPaginator")
