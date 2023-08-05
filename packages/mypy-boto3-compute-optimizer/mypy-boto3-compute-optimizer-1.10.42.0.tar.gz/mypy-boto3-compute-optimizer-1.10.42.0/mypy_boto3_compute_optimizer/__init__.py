fce2edd39a181d39171d9953159dc2abd4889f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package mypy-boto3-compute-optimizer/1.10.42.0/mypy-boto3-compute-optimizer-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-compute-optimizer/mypy-boto3-compute-optimizer-1.10.42.0.tar.gz/mypy-boto3-compute-optimizer-1.10.42.0/mypy_boto3_compute_optimizer/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for compute-optimizer service"
+from mypy_boto3_compute_optimizer.client import (
+    ComputeOptimizerClient,
+    ComputeOptimizerClient as Client,
+)
+
+
+__all__ = ("Client", "ComputeOptimizerClient")
