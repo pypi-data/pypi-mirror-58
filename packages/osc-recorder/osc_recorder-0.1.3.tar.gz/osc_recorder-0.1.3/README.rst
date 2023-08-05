c3a33f7b13121f2e079467d0a0878ead9bf3f36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:32 +0000

    Add package osc-recorder/0.1.3/osc_recorder-0.1.3.tar.gz

---
+++ packages/osc-recorder/osc_recorder-0.1.3.tar.gz/osc_recorder-0.1.3/README.rst
@@ -0,0 +1,47 @@
+============
+OSC-recorder
+============
+
+
+.. image:: https://img.shields.io/pypi/v/osc_recorder.svg
+        :target: https://pypi.python.org/pypi/osc_recorder
+
+.. .. image:: https://img.shields.io/travis/oishefarhan/OSC-recorder.svg
+..         :target: https://travis-ci.org/oishefarhan/OSC-recorder
+
+.. image:: https://readthedocs.org/projects/osc-recorder/badge/?version=latest
+        :target: https://osc-recorder.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+Python scripts to capture and replay OSC packets. Built on top of pyliblo.
+
+Documentation: https://osc-recorder.readthedocs.io.
+
+Installation
+------------
+* Download and install liblo_ first
+* Install pyliblo: :code:`pip install pyliblo`
+* If it doesn't work, follow instructions here: pyliblo_
+
+If you want to install from source.
+
+Clone the repo and run :code:`pip install .` from the folder.
+
+
+.. _liblo: http://liblo.sourceforge.net/
+.. _pyliblo: http://das.nasophon.de/pyliblo/
+
+
+TODO
+----
+* Fix sphinx autodoc for modules
+
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/osc-recorder/osc_recorder-0.1.3.tar.gz/osc_recorder-0.1.3/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import os
+import sys
+
+from distutils.command.build_scripts import build_scripts
+from distutils import util, log
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['numpy']
+
+setup_requirements = []
+
+test_requirements = ['pytest>=3.5.0']
+
+class build_scripts_rename(build_scripts):
+    def copy_scripts(self):
+        build_scripts.copy_scripts(self)
+        # remove the .py extension from scripts
+        for s in self.scripts:
+            f = util.convert_path(s)
+            before = os.path.join(self.build_dir, os.path.basename(f))
+            after = os.path.splitext(before)[0]
+            log.info("renaming %s -> %s" % (before, after))
+            os.rename(before, after)
+
+
+cmdclass = {
+    'build_scripts': build_scripts_rename,
+}
+
+
+setup(
+    author="Oishe Farhan",
+    author_email='oishe.farhan@interaxon.ca',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Python scripts to capture and replay OSC packets. Built on top of pyliblo.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='osc_recorder',
+    name='osc_recorder',
+    packages=find_packages(include=['osc_recorder']),
+    scripts=['scripts/capture_osc.py', 'scripts/replay_osc.py', 'scripts/osc_to_csv.py'],
+    cmdclass=cmdclass,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/oishefarhan/osc_recorder',
+    version='0.1.3',
+    zip_safe=False,
+)
