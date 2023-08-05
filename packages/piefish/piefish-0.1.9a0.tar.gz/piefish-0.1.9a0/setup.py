6362215e672de3cd953ffaaa8081c0709756723f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package piefish/0.1.9a0/piefish-0.1.9a0.tar.gz

---
+++ packages/piefish/piefish-0.1.9a0.tar.gz/piefish-0.1.9a0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='piefish',
+      version='0.1.9a',
+      description='A 2D graphics API that mimics the Processing API',
+      url='http://github.com/pb2002/PyVis',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Pepijn Bakker',
+      author_email='bakker.pepijn@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+	  install_requires=[
+          'pygame',
+		  'colorama'
+      ],
+      zip_safe=True)
+      
