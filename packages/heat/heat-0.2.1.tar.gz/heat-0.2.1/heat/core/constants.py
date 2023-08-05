a3cd23cfaf2412bc8fe92009dc14ec8a9a667918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:18 +0000

    Add package heat/0.2.1/heat-0.2.1.tar.gz

---
+++ packages/heat/heat-0.2.1.tar.gz/heat-0.2.1/heat/core/constants.py
@@ -0,0 +1,15 @@
+# Definition of HeAT Constants
+
+__all__ = ["e", "Euler", "inf", "Inf", "Infty", "Infinity", "nan", "NaN", "pi"]
+
+INF = float("inf")
+NAN = float("nan")
+NINF = -float("inf")
+PI = 3.141592653589793
+E = 2.718281828459045
+
+# aliases
+inf = Inf = Infty = Infinity = INF
+nan = NaN = NAN
+pi = PI
+e = Euler = E
