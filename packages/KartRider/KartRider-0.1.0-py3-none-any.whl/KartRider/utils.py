25fae3ae93812d8fad0635b6c01704b1970cec75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:05 +0000

    Add package KartRider/0.1.0/KartRider-0.1.0-py3-none-any.whl

---
+++ packages/KartRider/KartRider-0.1.0-py3-none-any.whl/KartRider/utils.py
@@ -0,0 +1,17 @@
+import datetime
+
+
+def _change_dt_tostr(dt: datetime.datetime):
+    format = '%Y-%m-%d %H:%M:%S'
+    return dt.strftime(format)
+
+
+def _change_str_todt(date: str):
+    format = '%Y-%m-%dT%H:%M:%S'
+    try:
+        dt = datetime.datetime.strptime(date, format)
+    except ValueError:
+        format = '%Y-%m-%dT%H:%M:%S.%f'
+        dt = datetime.datetime.strptime(date, format)
+
+    return dt
