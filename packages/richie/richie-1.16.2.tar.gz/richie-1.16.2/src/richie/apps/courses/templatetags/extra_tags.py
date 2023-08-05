a061d50a14f0122a9ba61b53e7653a5c152dcb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:08 +0000

    Add package richie/1.16.2/richie-1.16.2.tar.gz

---
+++ packages/richie/richie-1.16.2.tar.gz/richie-1.16.2/src/richie/apps/courses/templatetags/extra_tags.py
@@ -0,0 +1,244 @@
+"""Custom template tags for the courses application of Richie."""
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+from django.template.loader import render_to_string
+
+from classytags.arguments import Argument, MultiValueArgument
+from classytags.core import Options, Tag
+from classytags.utils import flatten_context
+from cms.exceptions import PlaceholderNotFound
+from cms.templatetags.cms_tags import (
+    Placeholder,
+    PlaceholderOptions,
+    _get_page_by_untyped_arg,
+)
+from cms.toolbar.utils import get_toolbar_from_request
+from cms.utils import get_site_id
+from cms.utils.placeholder import validate_placeholder_name
+from cms.utils.plugins import get_plugins
+
+# pylint: disable=invalid-name
+register = template.Library()
+
+
+@register.tag("page_placeholder")
+class PagePlaceholder(Placeholder):
+    """
+    This template node is used to output page content and
+    is also used in the admin to dynamically generate input fields.
+    eg:
+    {% page_placeholder "sidebar" page_lookup %}
+    {% page_placeholder "sidebar" page_lookup inherit %}
+    {% page_placeholder "sidebar" page_lookup as varname %}
+    {% page_placeholder "sidebar" page_lookup or %}
+        <div>No content</div>
+    {% endpage_placeholder %}
+    Keyword arguments:
+        name: the name of the placeholder
+        page_lookup: lookup argument for Page. See `_get_page_by_untyped_arg()` for detailed
+            information on the allowed types and their interpretation for the `page_lookup`
+            argument.
+        inherit : optional argument which if given will result in inheriting
+            the content of the placeholder with the same name on parent pages
+        varname: context variable name. Output will be added to template context as this variable
+            instead of being returned.
+        or: optional argument which if given will make the template tag a block
+            tag whose content is shown if the placeholder is empty
+    """
+
+    name = "page_placeholder"
+    options = PlaceholderOptions(
+        Argument("name", resolve=False),
+        Argument("page_lookup"),
+        MultiValueArgument("extra_bits", required=False, resolve=False),
+        blocks=[("endpage_placeholder", "nodelist")],
+    )
+
+    # pylint: disable=arguments-differ,too-many-arguments
+    def render_tag(self, context, name, page_lookup, extra_bits, nodelist=None):
+        validate_placeholder_name(name)
+
+        request = context.get("request")
+
+        if request:
+            page = _get_page_by_untyped_arg(page_lookup, request, get_site_id(None))
+
+            toolbar = get_toolbar_from_request(request)
+            renderer = toolbar.get_content_renderer()
+
+            inherit = "inherit" in extra_bits
+
+            # A placeholder is only editable on its own page
+            editable = page == request.current_page
+
+            try:
+                content = renderer.render_page_placeholder(
+                    slot=name,
+                    context=context,
+                    inherit=inherit,
+                    page=page,
+                    nodelist=nodelist,
+                    editable=editable,
+                )
+            except PlaceholderNotFound:
+                content = ""
+        else:
+            content = ""
+
+        if not content and nodelist:
+            content = nodelist.render(context)
+
+        if "as" in extra_bits:
+            try:
+                varname = extra_bits[extra_bits.index("as") + 1]
+            except IndexError:
+                raise template.TemplateSyntaxError(
+                    'the "as" word should be followed by the variable name'
+                )
+            context[varname] = content
+            return ""
+
+        return content
+
+
+@register.tag("get_placeholder_plugins")
+class GetPlaceholderPlugins(Placeholder):
+    """
+    A template tag that declares a placeholder and sets its plugins as a context variable
+    instead of rendering them eg:
+
+        {% get_placeholder_plugins "logo" as varname %}
+        {% get_placeholder_plugins "logo" page_lookup as varname %}
+        {% get_placeholder_plugins "logo" page_lookup as varname or %}
+            <div>No content</div>
+        {% endget_placeholder_plugins %}
+
+    This tag can typically be used in association with the block_plugin tag, in a placeholder
+    limited to one plugin, to customize the way it is rendered eg:
+
+        {% get_placeholder_plugins "logo" page_lookup as plugins %}
+        {% blockplugin plugins.0 %}
+            <img src="{% thumbnail instance.picture 300x150 %}"/>
+        {% endblockplugin %}
+
+    Keyword arguments:
+        name: the name of the placeholder
+        page_lookup[optional]: lookup argument for Page. See `_get_page_by_untyped_arg()`
+            for detailed information on the allowed types and their interpretation for the
+            `page_lookup` argument.
+        varname: context variable name. Output will be added to template context as this variable
+            instead of being returned.
+        or: optional argument which if given will make the template tag a block
+            tag whose content is shown if the placeholder is empty
+
+    Note: We must derive from the Placeholder class so that the tag is recognized as a
+          placeholder and shown in the structure toolbar.
+    """
+
+    name = "get_placeholder_plugins"
+    options = PlaceholderOptions(
+        Argument("name", resolve=False),
+        Argument("page_lookup", required=False),
+        "as",
+        Argument("varname", resolve=False),
+        MultiValueArgument("extra_bits", required=False, resolve=False),
+        blocks=[("endget_placeholder_plugins", "nodelist")],
+    )
+
+    # pylint: disable=arguments-differ,too-many-arguments
+    def render_tag(
+        self, context, name, page_lookup, varname, extra_bits, nodelist=None
+    ):
+        """
+        Retrieves the placeholder's plugins and set them as a variable in the template context.
+        If the placeholder is empty, render the block as fallback content and return the
+        resulting HTML.
+        If the placholder is editable, the edit script and markup are added to the HTML content.
+        """
+        content = ""
+        request = context.get("request")
+
+        if request:
+
+            context[varname] = []
+            page = _get_page_by_untyped_arg(page_lookup, request, get_site_id(None))
+
+            if not page:
+                return ""
+
+            try:
+                placeholder = page.placeholders.get(slot=name)
+            except ObjectDoesNotExist:
+                return ""
+            else:
+                context[varname] = [
+                    cms_plugin.get_plugin_instance()[0]
+                    for cms_plugin in get_plugins(
+                        request, placeholder, template=page.get_template()
+                    )
+                ]
+
+            # Default content if there is no plugins in the placeholder
+            if not context[varname] and nodelist:
+                content = nodelist.render(context)
+
+            # Add the edit script and markup to the content, only if the placeholder is editable
+            # and the visited page is the one on which the placeholder is declared.
+            toolbar = get_toolbar_from_request(request)
+            if placeholder.page == request.current_page and toolbar.edit_mode_active:
+                renderer = toolbar.get_content_renderer()
+                data = renderer.get_editable_placeholder_context(placeholder, page=page)
+                data["content"] = content
+                content = renderer.placeholder_edit_template.format(**data)
+
+        return content
+
+
+@register.tag()
+class BlockPlugin(Tag):
+    """
+    Like DjangoCMS 'render_plugin_block' but only includes the edit script and markup when
+    the related placeholder is editable.
+
+    This issue was raised to DjangoCMS and we need our own template tag until they find a way
+    to fix it in DjangoCMS (https://github.com/divio/django-cms/issues/6683).
+    """
+
+    name = "blockplugin"
+    template = "cms/toolbar/plugin.html"
+    options = Options(Argument("plugin"), blocks=[("endblockplugin", "nodelist")])
+
+    # pylint: disable=arguments-differ
+    def render_tag(self, context, plugin, nodelist):
+        """
+        Renders the block for the plugin and returns the resulting HTML leaving the temmpate
+        context untouched.
+        If the placholder is editable, the edit script and markup are added to the rendered HTML.
+        """
+        request = context.get("request")
+        if not plugin or not request:
+            return ""
+
+        # Add the plugin and its rendered content to an internal context
+        internal_context = flatten_context(context)
+        internal_context["instance"] = plugin
+        internal_context["content"] = nodelist.render(context.new(internal_context))
+
+        # Add the edit script and markup to the content, only if the placeholder is editable
+        # and the visited page is the one on which the plugin's placeholder is declared.
+        toolbar = get_toolbar_from_request(request)
+        if plugin.placeholder.page == request.current_page and toolbar.edit_mode_active:
+            return render_to_string(self.template, internal_context)
+
+        return internal_context["content"]
+
+
+@register.filter()
+def is_empty_placeholder(page, slot):
+    """A template filter to determine if a placeholder is empty.
+
+    This is useful when we don't want to include any wrapper markup in our template unless
+    the placeholder unless it actually contains plugins.
+    """
+    placeholder = page.placeholders.get(slot=slot)
+    return not placeholder.cmsplugin_set.exists()
---
+++ packages/richie/richie-1.16.2.tar.gz/richie-1.16.2/src/richie/apps/search/index_manager.py
@@ -0,0 +1,142 @@
+"""
+ElasticSearch indexes utilities.
+"""
+from functools import reduce
+
+from django.conf import settings
+from django.utils import timezone
+
+from elasticsearch.client import IndicesClient
+from elasticsearch.exceptions import NotFoundError
+from elasticsearch.helpers import bulk
+
+from . import ES_CLIENT
+from .defaults import ES_CHUNK_SIZE
+from .indexers import ES_INDICES
+from .text_indexing import ANALYSIS_SETTINGS
+
+
+def richie_bulk(actions):
+    """Wrap bulk helper to set default parameters."""
+    return bulk(
+        actions=actions,
+        chunk_size=getattr(settings, "RICHIE_ES_CHUNK_SIZE", ES_CHUNK_SIZE),
+        client=ES_CLIENT,
+        stats_only=True,
+    )
+
+
+def get_indexes_by_alias(existing_indexes, alias):
+    """
+    Get existing index(es) for an alias. Support multiple existing aliases so the command
+    always 'cleans up' the aliases even if they got messed up somehow.
+    """
+    for index, details in existing_indexes.items():
+        if alias in details.get("aliases", {}):
+            yield index, alias
+
+
+def perform_create_index(indexable, logger):
+    """
+    Create a new index in ElasticSearch from an indexable instance
+    """
+    indices_client = IndicesClient(client=ES_CLIENT)
+    # Create a new index name, suffixing its name with a timestamp
+    new_index = "{:s}_{:s}".format(
+        indexable.index_name, timezone.now().strftime("%Y-%m-%d-%Hh%Mm%S.%fs")
+    )
+
+    # Create the new index
+    logger.info('Creating a new Elasticsearch index "{:s}"...'.format(new_index))
+    indices_client.create(index=new_index)
+
+    # The index needs to be closed before we set an analyzer
+    indices_client.close(index=new_index)
+    indices_client.put_settings(body=ANALYSIS_SETTINGS, index=new_index)
+    indices_client.open(index=new_index)
+
+    indices_client.put_mapping(
+        body=indexable.mapping, doc_type=indexable.document_type, index=new_index
+    )
+
+    # Populate the new index with data provided from our indexable class
+    richie_bulk(indexable.get_es_documents(new_index))
+
+    # Return the name of the index we just created in ElasticSearch
+    return new_index
+
+
+def regenerate_indexes(logger):
+    """
+    Create new indexes for our indexables and replace possible existing indexes with
+    a new one only once it has successfully built it.
+    """
+    # Prepare the client we'll be using to handle indexes
+    indices_client = IndicesClient(client=ES_CLIENT)
+
+    # Get all existing indexes once; we'll look up into this list many times
+    try:
+        existing_indexes = indices_client.get_alias("*")
+    except NotFoundError:
+        # Provide a fallback empty list so we don't have to check for its existence later on
+        existing_indexes = []
+
+    # Create a new index for each of those modules
+    # NB: we're mapping perform_create_index which produces side-effects
+    indexes_to_create = zip(
+        list(map(lambda ix: perform_create_index(ix, logger), ES_INDICES)), ES_INDICES
+    )
+
+    # Prepare to alias them so they can be swapped-in for the previous versions
+    actions_to_create_aliases = [
+        {"add": {"index": index, "alias": ix.index_name}}
+        for index, ix in indexes_to_create
+    ]
+
+    # Get the previous indexes for every alias
+    indexes_to_unalias = reduce(
+        lambda acc, ix: acc
+        + list(get_indexes_by_alias(existing_indexes, ix.index_name)),
+        ES_INDICES,
+        [],
+    )
+
+    # Prepare to un-alias them so they can be swapped-out for the new versions
+    # NB: use chain to flatten the list of generators
+    actions_to_delete_aliases = [
+        {"remove": {"index": index, "alias": alias}}
+        for index, alias in indexes_to_unalias
+    ]
+
+    # Identify orphaned indexes
+    # NB: we *must* do this before the update_aliases call so we don't immediately prune
+    # version n-1 of all our indexes
+    useless_indexes = [
+        index for index, details in existing_indexes.items() if "aliases" not in details
+    ]
+
+    # Replace the old indexes with the new ones in 1 atomic operation to avoid outage
+    indices_client.update_aliases(
+        dict(actions=actions_to_create_aliases + actions_to_delete_aliases)
+    )
+
+    # Cleanup step: do prune older indexes that are now useless
+    for useless_index in useless_indexes:
+        # Disable keyword arguments checking as elasticsearch-py uses a decorator to list
+        # valid query parameters and inject them as kwargs. I won't say a word about this
+        # anti-pattern.
+        #
+        # pylint: disable=unexpected-keyword-arg
+        indices_client.delete(index=useless_index, ignore=[400, 404])
+
+
+def store_es_scripts(logger):
+    """
+    Iterate over the indexers listed in the settings, import them, and store the scripts
+    they define on their "scripts" key in ElasticSearch
+    """
+    for indexer in ES_INDICES:
+        for script_id, script_body in indexer.scripts.items():
+            if logger:
+                logger.info('Storing script "{:s}"...'.format(script_id))
+            ES_CLIENT.put_script(id=script_id, body=script_body)
