3880aa2ca34bb2c9fbb6afce109c32e74125f78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package mypy-boto3-kinesisanalytics/1.10.42.1/mypy-boto3-kinesisanalytics-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-kinesisanalytics/mypy-boto3-kinesisanalytics-1.10.42.1.tar.gz/mypy-boto3-kinesisanalytics-1.10.42.1/mypy_boto3_kinesisanalytics/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for kinesisanalytics service"
+from mypy_boto3_kinesisanalytics.client import (
+    KinesisAnalyticsClient as Client,
+    KinesisAnalyticsClient,
+)
+
+
+__all__ = ("Client", "KinesisAnalyticsClient")
