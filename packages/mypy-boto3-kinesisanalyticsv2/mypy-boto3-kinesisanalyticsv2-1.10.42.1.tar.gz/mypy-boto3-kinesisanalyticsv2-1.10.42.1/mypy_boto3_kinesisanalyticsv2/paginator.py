b92c2ff7b68aa8111012e951921788f2374a2d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package mypy-boto3-kinesisanalyticsv2/1.10.42.1/mypy-boto3-kinesisanalyticsv2-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-kinesisanalyticsv2/mypy-boto3-kinesisanalyticsv2-1.10.42.1.tar.gz/mypy-boto3-kinesisanalyticsv2-1.10.42.1/mypy_boto3_kinesisanalyticsv2/paginator.py
@@ -0,0 +1,41 @@
+"Main interface for kinesisanalyticsv2 service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_kinesisanalyticsv2.type_defs import (
+    ListApplicationSnapshotsResponseTypeDef,
+    ListApplicationsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("ListApplicationSnapshotsPaginator", "ListApplicationsPaginator")
+
+
+class ListApplicationSnapshotsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListApplicationSnapshots documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/kinesisanalyticsv2.html#KinesisAnalyticsV2.Paginator.ListApplicationSnapshots)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, ApplicationName: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListApplicationSnapshotsResponseTypeDef, None, None]:
+        """
+        [ListApplicationSnapshots.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/kinesisanalyticsv2.html#KinesisAnalyticsV2.Paginator.ListApplicationSnapshots.paginate)
+        """
+
+
+class ListApplicationsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListApplications documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/kinesisanalyticsv2.html#KinesisAnalyticsV2.Paginator.ListApplications)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListApplicationsResponseTypeDef, None, None]:
+        """
+        [ListApplications.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/kinesisanalyticsv2.html#KinesisAnalyticsV2.Paginator.ListApplications.paginate)
+        """
