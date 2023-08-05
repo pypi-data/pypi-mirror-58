678c1eddf6949bd38dcfc67360ed0f33ce16a0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:06 +0000

    Add package fortools/0.0.10/fortools-0.0.10-py3-none-any.whl

---
+++ packages/fortools/fortools-0.0.10-py3-none-any.whl/forlib/processing/recycle_analysis.py
@@ -0,0 +1,105 @@
+# This is only analysis Recycle Bin $I File- #
+import struct
+import binascii
+import os
+from datetime import datetime
+from datetime import timedelta
+import json
+import forlib.calc_hash as calc_hash
+import forlib.processing.convert_time as convert_time
+
+
+class RecycleAnalysis:
+    def __init__(self, file, path, hash_v):
+        self.__file = file
+        self.__path = path
+        self.__hash_value = [hash_v]
+        self.__recycle_json = self.__make_json()
+        self.__cal_hash()
+
+    def __i_name(self):
+        json_list = []
+        name = os.path.basename(self.__path)
+        rc_obj = {"$I Name": str(name)}
+        json.dumps(rc_obj)
+        json_list.append(rc_obj)
+
+        return json_list
+
+    def __header(self):
+        json_list = []
+        self.__file.seek(0)
+        fileheader = self.__file.read(8)
+        fileheader = binascii.hexlify(fileheader).decode('ascii')
+        rc_obj = {"File Header": str(fileheader)}
+        json.dumps(rc_obj)
+        json_list.append(rc_obj)
+
+        return json_list
+
+    def __size(self):
+        json_list = []
+        self.__file.seek(8)
+        file_size = struct.unpack('<i', self.__file.read(4))[0] + struct.unpack('<i', self.__file.read(4))[0]
+        rc_obj = {"Original File Size": str(file_size)}
+        json.dumps(rc_obj)
+        json_list.append(rc_obj)
+
+        return json_list
+
+    def __time(self):
+        json_list = []
+        self.__file.seek(16)
+        filedatetime = struct.unpack_from('<q', self.__file.read(8))[0]
+        filedatetime = convert_time.convert_time(filedatetime)
+        deleted_time = filedatetime.strftime("%Y-%m-%d %H:%M:%S")
+        time_zone = filedatetime.strftime("%Z")
+
+        rc_obj = {"Time Zone": time_zone,
+                  "File Deleted Time": deleted_time}
+        json.dumps(rc_obj)
+        json_list.append(rc_obj)
+
+        return json_list
+
+    def __original_path(self):
+        json_list = []
+        self.__file.seek(24)
+        path = str(self.__file.read(), 'cp1252')
+        path = path.replace('\x00', '').encode('utf-8', 'ignore').decode('cp949', 'ignore')
+        rc_obj = {"Original File Path": str(path)}
+        json.dumps(rc_obj)
+        json_list.append(rc_obj)
+
+        return json_list
+
+    # calculate hash value after parsing
+    def __cal_hash(self):
+        self.__hash_value.append(calc_hash.get_hash(self.__path, 'after'))
+
+    def get_hash(self):
+        return self.__hash_value
+
+    def __make_json(self):
+        info_list = []
+        temp = dict()
+
+        temp['$I Name'] = self.__i_name()[0]['$I Name']
+        temp['File Header'] = self.__header()[0]['File Header']
+        temp['Original File Size'] = self.__size()[0]['Original File Size']
+        temp['Time Zone'] = self.__time()[0]['Time Zone']
+        temp['File Deleted Time'] = self.__time()[0]['File Deleted Time']
+        temp['Original File Path'] = self.__original_path()[0]['Original File Path']
+
+        info_list.append(temp)
+
+        return info_list
+
+    def show_all_info(self):
+        for i in range(0, len(self.__recycle_json)):
+            print(self.__recycle_json[i])
+
+    def get_all_info(self):
+        return self.__recycle_json
+
+
---
+++ packages/fortools/fortools-0.0.10-py3-none-any.whl/forlib/processing/winsearch_analysis.py
@@ -0,0 +1,79 @@
+import pyesedb
+import binascii
+import forlib.calc_hash as calc_hash
+import forlib.processing.convert_time as convert_time
+
+class WinSearchAnalysis:
+
+    def __init__(self, file, path, hash_v):
+        self.winsearch_list = []
+        self.__file = file
+        if self.__file==-1:
+            self.__winsearch_list==""
+        else:
+            self.__parse()
+        self.__hash_value = [hash_v]
+        self.__path = path
+        self.__cal_hash()
+
+    def __parse(self):
+        edb_data_table = self.__file.get_table_by_name("SystemIndex_0A")
+        no = 0
+        for row in edb_data_table.records:
+            no += 1
+            mkdict = dict()
+            mkdict["index"] = no
+            mkdict["type"] = "window search.edb"
+            mkdict["timezone"]=convert_time.convert_time(0).strftime("%Z")
+            mkdict["DocID"] = row.get_value_data_as_integer(0)
+            mkdict["FileName"] = row.get_value_data_as_string(31).split('\\')[-1]
+            if row.get_value_data(5) is None:
+                mkdict["System_DateModified"] = convert_time.convert_time(0).strftime("%Y-%m-%d %H:%M:%S")
+            else:
+                mkdict["System_DateModified"] = convert_time.convert_time(int(bytes.decode(binascii.hexlify(row.get_value_data(5))), 16)).strftime("%Y-%m-%d %H:%M:%S")
+
+            if row.get_value_data(6) is None:
+                mkdict["System_DateCreated"] = convert_time.convert_time(0).strftime("%Y-%m-%d %H:%M:%S")
+            else:
+                mkdict["System_DateCreated"] = convert_time.convert_time(int(bytes.decode(binascii.hexlify(row.get_value_data(6))), 16)).strftime("%Y-%m-%d %H:%M:%S")
+
+            if row.get_value_data(7) is None:
+                mkdict["System_DateAccessed"] = convert_time.convert_time(0).strftime("%Y-%m-%d %H:%M:%S")
+            else:
+                mkdict["System_DateAccessed"] = convert_time.convert_time(int(bytes.decode(binascii.hexlify(row.get_value_data(7))), 16)).strftime("%Y-%m-%d %H:%M:%S")
+
+            mkdict["System_IsFolder"] = int(bytes.decode(binascii.hexlify(row.get_value_data(19))), 16)
+
+            if mkdict["System_IsFolder"] == 1 or row.get_value_data(3) is None:
+                mkdict["System_Size"] = ""
+            else:
+                mkdict["System_Size"] = int(bytes.decode(binascii.hexlify(row.get_value_data(3))), 16)
+
+            mkdict["System_MIMEType"] = row.get_value_data_as_string(21)
+            mkdict["System_ItemPathDisplay"] = row.get_value_data_as_string(31)
+            if row.get_value_data(193) is None:
+                mkdict["System_Search_AutoSummary"] = ""
+            else:
+                mkdict["System_Search_AutoSummary"] = ""
+                # data1 = row.get_value_data(193)
+                # data = bytes.decode(binascii.hexlify(row.get_value_data(193)))
+
+            mkdict["System_ItemTypeText"] = row.get_value_data_as_string(253)
+            mkdict["System_FileExtension"] = row.get_value_data_as_string(262)
+            mkdict["System_ItemPathDisplayNarrow"] = row.get_value_data_as_string(284)
+            mkdict["System_FileName"] = row.get_value_data_as_string(363)
+
+            self.winsearch_list.append(mkdict)
+
+    def show_all_info(self):
+        for i in self.winsearch_list:
+            print(i)
+
+    def get_all_info(self):
+        return self.winsearch_list
+
+    def __cal_hash(self):
+        self.__hash_value.append(calc_hash.get_hash(self.__path, 'after'))
+
+    def get_hash(self):
+        return self.__hash_value
