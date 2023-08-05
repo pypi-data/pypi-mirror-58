5db73546325c56066c57ce8ececc1ac30ecd9d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:25 +0000

    Add package mypy-boto3-dms/1.10.42.0/mypy-boto3-dms-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-dms/mypy-boto3-dms-1.10.42.0.tar.gz/mypy-boto3-dms-1.10.42.0/mypy_boto3_dms/__init__.py
@@ -0,0 +1,57 @@
+"Main interface for dms service"
+from mypy_boto3_dms.client import (
+    DatabaseMigrationServiceClient as Client,
+    DatabaseMigrationServiceClient,
+)
+from mypy_boto3_dms.paginator import (
+    DescribeCertificatesPaginator,
+    DescribeConnectionsPaginator,
+    DescribeEndpointTypesPaginator,
+    DescribeEndpointsPaginator,
+    DescribeEventSubscriptionsPaginator,
+    DescribeEventsPaginator,
+    DescribeOrderableReplicationInstancesPaginator,
+    DescribeReplicationInstancesPaginator,
+    DescribeReplicationSubnetGroupsPaginator,
+    DescribeReplicationTaskAssessmentResultsPaginator,
+    DescribeReplicationTasksPaginator,
+    DescribeSchemasPaginator,
+    DescribeTableStatisticsPaginator,
+)
+from mypy_boto3_dms.waiter import (
+    EndpointDeletedWaiter,
+    ReplicationInstanceAvailableWaiter,
+    ReplicationInstanceDeletedWaiter,
+    ReplicationTaskDeletedWaiter,
+    ReplicationTaskReadyWaiter,
+    ReplicationTaskRunningWaiter,
+    ReplicationTaskStoppedWaiter,
+    TestConnectionSucceedsWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "DatabaseMigrationServiceClient",
+    "DescribeCertificatesPaginator",
+    "DescribeConnectionsPaginator",
+    "DescribeEndpointTypesPaginator",
+    "DescribeEndpointsPaginator",
+    "DescribeEventSubscriptionsPaginator",
+    "DescribeEventsPaginator",
+    "DescribeOrderableReplicationInstancesPaginator",
+    "DescribeReplicationInstancesPaginator",
+    "DescribeReplicationSubnetGroupsPaginator",
+    "DescribeReplicationTaskAssessmentResultsPaginator",
+    "DescribeReplicationTasksPaginator",
+    "DescribeSchemasPaginator",
+    "DescribeTableStatisticsPaginator",
+    "EndpointDeletedWaiter",
+    "ReplicationInstanceAvailableWaiter",
+    "ReplicationInstanceDeletedWaiter",
+    "ReplicationTaskDeletedWaiter",
+    "ReplicationTaskReadyWaiter",
+    "ReplicationTaskRunningWaiter",
+    "ReplicationTaskStoppedWaiter",
+    "TestConnectionSucceedsWaiter",
+)
