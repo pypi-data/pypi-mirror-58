48c28e02bc45fb024615e56d91cdc317118c3d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package mypy-boto3-transcribe/1.10.43.0/mypy-boto3-transcribe-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-transcribe/mypy-boto3-transcribe-1.10.43.0.tar.gz/mypy-boto3-transcribe-1.10.43.0/mypy_boto3_transcribe/__init__.py
@@ -0,0 +1,5 @@
+"Main interface for transcribe service"
+from mypy_boto3_transcribe.client import TranscribeServiceClient, TranscribeServiceClient as Client
+
+
+__all__ = ("Client", "TranscribeServiceClient")
