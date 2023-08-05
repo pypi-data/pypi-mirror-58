4f0d7e7591aeb9688a627a4d7a6645da16292011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package royalpack/5.1.9/royalpack-5.1.9.tar.gz

---
+++ packages/royalpack/royalpack-5.1.9.tar.gz/royalpack-5.1.9/royalpack/tables/__init__.py
@@ -0,0 +1,28 @@
+# Imports go here!
+from .diario import Diario
+from .aliases import Alias
+from .wikipages import WikiPage
+from .wikirevisions import WikiRevision
+from .bios import Bio
+from .reminders import Reminder
+from .triviascores import TriviaScore
+from .mmevents import MMEvent
+from .mmresponse import MMResponse
+from .leagueoflegends import LeagueOfLegends
+
+# Enter the tables of your Pack here!
+available_tables = [
+    Diario,
+    Alias,
+    WikiPage,
+    WikiRevision,
+    Bio,
+    Reminder,
+    TriviaScore,
+    MMEvent,
+    MMResponse,
+    LeagueOfLegends
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [table.__name__ for table in available_tables]
