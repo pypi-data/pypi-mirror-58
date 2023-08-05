8763c43ae6f351d97dd60cc72f4bcbca3e24d598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package modelevaluationammh/0.2.1/modelevaluationammh-0.2.1.tar.gz

---
+++ packages/modelevaluationammh/modelevaluationammh-0.2.1.tar.gz/modelevaluationammh-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='modelevaluationammh',
+      version='0.2.1',
+      description='Model Evaluation tools',
+      py_modules=['modelevaluationammh'],
+      packages=['modelevaluationammh'],
+      license='MIT',
+      author = 'Alder Martinez & Michael Howard',
+      author_email = 'aldermartinez@gmail.com',
+      install_requires=[
+          'sklearn',
+          'pandas',
+          'numpy',
+          'matplotlib'
+      ],
+      zip_safe=False)
