3a11ec29be5d60e47f9bafd3843d99453d728e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package pwcnet/0.1/pwcnet-0.1.tar.gz

---
+++ packages/pwcnet/pwcnet-0.1.tar.gz/pwcnet-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+  name = 'pwcnet',
+  packages = ['pwcnet'],
+  version = '0.1',
+  description = 'A pytorch port of PWC-Net',
+  author = 'Keunhong Park',
+  author_email = 'kpar@cs.washington.edu',
+  url = 'https://github.com/keunhong/pytorch-pwc',
+  download_url = 'https://github.com/keunhong/pytorch-pwc/archive/0.1.tar.gz',
+  keywords = ['python', 'pwcnet'],
+  classifiers = [],
+)
+
