694eb9eee7873bb9528dcbd48938df63585890a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package mypy-boto3-marketplacecommerceanalytics/1.10.43.0/mypy-boto3-marketplacecommerceanalytics-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-marketplacecommerceanalytics/mypy-boto3-marketplacecommerceanalytics-1.10.43.0.tar.gz/mypy-boto3-marketplacecommerceanalytics-1.10.43.0/mypy_boto3_marketplacecommerceanalytics/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for marketplacecommerceanalytics service"
+from mypy_boto3_marketplacecommerceanalytics.client import (
+    MarketplaceCommerceAnalyticsClient,
+    MarketplaceCommerceAnalyticsClient as Client,
+)
+
+
+__all__ = ("Client", "MarketplaceCommerceAnalyticsClient")
