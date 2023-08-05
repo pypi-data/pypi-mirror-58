f891c34e2750c181b6e3153a4c2ad7a7b1cb6cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package mypy-boto3-elasticache/1.10.41.0/mypy-boto3-elasticache-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-elasticache/mypy-boto3-elasticache-1.10.41.0.tar.gz/mypy-boto3-elasticache-1.10.41.0/mypy_boto3_elasticache/__init__.py
@@ -0,0 +1,48 @@
+"Main interface for elasticache service"
+from mypy_boto3_elasticache.client import ElastiCacheClient, ElastiCacheClient as Client
+from mypy_boto3_elasticache.paginator import (
+    DescribeCacheClustersPaginator,
+    DescribeCacheEngineVersionsPaginator,
+    DescribeCacheParameterGroupsPaginator,
+    DescribeCacheParametersPaginator,
+    DescribeCacheSecurityGroupsPaginator,
+    DescribeCacheSubnetGroupsPaginator,
+    DescribeEngineDefaultParametersPaginator,
+    DescribeEventsPaginator,
+    DescribeReplicationGroupsPaginator,
+    DescribeReservedCacheNodesOfferingsPaginator,
+    DescribeReservedCacheNodesPaginator,
+    DescribeServiceUpdatesPaginator,
+    DescribeSnapshotsPaginator,
+    DescribeUpdateActionsPaginator,
+)
+from mypy_boto3_elasticache.waiter import (
+    CacheClusterAvailableWaiter,
+    CacheClusterDeletedWaiter,
+    ReplicationGroupAvailableWaiter,
+    ReplicationGroupDeletedWaiter,
+)
+
+
+__all__ = (
+    "CacheClusterAvailableWaiter",
+    "CacheClusterDeletedWaiter",
+    "Client",
+    "DescribeCacheClustersPaginator",
+    "DescribeCacheEngineVersionsPaginator",
+    "DescribeCacheParameterGroupsPaginator",
+    "DescribeCacheParametersPaginator",
+    "DescribeCacheSecurityGroupsPaginator",
+    "DescribeCacheSubnetGroupsPaginator",
+    "DescribeEngineDefaultParametersPaginator",
+    "DescribeEventsPaginator",
+    "DescribeReplicationGroupsPaginator",
+    "DescribeReservedCacheNodesOfferingsPaginator",
+    "DescribeReservedCacheNodesPaginator",
+    "DescribeServiceUpdatesPaginator",
+    "DescribeSnapshotsPaginator",
+    "DescribeUpdateActionsPaginator",
+    "ElastiCacheClient",
+    "ReplicationGroupAvailableWaiter",
+    "ReplicationGroupDeletedWaiter",
+)
---
+++ packages/mypy-boto3-elasticache/mypy-boto3-elasticache-1.10.41.0.tar.gz/mypy-boto3-elasticache-1.10.41.0/mypy_boto3_elasticache/waiter.py
@@ -0,0 +1,89 @@
+"Main interface for elasticache service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_elasticache.type_defs import WaiterConfigTypeDef
+
+
+__all__ = (
+    "CacheClusterAvailableWaiter",
+    "CacheClusterDeletedWaiter",
+    "ReplicationGroupAvailableWaiter",
+    "ReplicationGroupDeletedWaiter",
+)
+
+
+class CacheClusterAvailableWaiter(Boto3Waiter):
+    """
+    [Waiter.CacheClusterAvailable documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.CacheClusterAvailable)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        CacheClusterId: str = None,
+        MaxRecords: int = None,
+        Marker: str = None,
+        ShowCacheNodeInfo: bool = None,
+        ShowCacheClustersNotInReplicationGroups: bool = None,
+        WaiterConfig: WaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [CacheClusterAvailable.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.CacheClusterAvailable.wait)
+        """
+
+
+class CacheClusterDeletedWaiter(Boto3Waiter):
+    """
+    [Waiter.CacheClusterDeleted documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.CacheClusterDeleted)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        CacheClusterId: str = None,
+        MaxRecords: int = None,
+        Marker: str = None,
+        ShowCacheNodeInfo: bool = None,
+        ShowCacheClustersNotInReplicationGroups: bool = None,
+        WaiterConfig: WaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [CacheClusterDeleted.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.CacheClusterDeleted.wait)
+        """
+
+
+class ReplicationGroupAvailableWaiter(Boto3Waiter):
+    """
+    [Waiter.ReplicationGroupAvailable documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.ReplicationGroupAvailable)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        ReplicationGroupId: str = None,
+        MaxRecords: int = None,
+        Marker: str = None,
+        WaiterConfig: WaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [ReplicationGroupAvailable.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.ReplicationGroupAvailable.wait)
+        """
+
+
+class ReplicationGroupDeletedWaiter(Boto3Waiter):
+    """
+    [Waiter.ReplicationGroupDeleted documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.ReplicationGroupDeleted)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        ReplicationGroupId: str = None,
+        MaxRecords: int = None,
+        Marker: str = None,
+        WaiterConfig: WaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [ReplicationGroupDeleted.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/elasticache.html#ElastiCache.Waiter.ReplicationGroupDeleted.wait)
+        """
