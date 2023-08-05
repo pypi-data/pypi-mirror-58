e2f947932df4d96758b84f106aa0d7548466b660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package mypy-boto3-application-insights/1.10.40.2/mypy-boto3-application-insights-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-application-insights/mypy-boto3-application-insights-1.10.40.2.tar.gz/mypy-boto3-application-insights-1.10.40.2/mypy_boto3_application_insights/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for application-insights service"
+from mypy_boto3_application_insights.client import (
+    ApplicationInsightsClient,
+    ApplicationInsightsClient as Client,
+)
+
+
+__all__ = ("ApplicationInsightsClient", "Client")
