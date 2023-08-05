9b503dacd85b8d0ea3d28b17c8ec34f9326cadb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package idem/5/idem-5-py3-none-any.whl

---
+++ packages/idem/idem-5-py3-none-any.whl/idem/conf.py
@@ -0,0 +1,60 @@
+CLI_CONFIG = {
+    'sls_sources': {
+        'default': ['file://'],
+        'nargs': '*',
+        'help': 'list off the sources that should be used for gathering sls files and data',
+        },
+    'test': {
+        'options': ['-t'],
+        'default': False,
+        'action': 'store_true',
+        'help': 'Set the idem run to execute in test mode. No changes will be made, idem will only detect if changes will be made in a real run.',
+        },
+    'tree': {
+        'default': '',
+        'options': ['-T'],
+        'help': 'The directory containing sls files',
+        },
+    'takara_unit': {
+        'options': ['-u'],
+        'default': None,
+        'help': 'The Takara unit to work with, This enables Takara as a backend for secret storage in your idem states',
+        },
+    'seal_raw': {
+        'default': None,
+        'help': 'DO NOT USE! This option allows you to pass Takara unsealing secrets as command line arguments! This should only be used for testing!!',
+        },
+    'cache_dir': {
+        'default': '/var/cache/idem',
+        'help': 'The location to use for the cache directory',
+        },
+    'root_dir': {
+        'default': '/',
+        'help': 'The root directory to run idem from. By default it will be "/", or in the case of running as non-root it is set to <HOMEDIR>/.idem',
+        },
+    'render': {
+        'default': 'jinja|yaml',
+        'help': 'The render pipe to use, this allows for the language to be specified',
+        },
+    'runtime': {
+        'default': 'serial',
+        'help': 'Select which execution runtime to use',
+        },
+    'output': {
+        'default': 'idem',
+        'help': 'The putputter to use to display data',
+        },
+    'sls': {
+        'default': [],
+        'nargs': '*',
+        'help': 'A space delimited list of sls refs to execute',
+        },
+    }
+CONFIG = {}
+GLOBAL = {}
+SUBS = {}
+DYNE = {
+        'exec': ['exec'],
+        'states': ['states'],
+        'output': ['output'],
+        }
