5231725a79bab1cfe2d4d0bfef86acc29ff08549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:09 +0000

    Add package tropoform/0.3.5/tropoform-0.3.5.tar.gz

---
+++ packages/tropoform/tropoform-0.3.5.tar.gz/tropoform-0.3.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tropoform',
+    version='0.3.5',
+    entry_points={'console_scripts': ['tropoform=tropoform.tropoform:main']},
+    author="Brian Peterson",
+    author_email="brian.peterson@cloudshift.cc",
+    description="A Terraform like utility for managing AWS Cloud Formation Stacks with troposphere",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cloudshiftstrategies/tropoform",
+    packages=setuptools.find_namespace_packages(),
+    install_requires=[
+        'boto3',
+        'botocore',
+        'troposphere',
+        'awacs',
+        'PyYAML',
+        'colorlog',
+        ],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
