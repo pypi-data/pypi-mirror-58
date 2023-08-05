13172706205cab34c09cada544f674951cc4750f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package assemblyline-core/4.0.0.dev145/assemblyline_core-4.0.0.dev145-py3-none-any.whl

---
+++ packages/assemblyline-core/assemblyline_core-4.0.0.dev145-py3-none-any.whl/assemblyline_core/metrics/metrics_server.py
@@ -0,0 +1,412 @@
+#!/usr/bin/env python
+
+import elasticapm
+import elasticsearch
+import json
+import sys
+import time
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from collections import Counter
+from threading import Lock
+
+from assemblyline_core.metrics.heartbeat_formatter import HeartbeatFormatter
+from assemblyline_core.server_base import ServerBase
+from assemblyline.common.isotime import now_as_iso
+from assemblyline.common import forge
+from assemblyline.remote.datatypes.queues.comms import CommsQueue
+
+METRICS_QUEUE = "assemblyline_metrics"
+NON_AGGREGATED = ['scaler', 'scaler-status']
+
+
+class ILMException(Exception):
+    pass
+
+
+def cleanup_metrics(input_dict):
+    output_dict = {}
+    for k, v in input_dict.items():
+        items = k.split(".")
+        parent = output_dict
+        for i in items:
+            if i not in parent:
+                if items.index(i) == (len(items) - 1):
+                    # noinspection PyBroadException
+                    try:
+                        parent[i] = int(v)
+                    except Exception:  # pylint:disable=W0702
+                        if v == "true":
+                            parent[i] = True
+                        elif v == "false":
+                            parent[i] = False
+                        else:
+                            parent[i] = v
+
+                    break
+                else:
+                    parent[i] = {}
+            parent = parent[i]
+
+    return output_dict
+
+
+def ilm_policy_exists(es, name):
+    conn = es.transport.get_connection()
+    pol_req = conn.session.get(f"{conn.base_url}/_ilm/policy/{name}")
+    return pol_req.ok
+
+
+def create_ilm_policy(es, name, ilm_config):
+    data_base = {
+        "policy": {
+            "phases": {
+                "hot": {
+                    "min_age": "0ms",
+                    "actions": {
+                        "set_priority": {
+                            "priority": 100
+                        },
+                        "rollover": {
+                            "max_age": f"{ilm_config['warm']}{ilm_config['unit']}"
+                        }
+                    }
+                },
+                "warm": {
+                    "actions": {
+                        "set_priority": {
+                            "priority": 50
+                        }
+                    }
+                },
+                "cold": {
+                    "min_age": f"{ilm_config['cold']}{ilm_config['unit']}",
+                    "actions": {
+                        "set_priority": {
+                            "priority": 20
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if ilm_config['delete']:
+        data_base['policy']['phases']['delete'] = {
+            "min_age": f"{ilm_config['delete']}{ilm_config['unit']}",
+            "actions": {
+                "delete": {}
+            }
+        }
+
+    conn = es.transport.get_connection()
+    pol_req = conn.session.put(f"{conn.base_url}/_ilm/policy/{name}",
+                               headers={"Content-Type": "application/json"},
+                               data=json.dumps(data_base))
+    if not pol_req.ok:
+        raise ILMException(f"ERROR: Failed to create ILM policy: {name}")
+
+
+class MetricsServer(ServerBase):
+    """
+    There can only be one of these type of metrics server running because it runs of a pubsub queue.
+    """
+    def __init__(self, config=None):
+        super().__init__('assemblyline.metrics_aggregator', shutdown_timeout=65)
+        self.config = config or forge.get_config()
+        self.elastic_hosts = self.config.core.metrics.elasticsearch.hosts
+
+        if not self.elastic_hosts:
+            self.log.error("No elasticsearch cluster defined to store metrics. All gathered stats will be ignored...")
+            sys.exit(1)
+
+        self.scheduler = BackgroundScheduler(daemon=True)
+        self.metrics_queue = None
+        self.es = None
+        self.counters_lock = Lock()
+        self.counters = {}
+
+        if self.config.core.metrics.apm_server.server_url is not None:
+            self.log.info(f"Exporting application metrics to: {self.config.core.metrics.apm_server.server_url}")
+            elasticapm.instrument()
+            self.apm_client = elasticapm.Client(server_url=self.config.core.metrics.apm_server.server_url,
+                                                service_name="metrics_aggregator")
+        else:
+            self.apm_client = None
+
+    def try_run(self):
+        self.metrics_queue = CommsQueue(METRICS_QUEUE)
+        self.es = elasticsearch.Elasticsearch(hosts=self.elastic_hosts,
+                                              connection_class=elasticsearch.RequestsHttpConnection)
+
+        self.scheduler.add_job(self._create_aggregated_metrics, 'interval', seconds=60)
+        self.scheduler.start()
+
+        while self.running:
+            for msg in self.metrics_queue.listen():
+                # APM Transaction start
+                if self.apm_client:
+                    self.apm_client.begin_transaction('metrics')
+
+                m_name = msg.pop('name', None)
+                m_type = msg.pop('type', None)
+                msg.pop('host', None)
+                msg.pop('instance', None)
+
+                self.log.debug(f"Received {m_type.upper()} metrics message")
+                if not m_name or not m_type:
+                    # APM Transaction end
+                    if self.apm_client:
+                        self.apm_client.end_transaction('process_message', 'invalid_message')
+
+                    continue
+
+                with self.counters_lock:
+                    c_key = (m_name, m_type)
+                    if c_key not in self.counters or m_type in NON_AGGREGATED:
+                        self.counters[c_key] = Counter(msg)
+                    else:
+                        self.counters[c_key].update(Counter(msg))
+
+                # APM Transaction end
+                if self.apm_client:
+                    self.apm_client.end_transaction('process_message', 'success')
+
+    def _create_aggregated_metrics(self):
+        self.log.info("Copying counters ...")
+        # APM Transaction start
+        if self.apm_client:
+            self.apm_client.begin_transaction('metrics')
+
+        with self.counters_lock:
+            counter_copy, self.counters = self.counters, {}
+
+        self.log.info("Aggregating metrics ...")
+        timestamp = now_as_iso()
+        for component, counts in counter_copy.items():
+            component_name, component_type = component
+            output_metrics = {'name': component_name, 'type': component_type}
+
+            for key, value in counts.items():
+                # Skip counts, they will be paired with a time entry and we only want to count it once
+                if key.endswith('.c'):
+                    continue
+                # We have an entry that is a timer, should also have a .c count
+                elif key.endswith('.t'):
+                    name = key.rstrip('.t')
+                    output_metrics[name] = counts[key] / counts.get(name + ".c", 1)
+                    output_metrics[name + "_count"] = counts.get(name + ".c", 0)
+                # Plain old metric, no modifications needed
+                else:
+                    output_metrics[key] = value
+
+            output_metrics['timestamp'] = timestamp
+            output_metrics = cleanup_metrics(output_metrics)
+
+            self.log.info(output_metrics)
+            try:
+                index = f"al_metrics_{component_type}"
+                policy = f"{index}_policy"
+                while not ilm_policy_exists(self.es, policy):
+                    self.log.debug(f"ILM Policy {policy.upper()} does not exists. Creating it now...")
+                    try:
+                        create_ilm_policy(self.es, policy, self.config.core.metrics.elasticsearch.as_primitives())
+                    except ILMException:
+                        time.sleep(0.1)
+                        pass
+
+                if not self.es.indices.exists_template(index):
+                    self.log.debug(f"Index template {index.upper()} does not exists. Creating it now...")
+
+                    template_body = {
+                        "index_patterns": [f"{index}-*"],
+                        "order": 1,
+                        "settings": {
+                            "index.lifecycle.name": policy,
+                            "index.lifecycle.rollover_alias": index
+                        }
+                    }
+
+                    try:
+                        self.es.indices.put_template(index, template_body)
+                    except elasticsearch.exceptions.RequestError as e:
+                        if "resource_already_exists_exception" not in str(e):
+                            raise
+                        self.log.warning(f"Tried to create an index template that already exists: {index.upper()}")
+
+                if not self.es.indices.exists_alias(index):
+                    self.log.debug(f"Index alias {index.upper()} does not exists. Creating it now...")
+
+                    index_body = {"aliases": {index: {"is_write_index": True}}}
+
+                    try:
+                        self.es.indices.create(f"{index}-000001", index_body)
+                    except elasticsearch.exceptions.RequestError as e:
+                        if "resource_already_exists_exception" not in str(e):
+                            raise
+                        self.log.warning(f"Tried to create an index template that "
+                                         f"already exists: {index.upper()}-000001")
+
+                self.es.index(index=index, body=output_metrics)
+            except Exception as e:
+                self.log.exception(e)
+
+        self.log.info("Metrics aggregated. Waiting for next run...")
+
+        # APM Transaction end
+        if self.apm_client:
+            self.apm_client.end_transaction('aggregate_metrics', 'success')
+
+
+# noinspection PyBroadException
+class HeartbeatManager(ServerBase):
+    def __init__(self, config=None):
+        super().__init__('assemblyline.heartbeat_manager')
+        self.config = config or forge.get_config()
+        self.datastore = forge.get_datastore()
+        self.metrics_queue = CommsQueue(METRICS_QUEUE)
+        self.scheduler = BackgroundScheduler(daemon=True)
+        self.hm = HeartbeatFormatter("heartbeat_manager", self.log, config=self.config)
+
+        self.counters_lock = Lock()
+        self.counters = {}
+        self.rolling_window = {}
+        self.window_ttl = {}
+        self.ttl = self.config.core.metrics.export_interval * 2
+        self.window_size = int(60 / self.config.core.metrics.export_interval)
+        if self.window_size != 60 / self.config.core.metrics.export_interval:
+            self.log.warning("Cannot calculate a proper window size for reporting heartbeats. "
+                             "Metrics reported during hearbeat will be wrong.")
+
+        if self.config.core.metrics.apm_server.server_url is not None:
+            self.log.info(f"Exporting application metrics to: {self.config.core.metrics.apm_server.server_url}")
+            elasticapm.instrument()
+            self.apm_client = elasticapm.Client(server_url=self.config.core.metrics.apm_server.server_url,
+                                                service_name="heartbeat_manager")
+        else:
+            self.apm_client = None
+
+    def try_run(self):
+        self.scheduler.add_job(self._export_hearbeats, 'interval', seconds=self.config.core.metrics.export_interval)
+        self.scheduler.start()
+
+        while self.running:
+            for msg in self.metrics_queue.listen():
+                # APM Transaction start
+                if self.apm_client:
+                    self.apm_client.begin_transaction('heartbeat')
+
+                m_name = msg.pop('name', None)
+                m_type = msg.pop('type', None)
+                m_host = msg.pop('host', None)
+                msg.pop('instance', None)
+
+                self.log.debug(f"Received {m_type.upper()} metrics message")
+                if not m_name or not m_type or not m_host:
+                    # APM Transaction end
+                    if self.apm_client:
+                        self.apm_client.end_transaction('process_message', 'invalid_message')
+
+                    continue
+
+                with self.counters_lock:
+                    c_key = (m_name, m_type, m_host)
+                    if c_key not in self.counters or m_type in NON_AGGREGATED:
+                        self.counters[c_key] = Counter(msg)
+                    else:
+                        self.counters[c_key].update(Counter(msg))
+
+                # APM Transaction end
+                if self.apm_client:
+                    self.apm_client.end_transaction('process_message', 'success')
+
+    def _export_hearbeats(self):
+        try:
+            self.log.info("Expiring unused counters...")
+            # APM Transaction start
+            if self.apm_client:
+                self.apm_client.begin_transaction('heartbeat')
+
+            c_time = time.time()
+            for k in list(self.window_ttl.keys()):
+                if self.window_ttl.get(k, c_time) < c_time:
+                    c_name, c_type, c_host = k
+                    self.log.info(f"Counter {c_name} [{c_type}] for host {c_host} is expired")
+                    del self.window_ttl[k]
+                    del self.rolling_window[k]
+
+            self.log.info("Saving current counters to rolling window ...")
+            with self.counters_lock:
+                counter_copy, self.counters = self.counters, {}
+
+            for w_key, counter in counter_copy.items():
+                _, m_type, _ = w_key
+                if w_key not in self.rolling_window or m_type in NON_AGGREGATED:
+                    self.rolling_window[w_key] = [counter]
+                else:
+                    self.rolling_window[w_key].append(counter)
+
+                self.rolling_window[w_key] = self.rolling_window[w_key][-self.window_size:]
+                self.window_ttl[w_key] = time.time() + self.ttl
+
+            self.log.info("Compiling service list...")
+            aggregated_counters = {}
+            for service in [s['name'] for s in self.datastore.list_all_services(as_obj=False) if s['enabled']]:
+                data = {
+                    'cache_hit': 0,
+                    'cache_miss': 0,
+                    'cache_skipped': 0,
+                    'execute': 0,
+                    'fail_recoverable': 0,
+                    'fail_nonrecoverable': 0,
+                    'scored': 0,
+                    'not_scored': 0,
+                    'instances': 0
+                }
+                aggregated_counters[(service, 'service')] = Counter(data)
+
+            self.log.info("Aggregating heartbeat data...")
+            for component_parts, counters_list in self.rolling_window.items():
+                c_name, c_type, c_host = component_parts
+
+                # Expiring data outside of the window
+                counters_list = counters_list[-self.window_size:]
+
+                key = (c_name, c_type)
+                if key not in aggregated_counters:
+                    aggregated_counters[key] = Counter()
+
+                aggregated_counters[key]['instances'] += 1
+
+                for c in counters_list:
+                    aggregated_counters[key].update(c)
+
+            self.log.info("Generating heartbeats...")
+            for aggregated_parts, counter in aggregated_counters.items():
+                agg_c_name, agg_c_type = aggregated_parts
+                with elasticapm.capture_span(name=f"{agg_c_type}.{agg_c_name}", span_type="send_heartbeat"):
+
+                    metrics_data = {}
+                    for key, value in counter.items():
+                        # Skip counts, they will be paired with a time entry and we only want to count it once
+                        if key.endswith('.c'):
+                            continue
+                        # We have an entry that is a timer, should also have a .c count
+                        elif key.endswith('.t'):
+                            name = key.rstrip('.t')
+                            metrics_data[name] = value / max(counter.get(name + ".c", 1), 1)
+                            metrics_data[name + "_count"] = counter.get(name + ".c", 0)
+                        # Plain old metric, no modifications needed
+                        else:
+                            metrics_data[key] = value
+
+                    agg_c_instances = metrics_data.pop('instances', 1)
+                    metrics_data.pop('instances_count', None)
+                    self.hm.send_heartbeat(agg_c_type, agg_c_name, metrics_data, agg_c_instances)
+
+            # APM Transaction end
+            if self.apm_client:
+                self.apm_client.end_transaction('send_heartbeats', 'success')
+
+        except Exception:
+            self.log.exception("Unknown exception occurred during heartbeat creation:")
---
+++ packages/assemblyline-core/assemblyline_core-4.0.0.dev145-py3-none-any.whl/assemblyline_core/scaler/controllers/docker_ctl.py
@@ -0,0 +1,219 @@
+import os
+from typing import Dict
+from assemblyline.odm.models.service import DockerConfig
+from .interface import ControllerInterface, ServiceControlError
+
+# How to identify the update volume as a whole, in a way that the underlying container system recognizes.
+FILE_UPDATE_VOLUME = os.environ.get('FILE_UPDATE_VOLUME', None)
+
+# Where to find the update directory inside this container.
+FILE_UPDATE_DIRECTORY = os.environ.get('FILE_UPDATE_DIRECTORY', None)
+
+
+class DockerController(ControllerInterface):
+    """A controller for *non* swarm mode docker."""
+    def __init__(self, logger, prefix='', labels=None, cpu_overallocation=1, memory_overallocation=1, network='default'):
+        """
+        :param logger: A logger to report status and debug information.
+        :param prefix: A prefix used to distinguish containers launched by this controller.
+        :param cpu_overallocation: A multiplier on CPU usage. (2 means act like there are twice as many CPU present)
+        :param memory_overallocation: A multiplier on memory usage. (2 means act like there is twice as much memory)
+        """
+        # Connect to the host docker port
+        import docker
+        self.client = docker.from_env()
+        self.log = logger
+        self.global_mounts = []
+        self._prefix = prefix
+        self._labels = labels
+        self.network = network
+
+        # CPU and memory reserved for the host
+        self._reserved_cpu = 0.3
+        self._reserved_mem = 500
+        self.cpu_overallocation = cpu_overallocation
+        self.memory_overallocation = memory_overallocation
+        self._profiles = {}
+
+        # Prefetch some info that shouldn't change while we are running
+        self._info = self.client.info()
+
+        # We aren't checking for swarm nodes
+        assert not self._info['Swarm']['NodeID']
+
+    def add_profile(self, profile):
+        """Tell the controller about a service profile it needs to manage."""
+        self._profiles[profile.name] = profile
+
+    def _start(self, service_name):
+        """Launch a docker container in a manner suitable for Assembylyline."""
+        container_name = self._name_container(service_name)
+        prof = self._profiles[service_name]
+        cfg = prof.container_config
+        labels = dict(self._labels)
+        labels.update({'component': service_name})
+
+        volumes = {row[0]: {'bind': row[1], 'mode': 'ro'} for row in self.global_mounts}
+
+        volumes[os.path.join(FILE_UPDATE_VOLUME, service_name)] = {'bind': '/mount/updates/', 'mode': 'ro'}
+        if not os.path.exists(os.path.join(FILE_UPDATE_DIRECTORY, service_name)):
+            os.makedirs(os.path.join(FILE_UPDATE_DIRECTORY, service_name), 0x777)
+
+        self.client.containers.run(
+            image=cfg.image,
+            name=container_name,
+            cpu_period=100000,
+            cpu_quota=int(100000*cfg.cpu_cores),
+            mem_limit=f'{cfg.ram_mb}m',
+            labels=labels,
+            restart_policy={'Name': 'always'},
+            command=cfg.command,
+            volumes=volumes,
+            network=self.network,
+            environment=[f'{_e.name}={_e.value}' for _e in cfg.environment] + ['UPDATE_PATH=/mount/updates/'],
+            detach=True,
+        )
+
+    def _start_container(self, name, labels, volumes, cfg: DockerConfig):
+        """Launch a docker container."""
+        self.client.containers.run(
+            image=cfg.image,
+            name=name,
+            cpu_period=100000,
+            cpu_quota=int(100000*cfg.cpu_cores),
+            mem_limit=f'{cfg.ram_mb}m',
+            labels=labels,
+            restart_policy={'Name': 'always'},
+            command=cfg.command,
+            volumes=volumes,
+            network=self.network,
+            environment=[f'{_e.name}={_e.value}' for _e in cfg.environment],
+            detach=True,
+        )
+
+    def _name_container(self, service_name):
+        """Find an unused name for a container.
+
+        Container names must be unique, but we want our names to be predictable and informative.
+        Cycle through the pattern we want until we find the lowest free numerical suffix.
+        """
+        # Load all container names on the system now
+        used_names = []
+        for container in self.client.containers.list(all=True):
+            used_names.append(container.name)
+
+        # Try names until one works
+        used_names = set(used_names)
+        index = 0
+        while True:
+            name = f'{service_name}_{index}'
+            if self._prefix:
+                name = self._prefix + '_' + name
+            if name not in used_names:
+                return name
+            index += 1
+
+    def cpu_info(self):
+        """Try to estimate how much CPU the docker host has unreserved.
+
+        NOTE: There is probably a better way to do this.
+        """
+        total_cpu = cpu = self._info['NCPU'] * self.cpu_overallocation - self._reserved_cpu
+        for container in self.client.containers.list():
+            if container.attrs['HostConfig']['CpuPeriod']:
+                cpu -= container.attrs['HostConfig']['CpuQuota']/container.attrs['HostConfig']['CpuPeriod']
+        self.log.debug(f'Total CPU available {cpu}/{self._info["NCPU"]}')
+        return cpu, total_cpu
+
+    def memory_info(self):
+        """Try to estimate how much RAM the docker host has unreserved.
+
+        NOTE: There is probably a better way to do this.
+        """
+        mega = 2**20
+        total_mem = mem = self._info['MemTotal']/mega * self.memory_overallocation - self._reserved_mem
+        for container in self.client.containers.list():
+            mem -= container.attrs['HostConfig']['Memory']/mega
+        self.log.debug(f'Total Memory available {mem}/{self._info["MemTotal"]/mega}')
+        return mem, total_mem
+
+    def get_target(self, service_name):
+        """Get how many instances of a service we expect to be running.
+
+        Since we start our containers with 'restart always' we just need to count how many
+        docker is currently trying to keep running.
+        """
+        running = 0
+        for container in self.client.containers.list(filters={'label': f'component={service_name}'}):
+            if container.status in {'restarting', 'running'}:
+                running += 1
+            elif container.status in {'created', 'removing', 'paused', 'exited', 'dead'}:
+                pass
+            else:
+                self.log.warning(f"Unknown docker status string: {container.status}")
+        return running
+
+    def set_target(self, service_name, target):
+        """Change how many instances of a service docker is trying to keep up.
+
+        This is managed by killing extra containers at random, or launching new ones.
+        """
+        try:
+            running = self.get_target(service_name)
+            self.log.debug(f"New target for {service_name}: {running} -> {target}")
+            delta = target - running
+
+            if delta < 0:
+                # Kill off delta instances of of the service
+                running = [container for container in self.client.containers.list(filters={'label': f'component={service_name}'})
+                           if container.status in {'restarting', 'running'}]
+                running = running[0:-delta]
+                for container in running:
+                    container.kill()
+
+            if delta > 0:
+                # Start delta instances of the service
+                for _ in range(delta):
+                    self._start(service_name)
+
+            # Every time we change our container allocation do a little clean up to keep things fresh
+            self.client.containers.prune()
+            self.client.volumes.prune()
+        except Exception as error:
+            raise ServiceControlError(str(error), service_name)
+
+    def stop_container(self, service_name, container_id):
+        import docker.errors
+        container = None
+        try:
+            # First try the given container id in case its actually correct
+            container = self.client.containers.get(container_id)
+        except docker.errors.NotFound:
+            for possible_container in self.client.containers.list(filters={'label': f'component={service_name}'}):
+                if possible_container.id.startswith(container_id) or possible_container.name == container_id:
+                    container = possible_container
+                    break
+
+        if container and container.labels.get('component') == service_name and container.status == 'running':
+            container.kill()
+
+    def restart(self, service):
+        for container in self.client.containers.list(filters={'label': f'component={service.name}'}):
+            container.kill()
+
+    def get_running_container_names(self):
+        out = []
+        for container in self.client.containers.list():
+            out.append(container.id)
+            out.append(container.id[:12])
+            out.append(container.name)
+        return out
+
+    def start_stateful_container(self, service_name, deployment_name, spec, labels):
+        volumes = {_n: {'bind': _v.mount_path, 'mode': 'rw'} for _n, _v in spec.volumes.items()}
+        self._start_container(name=deployment_name, labels=labels, volumes=volumes, cfg=spec.container)
+
+    def stop_containers(self, labels):
+        label_strings = [f'{name}={value}' for name, value in labels.items()]
+        for container in self.client.containers.list(filters={'label': label_strings}):
+            container.stop()
