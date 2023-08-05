6bbd2227341c8136dbf4b3188285b3ce6d07d6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package ppinot4py/0.1.16/PPINot4Py-0.1.16-py3-none-any.whl

---
+++ packages/ppinot4py/PPINot4Py-0.1.16-py3-none-any.whl/PPINot4Py/functionsFrame/measuresDefinitionAux.py
@@ -0,0 +1,32 @@
+from PPINot4Py.state import DataObjectState
+from PPINot4Py.condition import Condition
+from PPINot4Py.measures import base
+
+def auxiliarLinearTimeMeasure(fromCondition, ToCondition, firstTo, precondition = ''):
+    
+    countStateTimeA = DataObjectState.DataObjectState(fromCondition)
+    countConditionTimeA = Condition.TimeInstantCondition(countStateTimeA)
+    countMeasureTimeA = base.CountMeasure(countConditionTimeA)
+        
+    countStateTimeC = DataObjectState.DataObjectState(ToCondition)
+    countConditionTimeC = Condition.TimeInstantCondition(countStateTimeC)
+    countMeasureTimeC = base.CountMeasure(countConditionTimeC)
+        
+    timeMeasureLinearA = base.TimeMeasure(countMeasureTimeA, countMeasureTimeC, 'LINEAR', 'SUM', firstTo, precondition)
+    
+    return timeMeasureLinearA
+
+
+def auxiliarCyclicTimeMeasure(fromCondition, ToCondition, operation, precondition = ''):
+    
+    countStateTimeA = DataObjectState.DataObjectState(fromCondition)
+    countConditionTimeA = Condition.TimeInstantCondition(countStateTimeA)
+    countMeasureTimeA = base.CountMeasure(countConditionTimeA)
+        
+    countStateTimeC = DataObjectState.DataObjectState(ToCondition)
+    countConditionTimeC = Condition.TimeInstantCondition(countStateTimeC)
+    countMeasureTimeC = base.CountMeasure(countConditionTimeC)
+        
+    timeMeasureCyclic = base.TimeMeasure(countMeasureTimeA, countMeasureTimeC, 'CYCLIC', operation, True, precondition)
+    
+    return timeMeasureLinearA
