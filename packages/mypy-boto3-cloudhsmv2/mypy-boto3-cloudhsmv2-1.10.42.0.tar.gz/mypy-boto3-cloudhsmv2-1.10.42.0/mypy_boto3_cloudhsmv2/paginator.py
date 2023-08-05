5d1f155224220daabfaf6397e0b0d50a088717f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package mypy-boto3-cloudhsmv2/1.10.42.0/mypy-boto3-cloudhsmv2-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-cloudhsmv2/mypy-boto3-cloudhsmv2-1.10.42.0.tar.gz/mypy-boto3-cloudhsmv2-1.10.42.0/mypy_boto3_cloudhsmv2/paginator.py
@@ -0,0 +1,59 @@
+"Main interface for cloudhsmv2 service Paginators"
+from __future__ import annotations
+
+from typing import Dict, Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_cloudhsmv2.type_defs import (
+    DescribeBackupsResponseTypeDef,
+    DescribeClustersResponseTypeDef,
+    ListTagsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("DescribeBackupsPaginator", "DescribeClustersPaginator", "ListTagsPaginator")
+
+
+class DescribeBackupsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeBackups documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/cloudhsmv2.html#CloudHSMV2.Paginator.DescribeBackups)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        Filters: Dict[str, List[str]] = None,
+        SortAscending: bool = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeBackupsResponseTypeDef, None, None]:
+        """
+        [DescribeBackups.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/cloudhsmv2.html#CloudHSMV2.Paginator.DescribeBackups.paginate)
+        """
+
+
+class DescribeClustersPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeClusters documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/cloudhsmv2.html#CloudHSMV2.Paginator.DescribeClusters)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, Filters: Dict[str, List[str]] = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[DescribeClustersResponseTypeDef, None, None]:
+        """
+        [DescribeClusters.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/cloudhsmv2.html#CloudHSMV2.Paginator.DescribeClusters.paginate)
+        """
+
+
+class ListTagsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListTags documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/cloudhsmv2.html#CloudHSMV2.Paginator.ListTags)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, ResourceId: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListTagsResponseTypeDef, None, None]:
+        """
+        [ListTags.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/cloudhsmv2.html#CloudHSMV2.Paginator.ListTags.paginate)
+        """
