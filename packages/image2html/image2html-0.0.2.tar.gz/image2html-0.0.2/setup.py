e4c49aca923155819c281c8759a36857abdc63ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package image2html/0.0.2/image2html-0.0.2.tar.gz

---
+++ packages/image2html/image2html-0.0.2.tar.gz/image2html-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='image2html',
+    version='0.0.2',
+    author='ZQPei',
+    author_email='peiziqiang@gmail.com',
+    packages=['image2html'],
+    install_requires=["huster"],
+    # scripts=['utils/build_server_scripts'],
+    entry_points = {
+              'console_scripts': [
+                  'image2html = image2html.image2html:image2html',
+                  'make_flist = image2html.flist:make_flist',
+              ],
+          },
+    url='https://github.com/ZQPei/image2html',
+    description='Display images with html file.',
+    # long_description=open('./README.md', 'r').read(), 
+    # long_description_content_type="text/markdown",
+    platforms=["all"],
+    license='MIT',
+)
