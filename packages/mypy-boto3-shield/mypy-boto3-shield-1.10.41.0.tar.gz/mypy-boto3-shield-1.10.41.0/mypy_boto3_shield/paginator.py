90d7b382b68193e55c271ca682695214103f908b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package mypy-boto3-shield/1.10.41.0/mypy-boto3-shield-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-shield/mypy-boto3-shield-1.10.41.0.tar.gz/mypy-boto3-shield-1.10.41.0/mypy_boto3_shield/paginator.py
@@ -0,0 +1,46 @@
+"Main interface for shield service Paginators"
+from __future__ import annotations
+
+from typing import Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_shield.type_defs import (
+    ListAttacksResponseTypeDef,
+    ListProtectionsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    TimeRangeTypeDef,
+)
+
+
+__all__ = ("ListAttacksPaginator", "ListProtectionsPaginator")
+
+
+class ListAttacksPaginator(Boto3Paginator):
+    """
+    [Paginator.ListAttacks documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/shield.html#Shield.Paginator.ListAttacks)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        ResourceArns: List[str] = None,
+        StartTime: TimeRangeTypeDef = None,
+        EndTime: TimeRangeTypeDef = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[ListAttacksResponseTypeDef, None, None]:
+        """
+        [ListAttacks.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/shield.html#Shield.Paginator.ListAttacks.paginate)
+        """
+
+
+class ListProtectionsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListProtections documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/shield.html#Shield.Paginator.ListProtections)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListProtectionsResponseTypeDef, None, None]:
+        """
+        [ListProtections.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/shield.html#Shield.Paginator.ListProtections.paginate)
+        """
