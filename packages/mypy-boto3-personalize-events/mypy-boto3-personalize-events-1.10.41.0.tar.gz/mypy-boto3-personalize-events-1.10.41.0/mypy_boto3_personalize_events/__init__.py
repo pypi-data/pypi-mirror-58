a5f16655cb0e34b8fc60de564a16b3eb43e9774a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package mypy-boto3-personalize-events/1.10.41.0/mypy-boto3-personalize-events-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-personalize-events/mypy-boto3-personalize-events-1.10.41.0.tar.gz/mypy-boto3-personalize-events-1.10.41.0/mypy_boto3_personalize_events/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for personalize-events service"
+from mypy_boto3_personalize_events.client import (
+    PersonalizeEventsClient as Client,
+    PersonalizeEventsClient,
+)
+
+
+__all__ = ("Client", "PersonalizeEventsClient")
