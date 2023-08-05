f5ffe9d22103f7f551a6873bdb00b24c733d895f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package odooapiclient/0.5/odooapiclient-0.5-py3-none-any.whl

---
+++ packages/odooapiclient/odooapiclient-0.5-py3-none-any.whl/odooapiclient/client.py
@@ -0,0 +1,35 @@
+from . import service_tools
+
+
+class Client(object):
+    
+    def __init__(self, protocol='http', host='localhost', port=8069, dbname=None, ssl=False):
+        if ssl:
+            protocol, port = 'https', 443
+        
+        self._host = host
+        self._port = port
+        self._db = dbname
+        self._protocol = protocol
+        
+        self._url = "{protocol}://{host}:{port}".format(
+            protocol=self._protocol, 
+            host=self._host, 
+            port=self._port
+        )
+
+    def authenticate(self, login, pwd):
+        self._login, self._password = login, pwd
+        service = service_tools.Connection(self._url, 'common')
+        self._uid = service.authenticate(self._db, login, pwd, {})
+        return self._uid
+
+    def search(self, model, domain=False, context=None, **kwargs):
+        if not context:
+            context = {}
+        if not kwargs:
+            kwargs ={}
+        kwargs.update({'context': context})
+        service = service_tools.Connection(self._url, 'object')
+        response = service.models(self._db, self._uid, self._password, model, 'search', domain or [], **kwargs)
+        return response
