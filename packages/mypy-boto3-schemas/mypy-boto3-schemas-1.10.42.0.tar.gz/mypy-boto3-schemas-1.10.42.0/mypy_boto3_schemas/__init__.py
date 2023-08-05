a4c669152de9f96195a0c4f2ce329e0912e55852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package mypy-boto3-schemas/1.10.42.0/mypy-boto3-schemas-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-schemas/mypy-boto3-schemas-1.10.42.0.tar.gz/mypy-boto3-schemas-1.10.42.0/mypy_boto3_schemas/__init__.py
@@ -0,0 +1,22 @@
+"Main interface for schemas service"
+from mypy_boto3_schemas.client import SchemasClient, SchemasClient as Client
+from mypy_boto3_schemas.paginator import (
+    ListDiscoverersPaginator,
+    ListRegistriesPaginator,
+    ListSchemaVersionsPaginator,
+    ListSchemasPaginator,
+    SearchSchemasPaginator,
+)
+from mypy_boto3_schemas.waiter import CodeBindingExistsWaiter
+
+
+__all__ = (
+    "Client",
+    "CodeBindingExistsWaiter",
+    "ListDiscoverersPaginator",
+    "ListRegistriesPaginator",
+    "ListSchemaVersionsPaginator",
+    "ListSchemasPaginator",
+    "SchemasClient",
+    "SearchSchemasPaginator",
+)
