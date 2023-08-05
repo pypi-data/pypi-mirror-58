f3ccdcdb189f05be4132cbd9ab06ec1ded76842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:32 +0000

    Add package classifier-yan-zababurina/0.5/classifier_yan_zababurina-0.5.tar.gz

---
+++ packages/classifier-yan-zababurina/classifier_yan_zababurina-0.5.tar.gz/classifier_yan_zababurina-0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='classifier_yan_zababurina',
+      version='0.5',
+      description='Example Package',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='intents',
+      url='http://github.com/theKirill/GensimSample',
+      author='Yanyushkin',
+      author_email='k.a.yanushkin@gmail.com',
+      license='MIT',
+      packages=['classifier_yan_zababurina'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['example-command=classifier_yan_zababurina.command_line:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
