fec778aea1a4e42b03f98ba6d4026525b9a2ffd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package pygamegui/0.0.9/pygamegui-0.0.9.tar.gz

---
+++ packages/pygamegui/pygamegui-0.0.9.tar.gz/pygamegui-0.0.9/pygamegui/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .gui import LinearGradient, Manager, View
+
+__version__ = "0.0.9"
+__copyright__ = "2019"
+__license__ = "LGPLv3"
+__authors__ = ["Ethosa"]
+
+if __name__ == '__main__':
+    print(LinearGradient, View, Manager)
---
+++ packages/pygamegui/pygamegui-0.0.9.tar.gz/pygamegui-0.0.9/pygamegui/gui/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .LinearGradient import LinearGradient
+from .Manager import Manager
+from .View import View
+from .AnimatedView import AnimatedView
+from .TextView import TextView
+from .ImageView import ImageView
+from .LinearLayout import LinearLayout
+
+if __name__ == '__main__':
+    print(LinearGradient, View, Manager,
+          AnimatedView, TextView, ImageView,
+          LinearLayout)
