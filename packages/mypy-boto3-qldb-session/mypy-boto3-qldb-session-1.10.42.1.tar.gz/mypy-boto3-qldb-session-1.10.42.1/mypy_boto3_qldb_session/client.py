77aa52d38770e19126ad8d022594e00d1cba05a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package mypy-boto3-qldb-session/1.10.42.1/mypy-boto3-qldb-session-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-qldb-session/mypy-boto3-qldb-session-1.10.42.1.tar.gz/mypy-boto3-qldb-session-1.10.42.1/mypy_boto3_qldb_session/client.py
@@ -0,0 +1,70 @@
+"Main interface for qldb-session service Client"
+from __future__ import annotations
+
+from typing import Any, Dict
+from botocore.client import BaseClient
+from botocore.exceptions import ClientError as Boto3ClientError
+
+# pylint: disable=import-self
+import mypy_boto3_qldb_session.client as client_scope
+from mypy_boto3_qldb_session.type_defs import (
+    CommitTransactionRequestTypeDef,
+    ExecuteStatementRequestTypeDef,
+    FetchPageRequestTypeDef,
+    SendCommandResultTypeDef,
+    StartSessionRequestTypeDef,
+)
+
+
+__all__ = ("QLDBSessionClient",)
+
+
+class QLDBSessionClient(BaseClient):
+    """
+    [QLDBSession.Client documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/qldb-session.html#QLDBSession.Client)
+    """
+
+    exceptions: client_scope.Exceptions
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Client.can_paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/qldb-session.html#QLDBSession.Client.can_paginate)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> None:
+        """
+        [Client.generate_presigned_url documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/qldb-session.html#QLDBSession.Client.generate_presigned_url)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def send_command(
+        self,
+        SessionToken: str = None,
+        StartSession: StartSessionRequestTypeDef = None,
+        StartTransaction: Dict[str, Any] = None,
+        EndSession: Dict[str, Any] = None,
+        CommitTransaction: CommitTransactionRequestTypeDef = None,
+        AbortTransaction: Dict[str, Any] = None,
+        ExecuteStatement: ExecuteStatementRequestTypeDef = None,
+        FetchPage: FetchPageRequestTypeDef = None,
+    ) -> SendCommandResultTypeDef:
+        """
+        [Client.send_command documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/qldb-session.html#QLDBSession.Client.send_command)
+        """
+
+
+class Exceptions:
+    BadRequestException: Boto3ClientError
+    ClientError: Boto3ClientError
+    InvalidSessionException: Boto3ClientError
+    LimitExceededException: Boto3ClientError
+    OccConflictException: Boto3ClientError
+    RateExceededException: Boto3ClientError
