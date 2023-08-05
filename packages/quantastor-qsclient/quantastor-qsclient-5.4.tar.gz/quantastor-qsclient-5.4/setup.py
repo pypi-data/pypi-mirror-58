16ecff999f42a7493485150864e0d4968f892beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:34 +0000

    Add package quantastor-qsclient/5.4/quantastor-qsclient-5.4.tar.gz

---
+++ packages/quantastor-qsclient/quantastor-qsclient-5.4.tar.gz/quantastor-qsclient-5.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description_txt = fh.read()
+
+setuptools.setup(
+    name='quantastor-qsclient',
+    version='5.4',
+    scripts=['quantastor/qs_client.py'],
+    author="OSNEXUS Corporation",
+    author_email="support@osnexus.com",
+    description="QuantaStor REST API python library",
+    long_description=long_description_txt,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OSNEXUS/QSPyClient",
+    packages=setuptools.find_packages(),
+    install_requires=['urllib3','requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+    ],
+
+ )
+
+# User: OSNEXUS
+# Password: rPTF!J^BL:YL98%{
