45c649837a67ec6b03e6bf138afdc34ec14095e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:56 +0000

    Add package mypy-boto3-route53resolver/1.10.40.2/mypy-boto3-route53resolver-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-route53resolver/mypy-boto3-route53resolver-1.10.40.2.tar.gz/mypy-boto3-route53resolver-1.10.40.2/mypy_boto3_route53resolver/paginator.py
@@ -0,0 +1,26 @@
+"Main interface for route53resolver service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_route53resolver.type_defs import (
+    ListTagsForResourceResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("ListTagsForResourcePaginator",)
+
+
+class ListTagsForResourcePaginator(Boto3Paginator):
+    """
+    [Paginator.ListTagsForResource documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/route53resolver.html#Route53Resolver.Paginator.ListTagsForResource)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, ResourceArn: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListTagsForResourceResponseTypeDef, None, None]:
+        """
+        [ListTagsForResource.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/route53resolver.html#Route53Resolver.Paginator.ListTagsForResource.paginate)
+        """
