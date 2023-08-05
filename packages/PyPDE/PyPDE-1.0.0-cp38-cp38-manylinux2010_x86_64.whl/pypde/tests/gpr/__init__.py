1cfe2eef8780817269571132b074946f83ddac93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:10 +0000

    Add package PyPDE/1.0.0/PyPDE-1.0.0-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/PyPDE/PyPDE-1.0.0-cp38-cp38-manylinux2010_x86_64.whl/pypde/tests/gpr/__init__.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from numpy import arange, array, eye, zeros
+
+from pypde import pde_solver
+from pypde.tests.exact import viscous_shock_exact
+from pypde.tests.gpr.misc.utils import Cvec
+from pypde.tests.gpr.system import B_gpr, F_gpr, S_gpr
+
+
+def viscous_shock_gpr():
+    """ 10.1016/j.jcp.2016.02.015
+        4.13 Viscous shock profile
+    """
+
+    tf = 0.2
+    nx = 200
+
+    x = arange(-0.5, 0.5, 1 / nx)
+    ρ = zeros(nx)
+    p = zeros(nx)
+    v = zeros(nx)
+    for i in range(nx):
+        ρ[i], p[i], v[i] = viscous_shock_exact(x[i])
+
+    v -= v[0]  # Velocity in shock 0
+
+    u = zeros([nx, 17])
+    for i in range(nx):
+        A = (ρ[i])**(1 / 3) * eye(3)
+        J = zeros(3)
+        u[i] = Cvec(ρ[i], p[i], array([v[i], 0, 0]), A, J)
+
+    L = [1.]
+
+    ret = pde_solver(u, tf, L, F=F_gpr, B=B_gpr, S=S_gpr, cfl=0.6)
+
+    plt.plot(ret[-1, :, 2] / ret[-1, :, 0])
+    plt.show()
+
+    return ret
---
+++ packages/PyPDE/PyPDE-1.0.0-cp38-cp38-manylinux2010_x86_64.whl/pypde/utils.py
@@ -0,0 +1,91 @@
+import inspect
+import sys
+from ctypes import CDLL, POINTER, c_bool, c_double, c_int, c_void_p
+from importlib.util import find_spec
+from sys import platform
+
+from numpy import array
+
+ADER_ARGTYPES = [
+    c_void_p, c_void_p, c_void_p, c_bool, c_bool, c_bool,
+    POINTER(c_double), c_double,
+    POINTER(c_int), c_int,
+    POINTER(c_double), c_double,
+    POINTER(c_int), c_bool, c_int, c_int, c_int, c_int, c_bool,
+    POINTER(c_double), c_int
+]
+
+BOUNDARIES = {'transitive': 0, 'periodic': 1}
+
+
+def nargs(func):
+    return len(inspect.signature(func).parameters)
+
+
+def c_ptr(arr):
+
+    if arr.dtype == 'int32':
+        ptr = POINTER(c_int)
+    elif arr.dtype == 'float64':
+        ptr = POINTER(c_double)
+    else:
+        print('invalid array type')
+
+    return arr.ctypes.data_as(ptr)
+
+
+def parse_boundary_types(boundaryTypes, ndim):
+
+    errMsg = ('boundaryTypes must be a string from {"transitive", "periodic"} '
+              'or a list of such strings, of length equal to the number of '
+              'dimensions of the domain.')
+
+    if isinstance(boundaryTypes, str):
+        try:
+            ret = [BOUNDARIES[boundaryTypes]] * ndim
+        except KeyError:
+            print(errMsg)
+            sys.exit(1)
+
+    elif isinstance(boundaryTypes, list):
+
+        if len(boundaryTypes) != ndim:
+            print(errMsg)
+            sys.exit(1)
+
+        try:
+            ret = [BOUNDARIES[b] for b in boundaryTypes]
+        except KeyError:
+            print(errMsg)
+            sys.exit(1)
+
+    else:
+        print(errMsg)
+        sys.exit(1)
+
+    return array(ret, dtype='int32')
+
+
+def get_cdll():
+
+    loc = find_spec('pypde').submodule_search_locations[0]
+
+    if platform == "linux" or platform == "linux2":
+        end = 'so'
+    if platform == "darwin":
+        end = 'dylib'
+    if platform == "win32":
+        end = 'dll'
+
+    return CDLL(loc + '/build/libpypde.' + end)
+
+
+def create_solver():
+
+    libpypde = get_cdll()
+    solver = libpypde.pde_solver
+
+    solver.argtypes = ADER_ARGTYPES
+    solver.restype = None
+
+    return solver
