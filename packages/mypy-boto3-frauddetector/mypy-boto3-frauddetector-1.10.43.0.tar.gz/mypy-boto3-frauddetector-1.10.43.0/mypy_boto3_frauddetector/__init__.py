d2f8905b178985d9fc575743a430ceae68e9bc0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package mypy-boto3-frauddetector/1.10.43.0/mypy-boto3-frauddetector-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-frauddetector/mypy-boto3-frauddetector-1.10.43.0.tar.gz/mypy-boto3-frauddetector-1.10.43.0/mypy_boto3_frauddetector/__init__.py
@@ -0,0 +1,5 @@
+"Main interface for frauddetector service"
+from mypy_boto3_frauddetector.client import FraudDetectorClient, FraudDetectorClient as Client
+
+
+__all__ = ("Client", "FraudDetectorClient")
