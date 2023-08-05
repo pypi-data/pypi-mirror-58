12177cec2cdff20bf1a10a4bd9f08403e5869f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:56 +0000

    Add package openPMD-api/0.10.2a0/openPMD-api-0.10.2a0.tar.gz

---
+++ packages/openPMD-api/openPMD-api-0.10.2a0.tar.gz/openPMD-api-0.10.2a0/include/openPMD/IterationEncoding.hpp
@@ -0,0 +1,42 @@
+/* Copyright 2017-2019 Fabian Koller
+ *
+ * This file is part of openPMD-api.
+ *
+ * openPMD-api is free software: you can redistribute it and/or modify
+ * it under the terms of of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * openPMD-api is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with openPMD-api.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <iosfwd>
+
+
+namespace openPMD
+{
+/** Encoding scheme of an Iterations Series'.
+ *
+ * @see https://github.com/openPMD/openPMD-standard/blob/latest/STANDARD.md#iterations-and-time-series
+ */
+enum class IterationEncoding
+{
+    fileBased, groupBased
+};
+} // openPMD
+
+namespace std
+{
+    ostream&
+    operator<<(ostream&, openPMD::IterationEncoding);
+} // std
---
+++ packages/openPMD-api/openPMD-api-0.10.2a0.tar.gz/openPMD-api-0.10.2a0/include/openPMD/version.hpp
@@ -0,0 +1,37 @@
+/* Copyright 2017-2019 Fabian Koller, Axel Huebl
+ *
+ * This file is part of openPMD-api.
+ *
+ * openPMD-api is free software: you can redistribute it and/or modify
+ * it under the terms of of either the GNU General Public License or
+ * the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * openPMD-api is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with openPMD-api.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+// version of the openPMD-api library
+#define OPENPMDAPI_VERSION_MAJOR 0
+#define OPENPMDAPI_VERSION_MINOR 10
+#define OPENPMDAPI_VERSION_PATCH 2
+#define OPENPMDAPI_VERSION_LABEL "alpha"
+
+// maximum supported version of the openPMD standard (read & write)
+#define OPENPMD_STANDARD_MAJOR 1
+#define OPENPMD_STANDARD_MINOR 1
+#define OPENPMD_STANDARD_PATCH 0
+
+// minimum supported version of the openPMD standard (read)
+#define OPENPMD_STANDARD_MIN_MAJOR 1
+#define OPENPMD_STANDARD_MIN_MINOR 0
+#define OPENPMD_STANDARD_MIN_PATCH 0
