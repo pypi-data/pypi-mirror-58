a4eb06532b057f63da241f7611c2a842e32c94a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package mypy-boto3-personalize-events/1.10.40.2/mypy-boto3-personalize-events-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-personalize-events/mypy-boto3-personalize-events-1.10.40.2.tar.gz/mypy-boto3-personalize-events-1.10.40.2/mypy_boto3_personalize_events/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for personalize-events service"
+from mypy_boto3_personalize_events.client import (
+    PersonalizeEventsClient,
+    PersonalizeEventsClient as Client,
+)
+
+
+__all__ = ("Client", "PersonalizeEventsClient")
---
+++ packages/mypy-boto3-personalize-events/mypy-boto3-personalize-events-1.10.40.2.tar.gz/mypy-boto3-personalize-events-1.10.40.2/mypy_boto3_personalize_events/client.py
@@ -0,0 +1,52 @@
+"Main interface for personalize-events service Client"
+from __future__ import annotations
+
+from typing import Any, Dict, List
+from botocore.client import BaseClient
+from botocore.exceptions import ClientError as Boto3ClientError
+
+# pylint: disable=import-self
+import mypy_boto3_personalize_events.client as client_scope
+from mypy_boto3_personalize_events.type_defs import EventTypeDef
+
+
+__all__ = ("PersonalizeEventsClient",)
+
+
+class PersonalizeEventsClient(BaseClient):
+    """
+    [PersonalizeEvents.Client documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/personalize-events.html#PersonalizeEvents.Client)
+    """
+
+    exceptions: client_scope.Exceptions
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Client.can_paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/personalize-events.html#PersonalizeEvents.Client.can_paginate)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> None:
+        """
+        [Client.generate_presigned_url documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/personalize-events.html#PersonalizeEvents.Client.generate_presigned_url)
+        """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def put_events(
+        self, trackingId: str, sessionId: str, eventList: List[EventTypeDef], userId: str = None
+    ) -> None:
+        """
+        [Client.put_events documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.40/reference/services/personalize-events.html#PersonalizeEvents.Client.put_events)
+        """
+
+
+class Exceptions:
+    ClientError: Boto3ClientError
+    InvalidInputException: Boto3ClientError
