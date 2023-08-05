c7a08bd75d263e09056385bd44afe70d72f41a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package sogou-tr-free/0.0.8/sogou-tr-free-0.0.8.tar.gz

---
+++ packages/sogou-tr-free/sogou-tr-free-0.0.8.tar.gz/sogou-tr-free-0.0.8/sogou_tr/__main__.py
@@ -0,0 +1,10 @@
+import sys
+import google_tr
+
+print('__main__')
+print('__main__.__name__', __name__)
+print('__main__.__package__', __package__)
+
+print('sys.path', sys.path)
+
+google_tr.main()
