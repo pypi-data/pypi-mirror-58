27db4ff222d3f8e75b66888303942dbfa63d4ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:45 +0000

    Add package mypy-boto3-kinesis-video-archived-media/1.10.42.0/mypy-boto3-kinesis-video-archived-media-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-kinesis-video-archived-media/mypy-boto3-kinesis-video-archived-media-1.10.42.0.tar.gz/mypy-boto3-kinesis-video-archived-media-1.10.42.0/mypy_boto3_kinesis_video_archived_media/__init__.py
@@ -0,0 +1,9 @@
+"Main interface for kinesis-video-archived-media service"
+from mypy_boto3_kinesis_video_archived_media.client import (
+    KinesisVideoArchivedMediaClient as Client,
+    KinesisVideoArchivedMediaClient,
+)
+from mypy_boto3_kinesis_video_archived_media.paginator import ListFragmentsPaginator
+
+
+__all__ = ("Client", "KinesisVideoArchivedMediaClient", "ListFragmentsPaginator")
