6c64e8b1014610c5036d285964e21104bdf102a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:31 +0000

    Add package mypy-boto3-codeguruprofiler/1.10.41.0/mypy-boto3-codeguruprofiler-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-codeguruprofiler/mypy-boto3-codeguruprofiler-1.10.41.0.tar.gz/mypy-boto3-codeguruprofiler-1.10.41.0/mypy_boto3_codeguruprofiler/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for codeguruprofiler service"
+from mypy_boto3_codeguruprofiler.client import (
+    CodeGuruProfilerClient as Client,
+    CodeGuruProfilerClient,
+)
+
+
+__all__ = ("Client", "CodeGuruProfilerClient")
