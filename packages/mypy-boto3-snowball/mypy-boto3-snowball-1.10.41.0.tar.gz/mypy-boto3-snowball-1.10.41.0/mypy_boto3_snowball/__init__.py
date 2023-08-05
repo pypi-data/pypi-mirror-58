586b465cee84cdb0c940f408fdbc545d995b5292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package mypy-boto3-snowball/1.10.41.0/mypy-boto3-snowball-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-snowball/mypy-boto3-snowball-1.10.41.0.tar.gz/mypy-boto3-snowball-1.10.41.0/mypy_boto3_snowball/__init__.py
@@ -0,0 +1,20 @@
+"Main interface for snowball service"
+from mypy_boto3_snowball.client import SnowballClient as Client, SnowballClient
+from mypy_boto3_snowball.paginator import (
+    DescribeAddressesPaginator,
+    ListClusterJobsPaginator,
+    ListClustersPaginator,
+    ListCompatibleImagesPaginator,
+    ListJobsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeAddressesPaginator",
+    "ListClusterJobsPaginator",
+    "ListClustersPaginator",
+    "ListCompatibleImagesPaginator",
+    "ListJobsPaginator",
+    "SnowballClient",
+)
