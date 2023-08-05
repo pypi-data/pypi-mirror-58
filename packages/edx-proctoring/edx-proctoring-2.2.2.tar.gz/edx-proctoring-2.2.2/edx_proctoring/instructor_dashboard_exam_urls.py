f3b9dd4e76dcc181eb2e99dcb71e33205161b869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package edx-proctoring/2.2.2/edx-proctoring-2.2.2.tar.gz

---
+++ packages/edx-proctoring/edx-proctoring-2.2.2.tar.gz/edx-proctoring-2.2.2/edx_proctoring/instructor_dashboard_exam_urls.py
@@ -0,0 +1,18 @@
+"""
+URL mapping for the exam instructor dashboard.
+"""
+
+from __future__ import absolute_import
+
+from django.conf import settings
+from django.conf.urls import url
+
+from edx_proctoring import views
+
+urlpatterns = [
+    url(
+        r'edx_proctoring/v1/instructor/{}/(?P<exam_id>\d+)$'.format(settings.COURSE_ID_PATTERN),
+        views.InstructorDashboard.as_view(),
+        name='instructor_dashboard_exam'
+    ),
+]
