ad6486b532f9d0be7830c08fe032987d971f6774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:42 +0000

    Add package bspump/19.12/bspump-19.12-py3-none-any.whl

---
+++ packages/bspump/bspump-19.12-py3-none-any.whl/bspump/unittest/pipeline.py
@@ -0,0 +1,41 @@
+from .sink import UnitTestSink
+from .source import UnitTestSource
+from ..pipeline import Pipeline
+from ..trigger import PubSubTrigger
+from ..abc.processor import Processor
+from ..application import BSPumpApplication
+
+
+class UnitTestPipeline(Pipeline):
+
+	def __init__(self, app: type(BSPumpApplication), processor: type(Processor), *args, **kwargs):
+		"""
+		Build Pipeline with given app and processor
+
+		:param processor: Processor to test - available as self.Processor
+		:param args: Optional arguments for processor
+		:param kwargs: Optional key-word arguments for processor
+		"""
+		super().__init__(app, "UnitTestPipeline")
+
+		self.Source = UnitTestSource(app, self).on(
+			PubSubTrigger(app, "unittest.go!", self.PubSub)
+		)
+		self.Processor = processor(app, self, *args, **kwargs)
+		self.Sink = UnitTestSink(app, self)
+
+		self.PubSub.subscribe("bspump.pipeline.cycle_end!", self._on_finished)
+
+		self.build(self.Source, self.Processor, self.Sink)
+
+
+	def unittest_start(self):
+		"""
+		Start the pipeline
+		"""
+
+		self.PubSub.publish("unittest.go!")
+
+
+	def _on_finished(self, event_name, pipeline):
+		self.App.stop()
---
+++ packages/bspump/bspump-19.12-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,10 @@
+from .analyzer import *
+from .avro import *
+from .common import *
+from .crypto import *
+from .elasticsearch import *
+from .file import *
+from .filter import *
+from .matrix import *
+from .test_config_defaults import *
+
