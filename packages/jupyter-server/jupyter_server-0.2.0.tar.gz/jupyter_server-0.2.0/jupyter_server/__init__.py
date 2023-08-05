4fe8a453300927ac961a21ad93eb8d46c9a7d8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:22 +0000

    Add package jupyter-server/0.2.0/jupyter_server-0.2.0.tar.gz

---
+++ packages/jupyter-server/jupyter_server-0.2.0.tar.gz/jupyter_server-0.2.0/jupyter_server/__init__.py
@@ -0,0 +1,12 @@
+"""The Jupyter Server"""
+
+from ._version import version_info, __version__
+import os
+
+DEFAULT_STATIC_FILES_PATH = os.path.join(os.path.dirname(__file__), "static")
+DEFAULT_TEMPLATE_PATH_LIST = [
+    os.path.dirname(__file__),
+    os.path.join(os.path.dirname(__file__), "templates"),
+]
+
+del os
