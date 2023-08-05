17ded685c737153f13fc9af02a86de623d91f255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:56 +0000

    Add package mypy-boto3-route53/1.10.42.0/mypy-boto3-route53-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-route53/mypy-boto3-route53-1.10.42.0.tar.gz/mypy-boto3-route53-1.10.42.0/mypy_boto3_route53/paginator.py
@@ -0,0 +1,95 @@
+"Main interface for route53 service Paginators"
+from __future__ import annotations
+
+from typing import Generator
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_route53.type_defs import (
+    ListHealthChecksResponseTypeDef,
+    ListHostedZonesResponseTypeDef,
+    ListQueryLoggingConfigsResponseTypeDef,
+    ListResourceRecordSetsResponseTypeDef,
+    ListVPCAssociationAuthorizationsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = (
+    "ListHealthChecksPaginator",
+    "ListHostedZonesPaginator",
+    "ListQueryLoggingConfigsPaginator",
+    "ListResourceRecordSetsPaginator",
+    "ListVPCAssociationAuthorizationsPaginator",
+)
+
+
+class ListHealthChecksPaginator(Boto3Paginator):
+    """
+    [Paginator.ListHealthChecks documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListHealthChecks)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListHealthChecksResponseTypeDef, None, None]:
+        """
+        [ListHealthChecks.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListHealthChecks.paginate)
+        """
+
+
+class ListHostedZonesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListHostedZones documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListHostedZones)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, DelegationSetId: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListHostedZonesResponseTypeDef, None, None]:
+        """
+        [ListHostedZones.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListHostedZones.paginate)
+        """
+
+
+class ListQueryLoggingConfigsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListQueryLoggingConfigs documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListQueryLoggingConfigs)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, HostedZoneId: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListQueryLoggingConfigsResponseTypeDef, None, None]:
+        """
+        [ListQueryLoggingConfigs.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListQueryLoggingConfigs.paginate)
+        """
+
+
+class ListResourceRecordSetsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListResourceRecordSets documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListResourceRecordSets)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, HostedZoneId: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Generator[ListResourceRecordSetsResponseTypeDef, None, None]:
+        """
+        [ListResourceRecordSets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListResourceRecordSets.paginate)
+        """
+
+
+class ListVPCAssociationAuthorizationsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListVPCAssociationAuthorizations documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListVPCAssociationAuthorizations)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        HostedZoneId: str,
+        MaxResults: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[ListVPCAssociationAuthorizationsResponseTypeDef, None, None]:
+        """
+        [ListVPCAssociationAuthorizations.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Paginator.ListVPCAssociationAuthorizations.paginate)
+        """
---
+++ packages/mypy-boto3-route53/mypy-boto3-route53-1.10.42.0.tar.gz/mypy-boto3-route53-1.10.42.0/mypy_boto3_route53/waiter.py
@@ -0,0 +1,20 @@
+"Main interface for route53 service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_route53.type_defs import WaiterConfigTypeDef
+
+
+__all__ = ("ResourceRecordSetsChangedWaiter",)
+
+
+class ResourceRecordSetsChangedWaiter(Boto3Waiter):
+    """
+    [Waiter.ResourceRecordSetsChanged documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Waiter.ResourceRecordSetsChanged)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, Id: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [ResourceRecordSetsChanged.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/route53.html#Route53.Waiter.ResourceRecordSetsChanged.wait)
+        """
