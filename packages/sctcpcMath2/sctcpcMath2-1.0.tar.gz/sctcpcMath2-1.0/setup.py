f1c26a60fae9c7b94bc43d247a45d24a44626f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:27 +0000

    Add package sctcpcMath2/1.0/sctcpcMath2-1.0.tar.gz

---
+++ packages/sctcpcMath2/sctcpcMath2-1.0.tar.gz/sctcpcMath2-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='sctcpcMath2',
+    version='1.0',
+    description='这是第一个对外发布的模块，测试模块',
+    author='tyan',
+    author_email='tyan@163.com',
+    py_modules=['sctcpcMath2.demo1','sctcpcMath2.demo2']
+)
