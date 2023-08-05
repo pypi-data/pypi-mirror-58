5f2a52df3933bc018d9946db1ce8923232554ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:03 +0000

    Add package openml-speed-dating-pipeline-steps/0.2.0/openml_speed_dating_pipeline_steps-0.2.0.tar.gz

---
+++ packages/openml-speed-dating-pipeline-steps/openml_speed_dating_pipeline_steps-0.2.0.tar.gz/openml_speed_dating_pipeline_steps-0.2.0/openml_speed_dating_pipeline_steps/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for OpenML Speed Dating Pipeline Steps."""
+
+__author__ = """Ben Auffarth"""
+__email__ = 'auffarth@gmail.com'
+__version__ = '0.2.0'
+
+from .openml_speed_dating_pipeline_steps import (
+    RangeTransformer, FloatTransformer,
+    PandasPicker, PandasPicker2
+)
