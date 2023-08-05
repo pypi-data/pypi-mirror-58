3f9b16b4add029eea106d0a5709abe78a4f863b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package mypy-boto3-polly/1.10.42.1/mypy-boto3-polly-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-polly/mypy-boto3-polly-1.10.42.1.tar.gz/mypy-boto3-polly-1.10.42.1/mypy_boto3_polly/__init__.py
@@ -0,0 +1,16 @@
+"Main interface for polly service"
+from mypy_boto3_polly.client import PollyClient, PollyClient as Client
+from mypy_boto3_polly.paginator import (
+    DescribeVoicesPaginator,
+    ListLexiconsPaginator,
+    ListSpeechSynthesisTasksPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeVoicesPaginator",
+    "ListLexiconsPaginator",
+    "ListSpeechSynthesisTasksPaginator",
+    "PollyClient",
+)
