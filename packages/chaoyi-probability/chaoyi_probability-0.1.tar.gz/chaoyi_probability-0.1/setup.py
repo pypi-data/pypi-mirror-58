98766b03494c4affbfa6e000930fb194abc8e83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:25 +0000

    Add package chaoyi-probability/0.1/chaoyi_probability-0.1.tar.gz

---
+++ packages/chaoyi-probability/chaoyi_probability-0.1.tar.gz/chaoyi_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='chaoyi_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['chaoyi_probability'],
+      author='Chaoyi Wang',
+      zip_safe=False)
