c35d34099d9728657bdfefedd0a5b7c8a4a876e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:19 +0000

    Add package sprockets.mixins.correlation/3.0.0/sprockets.mixins.correlation-3.0.0.tar.gz

---
+++ packages/sprockets.mixins.correlation/sprockets.mixins.correlation-3.0.0.tar.gz/sprockets.mixins.correlation-3.0.0/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import codecs
+from os import path
+import sys
+
+import setuptools
+
+from sprockets.mixins import correlation
+
+
+def read_requirements(name):
+    requirements = []
+    try:
+        with open(path.join('requires', name)) as req_file:
+            for line in req_file:
+                if '#' in line:
+                    line = line[:line.index('#')]
+                line = line.strip()
+                if line.startswith('-r'):
+                    requirements.extend(read_requirements(line[2:].strip()))
+                elif line and not line.startswith('-'):
+                    requirements.append(line)
+    except IOError:
+        pass
+    return requirements
+
+
+setuptools.setup(
+    name='sprockets.mixins.correlation',
+    version=correlation.__version__,
+    description='Stuff to correlate requests, logs, and the like',
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    url='https://github.com/sprockets/sprockets.mixins.correlation.git',
+    author='AWeber Communications',
+    author_email='api@aweber.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=setuptools.find_packages(),
+    namespace_packages=['sprockets'],
+    install_requires=read_requirements('installation.txt'),
+    tests_require=read_requirements('testing.txt'),
+    test_suite='nose.collector',
+    zip_safe=True,
+)
