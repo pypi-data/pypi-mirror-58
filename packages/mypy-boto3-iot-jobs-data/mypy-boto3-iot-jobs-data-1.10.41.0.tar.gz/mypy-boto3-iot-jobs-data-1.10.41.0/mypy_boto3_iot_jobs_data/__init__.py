7182f71efc9aba4893e1891980ea2c6c8e78e190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package mypy-boto3-iot-jobs-data/1.10.41.0/mypy-boto3-iot-jobs-data-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-iot-jobs-data/mypy-boto3-iot-jobs-data-1.10.41.0.tar.gz/mypy-boto3-iot-jobs-data-1.10.41.0/mypy_boto3_iot_jobs_data/__init__.py
@@ -0,0 +1,5 @@
+"Main interface for iot-jobs-data service"
+from mypy_boto3_iot_jobs_data.client import IoTJobsDataPlaneClient as Client, IoTJobsDataPlaneClient
+
+
+__all__ = ("Client", "IoTJobsDataPlaneClient")
