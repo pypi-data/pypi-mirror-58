72fdbe70c15cd23143632e513332408a424150c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package scrapy-warcio/0.0.5/scrapy-warcio-0.0.5.tar.gz

---
+++ packages/scrapy-warcio/scrapy-warcio-0.0.5.tar.gz/scrapy-warcio-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as rfh:
+    long_description = rfh.read()
+
+setup(
+    author='Steve Sisney',
+    author_email='steve@archive.org',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    description='Scrapy WARC I/O',
+    install_requires=['python-magic', 'pyyaml', 'scrapy', 'warctools'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name='scrapy-warcio',
+    packages=find_packages(),
+    tests_require=['pytest'],
+    url='https://github.com/internetarchive/scrapy-warcio',
+    version='0.0.5',
+    zip_safe=False
+)
