6a5f743dcf7236a58d9460af43d72a8eb58f1a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package mypy-boto3-mturk/1.10.43.0/mypy-boto3-mturk-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-mturk/mypy-boto3-mturk-1.10.43.0.tar.gz/mypy-boto3-mturk-1.10.43.0/mypy_boto3_mturk/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for mturk service"
+from mypy_boto3_mturk.client import MTurkClient as Client, MTurkClient
+from mypy_boto3_mturk.paginator import (
+    ListAssignmentsForHITPaginator,
+    ListBonusPaymentsPaginator,
+    ListHITsForQualificationTypePaginator,
+    ListHITsPaginator,
+    ListQualificationRequestsPaginator,
+    ListQualificationTypesPaginator,
+    ListReviewableHITsPaginator,
+    ListWorkerBlocksPaginator,
+    ListWorkersWithQualificationTypePaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListAssignmentsForHITPaginator",
+    "ListBonusPaymentsPaginator",
+    "ListHITsForQualificationTypePaginator",
+    "ListHITsPaginator",
+    "ListQualificationRequestsPaginator",
+    "ListQualificationTypesPaginator",
+    "ListReviewableHITsPaginator",
+    "ListWorkerBlocksPaginator",
+    "ListWorkersWithQualificationTypePaginator",
+    "MTurkClient",
+)
