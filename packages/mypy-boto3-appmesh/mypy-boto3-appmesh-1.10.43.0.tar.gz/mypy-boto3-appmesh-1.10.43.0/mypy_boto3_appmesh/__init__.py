ebef0b594c22fbe5d4b4d19d984cf2700f04e49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package mypy-boto3-appmesh/1.10.43.0/mypy-boto3-appmesh-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-appmesh/mypy-boto3-appmesh-1.10.43.0.tar.gz/mypy-boto3-appmesh-1.10.43.0/mypy_boto3_appmesh/__init__.py
@@ -0,0 +1,22 @@
+"Main interface for appmesh service"
+from mypy_boto3_appmesh.client import AppMeshClient, AppMeshClient as Client
+from mypy_boto3_appmesh.paginator import (
+    ListMeshesPaginator,
+    ListRoutesPaginator,
+    ListTagsForResourcePaginator,
+    ListVirtualNodesPaginator,
+    ListVirtualRoutersPaginator,
+    ListVirtualServicesPaginator,
+)
+
+
+__all__ = (
+    "AppMeshClient",
+    "Client",
+    "ListMeshesPaginator",
+    "ListRoutesPaginator",
+    "ListTagsForResourcePaginator",
+    "ListVirtualNodesPaginator",
+    "ListVirtualRoutersPaginator",
+    "ListVirtualServicesPaginator",
+)
