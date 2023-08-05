0d96578bdfe28de5e72cb2d89e4df9c174c584e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package concur/0.3.0/concur-0.3.0.tar.gz

---
+++ packages/concur/concur-0.3.0.tar.gz/concur-0.3.0/concur/__init__.py
@@ -0,0 +1,27 @@
+"""
+.. include:: ../README.md
+   :start-after: <!-- Start docs -->
+   :end-before: <!-- End docs -->
+
+.. include:: introduction.md
+
+.. include:: troubleshooting.md
+
+# API
+
+All the functions and classes from `concur.core`, `concur.widgets`, and `concur.extra_widgets` are re-exported in the
+`concur` module. The most convenient way to access them is by importing `concur` qualified:
+
+```python
+import concur as c
+```
+
+"""
+
+import concur.extra_widgets
+import concur.integrations
+import concur.draw
+
+from .core import *
+from .widgets import *
+from .extra_widgets import *
