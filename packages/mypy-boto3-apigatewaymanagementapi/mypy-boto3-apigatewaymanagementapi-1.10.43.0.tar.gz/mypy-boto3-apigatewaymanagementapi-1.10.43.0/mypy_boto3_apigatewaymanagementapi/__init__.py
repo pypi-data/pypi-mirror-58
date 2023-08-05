88a5a4af3bc18dd0d004584479686a4b49841f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package mypy-boto3-apigatewaymanagementapi/1.10.43.0/mypy-boto3-apigatewaymanagementapi-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-apigatewaymanagementapi/mypy-boto3-apigatewaymanagementapi-1.10.43.0.tar.gz/mypy-boto3-apigatewaymanagementapi-1.10.43.0/mypy_boto3_apigatewaymanagementapi/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for apigatewaymanagementapi service"
+from mypy_boto3_apigatewaymanagementapi.client import (
+    ApiGatewayManagementApiClient,
+    ApiGatewayManagementApiClient as Client,
+)
+
+
+__all__ = ("ApiGatewayManagementApiClient", "Client")
