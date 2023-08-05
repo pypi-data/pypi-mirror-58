3e2865c09f6555176b6e188a220139424e0965bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:36 +0000

    Add package akshare/0.3.6/akshare-0.3.6.tar.gz

---
+++ packages/akshare/akshare-0.3.6.tar.gz/akshare-0.3.6/tests/test_air.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2019/12/12 18:16
+contact: jindaxiang@163.com
+desc: 测试文件
+"""
+from akshare.air.aqi_study import air_hourly
+
+
+def test_air_hourly():
+    df_hourly = air_hourly('成都', '2019-12-10')
+    assert len(df_hourly) >= 10
+
+
+if __name__ == "__main__":
+    test_air_hourly()
