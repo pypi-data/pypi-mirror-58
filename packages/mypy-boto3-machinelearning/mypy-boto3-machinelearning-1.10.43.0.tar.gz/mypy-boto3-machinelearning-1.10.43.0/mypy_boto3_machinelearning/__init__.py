fce1d5146a85534cd457fce7862e0189778c18ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package mypy-boto3-machinelearning/1.10.43.0/mypy-boto3-machinelearning-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-machinelearning/mypy-boto3-machinelearning-1.10.43.0.tar.gz/mypy-boto3-machinelearning-1.10.43.0/mypy_boto3_machinelearning/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for machinelearning service"
+from mypy_boto3_machinelearning.client import MachineLearningClient, MachineLearningClient as Client
+from mypy_boto3_machinelearning.paginator import (
+    DescribeBatchPredictionsPaginator,
+    DescribeDataSourcesPaginator,
+    DescribeEvaluationsPaginator,
+    DescribeMLModelsPaginator,
+)
+from mypy_boto3_machinelearning.waiter import (
+    BatchPredictionAvailableWaiter,
+    DataSourceAvailableWaiter,
+    EvaluationAvailableWaiter,
+    MLModelAvailableWaiter,
+)
+
+
+__all__ = (
+    "BatchPredictionAvailableWaiter",
+    "Client",
+    "DataSourceAvailableWaiter",
+    "DescribeBatchPredictionsPaginator",
+    "DescribeDataSourcesPaginator",
+    "DescribeEvaluationsPaginator",
+    "DescribeMLModelsPaginator",
+    "EvaluationAvailableWaiter",
+    "MLModelAvailableWaiter",
+    "MachineLearningClient",
+)
