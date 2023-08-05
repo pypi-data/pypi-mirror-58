bbd60807da8bee1ea571dcefee0fc308b4856493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:27 +0000

    Add package mypy-boto3-acm/1.10.41.0/mypy-boto3-acm-1.10.41.0.tar.gz

---
+++ packages/mypy-boto3-acm/mypy-boto3-acm-1.10.41.0.tar.gz/mypy-boto3-acm-1.10.41.0/mypy_boto3_acm/paginator.py
@@ -0,0 +1,46 @@
+"Main interface for acm service Paginators"
+from __future__ import annotations
+
+import sys
+from typing import Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_acm.type_defs import (
+    FiltersTypeDef,
+    ListCertificatesResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("ListCertificatesPaginator",)
+
+
+class ListCertificatesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListCertificates documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/acm.html#ACM.Paginator.ListCertificates)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        CertificateStatuses: List[
+            Literal[
+                "PENDING_VALIDATION",
+                "ISSUED",
+                "INACTIVE",
+                "EXPIRED",
+                "VALIDATION_TIMED_OUT",
+                "REVOKED",
+                "FAILED",
+            ]
+        ] = None,
+        Includes: FiltersTypeDef = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[ListCertificatesResponseTypeDef, None, None]:
+        """
+        [ListCertificates.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.41/reference/services/acm.html#ACM.Paginator.ListCertificates.paginate)
+        """
