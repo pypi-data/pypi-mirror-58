fecaa38ff114e194a19736bf64f9daeda04745c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:50 +0000

    Add package mypy-boto3-mediatailor/1.10.40.2/mypy-boto3-mediatailor-1.10.40.2.tar.gz

---
+++ packages/mypy-boto3-mediatailor/mypy-boto3-mediatailor-1.10.40.2.tar.gz/mypy-boto3-mediatailor-1.10.40.2/mypy_boto3_mediatailor/__init__.py
@@ -0,0 +1,6 @@
+"Main interface for mediatailor service"
+from mypy_boto3_mediatailor.client import MediaTailorClient, MediaTailorClient as Client
+from mypy_boto3_mediatailor.paginator import ListPlaybackConfigurationsPaginator
+
+
+__all__ = ("Client", "ListPlaybackConfigurationsPaginator", "MediaTailorClient")
