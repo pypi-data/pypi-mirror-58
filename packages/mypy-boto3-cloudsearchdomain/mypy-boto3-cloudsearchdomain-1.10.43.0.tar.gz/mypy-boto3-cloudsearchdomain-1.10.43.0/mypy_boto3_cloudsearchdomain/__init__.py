6c0ad9c6de144f964c78af56143ae6592049f06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package mypy-boto3-cloudsearchdomain/1.10.43.0/mypy-boto3-cloudsearchdomain-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-cloudsearchdomain/mypy-boto3-cloudsearchdomain-1.10.43.0.tar.gz/mypy-boto3-cloudsearchdomain-1.10.43.0/mypy_boto3_cloudsearchdomain/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for cloudsearchdomain service"
+from mypy_boto3_cloudsearchdomain.client import (
+    CloudSearchDomainClient,
+    CloudSearchDomainClient as Client,
+)
+
+
+__all__ = ("Client", "CloudSearchDomainClient")
