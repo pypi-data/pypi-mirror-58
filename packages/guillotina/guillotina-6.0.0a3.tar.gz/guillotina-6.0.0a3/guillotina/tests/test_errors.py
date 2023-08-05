94da49cb5ff32812478d91166a91e59917138542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package guillotina/6.0.0a3/guillotina-6.0.0a3.tar.gz

---
+++ packages/guillotina/guillotina-6.0.0a3.tar.gz/guillotina-6.0.0a3/guillotina/tests/test_errors.py
@@ -0,0 +1,36 @@
+from guillotina.exceptions import DeserializationError
+from guillotina.exceptions import ValueDeserializationError
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_non_existing_container(container_requester):
+    async with container_requester as requester:
+        response, status = await requester("GET", "/db/non")
+        assert status == 404
+
+
+@pytest.mark.asyncio
+async def test_non_existing_registry(container_requester):
+    async with container_requester as requester:
+        response, status = await requester("GET", "/db/guillotina/@registry/non")
+        assert status == 404
+
+
+@pytest.mark.asyncio
+async def test_non_existing_type(container_requester):
+    async with container_requester as requester:
+        response, status = await requester("GET", "/db/guillotina/@types/non")
+        assert status == 404
+
+
+def test_deserialization_error_formats_error():
+    error = DeserializationError([{"error": "Foobar", "field": "foobar_field"}])
+    assert "foobar_field" in str(error)
+
+
+def test_value_serialization_error():
+    error = ValueDeserializationError("Foo", "Bar", "Something wrong")
+    assert error.field == "Foo"
+    assert error.value == "Bar"
