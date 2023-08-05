d7cbc16a6dbed00aa8a3bf7037f717c0c01a15c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:02 +0000

    Add package easybase/1.0.2/easybase-1.0.2-py2.py3-none-any.whl

---
+++ packages/easybase/easybase-1.0.2-py2.py3-none-any.whl/easybase/__init__.py
@@ -0,0 +1,19 @@
+"""
+EasyBase, a developer-friendly Python library to interact with Apache
+HBase. Support Time-Range scan and multi-version access
+"""
+# using thriftpy2 server as thrift bridge directly 
+from pkg_resources import resource_filename
+import thriftpy2
+
+thriftpy2.load(
+    resource_filename('easybase', 'HBase.thrift'),
+    module_name='HBase_thrift'
+    )
+
+from ._version import __version__
+
+
+from .connection import DEFAULT_HOST, DEFAULT_PORT, Connection
+from .table import Table
+from .pool import ConnectionPool, NoConnectionsAvailable
