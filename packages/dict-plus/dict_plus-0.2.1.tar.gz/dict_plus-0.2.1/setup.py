87d721e6cc70c26270ae8f1d6d5ae70a73400b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:42 +0000

    Add package dict-plus/0.2.1/dict_plus-0.2.1.tar.gz

---
+++ packages/dict-plus/dict_plus-0.2.1.tar.gz/dict_plus-0.2.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import json
+
+
+def version_info():
+    with open("version.json", "r") as f:
+        return json.load(f)
+
+
+def parse_requirements(requirement_file):
+    return ""
+    # Code doesn't work with python2.7 installation, just return the requirement hardcoded
+    # with open(requirement_file) as fi:
+    #     return fi.readlines()
+
+
+with open('./README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='dict_plus',
+    packages=['dict_plus'],
+    version=version_info()["version"],
+    description='Extended Dictionary Package',
+    author='Spencer Hanson',
+    author_email="spencerhanson3141@gmail.com",
+    long_description=long_description,
+    install_requires=parse_requirements('requirements.txt'),
+    keywords=['list', 'utilities', 'dictionary'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
+)
