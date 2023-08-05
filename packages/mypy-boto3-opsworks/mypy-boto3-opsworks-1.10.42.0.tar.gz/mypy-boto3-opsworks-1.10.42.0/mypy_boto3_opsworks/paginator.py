3ce5230dc8847b108e754c6c9f8a6c0e394f6b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:54 +0000

    Add package mypy-boto3-opsworks/1.10.42.0/mypy-boto3-opsworks-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-opsworks/mypy-boto3-opsworks-1.10.42.0.tar.gz/mypy-boto3-opsworks-1.10.42.0/mypy_boto3_opsworks/paginator.py
@@ -0,0 +1,26 @@
+"Main interface for opsworks service Paginators"
+from __future__ import annotations
+
+from typing import Generator, List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_opsworks.type_defs import DescribeEcsClustersResultTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("DescribeEcsClustersPaginator",)
+
+
+class DescribeEcsClustersPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeEcsClusters documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/opsworks.html#OpsWorks.Paginator.DescribeEcsClusters)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        EcsClusterArns: List[str] = None,
+        StackId: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Generator[DescribeEcsClustersResultTypeDef, None, None]:
+        """
+        [DescribeEcsClusters.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.42/reference/services/opsworks.html#OpsWorks.Paginator.DescribeEcsClusters.paginate)
+        """
