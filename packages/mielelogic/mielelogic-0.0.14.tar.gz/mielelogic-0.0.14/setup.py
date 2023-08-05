3e2bf3c6670b9edd0c6f5a61e18166d372fee786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package mielelogic/0.0.14/mielelogic-0.0.14.tar.gz

---
+++ packages/mielelogic/mielelogic-0.0.14.tar.gz/mielelogic-0.0.14/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mielelogic", # Replace with your own username
+    version="0.0.14",
+    author="PTST",
+    author_email="patrick@steffensen.io",
+    description="API interface for MieleLogic.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PTST/MieleLogic",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests >= 2.21.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
