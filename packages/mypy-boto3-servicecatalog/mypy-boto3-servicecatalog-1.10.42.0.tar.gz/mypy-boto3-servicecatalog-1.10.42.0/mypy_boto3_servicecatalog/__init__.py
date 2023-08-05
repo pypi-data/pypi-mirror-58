6a885db0dbcb1927300ddb989b120817e3ad2710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:58 +0000

    Add package mypy-boto3-servicecatalog/1.10.42.0/mypy-boto3-servicecatalog-1.10.42.0.tar.gz

---
+++ packages/mypy-boto3-servicecatalog/mypy-boto3-servicecatalog-1.10.42.0.tar.gz/mypy-boto3-servicecatalog-1.10.42.0/mypy_boto3_servicecatalog/__init__.py
@@ -0,0 +1,42 @@
+"Main interface for servicecatalog service"
+from mypy_boto3_servicecatalog.client import ServiceCatalogClient as Client, ServiceCatalogClient
+from mypy_boto3_servicecatalog.paginator import (
+    ListAcceptedPortfolioSharesPaginator,
+    ListConstraintsForPortfolioPaginator,
+    ListLaunchPathsPaginator,
+    ListOrganizationPortfolioAccessPaginator,
+    ListPortfoliosForProductPaginator,
+    ListPortfoliosPaginator,
+    ListPrincipalsForPortfolioPaginator,
+    ListProvisionedProductPlansPaginator,
+    ListProvisioningArtifactsForServiceActionPaginator,
+    ListRecordHistoryPaginator,
+    ListResourcesForTagOptionPaginator,
+    ListServiceActionsForProvisioningArtifactPaginator,
+    ListServiceActionsPaginator,
+    ListTagOptionsPaginator,
+    ScanProvisionedProductsPaginator,
+    SearchProductsAsAdminPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListAcceptedPortfolioSharesPaginator",
+    "ListConstraintsForPortfolioPaginator",
+    "ListLaunchPathsPaginator",
+    "ListOrganizationPortfolioAccessPaginator",
+    "ListPortfoliosForProductPaginator",
+    "ListPortfoliosPaginator",
+    "ListPrincipalsForPortfolioPaginator",
+    "ListProvisionedProductPlansPaginator",
+    "ListProvisioningArtifactsForServiceActionPaginator",
+    "ListRecordHistoryPaginator",
+    "ListResourcesForTagOptionPaginator",
+    "ListServiceActionsForProvisioningArtifactPaginator",
+    "ListServiceActionsPaginator",
+    "ListTagOptionsPaginator",
+    "ScanProvisionedProductsPaginator",
+    "SearchProductsAsAdminPaginator",
+    "ServiceCatalogClient",
+)
