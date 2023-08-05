309e71e46027111f473df296f9668824efc0dde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:12 +0000

    Add package mobilpy/0.4/mobilpy-0.4.tar.gz

---
+++ packages/mobilpy/mobilpy-0.4.tar.gz/mobilpy-0.4/setup.py
@@ -0,0 +1,36 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+# tutorial: 
+# https://python-packaging.readthedocs.io/en/latest/index.html
+setup(name='mobilpy',
+        version='0.4',
+        description='Library that helps create the XML necessary for online payments with MobilPay',
+        long_description=readme(),
+        keywords='online payments mobilpay netopia',
+        url='https://github.com/onel/mobilpy',
+        author='Andrei Onel',
+        author_email='andrei@edumo.org',
+        license='MIT',
+        py_modules=[
+            'mobilpy'
+        ],
+        packages=['mobilpy'],
+        # https://pypi.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Office/Business :: Financial',
+            'Topic :: Text Processing :: Markup :: XML'
+        ],
+        install_requires=[
+            'pyOpenSSL',
+            'pycryptodome'
+        ],
+        zip_safe=False)
