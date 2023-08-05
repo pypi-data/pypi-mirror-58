c3c9415d3d17d786f458e2c7560af597ded3e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package datawrapper/0.4.0/datawrapper-0.4.0.tar.gz

---
+++ packages/datawrapper/datawrapper-0.4.0.tar.gz/datawrapper-0.4.0/datawrapper/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for datawrapper."""
+
+__author__ = """Sergio Sanchez"""
+__email__ = "chekos@tacosdedatos.com"
+__version__ = "__version__ = '0.4.0'"
+
+import datawrapper.datawrapper as dw
+from IPython.display import HTML, Image
+
+Datawrapper = dw.Datawrapper
