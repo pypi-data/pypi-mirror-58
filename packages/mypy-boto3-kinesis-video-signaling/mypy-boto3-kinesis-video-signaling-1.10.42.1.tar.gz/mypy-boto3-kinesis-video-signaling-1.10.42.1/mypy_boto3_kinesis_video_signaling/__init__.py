1bd35c6535821c0b3ac5b49a1fed0000610cb47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package mypy-boto3-kinesis-video-signaling/1.10.42.1/mypy-boto3-kinesis-video-signaling-1.10.42.1.tar.gz

---
+++ packages/mypy-boto3-kinesis-video-signaling/mypy-boto3-kinesis-video-signaling-1.10.42.1.tar.gz/mypy-boto3-kinesis-video-signaling-1.10.42.1/mypy_boto3_kinesis_video_signaling/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for kinesis-video-signaling service"
+from mypy_boto3_kinesis_video_signaling.client import (
+    KinesisVideoSignalingChannelsClient as Client,
+    KinesisVideoSignalingChannelsClient,
+)
+
+
+__all__ = ("Client", "KinesisVideoSignalingChannelsClient")
