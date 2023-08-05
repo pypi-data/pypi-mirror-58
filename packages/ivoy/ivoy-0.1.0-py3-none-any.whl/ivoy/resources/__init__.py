93ef0169a6401da64617838ed8e31a17c50247f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:38 +0000

    Add package ivoy/0.1.0/ivoy-0.1.0-py3-none-any.whl

---
+++ packages/ivoy/ivoy-0.1.0-py3-none-any.whl/ivoy/resources/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'Budget',
+    'CarrierLocation',
+    'Order',
+    'OrderSharing',
+    'Resource',
+]
+
+from .base import Resource
+from .budget import Budget
+from .carrier_location import CarrierLocation
+from .order import Order
+from .order_sharing import OrderSharing
