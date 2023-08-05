c01e2b6be6df135877eb63729ecf3a14c3601fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:36 +0000

    Add package argo-workflows/2.1.3/argo-workflows-2.1.3.tar.gz

---
+++ packages/argo-workflows/argo-workflows-2.1.3.tar.gz/argo-workflows-2.1.3/argo/workflows/__init__.py
@@ -0,0 +1,9 @@
+"""Python client for Argo Workflows."""
+
+from argo.workflows import client
+from argo.workflows import config
+from argo.workflows import watch
+
+from argo.workflows.client import models
+
+from argo.workflows.__about__ import __version__
