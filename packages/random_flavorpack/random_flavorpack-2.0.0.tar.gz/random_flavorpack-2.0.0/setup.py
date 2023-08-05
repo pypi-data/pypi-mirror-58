cdf0ef338ee17a2aa671b799e95c6a5e901e7047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package random_flavorpack/2.0.0/random_flavorpack-2.0.0.tar.gz

---
+++ packages/random_flavorpack/random_flavorpack-2.0.0.tar.gz/random_flavorpack-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='random_flavorpack',
+    version='2.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://gitlab.com/serial-lab/random-flavorpack',
+    license='GPLv3',
+    author='SerialLab Corp',
+    author_email='slab@serial-lab.com',
+    description='Randomized Number Flavorpack Plugin for SerialBox',
+    keywords=('seriallab, quartet, randomized, numbers, serialbox, '
+             'level-4 quartet'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
