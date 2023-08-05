59829cee8f8b89be614870df72a5543cea181dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:19 +0000

    Add package ckan-harvesters/0.130/ckan_harvesters-0.130-py3-none-any.whl

---
+++ packages/ckan-harvesters/ckan_harvesters-0.130-py3-none-any.whl/harvester_adapters/ckan/local_settings.py
@@ -0,0 +1,6 @@
+CKAN_API_KEY = 'd0ff7be9-4edf-41e8-87a8-ff80fb6b5dbc'
+CKAN_BASE_URL = 'http://nginx:8080'
+
+HARVEST_SOURCE_ID = 'rrb-json'
+CKAN_ORG_ID = 'arlington-county'  # create in you local instance and get the ID
+CKAN_VALID_USER_ID = 'admin'
