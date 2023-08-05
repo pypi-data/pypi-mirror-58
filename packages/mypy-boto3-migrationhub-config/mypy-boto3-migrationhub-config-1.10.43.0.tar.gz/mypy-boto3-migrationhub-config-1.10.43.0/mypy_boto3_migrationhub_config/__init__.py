9ad5b102c9b682c39fc92388785204d2c0716650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package mypy-boto3-migrationhub-config/1.10.43.0/mypy-boto3-migrationhub-config-1.10.43.0.tar.gz

---
+++ packages/mypy-boto3-migrationhub-config/mypy-boto3-migrationhub-config-1.10.43.0.tar.gz/mypy-boto3-migrationhub-config-1.10.43.0/mypy_boto3_migrationhub_config/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for migrationhub-config service"
+from mypy_boto3_migrationhub_config.client import (
+    MigrationHubConfigClient,
+    MigrationHubConfigClient as Client,
+)
+
+
+__all__ = ("Client", "MigrationHubConfigClient")
