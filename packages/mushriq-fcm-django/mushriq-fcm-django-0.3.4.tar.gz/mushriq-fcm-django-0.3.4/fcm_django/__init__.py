3ce03d7a0e910b73f5dbf8070a489f3dfa93ef5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:17 +0000

    Add package mushriq-fcm-django/0.3.4/mushriq-fcm-django-0.3.4.tar.gz

---
+++ packages/mushriq-fcm-django/mushriq-fcm-django-0.3.4.tar.gz/mushriq-fcm-django-0.3.4/fcm_django/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "ysinjab"
+__email__ = "ysinjab@gmail.com"
+__version__ = "0.3.4"
+
+
+class NotificationError(Exception):
+    pass
+
+
+default_app_config = 'fcm_django.apps.FcmDjangoConfig'
