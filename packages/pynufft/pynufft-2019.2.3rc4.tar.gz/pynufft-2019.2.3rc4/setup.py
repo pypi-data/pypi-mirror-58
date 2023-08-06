abc3a6be5de5e03540565dd0d9242246fe5f6c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:03 +0000

    Add package pynufft/2019.2.3rc4/pynufft-2019.2.3rc4.tar.gz

---
+++ packages/pynufft/pynufft-2019.2.3rc4.tar.gz/pynufft-2019.2.3rc4/setup.py
@@ -0,0 +1,34 @@
+#from setuptools import setup
+#try:
+#    from pypandoc import convert
+#    read_md = lambda f: convert(f, 'rst')
+#except ImportError:
+#    print("warning: pypandoc module not found, could not convert Markdown to RST")
+#    read_md = lambda f: open(f, 'r').read()
+
+
+
+#import os
+#long_description = 'A pythonic non-uniform FFT'
+#if os.path.exists('README.txt'):
+#    long_description = open('README.txt').read()
+
+from setuptools import setup
+
+import sys
+#if sys.version_info[0] == 2:
+#    sys.exit("Sorry, Python 2 is not supported yet")
+setup(name='pynufft',
+      version='2019.2.3rc4',
+      description='Python non-uniform fast Fourier transform (PyNUFFT)',
+      author='Jyh-Miin Lin',
+      author_email='jyhmiinlin@gmail.com',
+      url = 'https://github.com/jyhmiinlin/pynufft', # use the URL to the github repo
+      install_requires = ['numpy', 'scipy'],
+      license='LGPLv3',
+      packages=['pynufft'],
+      package_dir={'pynufft':'.'},
+      package_data={'pynufft':['nufft','src','tests','linalg','nufft/*','src/*','src/*/*','tests/*','example/*', 'linalg/*']},
+      include_package_data=True,
+      zip_safe=False)
+	
