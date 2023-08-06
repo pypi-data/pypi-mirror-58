7e1d91112e61415f57efff6b42e7885a2f5d2b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:26 +0000

    Add package python-facebook-api/0.5.3/python_facebook_api-0.5.3-py2.py3-none-any.whl

---
+++ packages/python-facebook-api/python_facebook_api-0.5.3-py2.py3-none-any.whl/pyfacebook/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+
+from .api.facebook import Api  # noqa
+from .api.instagram import IgProApi  # noqa
+from .error import *  # noqa
+from .models import *  # noqa
