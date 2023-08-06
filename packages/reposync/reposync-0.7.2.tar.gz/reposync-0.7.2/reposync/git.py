ce37d7f53f3afd9e62f2280605f79976d4848550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:19 +0000

    Add package reposync/0.7.2/reposync-0.7.2.tar.gz

---
+++ packages/reposync/reposync-0.7.2.tar.gz/reposync-0.7.2/reposync/git.py
@@ -0,0 +1,40 @@
+import git
+
+
+class Git:
+    def __init__(self, config):
+        self.method = config.get("method")
+
+    def clone(self, repository):
+        print("Cloning", repository.path, "...")
+        try:
+            git.Repo.clone_from(self.get_clone_url(repository.url), repository.path)
+        except git.exc.GitCommandError:
+            print("Skipped cloning", repository.path)
+        else:
+            print("Cloned", repository.path)
+
+    def pull(self, repository):
+        print("Pulling", repository.path, "...")
+        try:
+            git.Git(repository.path).pull()
+        except git.exc.GitCommandError:
+            print("Failed pulling", repository.path)
+        else:
+            print("Pulled", repository.path)
+
+    # private
+
+    def get_clone_url(self, repository_url):
+        if self.method == "https":
+            return self.convert_to_https_url(repository_url)
+        if self.method == "ssh":
+            return self.convert_to_ssh_url(repository_url)
+
+    def convert_to_https_url(self, repository_url):
+        return "https://" + repository_url + ".git"
+
+    def convert_to_ssh_url(self, repository_url):
+        url_splits = repository_url.split("/")
+        host, path = url_splits[0], "/".join(url_splits[1:])
+        return "git@" + host + ":" + path + ".git"
