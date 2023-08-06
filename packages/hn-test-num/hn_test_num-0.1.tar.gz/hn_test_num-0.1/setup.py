73a39bafe4fe9d4f7fd1bc979aa9c265e2383a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package hn-test-num/0.1/hn_test_num-0.1.tar.gz

---
+++ packages/hn-test-num/hn_test_num-0.1.tar.gz/hn_test_num-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='hn_test_num',
+      version='0.1',
+      description='sum',
+      url='http://github.com/tongling/clinicaltrial',
+      author='waxx0319',
+      author_email='470755846@qq.com',
+      license='MIT',
+      packages=['hn_test_sum'],
+      zip_safe=False)
