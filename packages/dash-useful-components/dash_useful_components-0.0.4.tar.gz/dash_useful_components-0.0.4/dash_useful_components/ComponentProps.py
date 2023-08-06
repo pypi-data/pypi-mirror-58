bf6a3c6b3ff4b8a8805b9530293ce16ac9527623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:12 +0000

    Add package dash-useful-components/0.0.4/dash_useful_components-0.0.4.tar.gz

---
+++ packages/dash-useful-components/dash_useful_components-0.0.4.tar.gz/dash_useful_components-0.0.4/dash_useful_components/ComponentProps.py
@@ -0,0 +1,36 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class ComponentProps(Component):
+    """A ComponentProps component.
+ExampleComponent is an example component.
+It takes a property, `label`, and
+displays it.
+It renders an input with the property `value`
+which is editable by the user.
+
+Keyword arguments:
+- id (string; optional): The ID used to identify this component in Dash callbacks
+- component_props (a list of or a singular dash component, string or number; optional)
+- comp_array (list of a list of or a singular dash component, string or numbers; optional)"""
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, component_props=Component.UNDEFINED, comp_array=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'component_props', 'comp_array']
+        self._type = 'ComponentProps'
+        self._namespace = 'dash_useful_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'component_props', 'comp_array']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(ComponentProps, self).__init__(**args)
