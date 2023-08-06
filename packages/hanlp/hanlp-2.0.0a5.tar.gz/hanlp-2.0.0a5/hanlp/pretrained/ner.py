8bde7c2334a8c4ad59a9f7a91dea258cc75e9f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:06 +0000

    Add package hanlp/2.0.0a5/hanlp-2.0.0a5.tar.gz

---
+++ packages/hanlp/hanlp-2.0.0a5.tar.gz/hanlp-2.0.0a5/hanlp/pretrained/ner.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2019-12-30 20:07
+from hanlp.common.constant import HANLP_URL
+
+MSRA_NER_BERT_BASE_ZH = HANLP_URL + 'ner/ner_bert_base_msra_20191230_205748.zip'
+CONLL03_NER_BERT_BASE_UNCASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_uncased_en_20200101_175841.zip'
+CONLL03_NER_BERT_BASE_CASED_EN = HANLP_URL + 'ner/ner_conll03_bert_base_cased_en_20200103_234420.zip'
+
+ALL = {}
