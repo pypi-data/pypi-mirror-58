da0eb52c20108e8f06b20ef9f6324a274ef4d63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:38 +0000

    Add package wes/0.1.5.post1/wes-0.1.5.post1.tar.gz

---
+++ packages/wes/wes-0.1.5.post1.tar.gz/wes-0.1.5.post1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wes',
+      version='0.1.5post1',
+      description='Use Wes Anderson Color Palettes in Matplotlib & Seaborn',
+      url='https://github.com/ljwolf/wampl',
+      author='Levi John Wolf',
+      author_email='levi.john.wolf@gmail.com',
+      license='CC-BY-SA',
+      packages=['wes'],
+      install_requires=['matplotlib'],
+      zip_safe=False)
