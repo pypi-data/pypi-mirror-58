116637e5920d5282d296f9cda18171596204ab8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:01 +0000

    Add package assistant-chat-check/0.0.2/assistant_chat_check-0.0.2.tar.gz

---
+++ packages/assistant-chat-check/assistant_chat_check-0.0.2.tar.gz/assistant_chat_check-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import pathlib
+
+requires = [
+    "sklearn", "pymongo", "pubsub_ncs"
+]
+VERSION = ""
+_ROOT = pathlib.Path(__file__).parent
+with open(str(_ROOT / 'assistant_chat_check' / '__init__.py')) as f:
+    for line in f:
+        print(line)
+        if line.startswith('__version__ ='):
+            _, _, version = line.partition('=')
+            VERSION = version.strip(" \n'\"")
+            break
+    if VERSION == "":
+        raise RuntimeError(
+            'unable to read the version from assistant_chat_check/__init__.py')
+
+packages = [
+    "assistant_chat_check"
+]
+setup(
+    name='assistant_chat_check',
+    version=VERSION,
+    description='gensim生成モデルの評価',
+    url='https://gitlab.com/assistant-service/assistant-chat-keras.git',
+    author='nozomi.nishinohara',
+    author_email='nozomi.nishinohara@n-creativesystem.com',
+    keywords='',
+    packages=packages,
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
