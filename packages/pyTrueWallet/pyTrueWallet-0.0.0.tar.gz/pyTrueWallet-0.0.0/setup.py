bca5b26036429261ec389cd132ae14912018ca2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:45 +0000

    Add package pyTrueWallet/0.0.0/pyTrueWallet-0.0.0.tar.gz

---
+++ packages/pyTrueWallet/pyTrueWallet-0.0.0.tar.gz/pyTrueWallet-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name="pyTrueWallet",
+      description="Unoffical TrueMoney Wallet API Class for Python",
+      author="sctnightcore",
+      author_email="nightcoremax@gmail.com",
+      url="https://github.com/sctnightcore/pyTrueWallet",
+      packages=["pyTrueWallet"],
+      license="GPL v3"
+    )
