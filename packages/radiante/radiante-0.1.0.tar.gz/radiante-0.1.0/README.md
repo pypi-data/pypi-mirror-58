74810c5c23e41ea526ea3c1f18e9a4596c223678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:25 +0000

    Add package radiante/0.1.0/radiante-0.1.0.tar.gz

---
+++ packages/radiante/radiante-0.1.0.tar.gz/radiante-0.1.0/README.md
@@ -0,0 +1,3 @@
+# radiante
+
+Radiante
---
+++ packages/radiante/radiante-0.1.0.tar.gz/radiante-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="radiante", # Replace with your own username
+    version="0.1.0",
+    author="Yuhang (Steven) Wang",
+    author_email="",
+    description="Radiante",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "matplotlib",
+        "numpy",
+    ],
+)
