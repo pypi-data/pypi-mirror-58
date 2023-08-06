28053d24fa10014cef5118be0f6b2845b560b3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:26 +0000

    Add package mesh-atc/0.0.2/mesh_atc-0.0.2-py3-none-any.whl

---
+++ packages/mesh-atc/mesh_atc-0.0.2-py3-none-any.whl/mesh_atc/defaults.py
@@ -0,0 +1,18 @@
+from .lib.atc_pb2 import Status
+
+class Default:
+    channel = "/FlightData"
+    rpcURL = "127.0.0.1:5555"
+
+class Status:
+    SCHEDULED = Status.SCHEDULED
+    DELAYED = Status.DELAYED
+    CANCELLED = Status.CANCELLED
+    ON_TIME = Status.ON_TIME
+    GATE_OPEN = Status.GATE_OPEN
+    FINAL_CALL = Status.FINAL_CALL
+    GATE_CLOSED = Status.GATE_CLOSED
+    DEPARTED = Status.DEPARTED
+    IN_AIR = Status.IN_AIR
+    LANDED = Status.LANDED
+    ARRIVED = Status.ARRIVED   
