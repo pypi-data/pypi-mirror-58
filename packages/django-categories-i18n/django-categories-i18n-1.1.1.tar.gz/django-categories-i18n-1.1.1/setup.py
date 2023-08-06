4e967aa58674f2a5a1c322c39b1475e44ab59d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:01 +0000

    Add package django-categories-i18n/1.1.1/django-categories-i18n-1.1.1.tar.gz

---
+++ packages/django-categories-i18n/django-categories-i18n-1.1.1.tar.gz/django-categories-i18n-1.1.1/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+import sys
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-categories-i18n',
+    version=find_version('categories_i18n', '__init__.py'),
+    license='Apache 2.0',
+
+    install_requires=[
+        'django-mptt>=0.5.5',
+        'django-parler>=1.4',
+    ],
+    requires=[
+        'Django (>=1.7)',
+    ],
+
+    description='Translatable Categories model',
+    long_description=read('README.rst'),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/django-parler/django-categories-i18n',
+    download_url='https://github.com/django-parler/django-categories-i18n/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
