08a42d21f08e692948e094989f637a60a74f82a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:10 +0000

    Add package bwpy/0.1.3/bwpy-0.1.3.tar.gz

---
+++ packages/bwpy/bwpy-0.1.3.tar.gz/bwpy-0.1.3/bwpy/bwpy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import toml
+import json
+from sh import bw
+from .bitwarden.item import BitwardenItem
+from .bitwarden.collection import BitwardenCollection
+
+
+def version():
+    return toml.load("pyproject.toml")["tool"]["poetry"]["version"]
+
+
+def pull(args):
+    bw.sync()
+
+    collection = BitwardenCollection(
+        org_name=args.org, collection_name=args.collection,
+    )
+
+    print(collection.json(filter=args.item))
+
+
+def push(args):
+    item = BitwardenItem(
+        org_name=args.org, collection_name=args.collection, item_name=args.item
+    )
+
+    json_data = json.loads(args.json)
+
+    bw.sync()
+
+    if args.force:
+        result = item.upsert(json_data)
+    else:
+        result = item.insert(json_data)
+
+    if not args.silent:
+        print(f"successfully pushed item: {args.item} ({result['id']})")
+
+    bw.sync()
