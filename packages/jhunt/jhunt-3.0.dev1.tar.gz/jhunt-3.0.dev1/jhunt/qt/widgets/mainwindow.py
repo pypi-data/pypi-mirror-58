a1b786222b7cc3b6a74b09b7b3fd1b829ad0b8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:53 +0000

    Add package jhunt/3.0.dev1/jhunt-3.0.dev1.tar.gz

---
+++ packages/jhunt/jhunt-3.0.dev1.tar.gz/jhunt-3.0.dev1/jhunt/qt/widgets/mainwindow.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QMainWindow, QTabWidget
+
+from jhunt.qt.widgets.tabs.adverts import AdvertsTab
+from jhunt.qt.widgets.tabs.websites import WebsitesTab
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, adverts_data, websites_data):
+        super().__init__()
+
+        self.resize(800, 600)
+        self.setWindowTitle('Job Hunter')
+        self.statusBar().showMessage("Ready", 2000)
+
+        # Make widgets ####################################
+
+        self.tabs = QTabWidget(parent=self)
+        self.setCentralWidget(self.tabs)
+
+        # Add tabs
+        self.adverts_tab = AdvertsTab(adverts_data, parent=self.tabs)
+        self.websites_tab = WebsitesTab(websites_data, parent=self.tabs)
+
+        self.tabs.addTab(self.adverts_tab, "Job Adverts")
+        self.tabs.addTab(self.websites_tab, "Search")
+
+        # Show ############################################
+
+        self.show()
