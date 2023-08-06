2dbaf1b3cb2bcd8744ca1e729ecebd857a73d1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:54 +0000

    Add package chucknorrispy/1.1/chucknorrispy-1.1.tar.gz

---
+++ packages/chucknorrispy/chucknorrispy-1.1.tar.gz/chucknorrispy-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='chucknorrispy',
+      version='1.1',
+      description='API Wrapper for the Chuck Norris API',
+      author='Mithun Balasubramanian',
+      author_email='sendmithunb@gmail.com',
+      url='https://github.com/iiAviator/ChuckNorrisPy',
+     )
