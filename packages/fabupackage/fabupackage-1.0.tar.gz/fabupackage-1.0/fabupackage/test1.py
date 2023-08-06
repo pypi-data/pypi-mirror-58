24ca1cf678ae72d6f7dc5eb770d21dd367788381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:46 +0000

    Add package fabupackage/1.0/fabupackage-1.0.tar.gz

---
+++ packages/fabupackage/fabupackage-1.0.tar.gz/fabupackage-1.0/fabupackage/test1.py
@@ -0,0 +1,6 @@
+import math
+a=4
+b=5
+c=math.sin(a)
+help(math)
+#print(c)
---
+++ packages/fabupackage/fabupackage-1.0.tar.gz/fabupackage-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='fabupackage',
+    version='1.0',
+    description='这是一个对外发布模块，里面有一个算法',
+    author='luohai',
+    author_email='1049599263@qq.com',
+    py_modules=['fabupackage.test1','fabupackage.test2']
+)
