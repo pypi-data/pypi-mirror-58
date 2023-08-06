0f035ff9ad6a70c5127676fff6c03b1647318785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:30 +0000

    Add package seqfold/0.3.2/seqfold-0.3.2.tar.gz

---
+++ packages/seqfold/seqfold-0.3.2.tar.gz/seqfold-0.3.2/setup.py
@@ -0,0 +1,36 @@
+import sys
+
+from pkg_resources import VersionConflict, require
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+setup(
+    name="seqfold",
+    version="0.3.2",
+    description="Predict the minimum free energy structure of nucleic acids",
+    author="JJTimmons",
+    author_email="jtimmons@latticeautomation.com",
+    license="mit",
+    packages=find_packages(exclude=["data"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lattice-Automation/seqfold",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    entry_points={"console_scripts": ["seqfold=seqfold.fold:run"],},
+)
