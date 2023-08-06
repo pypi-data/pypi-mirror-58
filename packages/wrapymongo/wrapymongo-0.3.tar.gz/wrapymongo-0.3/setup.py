948c1b679ffc6cebfadda2b3ae07b72da8a2779a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:15 +0000

    Add package wrapymongo/0.3/wrapymongo-0.3.tar.gz

---
+++ packages/wrapymongo/wrapymongo-0.3.tar.gz/wrapymongo-0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(name='wrapymongo',
+      version='0.3',
+      description='A wrapper for pymonogo driver',
+      url='http://github.com/shreyasgokhale/wrapymongo',
+      author='Shreyas Gokhale',
+      author_email='shreyasgokhale@gmail.com',
+      license='MIT',
+      long_description='Wrapper modlue to handle our mongodb requests using pymongodb driver',
+      keywords=["mongo", "mongodb", "pymongo", "mongodriver", "wrapymongo"],
+      packages=['wrapymongo'],
+      install_requires=[
+          'pymongo'
+      ],
+      download_url = 'https://github.com/shreyasgokhale/wrapymongo/archive/v_03.tar.gz',
+      classifiers=[
+          # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+          'Development Status :: 4 - Beta',
+          # Define that your audience are developers
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',   # Again, pick a license
+          # Specify which pyhton versions that you want to support
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+
+      zip_safe=False)
