a26da82840fb8db076ded16b1a42368f93c8f7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:56 +0000

    Add package section/1.0/section-1.0.tar.gz

---
+++ packages/section/section-1.0.tar.gz/section-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='section',
+    version='1.0',
+    packages=find_packages(),
+    author='Yan Xing',
+    author_email='',
+    keywords='',
+    description='hottopic knowledge section ',
+    license='',
+    url='',
+    include_package_data=True,
+    install_requires=[],
+)
