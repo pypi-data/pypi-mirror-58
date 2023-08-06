bc0fa9e3bbbe368a30a2725a2707846c8957f5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:31 +0000

    Add package icsdscraper/0.0.2/icsdscraper-0.0.2.tar.gz

---
+++ packages/icsdscraper/icsdscraper-0.0.2.tar.gz/icsdscraper-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="icsdscraper",
+    version="0.0.2",
+    author="Achilleas Papakonstantinou",
+    author_email="achipap@hotmail.com",
+    description="ICSD website scraper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CheatModeON/icsd-scraper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
