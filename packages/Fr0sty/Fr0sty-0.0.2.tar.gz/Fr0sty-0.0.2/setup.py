d11310a2fc5ecdb289966d7f268a221df8ec319e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:57 +0000

    Add package Fr0sty/0.0.2/Fr0sty-0.0.2.tar.gz

---
+++ packages/Fr0sty/Fr0sty-0.0.2.tar.gz/Fr0sty-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+	name='Fr0sty',
+	version='0.0.2',
+	author='aaaaa12138',
+	author_email='705027616@qq.com',
+	license='MIT License',
+	packages=find_packages(),
+	platforms=["all"],
+	install_requires=[
+	],
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: Implementation',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Topic :: Software Development :: Libraries'
+	],
+)
