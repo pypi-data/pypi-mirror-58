d356e4f058937da977c2fe2879808e0dfd9dc43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:26 +0000

    Add package ywkd-tools/1.0.15/ywkd_tools-1.0.15.tar.gz

---
+++ packages/ywkd-tools/ywkd_tools-1.0.15.tar.gz/ywkd_tools-1.0.15/ywkd_tools/utils.py
@@ -0,0 +1,15 @@
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+def get_request_id():
+    try:
+        request_id = local.request_id
+    except AttributeError:
+        request_id = NO_REQUEST_ID if NO_REQUEST_ID else uuid1().hex
+    return request_id
