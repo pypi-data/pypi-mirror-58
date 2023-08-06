dff2922b86554c1784f24bfd33c4890df2b2a4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package ctarn/0/ctarn-0.tar.gz

---
+++ packages/ctarn/ctarn-0.tar.gz/ctarn-0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='ctarn',
+    version='0',
+    description='Tarn Yeong Ching',
+    url='http://res.ctarn.io',
+    author='Tarn Yeong Ching',
+    author_email='i@ctarn.io',
+    license='Public Domain',
+    packages=setuptools.find_packages(exclude=('test',)),
+)
