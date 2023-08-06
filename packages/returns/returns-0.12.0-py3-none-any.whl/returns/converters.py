1ab871daca8c80ded8d76b90b6449d9b5c482ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:07 +0000

    Add package returns/0.12.0/returns-0.12.0-py3-none-any.whl

---
+++ packages/returns/returns-0.12.0-py3-none-any.whl/returns/converters.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+from typing import TypeVar, overload
+
+from returns._generated import coalesce  # noqa: WPS436
+from returns.io import IO
+from returns.maybe import Maybe
+from returns.result import Failure, Result, Success
+
+# Contianer internals:
+_ValueType = TypeVar('_ValueType')
+_ErrorType = TypeVar('_ErrorType')
+
+# Re-export from generated:
+coalesce_maybe = coalesce._coalesce_maybe  # noqa: WPS437
+coalesce_result = coalesce._coalesce_result  # noqa: WPS437
+
+
+def result_to_maybe(
+    result_container: Result[_ValueType, _ErrorType],
+) -> Maybe[_ValueType]:
+    """
+    Converts ``Result`` container to ``Maybe`` container.
+
+    .. code:: python
+
+      >>> from returns.maybe import Some, Nothing
+      >>> from returns.result import Failure, Success
+      >>> result_to_maybe(Success(1)) == Some(1)
+      True
+      >>> result_to_maybe(Failure(1)) == Nothing
+      True
+      >>> result_to_maybe(Success(None)) == Nothing
+      True
+
+    """
+    return Maybe.new(result_container.value_or(None))
+
+
+def maybe_to_result(
+    maybe_container: Maybe[_ValueType],
+) -> Result[_ValueType, None]:
+    """
+    Converts ``Maybe`` container to ``Result`` container.
+
+    .. code:: python
+
+      >>> from returns.maybe import Some, Nothing
+      >>> from returns.result import Failure, Success
+      >>> maybe_to_result(Nothing) == Failure(None)
+      True
+      >>> maybe_to_result(Some(1)) == Success(1)
+      True
+      >>> maybe_to_result(Some(None)) == Failure(None)
+      True
+
+    """
+    inner_value = maybe_container.value_or(None)
+    if inner_value is not None:
+        return Success(inner_value)
+    return Failure(inner_value)
+
+
+@overload
+def join(container: IO[IO[_ValueType]]) -> IO[_ValueType]:
+    """Case for ``IO`` container."""
+
+
+@overload
+def join(container: Maybe[Maybe[_ValueType]]) -> Maybe[_ValueType]:
+    """Case for ``Maybe`` container."""
+
+
+@overload
+def join(
+    container: Result[Result[_ValueType, _ErrorType], _ErrorType],
+) -> Result[_ValueType, _ErrorType]:
+    """Case for ``Result`` container."""
+
+
+def join(container):
+    """
+    Joins two nested containers together.
+
+    .. code:: python
+
+      >>> from returns.maybe import Some
+      >>> from returns.result import Success
+      >>> from returns.io import IO
+      >>> join(IO(IO(1))) == IO(1)
+      True
+      >>> join(Some(Some(1))) == Some(1)
+      True
+      >>> join(Success(Success(1))) == Success(1)
+      True
+
+    """
+    return container._inner_value  # noqa: WPS437
---
+++ packages/returns/returns-0.12.0-py3-none-any.whl/returns/primitives/container.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta
+from typing import Any, Callable, NoReturn, TypeVar, Union
+
+from typing_extensions import Protocol, runtime
+
+from returns.primitives.exceptions import ImmutableStateError
+
+_ValueType = TypeVar('_ValueType', covariant=True)
+_NewValueType = TypeVar('_NewValueType')
+_ErrorType = TypeVar('_ErrorType', covariant=True)
+_NewErrorType = TypeVar('_NewErrorType')
+
+
+class BaseContainer(object, metaclass=ABCMeta):
+    """Utility class to provide all needed magic methods to the context."""
+
+    __slots__ = ('_inner_value',)
+    _inner_value: Any
+
+    def __init__(self, inner_value) -> None:
+        """
+        Wraps the given value in the Container.
+
+        'value' is any arbitrary value of any type including functions.
+        """
+        object.__setattr__(self, '_inner_value', inner_value)  # noqa: WPS609
+
+    def __setattr__(self, attr_name: str, attr_value: Any) -> NoReturn:
+        """Makes inner state of the containers immutable."""
+        raise ImmutableStateError()
+
+    def __delattr__(self, attr_name: str) -> NoReturn:  # noqa: WPS603
+        """Makes inner state of the containers immutable."""
+        raise ImmutableStateError()
+
+    def __str__(self) -> str:
+        """Converts to string."""
+        return '<{0}: {1}>'.format(
+            self.__class__.__qualname__.strip('_'),
+            str(self._inner_value),
+        )
+
+    def __eq__(self, other: Any) -> bool:
+        """Used to compare two 'Container' objects."""
+        if not isinstance(self, type(other)):
+            return False
+        return self._inner_value == other._inner_value  # noqa: WPS437
+
+    def __hash__(self) -> int:
+        """Used to use this value as a key."""
+        return hash(self._inner_value)
+
+    def __getstate__(self) -> Any:
+        """That's how this object will be pickled."""
+        return self._inner_value
+
+    def __setstate__(self, state: Any) -> None:
+        """Loading state from pickled data."""
+        object.__setattr__(self, '_inner_value', state)  # noqa: WPS609
+
+
+@runtime
+class Bindable(Protocol[_ValueType]):
+    """
+    Represents a "context" in which calculations can be executed.
+
+    ``Bindable`` allows you to bind together
+    a series of calculations while maintaining
+    the context of that specific container.
+    """
+
+    def bind(
+        self, function: Callable[[_ValueType], 'Bindable[_NewValueType]'],
+    ) -> 'Bindable[_NewValueType]':
+        """
+        Applies 'function' to the result of a previous calculation.
+
+        And returns a new container.
+        Works for containers that represent success.
+        Is the opposite of :meth:`Rescueable.rescue`.
+        """
+
+
+@runtime
+class Mappable(Protocol[_ValueType]):
+    """
+    Allows to chain wrapped values with regular functions.
+
+    Behaves like functor.
+    """
+
+    def map(  # noqa: A003
+        self, function: Callable[[_ValueType], _NewValueType],
+    ) -> 'Mappable[_NewValueType]':
+        """
+        Applies 'function' to the contents of the functor.
+
+        And returns a new functor value.
+        Is the opposite of :meth:`Fixable.fix`.
+        """
+
+
+@runtime
+class Fixable(Protocol[_ValueType, _ErrorType]):
+    """Represents containers that can be fixed and rescued."""
+
+    def fix(
+        self, function: Callable[[_ErrorType], _NewValueType],
+    ) -> 'Fixable[_NewValueType, _ErrorType]':
+        """
+        Applies 'function' to the error and transforms failure to success.
+
+        And returns a new functor value.
+        Works for containers that represent failure.
+        Is the opposite of :meth:`Mappable.map`.
+        """
+
+
+@runtime
+class Rescueable(Protocol[_NewValueType, _ErrorType]):
+    """
+    Represents a "context" in which calculations can be executed.
+
+    ``Rescueable`` allows you to bind together
+    a series of calculations while maintaining
+    the context of that specific container.
+    """
+
+    def rescue(
+        self,
+        function: Callable[
+            [_ErrorType], 'Rescueable[_NewValueType, _NewErrorType]',
+        ],
+    ) -> 'Rescueable[_NewValueType, _NewErrorType]':
+        """
+        Applies 'function' to the result of a previous calculation.
+
+        And returns a new container.
+        Works for containers that represent failure.
+        Is the opposite of :meth:`~bind`.
+        """
+
+
+@runtime
+class Unwrapable(Protocol[_ValueType]):
+    """Represents containers that can unwrap and return its wrapped value."""
+
+    def value_or(
+        self, default_value: _NewValueType,
+    ) -> Union[_ValueType, _NewValueType]:
+        """Forces to unwrap value from container or return a default."""
+
+    def unwrap(self) -> _ValueType:
+        """
+        Custom magic method to unwrap inner value from container.
+
+        Should be redefined for ones that actually have values.
+        And for ones that raise an exception for no values.
+
+        This method is the opposite of :meth:`~failure`.
+        """
+
+    def failure(self) -> _ErrorType:
+        """
+        Custom magic method to unwrap inner value from the failed container.
+
+        This method is the opposite of :meth:`~unwrap`.
+        """
+
+
+@runtime
+class UnwrapableFailure(Protocol[_ValueType, _ErrorType]):
+    """Allows to unwrap failures."""
+
+    def alt(
+        self,
+        function: Callable[[_ErrorType], _NewErrorType],
+    ) -> 'Fixable[_ValueType, _NewErrorType]':
+        """
+        Uses 'function' to transform one error to another.
+
+        And returns a new functor value.
+        Works for containers that represent failure.
+        Is the opposite of :meth:`~map`.
+        """
