e5ac9dd45c4cc17a6eacc46c0216f61e406c6b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:03 +0000

    Add package bitcoind-mock/0.0.1/bitcoind_mock-0.0.1.tar.gz

---
+++ packages/bitcoind-mock/bitcoind_mock-0.0.1.tar.gz/bitcoind_mock-0.0.1/bitcoind_mock/conf.py
@@ -0,0 +1,14 @@
+# bitcoind
+BTC_RPC_USER = "user"
+BTC_RPC_PASSWD = "passwd"
+BTC_RPC_HOST = "localhost"
+BTC_RPC_PORT = 18443
+
+# ZMQ
+FEED_PROTOCOL = "tcp"
+FEED_ADDR = "127.0.0.1"
+FEED_PORT = 28332
+
+# General
+TIME_BETWEEN_BLOCKS = 5
+TX_PER_BLOCK = 2500
