3f379c0a44c299a5add339df679141b6bfba4df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:33 +0000

    Add package data_process_hupo/0.0.1/data_process_hupo-0.0.1.tar.gz

---
+++ packages/data_process_hupo/data_process_hupo-0.0.1.tar.gz/data_process_hupo-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='data_process_hupo',
+    version="0.0.1",
+    description="this is data process model",
+    author="poge",
+    author_email="hupo4210@163.com",
+    license="MIT",
+    packages=["data_process_hupo"],
+    zip_safe=False
+)
