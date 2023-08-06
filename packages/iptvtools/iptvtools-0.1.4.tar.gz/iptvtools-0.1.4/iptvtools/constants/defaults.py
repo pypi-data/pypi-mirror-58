e3cc267668d40298e65903fcfee4a079503c4620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:11 +0000

    Add package iptvtools/0.1.4/iptvtools-0.1.4.tar.gz

---
+++ packages/iptvtools/iptvtools-0.1.4.tar.gz/iptvtools-0.1.4/iptvtools/constants/defaults.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: constants.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: Defaults for iptvtools.
+"""
+CONFIG = 'config.json'
+INPUT = 'https://iptv-org.github.io/iptv/index.m3u'
+MIN_HEIGHT = 1080
+OUTPUT = 'iptvtools.m3u'
+TIMEOUT = 10
+UDPXY = None
