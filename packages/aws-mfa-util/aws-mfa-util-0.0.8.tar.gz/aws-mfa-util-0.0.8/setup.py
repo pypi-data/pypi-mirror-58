c895a191d8294d96d8eda024c7474073bc2f1a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:58 +0000

    Add package aws-mfa-util/0.0.8/aws-mfa-util-0.0.8.tar.gz

---
+++ packages/aws-mfa-util/aws-mfa-util-0.0.8.tar.gz/aws-mfa-util-0.0.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name='aws-mfa-util',
+    version='0.0.8',
+    author='hiroya akita',
+    author_email='akky.develop@gmail.com',
+    description='AWS CLIにMFA適用アカウントを作成・更新するためのツール',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['boto3', 'PyInquirer'],
+    url='https://github.com/papi-tokei/aws-mfa-util',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts':[
+            'aws-mfa-util=aws_mfa_util:main'
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/aws-mfa-util/aws-mfa-util-0.0.8.tar.gz/aws-mfa-util-0.0.8/tests/test_main.py
@@ -0,0 +1,10 @@
+from aws_mfa_util.util import make_credential_str_list, convert_aws_credential
+
+
+def test_make_credential_str_list():
+    print('hello')
+    # with open('./sample_credentials', mode='r') as fd:
+    #     print('OK')
+    # tmp = make_credential_str_list('./sample_credentials')
+    # print(tmp)
+    assert True
