eeac80d0e5e08bb06c8dd1d8f6993e0094cd97f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:11 +0000

    Add package check-type/0.0.1/check_type-0.0.1-py3-none-any.whl

---
+++ packages/check-type/check_type-0.0.1-py3-none-any.whl/check_type/check_type.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 12:14:40 2020
+"""
+import sys
+import typeguard
+
+from typing import Callable, get_type_hints
+
+def check_type(func: Callable):
+    """
+    Runtime checking of types of the input arguments of ``func``.
+
+    Parameters
+    ----------
+    func : Callable
+        The function that you would like to have type checked.
+
+    Examples
+    --------
+    1. Inside a class method
+
+    .. code-block:: python
+
+        import check_type
+        class MyType:
+            def __init__(self, data: int):
+                check_type.check_type(self.__init__)
+                self.data = data
+
+        mt = MyType(3)
+
+    2. Inside a funciton
+
+    .. code-block:: python
+
+        import check_type
+        def func1(var: float):
+            check_type.check_type(func1)
+            print(var)
+    """
+    assert isinstance(func, Callable), "`func` must be a function."
+    local_vars = sys._getframe(1).f_locals
+    type_hints = get_type_hints(func)
+    for var_name, expected_type in type_hints.items():
+        typeguard.check_type(var_name, local_vars[var_name], expected_type)
