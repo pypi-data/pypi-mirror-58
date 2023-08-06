49330eb2898ec7a9f93afd50dd7206df7c672997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:56 +0000

    Add package loggly-python-handler/1.0.1/loggly_python_handler-1.0.1-py3-none-any.whl

---
+++ packages/loggly-python-handler/loggly_python_handler-1.0.1-py3-none-any.whl/loggly/handlers.py
@@ -0,0 +1,40 @@
+import logging
+import logging.handlers
+
+import traceback
+
+from requests_futures.sessions import FuturesSession
+
+session = FuturesSession()
+
+
+def response_callback(resp, *args, **kwargs):
+    """ Don't do anything with the response """
+    pass
+
+
+session.hooks['response'] = response_callback
+
+
+class HTTPSHandler(logging.Handler):
+    def __init__(self, url, fqdn=False, localname=None, facility=None):
+        logging.Handler.__init__(self)
+        self.url = url
+        self.fqdn = fqdn
+        self.localname = localname
+        self.facility = facility
+
+    def get_full_message(self, record):
+        if record.exc_info:
+            return '\n'.join(traceback.format_exception(*record.exc_info))
+        else:
+            return record.getMessage()
+
+    def emit(self, record):
+        try:
+            payload = self.format(record)
+            session.post(self.url, data=payload.encode("utf-8"))
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
