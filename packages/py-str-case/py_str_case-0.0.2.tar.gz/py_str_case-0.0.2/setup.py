a489bc6046db44b809979f225573296044d4c973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package py-str-case/0.0.2/py_str_case-0.0.2.tar.gz

---
+++ packages/py-str-case/py_str_case-0.0.2.tar.gz/py_str_case-0.0.2/setup.py
@@ -0,0 +1,29 @@
+# from distutils.core import setup
+from setuptools import setup
+import os
+
+
+rootdir = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(rootdir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="py_str_case",
+    version="0.0.2",
+    author="shiva",
+    author_email="shivashanker.chagamreddy@gmail.com",
+    description="String case converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShivaShankerReddy/py_string_case",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts= ['py_str_case.py'],
+    python_requires='>=3.0',
+)
