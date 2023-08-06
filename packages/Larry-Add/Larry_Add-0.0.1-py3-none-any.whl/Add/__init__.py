22f002f12965fcf914d15476c4d4eafa376299a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:05 +0000

    Add package Larry-Add/0.0.1/Larry_Add-0.0.1-py3-none-any.whl

---
+++ packages/Larry-Add/Larry_Add-0.0.1-py3-none-any.whl/Add/__init__.py
@@ -0,0 +1,10 @@
+class Animalia:
+    def __init__(self , x , y , z ):
+        self.name = y
+        self.location = z
+        self.example = x
+
+
+    def move(self , b , c ):
+        a = b + c
+        print(a)
