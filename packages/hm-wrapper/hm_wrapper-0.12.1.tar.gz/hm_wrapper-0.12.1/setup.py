3616142df6a07580f90ffd02478a0051309ca8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:15 +0000

    Add package hm-wrapper/0.12.1/hm_wrapper-0.12.1.tar.gz

---
+++ packages/hm-wrapper/hm_wrapper-0.12.1.tar.gz/hm_wrapper-0.12.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='hm_wrapper',
+    version='0.12.1',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    python_requires='>=3.6',
+    url='https://github.com/np-at/hm_wrapper',
+    license='GPLv3',
+    author='npraskins',
+    author_email='',
+    description=''
+)
