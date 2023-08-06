75946e1f84a89910a0c51a8bbb57cb6fc08aabba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package tinvest/1.0.8/tinvest-1.0.8-py3-none-any.whl

---
+++ packages/tinvest/tinvest-1.0.8-py3-none-any.whl/tinvest/async_client.py
@@ -0,0 +1,39 @@
+from contextlib import asynccontextmanager
+from functools import partial
+from typing import Any
+
+from aiohttp import ClientResponse, ClientSession
+
+from .base_client import BaseClient
+from .shemas import Error
+from .utils import set_default_headers
+
+
+class AsyncClient(BaseClient[ClientSession]):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._session is None:
+            self._session = ClientSession(raise_for_status=True)
+
+    @asynccontextmanager
+    async def request(self, method: str, path: str, response_model=None, **kwargs):
+        url = self._api + path
+        set_default_headers(kwargs, self._token)
+
+        async with self.session.request(method, url, **kwargs) as response:
+            setattr(
+                response, 'parse_json', partial(_parse_json, response, response_model),
+            )
+            setattr(response, 'parse_error', partial(_parse_json, response, Error))
+            yield response
+
+    async def close(self) -> None:
+        await self.session.close()
+
+
+async def _parse_json(
+    response: ClientResponse, response_model: Any = None, **kwargs: Any
+) -> Any:
+    if response_model is None:
+        return await response.json(**kwargs)
+    return response_model.parse_obj(await response.json(**kwargs))
---
+++ packages/tinvest/tinvest-1.0.8-py3-none-any.whl/tinvest/constants.py
@@ -0,0 +1,2 @@
+PRODUCTION = 'https://api-invest.tinkoff.ru/openapi'
+STREAMING = 'wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws'
