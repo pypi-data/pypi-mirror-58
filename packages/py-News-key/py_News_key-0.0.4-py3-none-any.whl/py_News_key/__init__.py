84b869f44e9a79487f875dc1261010ff79a4341c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:35 +0000

    Add package py-News-key/0.0.4/py_News_key-0.0.4-py3-none-any.whl

---
+++ packages/py-News-key/py_News_key-0.0.4-py3-none-any.whl/py_News_key/__init__.py
@@ -0,0 +1,13 @@
+#-*-coding:utf 8-*-
+
+version = "0.0.4"
+data = {
+    "hello": [
+        "1234",
+        "\u8fd9\u662f\u5907\u6ce8\u4fe1\u606f"
+    ],
+    "hut": [
+        "u897",
+        "\u5907\u6ce8\u8d26\u53f7\u4f7f\u7528\u4eba\u7684\u4fe1\u606f"
+    ]
+}
