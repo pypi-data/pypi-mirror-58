56c102af6497c203bac38b34841fe3936c55efae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:24 +0000

    Add package pymysql-pool/0.3.4/pymysql-pool-0.3.4.tar.gz

---
+++ packages/pymysql-pool/pymysql-pool-0.3.4.tar.gz/pymysql-pool-0.3.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+readme = 'README.md'
+setup(
+    name="pymysql-pool",
+    version="0.3.4",
+    url="https://github.com/jkklee/pymysql-pool",
+    author="ljk",
+    py_modules=['pymysqlpool'],
+    license="GPLv3",
+    author_email="chaoyuemyself@hotmail.com",
+    description="MySQL connection pool based pymysql",
+    long_description=open(readme, encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.4",
+    install_requires=['pymysql>=0.7.10'],
+    keywords=[
+        'pymysql pool',
+        'mysql connection pool',
+        'mysql multi threads'
+    ]
+)
