a183d94b8b967d84bef2e7e0e7aaf0d69e4fcc9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:30 +0000

    Add package pylogHandler/0.1.2/pylogHandler-0.1.2.tar.gz

---
+++ packages/pylogHandler/pylogHandler-0.1.2.tar.gz/pylogHandler-0.1.2/setup.py
@@ -0,0 +1,27 @@
+#############################################
+# File Name: setup.py
+# Author: renoyuan
+# Mail: renoyuan@foxmail.com
+# Created Time:  2019-12-30 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pylogHandler",
+    version="0.1.2",
+    # keywords=("pip", "pathtool", "timetool", "magetool", "mage"),
+    description="renoyuan log",
+    long_description="renoyuan log",
+    license="MIT Licence",
+
+    url="https://github.com/yhyPython110/pylogHandler.git",
+    author="renoyuan",
+    author_email="renoyuan@foxmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    # install_requires=[]
+)
