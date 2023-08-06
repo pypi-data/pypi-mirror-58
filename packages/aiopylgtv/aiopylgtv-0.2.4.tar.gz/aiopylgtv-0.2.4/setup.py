88fe246ee1273700efc739043d9a59f05d10aaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:13 +0000

    Add package aiopylgtv/0.2.4/aiopylgtv-0.2.4.tar.gz

---
+++ packages/aiopylgtv/aiopylgtv-0.2.4.tar.gz/aiopylgtv-0.2.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+      name = 'aiopylgtv',
+      packages = ['aiopylgtv'],
+      install_requires = ['websockets', 'asyncio', 'numpy'],
+      zip_safe = True,
+      version = '0.2.4',
+      description = 'Library to control webOS based LG Tv devices',
+      author = 'Josh Bendavid',
+      author_email = 'joshbendavid@gmail.com',
+      url = 'https://github.com/bendavid/aiopylgtv',
+      download_url = 'https://github.com/bendavid/aiopylgtv/archive/0.2.4.tar.gz',
+      keywords = ['webos', 'tv'],
+      classifiers = [],
+      entry_points={
+        'console_scripts': [
+            'aiopylgtvcommand=aiopylgtv.utils:aiopylgtvcommand',
+        ],
+      },
+)
