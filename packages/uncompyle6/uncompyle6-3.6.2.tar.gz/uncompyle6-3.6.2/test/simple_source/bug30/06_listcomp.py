3b43856fb643abbc256803061a305b723b51788e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:05 +0000

    Add package uncompyle6/3.6.2/uncompyle6-3.6.2.tar.gz

---
+++ packages/uncompyle6/uncompyle6-3.6.2.tar.gz/uncompyle6-3.6.2/test/simple_source/bug30/06_listcomp.py
@@ -0,0 +1,32 @@
+# Python 3.0 comprehensions can produce different code from
+# all other Python versions. Thanks, Python!
+
+# This code is RUNNABLE!
+
+# Adapted from 3.0 ast.py; uses comprehension implemented via CLOSURE
+def _format(node):
+    return [(a, int(b)) for a, b in node.items()]
+
+x = {'a': '1', 'b': '2'}
+assert [('a', 1), ('b', 2)] == _format(x)
+
+# Adapted from 3.0 cmd.py; ises "if" comprehension
+def monthrange(ary, dotext):
+    return [a[3:] for a in ary if a.startswith(dotext)]
+
+ary = ["Monday", "Twoday", "Monmonth"]
+assert ['day', 'month'] == monthrange(ary, "Mon")
+
+# From 3.0 cmd.py; uses "if not" comprehension
+def columnize(l):
+    return [i for i in range(len(l))
+            if not isinstance(l[i], str)]
+assert [0, 2] == columnize([1, 'a', 2])
+
+# From 3.7 test_generators
+# Bug was in handling the way list_if is optimized in 3.7+;
+# We need list_if37 and compare_chained37.
+def init_board(c):
+    return [io for io in c if 3 <= io < 5]
+
+assert init_board(list(range(6))) == [3, 4]
---
+++ packages/uncompyle6/uncompyle6-3.6.2.tar.gz/uncompyle6-3.6.2/test/simple_source/bug35/04_importlist.py
@@ -0,0 +1,16 @@
+# From 3.7 test_cmath.py
+# Had bug in 3.x in not having semantic importlist rule
+# bug is treating "import as" as "from xx import" while
+# still being able to hand "from xx import" properly
+
+# RUNNABLE!
+import os.path as osp
+from sys import path
+from os import sep, name
+import collections.abc
+
+assert osp.basename("a") == "a"
+assert path
+assert sep
+assert name
+assert collections.abc
