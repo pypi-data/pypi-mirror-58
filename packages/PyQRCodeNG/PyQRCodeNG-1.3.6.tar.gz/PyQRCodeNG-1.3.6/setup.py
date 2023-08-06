e93cca59eee8e40fe6464542ea670d2cc85d7f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:06 +0000

    Add package PyQRCodeNG/1.3.6/PyQRCodeNG-1.3.6.tar.gz

---
+++ packages/PyQRCodeNG/PyQRCodeNG-1.3.6.tar.gz/PyQRCodeNG-1.3.6/setup.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2013, Michael Nooner
+# Copyright (c) 2018 - 2020, Lars Heuer
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the names of its 
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from setuptools import setup
+import os
+import re
+import io
+
+
+def read(*filenames, **kwargs):
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(os.path.join(base_path, filename), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+version = re.search(r'''^__version__ = ["']([^'"]+)['"]''',
+                    read('pyqrcodeng/__init__.py'), flags=re.MULTILINE).group(1)
+
+
+setup(
+    name='PyQRCodeNG',
+    packages=['pyqrcodeng'],
+    version=version,
+    long_description=read('README.rst', 'CHANGES.rst'),
+    description='PyQRCode New Generation -- A QR code generator written purely in Python with SVG, EPS, PNG and terminal output.',
+    author='Lars Heuer',
+    author_email='heuer@semagia.com',
+    url='https://github.com/pyqrcode/pyqrcodeNG',
+    keywords=['qrcode', 'qr'],
+    license='BSD',
+    extras_require={
+      'PNG':  ['pypng>=0.0.13'],
+    },
+    entry_points = {'console_scripts': ['pyqr = pyqrcodeng.cli:main']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        ],
+)
