601667d468ef495431a1f3adb89fa7ae667895ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:03 +0000

    Add package infi.run-as/0.1.12/infi.run_as-0.1.12-py3-none-any.whl

---
+++ packages/infi.run-as/infi.run_as-0.1.12-py3-none-any.whl/infi/run_as/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.12"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.12)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '07241b0c39be63c3197648235244cc8b631fcce9'
+__git_head_subject__ = 'Finished Release v0.1.12'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2019-12-23 15:06:43'
