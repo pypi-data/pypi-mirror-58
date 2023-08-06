b6e17666c063da5be4ca419e21001cba13e712d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:59 +0000

    Add package udata/1.6.20.dev16901/udata-1.6.20.dev16901-py2.py3-none-any.whl

---
+++ packages/udata/udata-1.6.20.dev16901-py2.py3-none-any.whl/udata/static/site.f0f725d8f0284694259f.js
@@ -0,0 +1,2 @@
+webpackJsonp([44],{0:function(e,t,u){"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}u(975);var i=u(80),n=_interopRequireDefault(i),r=u(5),a=_interopRequireDefault(r),f=u(31),l=_interopRequireDefault(f);new l.default({mixins:[n.default],ready:function(){a.default.debug("Site page")}})},975:980});
+//# sourceMappingURL=site.f0f725d8f0284694259f.js.map
