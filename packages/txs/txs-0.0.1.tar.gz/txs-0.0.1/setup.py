845a5af521a7eb4fb675a2ddd95a0dcd76578cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:02 +0000

    Add package txs/0.0.1/txs-0.0.1.tar.gz

---
+++ packages/txs/txs-0.0.1.tar.gz/txs-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import txs
+
+setup(
+    name=txs.__name__,
+    version=txs.__version__,
+    author=txs.__author__,
+    author_email=txs.__author_email__,
+    description=('Wrapper around ffmpeg and mpv that generates '
+                 'x264 test encodes and compares them'),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/plotski/txs',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=[],
+    data_files=[('share/txs/lua', ['txs-compare.lua'])],
+    entry_points='''
+        [console_scripts]
+        txs=txs.main:run
+    ''',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    ],
+    python_requires='>=3.6',
+)
