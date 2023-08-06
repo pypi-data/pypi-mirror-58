69b7a3522a903d38a817553970e294afed08d5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:40 +0000

    Add package origo-sdk-python/0.0.8/origo-sdk-python-0.0.8.tar.gz

---
+++ packages/origo-sdk-python/origo-sdk-python-0.0.8.tar.gz/origo-sdk-python-0.0.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="origo-sdk-python",
+    version="0.0.8",
+    author="Oslo Origo",
+    author_email="dataplattform@oslo.kommune.no",
+    description="SDK for origo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.oslo.kommune.no/origo-dataplatform/origo-sdk-python",
+    packages=setuptools.find_packages(".", exclude=["tests*"]),
+    install_requires=[
+        "requests",
+        "urllib3",
+        "PrettyTable",
+        "python-keycloak-client",
+        "PyJWT",
+        "jsonschema",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
