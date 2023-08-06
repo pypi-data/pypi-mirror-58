56e9ede4c88a23d85399ec5c91583d1d01b29200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:29 +0000

    Add package mautrix-telegram/0.7.0/mautrix_telegram-0.7.0-py3-none-any.whl

---
+++ packages/mautrix-telegram/mautrix_telegram-0.7.0-py3-none-any.whl/mautrix_telegram/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.7.0'
+git_revision = 'a9abf9a1'
+version = '0.7.0'
+linkified_version = '[0.7.0](https://github.com/tulir/mautrix-telegram/releases/tag/v0.7.0)'
