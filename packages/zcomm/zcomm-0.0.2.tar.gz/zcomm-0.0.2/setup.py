c7dd1ea342d9a07bd9ce1c64f0c1964ba67e9646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:03 +0000

    Add package zcomm/0.0.2/zcomm-0.0.2.tar.gz

---
+++ packages/zcomm/zcomm-0.0.2.tar.gz/zcomm-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zcomm',
+    version='0.0.2',
+    packages=['zcomm'],
+    url='https://github.com/Lamden/zcomm',
+    license='GPL3',
+    author='Stuart Farmer',
+    author_email='stuart@lamden.io',
+    description='PyZMQ structures and objects to help write microservices.',
+    install_requires=['zmq']
+)
