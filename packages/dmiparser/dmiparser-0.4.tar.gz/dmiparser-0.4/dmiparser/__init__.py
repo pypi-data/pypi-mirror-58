40468d4e93c11d442e2691add281279956ca2a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:42 +0000

    Add package dmiparser/0.4/dmiparser-0.4.tar.gz

---
+++ packages/dmiparser/dmiparser-0.4.tar.gz/dmiparser-0.4/dmiparser/__init__.py
@@ -0,0 +1,5 @@
+from dmiparser.dmiparser import DmiParser
+
+__version__ = '0.4'
+__all__ = ['DmiParser']
+
---
+++ packages/dmiparser/dmiparser-0.4.tar.gz/dmiparser-0.4/tests/test_dmidecode_13.py
@@ -0,0 +1,24 @@
+import json
+from pathlib import Path
+from dmiparser import DmiParser
+
+RDIR = Path(Path(__file__).resolve()).parents[0]
+
+def test_dmidecode_13():
+    testnum = 0
+
+    with open(RDIR / 'dmidecode_13.txt', 'rt') as f:
+        text = f.read()
+        data = json.loads(str(DmiParser(text)))
+
+    for d in data:
+        '''
+        Installable Languages: 1
+                enUS
+        '''
+        if '0x000C' == d['handle']['id'] and 'BIOS Language Information' == d['name']:
+            assert(2 == len(d['props']['Installable Languages']['values']))
+            testnum += 1
+
+    assert(1 == testnum)
+
