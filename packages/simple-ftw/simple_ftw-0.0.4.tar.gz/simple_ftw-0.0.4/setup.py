de858513132e871daf4bfdc9ec1c721911044681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:36 +0000

    Add package simple-ftw/0.0.4/simple_ftw-0.0.4.tar.gz

---
+++ packages/simple-ftw/simple_ftw-0.0.4.tar.gz/simple_ftw-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="simple_ftw",
+    version="0.0.4",
+    author="Fiona Tahta-Wraith",
+    author_email="fionatw@outlook.com",
+    description="my first published package",
+    long_description=long_description,
+    loan_description_content_type="text/markdown",
+    url="https://github.com/fiona-tw/ftw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
