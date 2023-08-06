c9b868aae0dcc30cf7e9da8be40dc8d83ea12729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:58 +0000

    Add package dw8000-wav2syx-christofmuc/1.0.3/dw8000_wav2syx-christofmuc-1.0.3.tar.gz

---
+++ packages/dw8000-wav2syx-christofmuc/dw8000_wav2syx-christofmuc-1.0.3.tar.gz/dw8000_wav2syx-christofmuc-1.0.3/dw8000_wav2syx/dw8000_bin2syx.py
@@ -0,0 +1,24 @@
+#
+#  Copyright (c) 2019 Christof Ruch. All rights reserved.
+#
+#  Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+#
+
+import argparse
+from dw8000_wav2syx.dw8000_reverse_engineer import remap_tape_data_to_syx
+
+
+def bin2syx():
+    parser = argparse.ArgumentParser(description='Convert a Korg DW8000 bin file converted from tape into syx format')
+    parser.add_argument('binfile')
+    parser.add_argument('syxfile')
+    parser.add_argument('--verbose', type=bool, default=False)
+    parser.add_argument('--store', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    remap_tape_data_to_syx(tapefile=args.binfile, syxfile=args.syxfile, verbose=args.verbose, store=args.store)
+
+
+if __name__ == '__main__':
+    bin2syx()
