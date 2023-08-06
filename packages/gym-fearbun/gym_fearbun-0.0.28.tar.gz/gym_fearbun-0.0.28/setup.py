1125859d5b85d7d5a1b48b6bcec90b2ff3a2a65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:25 +0000

    Add package gym-fearbun/0.0.28/gym_fearbun-0.0.28.tar.gz

---
+++ packages/gym-fearbun/gym_fearbun-0.0.28.tar.gz/gym_fearbun-0.0.28/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setuptools
+
+setup(name='gym_fearbun',
+      version='0.0.28',
+      install_requires=['gym', 'six'],
+      url='https://github.com/rolypolyvg295/gym_fearbun/tree/master/gym_fearbun',
+      include_package_data=True,
+      packages=setuptools.find_packages(),
+      package_data={'gym_fearbun/maps': ['*.txt']},
+      license='MIT'
+)
