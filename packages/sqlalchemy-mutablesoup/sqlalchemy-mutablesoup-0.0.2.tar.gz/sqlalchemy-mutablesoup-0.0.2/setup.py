17c9cde0e19890f9285331dc4848e9205603afa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:07 +0000

    Add package sqlalchemy-mutablesoup/0.0.2/sqlalchemy-mutablesoup-0.0.2.tar.gz

---
+++ packages/sqlalchemy-mutablesoup/sqlalchemy-mutablesoup-0.0.2.tar.gz/sqlalchemy-mutablesoup-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sqlalchemy-mutablesoup",
+    version="0.0.2",
+    author="Dillon Bowen",
+    author_email="dsbowen@example.com",
+    description="Mutable BeautifulSoup database type",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dsbowen/sqlalchemy-mutablesoup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
