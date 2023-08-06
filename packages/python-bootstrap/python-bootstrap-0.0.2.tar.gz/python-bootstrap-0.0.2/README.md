7f2083be052fc018d50b3cf025ac1f01f223d0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:00 +0000

    Add package python-bootstrap/0.0.2/python-bootstrap-0.0.2.tar.gz

---
+++ packages/python-bootstrap/python-bootstrap-0.0.2.tar.gz/python-bootstrap-0.0.2/README.md
@@ -0,0 +1,1 @@
+# Test clean python structure
---
+++ packages/python-bootstrap/python-bootstrap-0.0.2.tar.gz/python-bootstrap-0.0.2/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import io
+import sys
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_require = []
+with io.open('requirements.txt') as f:
+    install_require = [l.strip() for l in f if not l.startswith('#')]
+
+
+needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+test_require = []
+with io.open('requirements-dev.txt') as f:
+    test_require = [l.strip() for l in f if not l.startswith('#')]
+
+
+setuptools.setup(
+    name="python-bootstrap",
+    version="0.0.2",
+    author="Alex Shemshurenko",
+    author_email="alexshe@wix.com",
+    description="python-bootstrap package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wix-private/hdc3/tree/master/apps-py/wixfra",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    tests_require=test_require,
+    setup_requires=pytest_runner,
+    install_requires=install_require
+)
+
+
+
