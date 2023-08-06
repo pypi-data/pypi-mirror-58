63bb2465568e686d56a124641414b51208eb31d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package heroqsx-distributions/0.1/heroqsx_distributions-0.1.tar.gz

---
+++ packages/heroqsx-distributions/heroqsx_distributions-0.1.tar.gz/heroqsx_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='heroqsx_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['heroqsx_distributions'],
+      author='Gebing Qiu',
+      author_email='1017809810@qq.com',
+      zip_safe=False)
