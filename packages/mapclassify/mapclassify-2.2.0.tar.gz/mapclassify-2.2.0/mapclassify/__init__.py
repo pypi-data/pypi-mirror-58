f7e4e3d948160960899230e0a6e69e50664c7140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:25 +0000

    Add package mapclassify/2.2.0/mapclassify-2.2.0.tar.gz

---
+++ packages/mapclassify/mapclassify-2.2.0.tar.gz/mapclassify-2.2.0/mapclassify/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "2.2.0"
+# __version__ has to be defined in the first line
+
+from .classifiers import (
+    BoxPlot,
+    EqualInterval,
+    FisherJenks,
+    FisherJenksSampled,
+    HeadTailBreaks,
+    JenksCaspall,
+    JenksCaspallForced,
+    JenksCaspallSampled,
+    MaxP,
+    MaximumBreaks,
+    NaturalBreaks,
+    Quantiles,
+    Percentiles,
+    StdMean,
+    UserDefined,
+    load_example,
+    gadf,
+    KClassifiers,
+    CLASSIFIERS,
+)
