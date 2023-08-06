f57c43d01ecc158db040a1c9ffeed78dd341324a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:05 +0000

    Add package PyHDFS/0.3.1/PyHDFS-0.3.1.tar.gz

---
+++ packages/PyHDFS/PyHDFS-0.3.1.tar.gz/PyHDFS-0.3.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+import re
+
+from setuptools import setup
+
+# Leaving this here because using "attr: ..." in the config file requires the package to be
+# importable, which might not be the case due to missing dependencies.
+with open(os.path.join(os.path.dirname(__file__), 'pyhdfs', '__init__.py')) as py:
+    version_match = re.search(r"__version__ = '(.+?)'", py.read())
+    assert version_match
+    version = version_match.group(1)
+
+setup(version=version)
