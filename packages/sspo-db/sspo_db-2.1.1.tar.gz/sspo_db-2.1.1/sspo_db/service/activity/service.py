971bc9176f1a3401b0577b48e99edc74ccd6b58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:43 +0000

    Add package sspo-db/2.1.1/sspo_db-2.1.1.tar.gz

---
+++ packages/sspo-db/sspo_db-2.1.1.tar.gz/sspo_db-2.1.1/sspo_db/service/activity/service.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sspo_db.model.activity.models import *
+from sspo_db.service.base_service import BaseService
+
+class ScrumDevelopmentTaskService(BaseService):
+    def __init__(self):
+        super(ScrumDevelopmentTaskService,self).__init__(ScrumDevelopmentTask)
+
+class ScrumIntentedDevelopmentTaskService(BaseService):
+    def __init__(self):
+        super(ScrumIntentedDevelopmentTaskService,self).__init__(ScrumIntentedDevelopmentTask)
+
+class ScrumPerformedDevelopmentTaskService(BaseService):
+    def __init__(self):
+        super(ScrumPerformedDevelopmentTaskService,self).__init__(ScrumPerformedDevelopmentTask)
+
