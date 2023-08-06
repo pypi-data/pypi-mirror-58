2f070bd13c3410f0f01f450b2de69ef36a20c537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package dnsstamps/1.3.0/dnsstamps-1.3.0-py3-none-any.whl

---
+++ packages/dnsstamps/dnsstamps-1.3.0-py3-none-any.whl/dnsstamps/__init__.py
@@ -0,0 +1,20 @@
+from .option import Option
+from .protocol import Protocol
+
+from .parameter import Parameter
+
+from .generator import build
+from .generator import create_dnscrypt
+from .generator import create_doh
+from .generator import create_dot
+from .generator import create_plain
+from .generator import create_dnscrypt_relay
+from .generator import prepare_dnscrypt
+from .generator import prepare_doh
+from .generator import prepare_dot
+from .generator import prepare_plain
+from .generator import prepare_dnscrypt_relay
+
+from .parser import parse
+
+from .formatter import format
---
+++ packages/dnsstamps/dnsstamps-1.3.0-py3-none-any.whl/dnsstamps/protocol.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Protocol(Enum):
+    PLAIN = 0
+    DNSCRYPT = 1
+    DOH = 2
+    DOT = 3
+    DNSCRYPT_RELAY = 129
