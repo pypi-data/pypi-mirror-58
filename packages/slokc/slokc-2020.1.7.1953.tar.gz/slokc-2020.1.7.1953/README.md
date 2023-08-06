6788326574d7d7214fa37f732dfbfc143a29f768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:51 +0000

    Add package slokc/2020.1.7.1953/slokc-2020.1.7.1953.tar.gz

---
+++ packages/slokc/slokc-2020.1.7.1953.tar.gz/slokc-2020.1.7.1953/README.md
@@ -0,0 +1,43 @@
+# suck_lord_kelvin_cock
+
+This is an automated Wi-Fi login program for the Kelvin Building. It has a store of some passcodes. Beyond this, it has a passcode command line option.
+
+# setup
+
+```Bash
+sudo pip install slokc
+```
+
+To set up globally a Linux desktop launcher with icon, execute the following:
+
+```Bash
+sudo wget --content-disposition -N -P /usr/share/icons/ https://raw.githubusercontent.com/wdbm/suck_lord_kelvin_cock/master/slokc/static/cock.svg
+
+sudo wget --content-disposition -N -P /usr/share/applications/ https://raw.githubusercontent.com/wdbm/suck_lord_kelvin_cock/master/slokc/static/suck_lord_kelvin_cock.desktop
+```
+
+# usage
+
+General usage using stored passcodes is as follows:
+
+```Bash
+suck_lord_kelvin_cock
+```
+
+Manual passcode specification can be made in a way like the following:
+
+```Bash
+suck_lord_kelvin_cock --passcode="dfae91"
+```
+
+A help message is accessible in the following way:
+
+```Bash
+suck_lord_kelvin_cock --help
+```
+
+![](https://i.imgur.com/yNKzPh7.png)
+
+# passcodes
+
+A source for passcodes is here: <http://www.physics.gla.ac.uk/dept-only/wlesscodes/>
---
+++ packages/slokc/slokc-2020.1.7.1953.tar.gz/slokc-2020.1.7.1953/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+    setuptools.setup(
+        name                 = 'slokc',
+        version              = '2020.01.07.1953',
+        description          = 'Kelvin Building automated Wi-Fi login program',
+        long_description     = long_description(),
+        url                  = 'https://github.com/wdbm/suck_lord_kelvin_cock',
+        author               = 'Will Breaden Madden',
+        author_email         = 'wbm@protonmail.ch',
+        license              = 'GPLv3',
+        packages             = setuptools.find_packages(),
+        install_requires     = [
+                               'docopt'
+                               ],
+        entry_points         = {
+                               'console_scripts': ('suck_lord_kelvin_cock = slokc.__init__:main')
+                               },
+        include_package_data = True,
+        zip_safe             = False
+    )
+
+def long_description(filename='README.md'):
+    if os.path.isfile(os.path.expandvars(filename)):
+      try:
+          import pypandoc
+          long_description = pypandoc.convert_file(filename, 'rst')
+      except ImportError:
+          long_description = open(filename).read()
+    else:
+        long_description = ''
+    return long_description
+
+if __name__ == '__main__':
+    main()
