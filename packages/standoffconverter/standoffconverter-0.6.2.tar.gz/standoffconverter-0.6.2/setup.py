677b8ce68a59e1af2cec42296af8cff7c05cd21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:20 +0000

    Add package standoffconverter/0.6.2/standoffconverter-0.6.2.tar.gz

---
+++ packages/standoffconverter/standoffconverter-0.6.2.tar.gz/standoffconverter-0.6.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='standoffconverter',
+      version='0.6.2',
+      description='converter from xml to standoff and back',
+      url='https://github.com/standoff-nlp/standoffconverter',
+      author='David Lassner',
+      author_email='lassner@tu-berlin.de',
+      license='MIT',
+      packages=['standoffconverter'],
+      install_requires=[
+          'lxml',
+          'numpy'
+      ],
+      zip_safe=False)
