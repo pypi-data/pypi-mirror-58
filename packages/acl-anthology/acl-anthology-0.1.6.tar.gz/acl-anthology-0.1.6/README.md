18a2eec1157dff4de12e4ca40662b0b740de9cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:19 +0000

    Add package acl-anthology/0.1.6/acl-anthology-0.1.6.tar.gz

---
+++ packages/acl-anthology/acl-anthology-0.1.6.tar.gz/acl-anthology-0.1.6/README.md
@@ -0,0 +1,21 @@
+# Acl-Anthology
+
+Script to get [ACL Anthology](https://www.aclweb.org/anthology/)
+
+[Pre-downloaded data is available at Kaggle](https://www.kaggle.com/takahirokubo0/acl-anthology-papers/home)
+
+
+## Install
+
+```
+pip install acl-anthology
+```
+
+## How to use
+
+```
+from acl.conference import Conference
+
+
+Conference.ACL(2018).retrieve("P18-5").to_dataframe().to_csv("acl2018.csv", index=False)
+```
---
+++ packages/acl-anthology/acl-anthology-0.1.6.tar.gz/acl-anthology-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="acl-anthology",
+    version="0.1.6",
+    description="Get ACL Anthology Papers.",
+    keywords=["machine learning", "nlp", "natural language processing"],
+    author="icoxfog417",
+    author_email="icoxfog417@yahoo.co.jp",
+    license="MIT",
+    packages=[
+        "acl",
+        ],
+    url="https://github.com/icoxfog417/acl-anthology",
+    install_requires=[
+        "requests>=2.22.0",
+        "beautifulsoup4>=4.8.2",
+        "arxiv>=0.5.1",
+        "pandas>=0.25.3",
+        "tqdm>=4.41.1"
+    ],
+)
