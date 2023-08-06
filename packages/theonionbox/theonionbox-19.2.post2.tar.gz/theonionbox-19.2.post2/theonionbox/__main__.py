c14f49d1b0f8ce5b612aad3debdcff73532cf3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:38 +0000

    Add package theonionbox/19.2.post2/theonionbox-19.2.post2.tar.gz

---
+++ packages/theonionbox/theonionbox-19.2.post2.tar.gz/theonionbox-19.2.post2/theonionbox/__main__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+import sys, os
+
+
+def main():
+
+    # the import executes the scripting part...
+    if __name__ == '__main__':
+        # we can't use relative imports here!!
+        # get an absolute path to the directory that contains mypackage
+        this_dir = os.path.dirname(os.path.join(os.getcwd(), __file__))
+        sys.path.append(os.path.normpath(os.path.join(this_dir, '..')))
+
+        from theonionbox import main as onion_main
+
+    else:
+        from .theonionbox import main as onion_main
+
+    # ... and main() launches the server!
+    onion_main()
+
+
+if __name__ == '__main__':
+    # args = sys.argv[1:]
+    main()
+
+
---
+++ packages/theonionbox/theonionbox-19.2.post2.tar.gz/theonionbox-19.2.post2/theonionbox/stamp.py
@@ -0,0 +1,4 @@
+__title__ = 'The Onion Box'
+__description__ = 'Dashboard to monitor Tor node operations.'
+__version__ = '19.2.post2'
+__stamp__ = '20191227|105236'
