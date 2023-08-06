eac6b2c30b8f491ea1cf4b4fe365922cfee3930d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:42 +0000

    Add package aliaa-distributions/0.1/aliaa_distributions-0.1.tar.gz

---
+++ packages/aliaa-distributions/aliaa_distributions-0.1.tar.gz/aliaa_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aliaa_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['aliaa_distributions'],
+      zip_safe=False)
