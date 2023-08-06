7b94fde4bafc0be51cf168d881440e9e9adccf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:18 +0000

    Add package pyarcher/0.4.1/pyarcher-0.4.1.tar.gz

---
+++ packages/pyarcher/pyarcher-0.4.1.tar.gz/pyarcher-0.4.1/pyarcher/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for RSA Archer Client Library."""
+from pyarcher.archer import Archer
+
+
+__author__ = """Kyle Cribbs"""
+__email__ = 'kylecribbs@outlook.com'
+__version__ = '0.4.1'
+__all__ = ["Archer"]
+
+name = "pyarcher"
+
