5c18a535797db5df887e947a261b2463d1e18001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:57 +0000

    Add package async-weather-api/0.5.0/async_weather_api-0.5.0-py3-none-any.whl

---
+++ packages/async-weather-api/async_weather_api-0.5.0-py3-none-any.whl/weather/view/home.py
@@ -0,0 +1,16 @@
+import quart
+
+blueprint: quart.blueprints.Blueprint = quart.blueprints.Blueprint(__name__, __name__)
+
+
+@blueprint.route("/")
+@blueprint.route("/index")
+async def index() -> str:
+    """Returns home page route."""
+    return await quart.render_template("index.html")
+
+
+@blueprint.errorhandler(404)
+async def not_found() -> quart.Response:
+    """Returns page not found."""
+    return quart.Response("The page was not found", status=404)
