ae3ff1898efdf3fa2ebf3f337ad5f0195aaed32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:49 +0000

    Add package iptvtools/0.1.1/iptvtools-0.1.1.tar.gz

---
+++ packages/iptvtools/iptvtools-0.1.1.tar.gz/iptvtools-0.1.1/iptvtools/constants/helps.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: constants.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: Helps for iptvtools.
+"""
+from . import defaults
+
+CONFIG = (
+    f'Configuration file with title and id filter information, defaults to '
+    f'`{defaults.CONFIG}`'
+)
+INPUT = (
+    f'Input playlist file/url, defaults to `{defaults.INPUT}`.'
+)
+MIN_HEIGHT = (
+    f'Minimal acceptable height/resolution, defaults to {defaults.MIN_HEIGHT}'
+    f'which means {defaults.MIN_HEIGHT}P. Set 0 to disable the filtering.'
+)
+OUTPUT = f'Output playlist file name, defaults to `{defaults.OUTPUT}`.'
+TEMPLATE = (
+    f'Template playlist file/url which have well-maintained channel '
+    f'information to cooperate with EPG and will replace the corresponding '
+    f'entry (except url) if matched , defaults to `{defaults.TEMPLATE}`.'
+)
+TIMEOUT = (
+    f'Acceptable timeout when retrieving stream information, defaults to '
+    f'{defaults.TIMEOUT}.'
+)
+UDPXY = (
+    f'UDP Proxy which will convert the url automatically, defaults to '
+    f'`{defaults.UDPXY}`.'
+)
