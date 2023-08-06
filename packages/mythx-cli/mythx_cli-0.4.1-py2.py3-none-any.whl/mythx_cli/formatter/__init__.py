b7f6fbf00e8778aeef769509c5d606aac874cfac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package mythx-cli/0.4.1/mythx_cli-0.4.1-py2.py3-none-any.whl

---
+++ packages/mythx-cli/mythx_cli-0.4.1-py2.py3-none-any.whl/mythx_cli/formatter/__init__.py
@@ -0,0 +1,12 @@
+from .json import JSONFormatter, PrettyJSONFormatter
+from .simple_stdout import SimpleFormatter
+from .tabular import TabularFormatter
+
+FORMAT_RESOLVER = {
+    "simple": SimpleFormatter(),
+    "json": JSONFormatter(),
+    "json-pretty": PrettyJSONFormatter(),
+    "table": TabularFormatter(),
+}
+
+__all__ = [JSONFormatter, PrettyJSONFormatter, SimpleFormatter, TabularFormatter]
