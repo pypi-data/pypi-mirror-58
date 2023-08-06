f2bddc816d9ee1c09ca2386e92b8c3712208269b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:11 +0000

    Add package test-plus/0.1.12/test_plus-0.1.12.tar.gz

---
+++ packages/test-plus/test_plus-0.1.12.tar.gz/test_plus-0.1.12/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from os import path
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+long_description = """Django Test Plus Extensions is a collection
+  of custom extensions for the Django Framework.
+  See the project page for more information:
+  http://github.com/tolgahanuzun/test_plus"""
+
+if os.path.isfile("readme.md"):
+  with open("readme.md") as f:
+      long_description = f.read()
+
+setup(
+  name = 'test_plus',
+  packages = find_packages(), 
+  version = '0.1.12',
+  long_description_content_type="text/markdown",
+  long_description=long_description,
+  description = 'Test analysis tool for Django',
+  author = 'Tolgahan Üzün',
+  author_email = 'mail@tolgahanuzun.com',
+  url = 'https://github.com/tolgahanuzun/test_plus',
+  download_url = 'https://github.com/tolgahanuzun/test_plus/tarball/0.1.10',
+  keywords = ['django', 'test', 'time'],
+  install_requires = [],
+  classifiers = [],
+)
