3174985ca948163acfe4fdbc1a5fda7896d8aec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:09 +0000

    Add package pacifica-proxy/0.1.5/pacifica-proxy-0.1.5.tar.gz

---
+++ packages/pacifica-proxy/pacifica-proxy-0.1.5.tar.gz/pacifica-proxy-0.1.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup and install the proxy."""
+from os import path
+try:  # pip version 9
+    from pip.req import parse_requirements
+except ImportError:
+    from pip._internal.req import parse_requirements
+from setuptools import setup, find_packages
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
+
+setup(
+    name='pacifica-proxy',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Pacifica Proxy',
+    url='https://github.com/pacifica/pacifica-uniqueid/',
+    long_description=open(path.join(
+        path.abspath(path.dirname(__file__)),
+        'README.md')).read(),
+    long_description_content_type='text/markdown',
+    author='David Brown',
+    author_email='david.brown@pnnl.gov',
+    packages=find_packages(include=['pacifica.*']),
+    namespace_packages=['pacifica'],
+    entry_points={
+        'console_scripts': [
+            'pacifica-proxy=pacifica.proxy.__main__:main'
+        ],
+    },
+    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+)
