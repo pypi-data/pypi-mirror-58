09645766b1cb65da168ce8518e488e6c366a38b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:02 +0000

    Add package qdds/1.1.5/qdds-1.1.5.tar.gz

---
+++ packages/qdds/qdds-1.1.5.tar.gz/qdds-1.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='qdds',
+    version='1.1.5',
+    description='Quick Django Dev Server shortcut using your IP and port 8000',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'benmcnelly',
+    author_email = 'me@benmcnelly.com',
+    url = 'https://github.com/benmcnelly/qdds',
+    py_modules=['devserver'],
+    install_requires=[
+        'Click',
+        'colorama',
+    ],
+    entry_points='''
+        [console_scripts]
+        devserver=devserver:cli
+    ''',
+)
