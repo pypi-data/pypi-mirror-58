070496ffb9fdede9c915ba87702a96c3f6d68bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:27 +0000

    Add package mytestlib-kitamaru/1.0.0/mytestlib-kitamaru-1.0.0.tar.gz

---
+++ packages/mytestlib-kitamaru/mytestlib-kitamaru-1.0.0.tar.gz/mytestlib-kitamaru-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-kitamaru',
+    version='1.0.0',
+    author='Kitamaru',
+    author_email='kz_hiei5968@ybb.ne.jp',
+    description='My Test Library.',
+    packages=find_packages()
+    )
