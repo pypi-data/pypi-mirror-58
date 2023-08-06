77fabdf1109e77047cec70f77355e45ce77f977b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:03 +0000

    Add package hypersRedisCluster/0.0.7/hypersRedisCluster-0.0.7.tar.gz

---
+++ packages/hypersRedisCluster/hypersRedisCluster-0.0.7.tar.gz/hypersRedisCluster-0.0.7/test/test_sqlite.py
@@ -0,0 +1,17 @@
+SECRET_KEY = "django_tests_secret_key"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": [
+            {"host": "127.0.0.1", "port": "7000"},
+        ],
+        "OPTIONS": {
+            "CLIENT_CLASS": "hypersRedisCluster.client.RedisClusterClient",
+        }
+    },
+}
+
+INSTALLED_APPS = (
+    "django.contrib.sessions",
+)
