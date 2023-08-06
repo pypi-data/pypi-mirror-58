daa8c87e1ad3b6e647a1ac2b993ad8ca6cc88811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:22 +0000

    Add package lexpp/1.1.0/lexpp-1.1.0.tar.gz

---
+++ packages/lexpp/lexpp-1.1.0.tar.gz/lexpp-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from codecs import open
+
+setup(
+    name='lexpp',
+    version='1.1.0',
+    description='A lexical pre-processing module for Japanese text',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='earlsuke',
+    author_email='ryosuke.mitani@gmail.com',
+    url='https://github.com/earlsuke',
+    license="Apache-2.0",
+    packages=['lexpp', 'lexpp.res'],
+    include_package_data=True,
+    package_data={'lexpp':['res/*.dict']},
+)
+
