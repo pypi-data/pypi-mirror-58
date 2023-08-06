1aec08f009462965bbaf091b65ad95c0a87b429b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:54 +0000

    Add package asdf/2.5.0/asdf-2.5.0.tar.gz

---
+++ packages/asdf/asdf-2.5.0.tar.gz/asdf-2.5.0/asdf/tags/core/__init__.py
@@ -0,0 +1,53 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+
+from ...types import AsdfType
+from ...yamlutil import custom_tree_to_tagged_tree
+
+
+class AsdfObject(dict, AsdfType):
+    name = 'core/asdf'
+    version = '1.1.0'
+
+
+class Software(dict, AsdfType):
+    name = 'core/software'
+    version = '1.0.0'
+
+
+class HistoryEntry(dict, AsdfType):
+    name = 'core/history_entry'
+    version = '1.0.0'
+
+
+class ExtensionMetadata(AsdfType):
+    name = 'core/extension_metadata'
+    version = '1.0.0'
+
+    def __init__(self, extension_class=None, software={}):
+        self.extension_class = extension_class
+        self.software = software
+
+    @classmethod
+    def from_tree(cls, node, ctx):
+        return cls(**node)
+
+    @classmethod
+    def to_tree(cls, node, ctx):
+        tree = {}
+        tree['extension_class'] = node.extension_class
+        tree['software'] = custom_tree_to_tagged_tree(node.software, ctx)
+        return tree
+
+
+class SubclassMetadata(dict, AsdfType):
+    name = 'core/subclass_metadata'
+    version = '1.0.0'
+
+
+from .constant import ConstantType
+from .ndarray import NDArrayType
+from .complex import ComplexType
+from .integer import IntegerType
+from .external_reference import ExternalArrayReference
---
+++ packages/asdf/asdf-2.5.0.tar.gz/asdf-2.5.0/asdf/tags/core/constant.py
@@ -0,0 +1,27 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+
+from ...types import AsdfType
+
+
+class Constant:
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+
+class ConstantType(AsdfType):
+    name = 'core/constant'
+    types = [Constant]
+
+    @classmethod
+    def from_tree(self, node, ctx):
+        return Constant(node)
+
+    @classmethod
+    def to_tree(self, data, ctx):
+        return data.value
