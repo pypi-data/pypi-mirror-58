9b5529ed78f1256e7848f717e913341b82760f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package anyio/1.2.3/anyio-1.2.3.tar.gz

---
+++ packages/anyio/anyio-1.2.3.tar.gz/anyio-1.2.3/anyio/pytest_plugin.py
@@ -0,0 +1,102 @@
+from functools import partial
+from inspect import iscoroutinefunction
+
+import pytest
+from async_generator import isasyncgenfunction
+
+from . import run, BACKENDS
+
+
+def pytest_configure(config):
+    config.addinivalue_line('markers', 'anyio: mark the (coroutine function) test to be run '
+                                       'asynchronously via anyio.')
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('AnyIO')
+    group.addoption(
+        '--anyio-backends', action='store', dest='anyio_backends', default=BACKENDS[0],
+        help='Comma separated list of backends to use for running asynchronous tests.',
+    )
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_fixture_setup(fixturedef, request):
+    def wrapper(*args, **kwargs):
+        backend = kwargs['anyio_backend']
+        if strip_backend:
+            del kwargs['anyio_backend']
+
+        if isasyncgenfunction(func):
+            gen = func(*args, **kwargs)
+            try:
+                value = run(gen.__anext__, backend=backend)
+            except StopAsyncIteration:
+                raise RuntimeError('Async generator did not yield')
+
+            yield value
+
+            try:
+                run(gen.__anext__, backend=backend)
+            except StopAsyncIteration:
+                pass
+            else:
+                run(gen.aclose, backend=backend)
+                raise RuntimeError('Async generator fixture did not stop')
+        else:
+            yield run(partial(func, *args, **kwargs), backend=backend)
+
+    func = fixturedef.func
+    if (isasyncgenfunction(func) or iscoroutinefunction(func)) and 'anyio' in request.keywords:
+        strip_backend = False
+        if 'anyio_backend' not in fixturedef.argnames:
+            fixturedef.argnames += ('anyio_backend',)
+            strip_backend = True
+
+        fixturedef.func = wrapper
+
+    yield
+
+
+def pytest_generate_tests(metafunc):
+    marker = metafunc.definition.get_closest_marker('anyio')
+    if marker:
+        metafunc.fixturenames.append('anyio_backend')
+
+    if 'anyio_backend' in metafunc.fixturenames:
+        backends = metafunc.config.getoption('anyio_backends')
+        if backends == 'all':
+            backends = BACKENDS
+        else:
+            backends = backends.replace(' ', '').split(',')
+
+        metafunc.parametrize('anyio_backend', backends, scope='session')
+
+
+def check_test_function_type(func):
+    if not iscoroutinefunction(func):
+        funcname = '{}.{}'.format(func.__module__, func.__qualname__)
+        pytest.fail('{} is not a coroutine function'.format(funcname))
+
+
+@pytest.mark.tryfirst
+def pytest_pyfunc_call(pyfuncitem):
+    def run_with_hypothesis(**kwargs):
+        run(partial(original_func, **kwargs), backend=backend)
+
+    if pyfuncitem.get_closest_marker('anyio'):
+        backend = pyfuncitem._request.getfixturevalue('anyio_backend')
+        if hasattr(pyfuncitem.obj, 'hypothesis'):
+            # Wrap the inner test function unless it's already wrapped
+            original_func = pyfuncitem.obj.hypothesis.inner_test
+            if original_func.__qualname__ != run_with_hypothesis.__qualname__:
+                check_test_function_type(original_func)
+                pyfuncitem.obj.hypothesis.inner_test = run_with_hypothesis
+
+            return False
+
+        check_test_function_type(pyfuncitem.obj)
+        funcargs = pyfuncitem.funcargs
+        testargs = {arg: funcargs[arg] for arg in pyfuncitem._fixtureinfo.argnames}
+        run(partial(pyfuncitem.obj, **testargs), backend=backend)
+        return True
---
+++ packages/anyio/anyio-1.2.3.tar.gz/anyio-1.2.3/docs/testing.rst
@@ -0,0 +1,74 @@
+Testing with AnyIO
+==================
+
+AnyIO provides built-in support for testing your library or application in the form of a pytest_
+plugin.
+
+.. _pytest: https://docs.pytest.org/en/latest/
+
+Creating asynchronous tests
+---------------------------
+
+To mark a coroutine function to be run via :func:`anyio.run`, simply add the ``@pytest.mark.anyio``
+decorator::
+
+    import pytest
+
+
+    @pytest.mark.anyio
+    async def test_something():
+        pass
+
+Asynchronous fixtures
+---------------------
+
+The plugin also supports coroutine functions as fixtures, for the purpose of setting up and tearing
+down asynchronous services used for tests::
+
+    import pytest
+
+
+    @pytest.fixture
+    async def server():
+        server = await setup_server()
+        yield server
+        await server.shutdown()
+
+
+    @pytest.mark.anyio
+    async def test_server(server):
+        result = await server.do_something()
+        assert result == 'foo'
+
+Any coroutine fixture that is activated by a test marked with ``@pytest.mark.anyio`` will be run
+with the same backend as the test itself. Both plain coroutine functions and asynchronous generator
+functions are supported in the same manner as pytest itself does with regular functions and
+generator functions.
+
+.. note:: If you need Python 3.5 compatibility, please use the async_generator_ library to replace
+          the async generator syntax that was introduced in Python 3.6.
+
+.. _async_generator: https://github.com/python-trio/async_generator
+
+Specifying the backend to run on
+--------------------------------
+
+By default, all tests are run against the default backend (asyncio). The pytest plugin provides a
+command line switch (``--anyio-backends``) for selecting which backend(s) to run your tests
+against. By specifying a special value, ``all``, it will run against all available backends.
+
+For example, to run your test suite against the curio and trio backends:
+
+.. code-block:: bash
+
+    pytest --anyio-backends=curio,trio
+
+Using AnyIO from regular tests
+------------------------------
+
+In rare cases, you may need to have tests that run against whatever backends you have chosen to
+work with. For this, you can add the ``anyio_backend`` parameter to your test. It will be filled
+in with the name of each of the selected backends in turn::
+
+    def test_something(anyio_backend):
+        assert anyio_backend in ('asyncio', 'curio', 'trio')
