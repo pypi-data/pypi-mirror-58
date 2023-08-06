40203e346d46cd7d11b410a36426e1bb3e0e510b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:59 +0000

    Add package jupyter-starters/0.2.1a0/jupyter_starters-0.2.1a0-py3-none-any.whl

---
+++ packages/jupyter-starters/jupyter_starters-0.2.1a0-py3-none-any.whl/jupyter_starters/types.py
@@ -0,0 +1,13 @@
+""" some types and constants
+"""
+# pylint: disable=too-few-public-methods
+NS = "starters"
+
+
+class Status:
+    """ pseudo-enum for managing statuses
+    """
+
+    DONE = "done"
+    CONTINUING = "continuing"
+    ERROR = "error"
