e77a83d641d55b852d0acc9f6d03ea639f76471e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:23 +0000

    Add package flake8-nb/0.1.4/flake8_nb-0.1.4.tar.gz

---
+++ packages/flake8-nb/flake8_nb-0.1.4.tar.gz/flake8_nb-0.1.4/docs/installation.rst
@@ -0,0 +1,51 @@
+.. highlight:: shell
+
+============
+Installation
+============
+
+
+Stable release
+--------------
+
+To install flake8-nb, run this command in your terminal:
+
+.. code-block:: console
+
+    $ pip install flake8-nb
+
+This is the preferred method to install flake8-nb, as it will always install the most recent stable release.
+
+If you don't have `pip`_ installed, this `Python installation guide`_ can guide
+you through the process.
+
+.. _pip: https://pip.pypa.io/en/stable/
+.. _Python installation guide: https://docs.python-guide.org/starting/installation/
+
+
+From sources
+------------
+
+The sources for flake8-nb can be downloaded from the `Github repo`_.
+
+You can either clone the public repository:
+
+.. code-block:: console
+
+    $ git clone git://github.com/s-weigand/flake8-nb
+
+Or download the `tarball`_:
+
+.. code-block:: console
+
+    $ curl -OJL https://github.com/s-weigand/flake8-nb/tarball/master
+
+Once you have a copy of the source, you can install it with:
+
+.. code-block:: console
+
+    $ python setup.py install
+
+
+.. _Github repo: https://github.com/s-weigand/flake8-nb
+.. _tarball: https://github.com/s-weigand/flake8-nb/tarball/master
---
+++ packages/flake8-nb/flake8_nb-0.1.4.tar.gz/flake8_nb-0.1.4/tests/test__main__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+import pytest
+
+from flake8_nb.__main__ import main
+from flake8_nb.parsers.notebook_parsers import InvalidNotebookWarning, NotebookParser
+
+from . import TEST_NOTEBOOK_BASE_PATH
+
+
+@pytest.mark.parametrize("keep_intermediate", [True, False])
+def test_run_main(capsys, keep_intermediate: bool):
+    argv = ["flake8_nb", TEST_NOTEBOOK_BASE_PATH]
+    if keep_intermediate:
+        argv.append("--keep-parsed-notebooks")
+    with pytest.raises(SystemExit):
+        with pytest.warns(InvalidNotebookWarning):
+            main(argv)
+    captured = capsys.readouterr()
+    result_output = captured.out
+    result_list = result_output.replace("\r", "").split("\n")
+    result_list.remove("")
+    expected_result_path = os.path.join(
+        os.path.dirname(__file__), "data", "expected_output.txt"
+    )
+    with open(expected_result_path, "r") as result_file:
+        expected_result_list = result_file.readlines()
+    assert len(expected_result_list) == len(result_list)
+    for expected_result in expected_result_list:
+        assert any(
+            [result.endswith(expected_result.rstrip("\n")) for result in result_list]
+        )
+
+    if keep_intermediate:
+        assert os.path.exists(NotebookParser.temp_path)
+        NotebookParser.clean_up()
+
+
+@pytest.mark.parametrize("keep_intermediate", [True, False])
+@pytest.mark.parametrize("cli_entrypoint", ["flake8_nb", "flake8-nb"])
+def test_syscall(cli_entrypoint: str, keep_intermediate: bool):
+    argv = [cli_entrypoint, TEST_NOTEBOOK_BASE_PATH]
+    if keep_intermediate:
+        argv.append("--keep-parsed-notebooks")
+    proc = subprocess.Popen(argv, stdout=subprocess.PIPE, universal_newlines=True)
+    result_list = []
+    for line in proc.stdout:
+        result_list.append(str(line))
+    expected_result_path = os.path.join(
+        os.path.dirname(__file__), "data", "expected_output.txt"
+    )
+    with open(expected_result_path, "r") as result_file:
+        expected_result_list = result_file.readlines()
+
+    assert len(expected_result_list) == len(result_list)
+
+    for expected_result in expected_result_list:
+        assert any([result.endswith(expected_result) for result in result_list])
