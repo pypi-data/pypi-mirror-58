03e569a1fc328b2423254cff17f28f6948b9201e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:57 +0000

    Add package foxtail-blog/0.9.2/foxtail-blog-0.9.2.tar.gz

---
+++ packages/foxtail-blog/foxtail-blog-0.9.2.tar.gz/foxtail-blog-0.9.2/foxtail_blog/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.template.defaultfilters import truncatechars
+from django.urls import reverse
+from django.utils.html import format_html
+
+from .models import Comment, Post
+
+
+class PostAdmin(ModelAdmin):
+    fieldsets = (
+        ('Content', {
+            'fields': ('title', 'tags', 'author', 'text')
+        }),
+        ('Image', {
+            'fields': ('image',),
+        }),
+        ('Advanced options', {
+            'fields': ('slug', 'allow_comments'),
+        }),
+    )
+
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ('created', 'tags', 'author')
+    list_display = ('title', 'tag_list', 'created', 'modified', 'author')
+
+    @staticmethod
+    def tag_list(obj):
+        return ", ".join(o.name for o in obj.tags.all().order_by('name'))
+
+
+class CommentAdmin(ModelAdmin):
+    list_display = ('text_preview', 'post_link', 'author', 'created_date')
+    raw_id_fields = ('author',)
+
+    def post_link(self, obj):
+        return format_html('<a href="{}">{}</a>',
+                           reverse("admin:foxtail_blog_post_change", args=(obj.post.pk,)),
+                           obj.post.title)
+
+    def text_preview(self, obj):
+        return truncatechars(obj.text, 50)
+
+    text_preview.short_description = "Comment"
+    post_link.short_description = "Post"
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
