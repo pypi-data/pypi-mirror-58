c2b993c373d86f253f4cf5c068e852ab189ac359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:12 +0000

    Add package scalene/0.3.1/scalene-0.3.1.tar.gz

---
+++ packages/scalene/scalene-0.3.1.tar.gz/scalene-0.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='scalene',
+      version='0.3.1',
+      description='Scalene: A high-resolution, low-overhead CPU and memory profiler for Python',
+      url='https://github.com/emeryberger/scalene',
+      author='Emery Berger',
+      author_email='emery@cs.umass.edu',
+      license='Apache License 2.0',
+          classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+     ],
+      packages=find_packages()
+)
