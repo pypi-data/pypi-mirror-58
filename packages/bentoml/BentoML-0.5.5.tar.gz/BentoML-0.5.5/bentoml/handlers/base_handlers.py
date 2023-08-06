8cc901f0d4d624d18ea5c81d84378f5f71fdd187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:37 +0000

    Add package bentoml/0.5.5/BentoML-0.5.5.tar.gz

---
+++ packages/bentoml/BentoML-0.5.5.tar.gz/BentoML-0.5.5/bentoml/handlers/base_handlers.py
@@ -0,0 +1,88 @@
+# Copyright 2019 Atalaya Tech, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+
+import pandas as pd
+import numpy as np
+
+from bentoml.exceptions import InvalidArgument
+
+
+class BentoHandler:
+    """Handler in BentoML is the layer between a user API request and
+    the input to user's API function.
+    """
+
+    HTTP_METHODS = ["POST", "GET"]
+
+    def handle_request(self, request, func):
+        """Handles an HTTP request, convert it into corresponding data
+        format that user API function is expecting, and return API
+        function result as the HTTP response to client
+
+        :param request: Flask request object
+        :param func: user API function
+        """
+        raise NotImplementedError
+
+    def handle_cli(self, args, func):
+        """Handles an CLI command call, convert CLI arguments into
+        corresponding data format that user API function is expecting, and
+        prints the API function result to console output
+
+        :param args: CLI arguments
+        :param func: user API function
+        """
+        raise NotImplementedError
+
+    def handle_aws_lambda_event(self, event, func):
+        """Handles a Lambda event, convert event dict into corresponding
+        data format that user API function is expecting, and use API
+        function result as response
+
+        :param event: A dict containing AWS lambda event information
+        :param func: user API function
+        """
+        raise NotImplementedError
+
+    @property
+    def request_schema(self):
+        return {"application/json": {"schema": {"type": "object"}}}
+
+    @property
+    def pip_dependencies(self):
+        return []
+
+
+def get_output_str(result, output_format, output_orient="records"):
+    if output_format == "str":
+        return str(result)
+    elif output_format == "json":
+        if isinstance(result, pd.DataFrame):
+            return result.to_json(orient=output_orient)
+        elif isinstance(result, np.ndarray):
+            return json.dumps(result.tolist())
+        else:
+            try:
+                return json.dumps(result)
+            except (TypeError, OverflowError):
+                # when result is not JSON serializable
+                return json.dumps(str(result))
+    else:
+        raise InvalidArgument("Output format {} is not supported".format(output_format))
