0684d276724353b5126253ebbb7e3641452de00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:13 +0000

    Add package django-single-instance-model/1.1.0/django_single_instance_model-1.1.0.tar.gz

---
+++ packages/django-single-instance-model/django_single_instance_model-1.1.0.tar.gz/django_single_instance_model-1.1.0/single_instance_model/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.shortcuts import redirect
+
+try:
+	# django 2.x
+	from django.urls import reverse
+
+except:
+	# django 1.x
+	from django.core.urlresolvers import reverse
+
+
+class SingleInstanceModelAdmin(admin.ModelAdmin):
+
+	def has_add_permission(self, *args, **kwargs):
+		return False
+
+	def has_delete_permission(self, *args, **kwargs):
+		return False
+
+	def changelist_view(self, request, extra_context=None):
+		current = request.build_absolute_uri()
+		instance = self.model.objects.get()
+		url = '{}{}/change/'.format(current, instance.id)
+		return redirect(url)
+
+	def response_post_save_change(self, request, obj):
+		res = self._response_post_save(request, obj)
+		current = request.build_absolute_uri()
+		url = ('/'.join(current.split('/')[:-4])) + '/'
+		return redirect(url)
