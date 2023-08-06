87fb8e874178c182d5cd1a44289b62d64d335c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:46 +0000

    Add package dmiparser/0.6/dmiparser-0.6-py3-none-any.whl

---
+++ packages/dmiparser/dmiparser-0.6-py3-none-any.whl/tests/test_dmidecode_full.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+from dmiparser import DmiParser
+
+RDIR = Path(Path(__file__).resolve()).parents[0]
+
+def test_dmidecode_full():
+    testnum = 0
+
+    with open(RDIR / 'dmidecode_full.txt', 'rt') as f:
+        text = f.read()
+        data = json.loads(str(DmiParser(text)))
+
+    '''
+    Handle 0xFEFF, DMI type 127, 4 bytes
+        End Of Table
+    '''
+    assert(111 == len(data))
+    testnum += 1
+
+    assert(1 == testnum)
+
