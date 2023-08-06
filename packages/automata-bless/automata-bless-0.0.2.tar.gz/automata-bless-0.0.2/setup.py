d0a007b6e613786f4ad10e2a14e15f098bc60fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:03 +0000

    Add package automata-bless/0.0.2/automata-bless-0.0.2.tar.gz

---
+++ packages/automata-bless/automata-bless-0.0.2.tar.gz/automata-bless-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='automata-bless',
+      version='0.0.2',
+      description='A CLI application to create user accounts on Linux systems from Gitlab users/group information to use with BLESS',
+      author='Brian Davis',
+      author_email='slimm609@gmail.com',
+      license='GPLv3',
+      packages=find_packages(),
+      url = "https://github.com/slimm609/automata_bless",
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'automata=automatagl.automatagl:main',
+          ],
+      },
+      install_requires=[
+          "certifi==2019.3.9",
+          "chardet==3.0.4",
+          "idna==2.8",
+          "PyYAML",
+          "requests==2.22.0",
+          "urllib3==1.25.3",
+          "boto3",
+      ],
+    )
