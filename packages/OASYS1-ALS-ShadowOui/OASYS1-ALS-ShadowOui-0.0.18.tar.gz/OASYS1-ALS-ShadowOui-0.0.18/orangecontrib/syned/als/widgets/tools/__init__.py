d51de45005cdc7eed63fe6027914bba5f94317d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:14 +0000

    Add package OASYS1-ALS-ShadowOui/0.0.18/OASYS1-ALS-ShadowOui-0.0.18.tar.gz

---
+++ packages/OASYS1-ALS-ShadowOui/OASYS1-ALS-ShadowOui-0.0.18.tar.gz/OASYS1-ALS-ShadowOui-0.0.18/orangecontrib/syned/als/widgets/tools/__init__.py
@@ -0,0 +1,12 @@
+
+# Category description for the widget registry
+
+NAME = "Syned ALS Tools"
+
+DESCRIPTION = "Widgets for Syned ALS Tools."
+
+BACKGROUND = "#fffeb3"
+
+ICON = "icons/tools.png"
+
+PRIORITY = 7.1
