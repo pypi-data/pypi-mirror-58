bc895e385b4c66937340512a724986afe4d65fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:26 +0000

    Add package django-url-permissions/0.1.1/django_url_permissions-0.1.1-py3-none-any.whl

---
+++ packages/django-url-permissions/django_url_permissions-0.1.1-py3-none-any.whl/permissions/app.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.conf.urls import url
+
+
+class PermissionsApplication(AppConfig):
+    name = 'UrlPermission.permissions'
+    verbose_name = "Permissions using urls"
+
+
