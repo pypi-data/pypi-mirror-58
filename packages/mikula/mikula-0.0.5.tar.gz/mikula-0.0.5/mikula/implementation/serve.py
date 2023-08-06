3f778a58e24dc02cf93ea9c206b485bc94732057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:49 +0000

    Add package mikula/0.0.5/mikula-0.0.5.tar.gz

---
+++ packages/mikula/mikula-0.0.5.tar.gz/mikula-0.0.5/mikula/implementation/serve.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+
+def serve(gallery, port):
+
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=gallery, **kwargs)
+
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        print(f"Serving on 'http://localhost:{port}'")
+        print("Ctrl + C to exit")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("done")
