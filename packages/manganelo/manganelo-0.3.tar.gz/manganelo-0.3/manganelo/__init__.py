636449603a669bb6833f25a9f8c137d12e8d6d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:42 +0000

    Add package manganelo/0.3/manganelo-0.3.tar.gz

---
+++ packages/manganelo/manganelo-0.3.tar.gz/manganelo-0.3/manganelo/__init__.py
@@ -0,0 +1,7 @@
+from manganelo.api.search_manga import SearchManga
+
+
+if __name__ == "__main__":
+	from manganelo.tests import test_manga_chapters as tests
+
+	tests.run_all_tests()
---
+++ packages/manganelo/manganelo-0.3.tar.gz/manganelo-0.3/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+  name="manganelo",
+  packages=["manganelo"],
+  version="0.3",
+  license="MIT",
+
+  description="Unofficial API for the Manganelo (and Mangakakalot) website.",
+  long_description="Unofficial API for the Manganelo (and Mangakakalot) website.",
+
+  author="Joshua Nixon",
+  author_email="nixonjoshua98@gmail.com",
+
+  url="https://github.com/nixonjoshua98/manganelo-api",
+
+  download_url="https://github.com/nixonjoshua98/manganelo-api/archive/v0.3.0.tar.gz",
+  
+  keywords=["manga", "manganelo", "scrapper", "web", "mangakakalot"],
+  
+  install_requires=[
+    "bs4",
+    "requests"
+  ],
+
+  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+  
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",  
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.7",
+  ],
+)
