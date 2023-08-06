f277a222631ef5a34ba201f5b4664104ac1d6527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:30 +0000

    Add package nubium-utils/0.1.0/nubium_utils-0.1.0-py3-none-any.whl

---
+++ packages/nubium-utils/nubium_utils-0.1.0-py3-none-any.whl/nubium_utils/faust_utils/instrumented_app.py
@@ -0,0 +1,16 @@
+import logging
+
+import faust
+
+
+LOGGER = logging.getLogger()
+
+
+class InstrumentedApp(faust.App):
+    """
+    Faust app with metric of number of agent exceptions
+    """
+
+    async def _on_agent_error(self, agent: faust.agents.AgentT, exc: BaseException) -> None:
+        self.wrapper.agent_exception(exc)
+        await super(InstrumentedApp, self)._on_agent_error(agent, exc)
