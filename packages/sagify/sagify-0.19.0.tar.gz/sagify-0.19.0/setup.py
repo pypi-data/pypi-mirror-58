731b4efeaa6ee0d07ee81d9dcda2bfaae51084da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:18 +0000

    Add package sagify/0.19.0/sagify-0.19.0.tar.gz

---
+++ packages/sagify/sagify-0.19.0.tar.gz/sagify-0.19.0/setup.py
@@ -0,0 +1,55 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='sagify',
+    version='0.19.0',
+    setup_cfg=True,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    packages=find_packages(where='.'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    setup_requires=['setuptools>=39.1.0'],
+    url='https://github.com/Kenza-AI/sagify',
+    package_data={
+        'sagify': [
+            'template/sagify/config.json',
+            'template/sagify/*.sh',
+            'template/sagify/Dockerfile',
+            'template/sagify/__init__.py',
+            'template/sagify/training/__init__.py',
+            'template/sagify/training/train',
+            'template/sagify/training/*.py',
+            'template/sagify/prediction/*.py',
+            'template/sagify/prediction/serve',
+            'template/sagify/prediction/nginx.conf',
+            'template/sagify/local_test/*.sh',
+            'template/sagify/local_test/test_dir/output/.gitkeep',
+            'template/sagify/local_test/test_dir/model/.gitkeep',
+            'template/sagify/local_test/test_dir/input/config/*.json',
+            'template/sagify/local_test/test_dir/input/data/training/'
+            '.gitkeep'
+        ]
+    },
+    install_requires=[
+        'boto3==1.9.220',
+        'click>=7.0, <7.0.99',
+        'docker>=3.7.0, <3.7.99',
+        'flask>=1.1.0, <1.1.99',
+        'paramiko>=2.4.2, <2.4.99',
+        'pathlib2>=2.3.0, <2.3.99',
+        'requests>=2.20.0, <2.20.99',
+        'sagemaker>=1.38.0, <1.38.99',
+        'six>=1.10, <1.11.99',
+        'future>=0.16.0, <0.17.99'
+    ],
+    test_suite='tests',
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'sagify=sagify.__main__:cli',
+        ],
+    }
+)
---
+++ packages/sagify/sagify-0.19.0.tar.gz/sagify-0.19.0/tests/commands/test_initialize.py
@@ -0,0 +1,75 @@
+import os
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from click.testing import CliRunner
+
+from sagify.__main__ import cli
+
+
+def test_init_happy_case():
+    runner = CliRunner()
+
+    with patch(
+            'sagify.commands.initialize._get_local_aws_profiles',
+            return_value=['default', 'sagemaker']
+    ):
+        with runner.isolated_filesystem():
+            result = runner.invoke(cli=cli, args=['init'], input='my_app\ny\n1\n2\nus-east-1\nrequirements.txt\n')
+
+            assert os.path.isfile('src/__init__.py')
+            assert os.path.isdir('src/sagify')
+            assert os.path.isdir('src/sagify/training')
+            assert os.path.isdir('src/sagify/prediction')
+            assert os.path.isfile('src/sagify/__init__.py')
+            assert os.path.isfile('src/sagify/build.sh')
+            assert os.path.isfile('src/sagify/push.sh')
+            assert os.path.isfile('src/sagify/Dockerfile')
+            assert os.path.isfile('src/sagify/training/__init__.py')
+            assert os.path.isfile('src/sagify/training/train')
+            assert os.path.isfile('src/sagify/training/training.py')
+            assert os.path.isfile('src/sagify/prediction/__init__.py')
+            assert os.path.isfile('src/sagify/prediction/nginx.conf')
+            assert os.path.isfile('src/sagify/prediction/predictor.py')
+            assert os.path.isfile('src/sagify/prediction/prediction.py')
+            assert os.path.isfile('src/sagify/prediction/wsgi.py')
+            assert os.path.isfile('src/sagify/prediction/serve')
+            assert os.path.isfile('src/sagify/local_test/train_local.sh')
+            assert os.path.isdir('src/sagify/local_test/test_dir/input/data/training')
+            assert os.path.isfile('src/sagify/local_test/test_dir/input/config/hyperparameters.json')
+            assert os.path.isdir('src/sagify/local_test/test_dir/model')
+            assert os.path.isdir('src/sagify/local_test/test_dir/output')
+
+    assert result.exit_code == 0
+
+
+def test_init_when_directory_already_exists():
+    runner = CliRunner()
+    with patch(
+            'sagify.commands.initialize._get_local_aws_profiles',
+            return_value=['default', 'sagemaker']
+    ):
+        with runner.isolated_filesystem():
+            os.makedirs('my_app/sagify')
+
+            result = runner.invoke(
+                cli=cli,
+                args=['init'],
+                input='my_app\nN\nmy_app\n1\n2\nus-east-1\nrequirements.txt\n'
+            )
+
+    assert result.exit_code == -1
+
+
+def test_init_when_aws_cli_is_not_configure_locally():
+    runner = CliRunner()
+    with patch(
+            'sagify.commands.initialize._get_local_aws_profiles',
+            return_value=[]
+    ):
+        with runner.isolated_filesystem():
+            result = runner.invoke(cli=cli, args=['init'], input='my_app\ny\n1\n2\nrequirements.txt\n')
+
+    assert result.exit_code == 1
