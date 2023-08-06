bb0cbaf593e12d67d547a95b61cad0f30880f5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:38 +0000

    Add package fiduswriter-citation-api-import/3.7.1/fiduswriter-citation-api-import-3.7.1.tar.gz

---
+++ packages/fiduswriter-citation-api-import/fiduswriter-citation-api-import-3.7.1.tar.gz/fiduswriter-citation-api-import-3.7.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_namespace_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='fiduswriter-citation-api-import',
+    version='3.7.1',
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    license='AGPL License',
+    description='A Fidus Writer plugin to import citations via API.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://www.github.org/fiduswriter/fiduswriter-citation-api-import',
+    author='Johannes Wilm',
+    author_email='johannes@fiduswriter.org',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
