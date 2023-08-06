813508052c6034c480c21d0403e629613b3e9fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:09 +0000

    Add package setl/0.1.2/setl-0.1.2.tar.gz

---
+++ packages/setl/setl-0.1.2.tar.gz/setl-0.1.2/src/setl/cmds/clean.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def _handle(project, options) -> int:
+    with project.ensure_build_envdir(options.python) as _:
+        project.clean()
+    return 0
+
+
+def get_parser(subparsers) -> argparse.ArgumentParser:
+    parser = subparsers.add_parser(
+        "clean", description="Clean up temporary files from build"
+    )
+    parser.set_defaults(steps=None, func=_handle)
+    return parser
