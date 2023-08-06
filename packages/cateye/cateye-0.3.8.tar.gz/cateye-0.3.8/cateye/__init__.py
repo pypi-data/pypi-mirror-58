67b733b0e5c82aece420a8e87d88e70a407ed217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:41 +0000

    Add package cateye/0.3.8/cateye-0.3.8.tar.gz

---
+++ packages/cateye/cateye-0.3.8.tar.gz/cateye-0.3.8/cateye/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from cateye.cateye import *
+
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+__version__ = '0.3.8'
