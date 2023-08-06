6c2a74017e7c76610dad0d6b9da8479d75cbaffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package syrupy/0.0.10/syrupy-0.0.10.tar.gz

---
+++ packages/syrupy/syrupy-0.0.10.tar.gz/syrupy-0.0.10/tests/test_amber_serializer.py
@@ -0,0 +1,77 @@
+from collections import namedtuple
+
+import pytest
+
+
+def test_non_snapshots(snapshot):
+    with pytest.raises(AssertionError):
+        assert "Lorem ipsum." == "Muspi merol."
+
+
+def test_empty_snapshot(snapshot):
+    assert snapshot == None  # noqa: E711
+    assert snapshot == ""
+
+
+@pytest.mark.parametrize(
+    "actual",
+    [
+        "",
+        r"Raw string",
+        r"Escaped \n",
+        r"Backslash \u U",
+        "🥞🐍🍯",
+        "singleline:",
+        "- singleline",
+        "multi-line\nline 2\nline 3",
+        "multi-line\nline 2\n  line 3",
+    ],
+    ids=lambda x: "",
+)
+def test_string(snapshot, actual):
+    assert snapshot == actual
+
+
+def test_multiple_snapshots(snapshot):
+    assert "First." == snapshot
+    snapshot.assert_match("Second.")
+    assert snapshot == "Third."
+
+
+@pytest.mark.parametrize(
+    "actual",
+    [
+        {"b": True, "c": "Some text.", "d": ["1", 2], "a": {"e": False}},
+        {"b": True, "c": "Some ttext.", "d": ["1", 2], "a": {"e": False}},
+    ],
+)
+def test_dict(snapshot, actual):
+    assert actual == snapshot
+
+
+def test_set(snapshot):
+    assert snapshot == {"this", "is", "a", "set"}
+    assert snapshot == {"contains", "frozen", frozenset({"1", "2"})}
+
+
+ExampleTuple = namedtuple("ExampleTuple", ["a", "b", "c", "d"])
+
+
+def test_tuple(snapshot):
+    assert snapshot == ("this", "is", ("a", "tuple"))
+    assert snapshot == ExampleTuple(a="this", b="is", c="a", d={"named", "tuple"})
+
+
+def test_numbers(snapshot):
+    assert snapshot == 3.5
+    assert snapshot == 7
+    assert snapshot == 2 / 6
+
+
+def test_list(snapshot):
+    assert snapshot == [1, 2, "string", {"key": "value"}]
+
+
+class TestClass:
+    def test_name(self, snapshot):
+        assert snapshot == "this is in a test class"
