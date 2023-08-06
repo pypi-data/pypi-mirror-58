6a3677a26f44a5bddfcf2e108ebc87d0dd2cf192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:13 +0000

    Add package torch-template/0.0.3/torch-template-0.0.3.tar.gz

---
+++ packages/torch-template/torch-template-0.0.3.tar.gz/torch-template-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import re
+import setuptools
+from setuptools import setup
+
+with open('torch_template/version.py') as fid:
+    try:
+        __version__, = re.findall( '__version__ = "(.*)"', fid.read() )
+    except:
+        raise ValueError("could not find version number")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torch-template',
+    version=__version__,
+    description='Torch_Template - A PyTorch template with commonly used models and tools',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/misads/torch_template',
+    author='Haoyu Xu',
+    author_email='xuhaoyu@tju.edu.cn',
+    license='MIT',
+    install_requires=[
+        "torch",
+        "numpy",
+        "torchvision",
+        "tensorboardX",
+    ],
+    packages=['torch_template', 'torch_template/utils'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X"
+    ],
+    python_requires='>=3.5',
+)
