3a5751e62534641a7a259e117923e28606e31b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:24 +0000

    Add package cloudmesh-cloud/4.2.0/cloudmesh_cloud-4.2.0-py2.py3-none-any.whl

---
+++ packages/cloudmesh-cloud/cloudmesh_cloud-4.2.0-py2.py3-none-any.whl/cloudmesh/cloud/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "4.2.0"
+
+from cloudmesh.common.debug import VERBOSE
+from cloudmesh.configuration.Config import Config
+from cloudmesh.common.parameter import Parameter
+from cloudmesh.common.console import Console
+from cloudmesh.common.StopWatch import StopWatch
+from cloudmesh.common.variables import Variables
+from cloudmesh.common3.Shell import Shell
+import cloudmesh.common.notebook
