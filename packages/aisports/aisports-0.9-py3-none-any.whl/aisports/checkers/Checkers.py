2a1cbffb8bb8d52d6cfe470e811aed4fe2ee45fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package aisports/0.9/aisports-0.9-py3-none-any.whl

---
+++ packages/aisports/aisports-0.9-py3-none-any.whl/aisports/checkers/Checkers.py
@@ -0,0 +1,13 @@
+class Checkers:
+    def __init__(self, api_key=""):
+        pass
+
+    def get_state(self):
+        pass
+
+    def on_turn(self, callback):
+        pass
+
+    def start(self):
+        print("Searching for opponent...")
+        pass
