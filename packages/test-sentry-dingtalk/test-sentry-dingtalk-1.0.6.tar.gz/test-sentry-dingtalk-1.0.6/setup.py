7390e0a4f8c53f31f820e3cb6ff7c62a2b5b8b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:50 +0000

    Add package test-sentry-dingtalk/1.0.6/test-sentry-dingtalk-1.0.6.tar.gz

---
+++ packages/test-sentry-dingtalk/test-sentry-dingtalk-1.0.6.tar.gz/test-sentry-dingtalk-1.0.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="test-sentry-dingtalk",
+    version='1.0.6',
+    author='zhaodazao',
+    author_email='zhaodazao@gmail.com',
+    url='https://code.aliyun.com/PlayGoods-Materiel/wanwu-senstry-dingtalk.git',
+    description='A Sentry extension which send errors stats to DingDing test',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry dingding test',
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
+    entry_points={
+        'sentry.plugins': [
+            'sentry_dingding = sentry_dingding.plugin:DingDingPlugin'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+    ]
+)
