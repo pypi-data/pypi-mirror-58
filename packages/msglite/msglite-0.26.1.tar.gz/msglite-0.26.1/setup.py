e23cda47ad13a3b30258e61573ab94a0638a5678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:15 +0000

    Add package msglite/0.26.1/msglite-0.26.1.tar.gz

---
+++ packages/msglite/msglite-0.26.1.tar.gz/msglite-0.26.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='msglite',
+    version='0.26.1',
+    description="Extracts emails and attachments saved in Microsoft Outlook's .msg files",  # noqa
+    url='https://github.com/alephdata/msglite',
+    author='OCCRP',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='GPL',
+    packages=['msglite'],
+    py_modules=['msglite'],
+    include_package_data=True,
+    install_requires=[
+        'olefile>=0.46',
+        'chardet',
+        'pytz',
+    ],
+)
