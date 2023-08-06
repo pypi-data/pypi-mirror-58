15df37e4b21c6d1412708f0a5e860f4796bada01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:11 +0000

    Add package cinderlib/1.0.1/cinderlib-1.0.1-py2.py3-none-any.whl

---
+++ packages/cinderlib/cinderlib-1.0.1-py2.py3-none-any.whl/cinderlib/persistence/__init__.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2018, Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import absolute_import
+import inspect
+
+from cinder.cmd import volume as volume_cmd
+import six
+from stevedore import driver
+
+from cinderlib import exception
+from cinderlib.persistence import base
+
+
+DEFAULT_STORAGE = 'memory'
+
+
+class MyDict(dict):
+    """Custom non clearable dictionary.
+
+    Required to overcome the nature of oslo.config where configuration comes
+    from files and command line input.
+
+    Using this dictionary we can load from memory everything and it won't clear
+    things when we dynamically load a driver and the driver has new
+    configuration options.
+    """
+    def clear(self):
+        pass
+
+
+def setup(config):
+    """Setup persistence to be used in cinderlib.
+
+    By default memory persistance will be used, but there are other mechanisms
+    available and other ways to use custom mechanisms:
+
+    - Persistence plugins: Plugin mechanism uses Python entrypoints under
+      namespace cinderlib.persistence.storage, and cinderlib comes with 3
+      different mechanisms, "memory", "dbms", and "memory_dbms".  To use any of
+      these one must pass the string name in the storage parameter and any
+      other configuration as keyword arguments.
+    - Passing a class that inherits from PersistenceDriverBase as storage
+      parameter and initialization parameters as keyword arguments.
+    - Passing an instance that inherits from PersistenceDriverBase as storage
+      parameter.
+    """
+    if config is None:
+        config = {}
+    else:
+        config = config.copy()
+
+    # Prevent driver dynamic loading clearing configuration options
+    volume_cmd.CONF._ConfigOpts__cache = MyDict()
+
+    # Default configuration is using memory storage
+    storage = config.pop('storage', None) or DEFAULT_STORAGE
+    if isinstance(storage, base.PersistenceDriverBase):
+        return storage
+
+    if inspect.isclass(storage) and issubclass(storage,
+                                               base.PersistenceDriverBase):
+        return storage(**config)
+
+    if not isinstance(storage, six.string_types):
+        raise exception.InvalidPersistence(storage)
+
+    persistence_driver = driver.DriverManager(
+        namespace='cinderlib.persistence.storage',
+        name=storage,
+        invoke_on_load=True,
+        invoke_kwds=config,
+    )
+    return persistence_driver.driver
---
+++ packages/cinderlib/cinderlib-1.0.1-py2.py3-none-any.whl/cinderlib/tests/unit/persistence/test_dbms.py
@@ -0,0 +1,123 @@
+# Copyright (c) 2018, Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import tempfile
+
+from cinder.db.sqlalchemy import api as sqla_api
+from cinder import objects as cinder_ovos
+from oslo_db import api as oslo_db_api
+
+import cinderlib
+from cinderlib.persistence import dbms
+from cinderlib.tests.unit.persistence import base
+
+
+class TestDBPersistence(base.BasePersistenceTest):
+    CONNECTION = 'sqlite:///' + tempfile.NamedTemporaryFile().name
+    PERSISTENCE_CFG = {'storage': 'db',
+                       'connection': CONNECTION}
+
+    def tearDown(self):
+        sqla_api.model_query(self.context, sqla_api.models.Snapshot).delete()
+        sqla_api.model_query(self.context,
+                             sqla_api.models.VolumeAttachment).delete()
+        sqla_api.model_query(self.context,
+                             sqla_api.models.Volume).delete()
+        sqla_api.get_session().query(dbms.KeyValue).delete()
+        super(TestDBPersistence, self).tearDown()
+
+    def test_db(self):
+        self.assertIsInstance(self.persistence.db,
+                              oslo_db_api.DBAPI)
+
+    def test_set_volume(self):
+        res = sqla_api.volume_get_all(self.context)
+        self.assertListEqual([], res)
+
+        vol = cinderlib.Volume(self.backend, size=1, name='disk')
+        expected = {'availability_zone': vol.availability_zone,
+                    'size': vol.size, 'name': vol.name}
+
+        self.persistence.set_volume(vol)
+
+        db_vol = sqla_api.volume_get(self.context, vol.id)
+        actual = {'availability_zone': db_vol.availability_zone,
+                  'size': db_vol.size, 'name': db_vol.display_name}
+
+        self.assertDictEqual(expected, actual)
+
+    def test_set_snapshot(self):
+        vol = cinderlib.Volume(self.backend, size=1, name='disk')
+        snap = cinderlib.Snapshot(vol, name='disk')
+
+        self.assertEqual(0, len(sqla_api.snapshot_get_all(self.context)))
+
+        self.persistence.set_snapshot(snap)
+
+        db_entries = sqla_api.snapshot_get_all(self.context)
+        self.assertEqual(1, len(db_entries))
+
+        ovo_snap = cinder_ovos.Snapshot(self.context)
+        ovo_snap._from_db_object(ovo_snap._context, ovo_snap, db_entries[0])
+        cl_snap = cinderlib.Snapshot(vol, __ovo=ovo_snap)
+
+        self.assertEqualObj(snap, cl_snap)
+
+    def test_set_connection(self):
+        vol = cinderlib.Volume(self.backend, size=1, name='disk')
+        conn = cinderlib.Connection(self.backend, volume=vol, connector={},
+                                    connection_info={'conn': {'data': {}}})
+
+        self.assertEqual(0,
+                         len(sqla_api.volume_attachment_get_all(self.context)))
+
+        self.persistence.set_connection(conn)
+
+        db_entries = sqla_api.volume_attachment_get_all(self.context)
+        self.assertEqual(1, len(db_entries))
+
+        ovo_conn = cinder_ovos.VolumeAttachment(self.context)
+        ovo_conn._from_db_object(ovo_conn._context, ovo_conn, db_entries[0])
+        cl_conn = cinderlib.Connection(vol.backend, volume=vol, __ovo=ovo_conn)
+
+        self.assertEqualObj(conn, cl_conn)
+
+    def test_set_key_values(self):
+        res = sqla_api.get_session().query(dbms.KeyValue).all()
+        self.assertListEqual([], res)
+
+        expected = [dbms.KeyValue(key='key', value='value')]
+        self.persistence.set_key_value(expected[0])
+
+        actual = sqla_api.get_session().query(dbms.KeyValue).all()
+        self.assertListEqualObj(expected, actual)
+
+    def test_create_volume_with_default_volume_type(self):
+        vol = cinderlib.Volume(self.backend, size=1, name='disk')
+        self.persistence.set_volume(vol)
+        self.assertEqual(self.persistence.DEFAULT_TYPE.id, vol.volume_type_id)
+        self.assertIs(self.persistence.DEFAULT_TYPE, vol.volume_type)
+        res = sqla_api.volume_type_get(self.context, vol.volume_type_id)
+        self.assertIsNotNone(res)
+        self.assertEqual('__DEFAULT__', res['name'])
+
+    def test_default_volume_type(self):
+        self.assertIsInstance(self.persistence.DEFAULT_TYPE,
+                              cinder_ovos.VolumeType)
+        self.assertEqual('__DEFAULT__', self.persistence.DEFAULT_TYPE.name)
+
+
+class TestMemoryDBPersistence(TestDBPersistence):
+    PERSISTENCE_CFG = {'storage': 'memory_db'}
