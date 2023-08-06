34587238050785e0664e02a2bbb130de9795f4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:39 +0000

    Add package spineapi/0.0.1/spineapi-0.0.1.tar.gz

---
+++ packages/spineapi/spineapi-0.0.1.tar.gz/spineapi-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spineapi",
+    version="0.0.1",
+    author="northfoxz",
+    author_email="firstera15@gmail.com",
+    description="A simple machine learning inference library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spineapi/spine-api",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "python-socketio[client]"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
