6752b42e4dbfafe4af46b6846fc639295bc0cd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:44 +0000

    Add package salang-saara/0.4.1/salang-saara-0.4.1.tar.gz

---
+++ packages/salang-saara/salang-saara-0.4.1.tar.gz/salang-saara-0.4.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+REQUIRED = [
+    "tabulate",
+    "simplejson",
+    "fire",
+]
+
+setup(name='salang-saara',
+      version='0.4.1',
+      description='A code for transliterating (romanizing) Arabic text using the ALA-LC standard',
+      author='CompMusic / MTG UPF',
+      url='https://github.com/samlet/saara',
+      install_requires=REQUIRED,
+      include_package_data=True,
+      packages=['aranasyn', 'arramooz', 'asmai', 'collocations', 'collocations.pyarabic', 'mishkal', 'mishkal.tashkeel', 'pyarabic', 'naftawayh', 'tashaphyne', 'qalsadi', 'qalsadi.libqutrub', 'arabic', 'CodernityDB'],
+
+      package_dir={
+              'tashaphyne':'tashaphyne',
+              'naftawayh':'naftawayh',
+              'pyarabic':'pyarabic',
+              'qalsadi':'qalsadi',
+              'libqutrub':'qalsadi/libqutrub',
+              'arabic':'',
+              'mishkal':'mishkal',
+              'saara':'saara',
+              },
+
+      package_data={
+              'arramooz': ['data/*'],
+              'collocations': ['data/*']}
+)
