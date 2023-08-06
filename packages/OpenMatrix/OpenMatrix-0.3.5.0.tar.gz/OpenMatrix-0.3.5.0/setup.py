69ffdc756d3631b7633062e6f35d2a39debc17e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:13 +0000

    Add package OpenMatrix/0.3.5.0/OpenMatrix-0.3.5.0.tar.gz

---
+++ packages/OpenMatrix/OpenMatrix-0.3.5.0.tar.gz/OpenMatrix-0.3.5.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# To push to PyPi/pip, use
+# python setup.py sdist bdist_wheel upload
+
+setup(
+    name='OpenMatrix',
+    keywords='openmatrix omx',
+    version='0.3.5.0',
+    author='Billy Charlton, Ben Stabler',
+    author_email='billy@okbecause.com, benstabler@yahoo.com',
+    packages=find_packages(),
+    url='https://github.com/osPlanning/omx',
+    license='Apache',
+    description='OMX, the open matrix data format',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "tables >= 3.1.0",
+        "numpy >= 1.5.0",
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ],
+    entry_points={
+        'console_scripts': [
+            'omx-validate = openmatrix.validator:command_line',
+        ],
+    }
+
+)
