7424c5ec49c537a957bb0e6a55753f592aa9bbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:29 +0000

    Add package jn-snapaa/1.0.12/jn_snapaa-1.0.12-py3-none-any.whl

---
+++ packages/jn-snapaa/jn_snapaa-1.0.12-py3-none-any.whl/snaper/snaper_tools.py
@@ -0,0 +1,6 @@
+import subprocess
+def easy_snap(source_path,dest_path,phantomjs_absolute_path="phantomjs.exe"):
+    subprocess.Popen(phantomjs_path+" snap.js "+source_name+" "+dest_name, shell=True, stderr=subprocess.STDOUT)
+
+
+
