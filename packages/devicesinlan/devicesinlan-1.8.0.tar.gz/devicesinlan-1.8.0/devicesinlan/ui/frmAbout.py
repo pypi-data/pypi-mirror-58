a39773ee4852c7ca7d6f1952c4d9454326a9f2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:42 +0000

    Add package devicesinlan/1.8.0/devicesinlan-1.8.0.tar.gz

---
+++ packages/devicesinlan/devicesinlan-1.8.0.tar.gz/devicesinlan-1.8.0/devicesinlan/ui/frmAbout.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QDialog
+from devicesinlan.ui.Ui_frmAbout import Ui_frmAbout
+from devicesinlan.version import __version__, __versiondate__
+
+class frmAbout(QDialog, Ui_frmAbout):
+    def __init__(self, parent = None, name = None, modal = False):
+        QDialog.__init__(self, parent)
+        if name:
+            self.setObjectName(name)
+        self.setupUi(self)
+        self.lblVersion.setText(self.tr("Version {} ({})".format(__version__,  __versiondate__)))
+        self.textBrowser.setHtml(
+            self.tr("""Project web page is in <a href="http://github.com/turulomio/devicesinlan/">http://github.com/turulomio/devicesinlan/</a><p> <p>""")+
+            self.tr("This program has been developed by Mariano Mu\xf1oz.<p>")+
+            self.tr("It has been translated by:")+
+            "<ul><li>Mariano Mu\xf1oz</li><li>Nadejda Adam</li></ul><p>\n"+
+            self.tr("to the following languages<p>")+
+            "<ul><li>English</li><li>Fran\xe7ais</li><li>Espa\xf1ol</li><li>Rom\xe2n</li><li>\u0420\u0443\u0441\u0441\u043a\u0438\u0439</li></ul><p>")
+
+
