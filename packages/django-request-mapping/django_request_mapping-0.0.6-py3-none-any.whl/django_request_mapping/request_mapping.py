555afb17f908cdcca3a52c8efae8b4dd87a3d45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:42 +0000

    Add package django-request-mapping/0.0.6/django_request_mapping-0.0.6-py3-none-any.whl

---
+++ packages/django-request-mapping/django_request_mapping-0.0.6-py3-none-any.whl/django_request_mapping/request_mapping.py
@@ -0,0 +1,11 @@
+"""
+@author: sazima
+@time: 2019/8/9 下午20:03
+@desc:
+"""
+
+
+class RequestMapping:
+    def __init__(self, value, method):
+        self.value = value
+        self.method = method
