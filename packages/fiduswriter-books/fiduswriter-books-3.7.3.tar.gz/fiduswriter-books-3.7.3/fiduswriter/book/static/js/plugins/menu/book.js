2b5b742a0e4a545ed0a62ed530a506f8e438a2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:38 +0000

    Add package fiduswriter-books/3.7.3/fiduswriter-books-3.7.3.tar.gz

---
+++ packages/fiduswriter-books/fiduswriter-books-3.7.3.tar.gz/fiduswriter-books-3.7.3/fiduswriter/book/static/js/plugins/menu/book.js
@@ -0,0 +1,17 @@
+// Adds a link to the book creator to the main menu on the overview pages.
+
+export class BookMenuItem {
+    constructor(menu) {
+        this.menu = menu
+    }
+
+    init() {
+        this.menu.navItems.push({
+            id: "books",
+            title: gettext('compose books'),
+            url: "/book/",
+            text: gettext('Books')
+        })
+    }
+
+}
