eaa31c34374406c2acd868bfa2a77633bf40b566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:18 +0000

    Add package xmind2testcase-interchen/1.3.3/xmind2testcase-interchen-1.3.3.tar.gz

---
+++ packages/xmind2testcase-interchen/xmind2testcase-interchen-1.3.3.tar.gz/xmind2testcase-interchen-1.3.3/xmind2testcase/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'xmind2testcase-interchen'
+__description__ = 'XMind2TestCase基于Python实现，提供了一个高效测试用例设计的解决方案！ forked from zhuifengshen/xmind2testcase'
+__keywords__ = 'xmind2testCase, testcase, test, testing, xmind, 思维导图, XMind思维导图',
+__url__ = 'https://github.com/interchen/xmind2testcase'
+__author__ = 'interchen'
+__author_email__ = 'inter.chen@gmail.com'
+__version__ = '1.3.3'
+__license__ = 'MIT'
+__cake__ = u'\u2728 \U0001f370 \u2728'
