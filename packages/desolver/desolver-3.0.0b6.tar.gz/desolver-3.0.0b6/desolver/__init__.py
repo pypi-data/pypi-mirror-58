1670fd66e0f100d1d7d7d78ec0bc48eb98c07afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:57 +0000

    Add package desolver/3.0.0b6/desolver-3.0.0b6.tar.gz

---
+++ packages/desolver/desolver-3.0.0b6.tar.gz/desolver-3.0.0b6/desolver/__init__.py
@@ -0,0 +1,15 @@
+from . import backend
+from . import exception_types
+
+from .differential_system import DiffRHS, rhs_prettifier, OdeSystem
+from . import utilities
+from . import integration_schemes
+from .integration_schemes import available_methods
+
+try:
+    # Pytest testing
+    from numpy._pytesttester import PytestTester
+    test = PytestTester(__name__)
+    del PytestTester
+except:
+    pass
