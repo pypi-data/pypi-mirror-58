5822c8961fa3df3ca6b914c9ce49de4a3d6dc03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:34 +0000

    Add package scrapy-do/0.4.0/scrapy_do-0.4.0-py3-none-any.whl

---
+++ packages/scrapy-do/scrapy_do-0.4.0-py3-none-any.whl/scrapy_do/ui/precache-manifest.3268877b79ed2c92165cd123f7493889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5351d3e613137ade080bb8b5e5bb0e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3913a30f36d5c709ce56",
+    "url": "/static/css/2.9521d75d.chunk.css"
+  },
+  {
+    "revision": "41d2aae35ec6cea787fb",
+    "url": "/static/css/main.91a93399.chunk.css"
+  },
+  {
+    "revision": "3913a30f36d5c709ce56",
+    "url": "/static/js/2.af4dbd70.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.af4dbd70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41d2aae35ec6cea787fb",
+    "url": "/static/js/main.79a16d2d.chunk.js"
+  },
+  {
+    "revision": "2c095eefcecc79b4387a",
+    "url": "/static/js/runtime-main.9dcbf164.js"
+  }
+]);
