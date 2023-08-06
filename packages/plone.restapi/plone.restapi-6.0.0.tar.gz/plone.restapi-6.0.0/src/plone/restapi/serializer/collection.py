f5187d3479226f59f6a06f2b610e3aec8a7861e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:41 +0000

    Add package plone.restapi/6.0.0/plone.restapi-6.0.0.tar.gz

---
+++ packages/plone.restapi/plone.restapi-6.0.0.tar.gz/plone.restapi-6.0.0/src/plone/restapi/serializer/collection.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.interfaces import ICollection
+from plone.restapi.batching import HypermediaBatch
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.interfaces import ISerializeToJsonSummary
+from plone.restapi.serializer.dxcontent import SerializeToJson
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+from zope.interface import Interface
+
+
+@implementer(ISerializeToJson)
+@adapter(ICollection, Interface)
+class SerializeCollectionToJson(SerializeToJson):
+    def __call__(self, version=None, include_items=True):
+        collection_metadata = super(SerializeCollectionToJson, self).__call__(
+            version=version
+        )
+        results = self.context.results(batch=False)
+        batch = HypermediaBatch(self.request, results)
+
+        results = collection_metadata
+        if not self.request.form.get("fullobjects"):
+            results["@id"] = batch.canonical_url
+        results["items_total"] = batch.items_total
+        if batch.links:
+            results["batching"] = batch.links
+
+        results["items"] = [
+            getMultiAdapter((brain, self.request), ISerializeToJsonSummary)()
+            for brain in batch
+        ]
+        return results
