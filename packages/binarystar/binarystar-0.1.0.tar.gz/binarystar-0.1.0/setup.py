e84dc9bcc71b5c0c94f3c62562656ae8e4bece52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:07 +0000

    Add package binarystar/0.1.0/binarystar-0.1.0.tar.gz

---
+++ packages/binarystar/binarystar-0.1.0.tar.gz/binarystar-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='binarystar',
+      version='0.1.0',
+      description='High level machine learning models training package',
+      url='https://github.com/ermenkov/binarystar',
+      author='Viktor Eremnkov',
+      author_email='viktor.ermenkov@gmail.com',
+      license='MIT',
+      packages=['binarystar'],
+      zip_safe=False)
