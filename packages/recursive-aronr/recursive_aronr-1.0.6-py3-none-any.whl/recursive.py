64ee5931f2f1ad6f0c18dfb4ea4c50995fb9d1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:55 +0000

    Add package recursive-aronr/1.0.6/recursive_aronr-1.0.6-py3-none-any.whl

---
+++ packages/recursive-aronr/recursive_aronr-1.0.6-py3-none-any.whl/recursive.py
@@ -0,0 +1,19 @@
+def list_recursive(src_list, level=0):
+
+    if level > 0:
+        idnt = 0
+        ident = ''
+        while idnt < level:
+            ident += ' '
+            idnt = idnt + 1
+    else:
+        """There is no indicates"""
+
+    for item in src_list:
+        if isinstance(item, list):
+            list_recursive(item, level + 1)
+        else:
+            if level > 0:
+                print(ident + item)
+            else:
+                print(item)
