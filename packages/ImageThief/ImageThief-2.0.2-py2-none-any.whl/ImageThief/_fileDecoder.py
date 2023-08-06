c6fb3677297641465ce4349361d68dd466d3a2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package ImageThief/2.0.2/ImageThief-2.0.2-py2-none-any.whl

---
+++ packages/ImageThief/ImageThief-2.0.2-py2-none-any.whl/ImageThief/_fileDecoder.py
@@ -0,0 +1,18 @@
+from pickle import loads as pickleLoader
+from ._globalDict import Data, reExp
+
+import re
+
+
+def Decoder(unicodeInput, filename, response=None):
+    if response is None:
+        response = {'extension': ''}
+    userExt = re.findall(reExp, filename)
+    if userExt != response.get('extension', ''):
+        print('Extension doesnt match')
+        return
+    f = open(filename, 'wb')
+    data = pickleLoader(unicodeInput)
+    for i in data:
+        f.write(i)
+    f.close()
