eaf89bf16f739a2df342f84223e12676aa054d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:12 +0000

    Add package gaus-bi-distrib/1.0/gaus_bi_distrib-1.0.tar.gz

---
+++ packages/gaus-bi-distrib/gaus_bi_distrib-1.0.tar.gz/gaus_bi_distrib-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_bi_distrib',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_bi_distrib'],
+      author = 'S. M. Asif Mahfuz',
+      author_email = 'asif.mahfuz001@gmail.com',
+      zip_safe=False)
