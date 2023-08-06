2a65003621d74a8d04a7c848ce9a2abfe27acf41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:27 +0000

    Add package pamqp/3.0.0a2/pamqp-3.0.0a2.tar.gz

---
+++ packages/pamqp/pamqp-3.0.0a2.tar.gz/pamqp-3.0.0a2/pamqp/common.py
@@ -0,0 +1,35 @@
+import datetime
+import decimal
+import struct
+import time
+import typing
+
+Timestamp = typing.Union[datetime.datetime, time.struct_time]
+FieldArray = typing.List['FieldValue']  # type: ignore
+FieldTable = typing.Dict[str, 'FieldValue']  # type: ignore
+FieldValue = typing.Union[bool,  # type: ignore
+                          bytearray,
+                          decimal.Decimal,
+                          FieldArray,
+                          FieldTable,
+                          float,
+                          int,
+                          None,
+                          str,
+                          Timestamp]
+
+
+class Struct:
+    """Simple object for getting to the struct objects"""
+    byte = struct.Struct('B')
+    double = struct.Struct('>d')
+    float = struct.Struct('>f')
+    integer = struct.Struct('>I')
+    long = struct.Struct('>l')
+    long_long_int = struct.Struct('>q')
+    short = struct.Struct('>h')
+    short_short = struct.Struct('>B')
+    timestamp = struct.Struct('>Q')
+    uint = struct.Struct('>i')
+    ulong = struct.Struct('>L')
+    ushort = struct.Struct('>H')
