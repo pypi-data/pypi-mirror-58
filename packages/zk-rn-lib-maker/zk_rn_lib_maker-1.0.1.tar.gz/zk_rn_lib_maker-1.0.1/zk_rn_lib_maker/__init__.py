3357305751d1578345308c02b7b63cfc043322b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:19 +0000

    Add package zk-rn-lib-maker/1.0.1/zk_rn_lib_maker-1.0.1.tar.gz

---
+++ packages/zk-rn-lib-maker/zk_rn_lib_maker-1.0.1.tar.gz/zk_rn_lib_maker-1.0.1/zk_rn_lib_maker/__init__.py
@@ -0,0 +1,16 @@
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "zk-rn-lib-maker"
+__summary__ = "create react-native lib for zhike"
+__uri__ = "https://gitee.com/smartstudy/zk_rn_lib_maker.git"
+
+__version__ = "1.0.0"
+
+__author__ = "JeffreyCao"
+__email__ = "je255reycao@outlook.com"
+
+__license__ = "MIT Licence"
+__copyright__ = "Copyright 2019 JeffreyCao"
