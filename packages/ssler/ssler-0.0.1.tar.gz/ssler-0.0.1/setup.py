c6f2021e3e3f33a58a8be0ac63d51978a8a1c7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:02 +0000

    Add package ssler/0.0.1/ssler-0.0.1.tar.gz

---
+++ packages/ssler/ssler-0.0.1.tar.gz/ssler-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:  # description to be used in pypi project page
+    long_description = fh.read()
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+install_requires = ['flask', 'requests']  # any requirements your package has
+
+setuptools.setup(
+    name="ssler",
+    version="0.0.1",
+    author="Abin Simon",
+    author_email="abinsimon10@gmail.com",
+    description="Easy SSL for your localhost server",
+    url="https://github.com/meain/ssler",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["ssler"],
+    install_requires=install_requires,
+    keywords=["ssl", "python", "localhost"],
+    classifiers=classifiers,
+    entry_points={"console_scripts": ["ssler = ssler.ssler:main"]},
+)
