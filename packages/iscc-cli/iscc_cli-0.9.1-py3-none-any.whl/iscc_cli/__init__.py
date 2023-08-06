4962e28e83066e3e4ec04e5a5a27d4e0bfa0bc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:53 +0000

    Add package iscc-cli/0.9.1/iscc_cli-0.9.1-py3-none-any.whl

---
+++ packages/iscc-cli/iscc_cli-0.9.1-py3-none-any.whl/iscc_cli/__init__.py
@@ -0,0 +1,11 @@
+import iscc_cli.monkeys
+import os
+import click
+
+__version__ = "0.9.1"
+
+APP_NAME = "iscc-cli"
+APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
+os.makedirs(iscc_cli.APP_DIR, exist_ok=True)
+
+os.environ["TIKA_PATH"] = APP_DIR
