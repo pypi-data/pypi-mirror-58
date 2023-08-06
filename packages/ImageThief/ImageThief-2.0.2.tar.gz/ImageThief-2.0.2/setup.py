ddf038c5b379f3537fdf33f6b600694bfd1c6927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package ImageThief/2.0.2/ImageThief-2.0.2.tar.gz

---
+++ packages/ImageThief/ImageThief-2.0.2.tar.gz/ImageThief-2.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+VERSION = "2.0.2"
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+setup(
+    name="ImageThief",
+    version=VERSION,
+    author="Dhanush",
+    author_email="dhanushdazz@gmail.com",
+    description="Get files from server running on default route",
+    long_description=long_description,
+    url="",
+    packages=['ImageThief'],
+    install_requires=["flask", "requests"],
+    keywords=['raspberrypi', 'image-processing', 'image-getter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
