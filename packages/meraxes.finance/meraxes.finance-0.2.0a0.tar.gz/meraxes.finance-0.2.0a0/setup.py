791be1f97cb174608c9f2f161299d216a09958fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package meraxes.finance/0.2.0a0/meraxes.finance-0.2.0a0.tar.gz

---
+++ packages/meraxes.finance/meraxes.finance-0.2.0a0.tar.gz/meraxes.finance-0.2.0a0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from meraxes.finance import __version__
+
+
+setup(
+    name='meraxes.finance',
+    version=__version__,
+
+    author='Stefan Bunde',
+    author_email='stefanbunde+git@gmail.com',
+
+    packages=['meraxes.finance'],
+
+    install_requires=[
+        'Django>=2',
+        'djangorestframework>=3.8',
+    ],
+    extras_require={
+        'release': [
+            'twine',
+        ],
+        'testing': [
+            'tox',
+        ],
+    },
+)
