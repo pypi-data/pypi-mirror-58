8b437fae99e624bbf5d9bc8d23d59ac07970a724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package infinity4py/0.0.1/infinity4py-0.0.1.tar.gz

---
+++ packages/infinity4py/infinity4py-0.0.1.tar.gz/infinity4py-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+import sys
+ 
+setup(
+    name="infinity4py",
+    version="0.0.1",
+    author="Chenwe-i-lin",
+    author_email="1846913566@qq.com",
+    description="infinity for python",
+    license="MIT",
+    url="https://github.com/NatriumLab/infinity4py",
+    packages=['infinity4py'],
+)
