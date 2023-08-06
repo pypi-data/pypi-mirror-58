879fae849f51beecc0dc9abf677c1be6788c6151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:19 +0000

    Add package pytokenizations/0.1.0/pytokenizations-0.1.0.tar.gz

---
+++ packages/pytokenizations/pytokenizations-0.1.0.tar.gz/src/lib.rs
@@ -0,0 +1,18 @@
+use pyo3::prelude::*;
+use tokenizations::{get_alignments, Alignment};
+
+#[pymodule]
+fn tokenizations(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", "0.1.0")?;
+
+    #[pyfn(m, "get_alignments")]
+    pub fn get_alignments_py(
+        _py: Python,
+        a: Vec<&str>,
+        b: Vec<&str>,
+    ) -> PyResult<(Alignment, Alignment)> {
+        Ok(get_alignments(&a, &b))
+    }
+
+    Ok(())
+}
