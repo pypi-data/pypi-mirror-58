181541146c6f2143a6c702d24925053ee8af36ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:13 +0000

    Add package py-tratto/0.0.3/py-tratto-0.0.3.tar.gz

---
+++ packages/py-tratto/py-tratto-0.0.3.tar.gz/py-tratto-0.0.3/setup.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="py-tratto",
+    version="0.0.3",
+    packages=find_packages(),
+    license="BSD 3-clause",
+    install_requires=['pexpect'],
+    author="HoxinhLuo",
+    author_email="2577229471@qq.com",
+    url="https://github.com/HoxinhLuo/pytratto",
+    description="Pytratto is a framework built to issue commands to cisco or huawei routers over ssh and telnet which requires pexpect.", 
+    summary="Pytratto is a framework built to issue commands to cisco or huawei routers over ssh and telnet which requires pexpect."
+)
