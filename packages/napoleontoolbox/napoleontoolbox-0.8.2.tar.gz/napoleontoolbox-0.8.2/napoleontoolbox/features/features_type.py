ee5710981cfbf4f18ef40efc75dedfa7cd5129b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package napoleontoolbox/0.8.2/napoleontoolbox-0.8.2.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-0.8.2.tar.gz/napoleontoolbox-0.8.2/napoleontoolbox/features/features_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class FeaturesType(Enum):
+    NONE = 1
+    RAW = 2
+    RAW_ADVANCED = 3
+    STANDARD = 4
+    STANDARD_STATIONARIZED = 5
+    STANDARD_STATIONARIZED_NORMALIZED = 6
+    ADVANCED = 7
