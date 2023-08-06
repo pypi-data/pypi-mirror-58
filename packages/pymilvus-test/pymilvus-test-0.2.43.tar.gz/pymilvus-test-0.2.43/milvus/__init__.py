a9280cdfc09414f48adc7cf6569426535b3c0fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:56 +0000

    Add package pymilvus-test/0.2.43/pymilvus-test-0.2.43.tar.gz

---
+++ packages/pymilvus-test/pymilvus-test-0.2.43.tar.gz/pymilvus-test-0.2.43/milvus/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .client.grpc_client import GrpcMilvus as Milvus
+from .client.prepare import Prepare
+from .client.types import IndexType, MetricType, Status
+from .client.exceptions import (
+    ParamError,
+    ConnectError,
+    NotConnectError,
+    RepeatingConnectError
+)
+from .client import __version__
+
+__all__ = ['Milvus', 'Prepare', 'Status', 'IndexType', 'MetricType',
+           'ParamError', 'ConnectError', 'NotConnectError', 'RepeatingConnectError',
+           '__version__']
