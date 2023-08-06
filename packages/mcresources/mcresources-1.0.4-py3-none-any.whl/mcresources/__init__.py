4011d7d5f3ff705699b0043f5e4ce25311a9b353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:35 +0000

    Add package mcresources/1.0.4/mcresources-1.0.4-py3-none-any.whl

---
+++ packages/mcresources/mcresources-1.0.4-py3-none-any.whl/mcresources/__init__.py
@@ -0,0 +1,6 @@
+#  Part of mcresources by Alex O'Neill
+#  Work under copyright. Licensed under MIT
+#  For more information see the project LICENSE file
+
+from mcresources.resource_manager import ResourceManager
+from mcresources.util import clean_generated_resources
