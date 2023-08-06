765beb76e7cf56c0355157632ab00d0bc30e5060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:52 +0000

    Add package suanpan/0.10.21/suanpan-0.10.21.tar.gz

---
+++ packages/suanpan/suanpan-0.10.21.tar.gz/suanpan-0.10.21/suanpan/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.g import g
+from suanpan.run import cli, env, run
+
+__version__ = "0.10.21"
+
+
+if __name__ == "__main__":
+    print("Suanpan SDK (ver: {})".format(__version__))
