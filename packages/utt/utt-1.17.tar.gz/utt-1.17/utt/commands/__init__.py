6b5864e30c4fa9f9f56d7db36c2355a5cb3dfeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:15 +0000

    Add package utt/1.17/utt-1.17.tar.gz

---
+++ packages/utt/utt-1.17.tar.gz/utt-1.17/utt/commands/__init__.py
@@ -0,0 +1,8 @@
+from . import add
+from . import config
+from . import edit
+from . import hello
+from . import report
+from . import stretch
+
+COMMAND_MODULES = [add, config, edit, hello, report, stretch]
