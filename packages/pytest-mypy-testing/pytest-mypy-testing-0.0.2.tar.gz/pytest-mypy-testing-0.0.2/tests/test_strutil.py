4715910c8806eba563eb807f144a941c11771241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:50 +0000

    Add package pytest-mypy-testing/0.0.2/pytest-mypy-testing-0.0.2.tar.gz

---
+++ packages/pytest-mypy-testing/pytest-mypy-testing-0.0.2.tar.gz/pytest-mypy-testing-0.0.2/tests/test_strutil.py
@@ -0,0 +1,30 @@
+import pytest
+
+from pytest_mypy_testing.strutil import common_prefix, dedent
+
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        ("", "", ""),
+        ("a", "a", "a"),
+        ("abc", "abcd", "abc"),
+        ("abcd", "abc", "abc"),
+        ("abc", "xyz", ""),
+    ],
+)
+def test_common_prefix(a: str, b: str, expected: str):
+    actual = common_prefix(a, b)
+    assert actual == expected
+
+
+def test_dedent():
+    input = """
+    foo
+    bar
+        baz
+    """
+
+    expected = "foo\nbar\n    baz\n"
+    actual = dedent(input)
+    assert actual == expected
