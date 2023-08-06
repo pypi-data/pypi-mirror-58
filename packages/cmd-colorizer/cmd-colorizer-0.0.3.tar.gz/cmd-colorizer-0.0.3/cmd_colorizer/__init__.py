cf470282c91f32cee75ea2ee3d63945d14225a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:59 +0000

    Add package cmd-colorizer/0.0.3/cmd-colorizer-0.0.3.tar.gz

---
+++ packages/cmd-colorizer/cmd-colorizer-0.0.3.tar.gz/cmd-colorizer-0.0.3/cmd_colorizer/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: KoChan-s
+
+from .colorizer import Colorizer, colored
+
+__copyright__ = "2020"
+__version__ = "0.0.3"
+__authors__ = ["KoChan-s"]
+
+if __name__ == '__main__':
+    print(Colorizer, colored)
