5dae24071f31231cd7fa74619dae2868c0eace90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:02 +0000

    Add package grpc-route/0.1.1.20010315/grpc_route-0.1.1.20010315.tar.gz

---
+++ packages/grpc-route/grpc_route-0.1.1.20010315.tar.gz/grpc_route-0.1.1.20010315/grpc_route/error.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+# @Time     : 2019-12-31 11:30
+# @Author   : binger
+
+# TODO：异常封装
+# class RPCConnectFail(Exception):
+#
+#     def __init__(self, code, msg):
+
+
+
+if __name__ == "__main__":
+    pass
