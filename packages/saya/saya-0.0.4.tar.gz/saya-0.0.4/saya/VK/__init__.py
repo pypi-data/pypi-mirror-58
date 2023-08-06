0e2d2f4b848f1e2c325ace16d56a4a27585769d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:11 +0000

    Add package saya/0.0.4/saya-0.0.4.tar.gz

---
+++ packages/saya/saya-0.0.4.tar.gz/saya-0.0.4/saya/VK/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .Vk import Vk
+from .LongPoll import LongPoll
+from .VkAuthManager import VkAuthManager
+from .Event import Event
+from .PushEvent import PushEvent
+from .VkScript import VkScript
+from .Uploader import Uploader
+
+if __name__ == '__main__':
+    print(Vk, LongPoll, VkAuthManager, Event, PushEvent, VkScript, Uploader)
---
+++ packages/saya/saya-0.0.4.tar.gz/saya-0.0.4/saya/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .VK import Vk, LongPoll, Event, PushEvent, VkAuthManager, VkScript, Uploader
+
+__copyright__ = "2020"
+__version__ = "0.0.4"
+__authors__ = ["Ethosa"]
+
+if __name__ == '__main__':
+    print(Vk, LongPoll, VkAuthManager, Event, PushEvent, VkScript, Uploader)
