507cc2038fa404727ebeb525bb21146dcd0ba7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package htmldate/0.6.0/htmldate-0.6.0.tar.gz

---
+++ packages/htmldate/htmldate-0.6.0.tar.gz/htmldate-0.6.0/htmldate/cli.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Implementing a basic command-line interface.
+"""
+
+## This file is available from https://github.com/adbar/htmldate
+## under GNU GPL v3 license
+
+import argparse
+#import logging
+import sys
+
+from .core import find_date
+from .utils import fetch_url
+
+
+def examine(htmlstring, extensive_bool=True, original_date=False, verbose_flag=False, maxdate=None):
+    """ Generic safeguards and triggers """
+    # safety check
+    if htmlstring is None:
+        sys.stderr.write('# ERROR: empty document\n')
+    elif len(htmlstring) > 10000000:
+        sys.stderr.write('# ERROR: file too large\n')
+    elif len(htmlstring) < 10:
+        sys.stderr.write('# ERROR: file too small\n')
+    # proceed
+    else:
+        result = find_date(htmlstring, extensive_search=extensive_bool, original_date=original_date, verbose=verbose_flag, max_date=maxdate)
+        return result
+    return None
+
+
+def main():
+    """ Run as a command-line utility. """
+    # arguments
+    argsparser = argparse.ArgumentParser()
+    argsparser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    argsparser.add_argument("-f", "--fast", help="fast mode: disable extensive search", action="store_false")
+    argsparser.add_argument("--original", help="original date prioritized", action="store_true")
+    argsparser.add_argument("-m", "--maxdate", help="latest acceptable date (YYYY-MM-DD)", type=str)
+    argsparser.add_argument("-i", "--inputfile", help="name of input file for batch processing (similar to wget -i)", type=str)
+    argsparser.add_argument("-u", "--URL", help="custom URL download", type=str)
+    args = argsparser.parse_args()
+
+    # process input on STDIN
+    if not args.inputfile:
+        # URL as input
+        if args.URL:
+            htmlstring = fetch_url(args.URL)
+            if htmlstring is None:
+                sys.exit('# ERROR no valid result for url: ' + args.URL + '\n') # exit code: 1
+        # unicode check
+        else:
+            try:
+                htmlstring = sys.stdin.read()
+            except UnicodeDecodeError as err:
+                # input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='latin-1')
+                sys.exit('# ERROR system/buffer encoding: ' + str(err) + '\n') # exit code: 1
+
+        result = examine(htmlstring, extensive_bool=args.fast, original_date=args.original, verbose_flag=args.verbose, maxdate=args.maxdate)
+        if result is not None:
+            sys.stdout.write(result + '\n')
+
+    # process input file line by line
+    else:
+        with open(args.inputfile, mode='r', encoding='utf-8') as inputfile: # errors='strict', buffering=1
+            for line in inputfile:
+                htmltext = fetch_url(line.strip())
+                result = examine(htmltext, extensive_bool=args.fast, original_date=args.original, verbose_flag=args.verbose, maxdate=args.maxdate)
+                if result is None:
+                    result = 'None'
+                sys.stdout.write(line.strip() + '\t' + result + '\n')
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/htmldate/htmldate-0.6.0.tar.gz/htmldate-0.6.0/htmldate/utils.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+# pylint:disable-msg=E0611,I1101
+"""
+Module bundling functions related to HTML processing.
+"""
+
+## This file is available from https://github.com/adbar/htmldate
+## under GNU GPL v3 license
+
+
+# standard
+import logging
+import re
+import socket
+from io import StringIO # Python 3
+import urllib3
+
+# libraries
+try:
+    import cchardet as chardet
+except ImportError:
+    import chardet
+import requests
+from lxml import etree, html
+
+
+LOGGER = logging.getLogger(__name__)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# LXML
+HTML_PARSER = html.HTMLParser() # encoding='utf8'
+
+
+
+def fetch_url(url):
+    """ Fetch page using requests/urllib3
+    Args:
+        URL: URL of the page to fetch
+    Returns:
+        request object (headers + body).
+    Raises:
+        Nothing.
+    """
+
+    # customize headers
+    headers = requests.utils.default_headers()
+    headers.update({
+        'Connection': 'close',  # another way to cover tracks
+        # 'User-Agent': '', # your string here
+    })
+    # send
+    try:
+        response = requests.get(url, timeout=30, verify=False, allow_redirects=True, headers=headers)
+    except (requests.exceptions.MissingSchema, requests.exceptions.InvalidURL):
+        LOGGER.error('malformed URL: %s', url)
+    except requests.exceptions.TooManyRedirects:
+        LOGGER.error('redirects: %s', url)
+    except requests.exceptions.SSLError as err:
+        LOGGER.error('SSL: %s %s', url, err)
+    except (socket.timeout, requests.exceptions.ConnectionError, requests.exceptions.Timeout, socket.error, socket.gaierror) as err:
+        LOGGER.error('connection: %s %s', url, err)
+    #except Exception as err:
+    #    logging.error('unknown: %s %s', url, err) # sys.exc_info()[0]
+    # if no error
+    else:
+        # safety checks
+        if int(response.status_code) != 200:
+            LOGGER.error('not a 200 response: %s', response.status_code)
+        elif response.text is None or len(response.text) < 100:
+            LOGGER.error('file too small/incorrect response: %s %s', url, len(response.text))
+        elif len(response.text) > 20000000:
+            LOGGER.error('file too large: %s %s', url, len(response.text))
+        else:
+            guessed_encoding = chardet.detect(response.content)['encoding']
+            LOGGER.debug('response/guessed encoding: %s / %s', response.encoding, guessed_encoding)
+            if guessed_encoding is not None:
+                try:
+                    htmltext = response.content.decode(guessed_encoding)
+                except UnicodeDecodeError:
+                    htmltext = response.text
+            else:
+                htmltext = response.text
+            # return here
+            return htmltext
+    # catchall
+    return None
+
+
+def load_html(htmlobject):
+    """Load object given as input and validate its type (accepted: LXML tree and string, HTML document or URL)"""
+    tree = None
+    if isinstance(htmlobject, (etree._ElementTree, html.HtmlElement)):
+        # copy tree
+        tree = htmlobject
+    elif isinstance(htmlobject, str):
+        # the string is a URL, download it
+        if re.search(r'^https?://[^ ]+$', htmlobject):
+            LOGGER.info('URL detected, downloading: %s', htmlobject)
+            htmltext = fetch_url(htmlobject)
+            if htmltext is not None:
+                htmlobject = htmltext
+            else:
+                return None
+        ## robust parsing
+        try:
+            # parse
+            tree = html.parse(StringIO(htmlobject), parser=HTML_PARSER)
+        except UnicodeDecodeError as err:
+            LOGGER.error('unicode %s', err)
+        except ValueError:
+            # try to parse a bytestring
+            try:
+                tree = html.fromstring(htmlobject.encode('utf8'))
+            except Exception as err:
+                LOGGER.error('parser bytestring %s', err)
+        except UnboundLocalError as err:
+            LOGGER.error('parsed string %s', err)
+        except (etree.XMLSyntaxError, AttributeError) as err:
+            LOGGER.error('parser %s', err)
+    else:
+        LOGGER.error('this type cannot be processed: %s', type(htmlobject))
+    return tree
