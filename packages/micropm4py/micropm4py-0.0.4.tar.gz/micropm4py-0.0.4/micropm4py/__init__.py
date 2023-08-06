e1313f38855caf8356116afc208e08b801bfb694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:16 +0000

    Add package micropm4py/0.0.4/micropm4py-0.0.4.tar.gz

---
+++ packages/micropm4py/micropm4py-0.0.4.tar.gz/micropm4py-0.0.4/micropm4py/__init__.py
@@ -0,0 +1,8 @@
+from micropm4py import log, petrinet, util, conversion
+
+__version__ = '0.0.4'
+__doc__ = "MicroPM4Py - Process Mining for Micro-Controllers"
+__author__ = 'PADS'
+__author_email__ = 'a.berti@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "a.berti@pads.rwth-aachen.de"
