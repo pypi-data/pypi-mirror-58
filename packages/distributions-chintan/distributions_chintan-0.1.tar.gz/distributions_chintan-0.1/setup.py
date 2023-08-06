117f9b228747453ba792f4c3c3fdb882381e0418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:21 +0000

    Add package distributions-chintan/0.1/distributions_chintan-0.1.tar.gz

---
+++ packages/distributions-chintan/distributions_chintan-0.1.tar.gz/distributions_chintan-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_chintan',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_chintan'],
+      zip_safe=False)
