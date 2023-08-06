4e4e1a9d35f8cf269efbd748a815bc962ae4bea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:53 +0000

    Add package simple-gen/2019.12.29/simple-gen-2019.12.29.tar.gz

---
+++ packages/simple-gen/simple-gen-2019.12.29.tar.gz/simple-gen-2019.12.29/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def long_long_description():
+    with open('README.md', 'r') as f:
+        desc = f.read()
+    return desc
+
+
+setup(
+    name='simple-gen',
+    version='2019.12.29',
+    description='gen files',
+    long_description=long_long_description(),
+    author='buglan',
+    author_email='1831353087@qq.com',
+    packages=find_packages(),
+    install_requires=['click'],
+    entry_points='''
+        [console_scripts]
+        hello = packages.gen:hello
+        s = packages.group:hello
+    ''',
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest',
+    ]
+
+)
