fecd935156533f3b1f41c8dbf019ad1d5408f2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package archetypes.multilingual/1.3.0/archetypes.multilingual-1.3.0.tar.gz

---
+++ packages/archetypes.multilingual/archetypes.multilingual-1.3.0.tar.gz/archetypes.multilingual-1.3.0/archetypes/multilingual/tests/test_doctests.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from archetypes.multilingual.testing import ARCHETYPESMULTILINGUAL_INTEGRATION_TESTING
+from archetypes.multilingual.testing import ARCHETYPESMULTILINGUAL_FUNCTIONAL_TESTING
+from archetypes.multilingual.testing import optionflags
+from plone.testing import layered
+
+import doctest
+import unittest2 as unittest
+
+integration_tests = [
+    "languageindependentfields.txt",
+    "multilingual.txt",
+    "monkey.txt",
+]
+
+functional_tests = []
+
+
+def test_suite():
+    return unittest.TestSuite(
+        [
+            layered(
+                doctest.DocFileSuite(
+                    "%s" % f,
+                    package="archetypes.multilingual.tests",
+                    optionflags=optionflags,
+                ),
+                layer=ARCHETYPESMULTILINGUAL_INTEGRATION_TESTING,
+            )
+            for f in integration_tests
+        ]
+        + [
+            layered(
+                doctest.DocFileSuite(
+                    "%s" % f,
+                    package="archetypes.multilingual.tests",
+                    optionflags=optionflags,
+                ),
+                layer=ARCHETYPESMULTILINGUAL_FUNCTIONAL_TESTING,
+            )
+            for f in functional_tests
+        ]
+    )
+
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="test_suite")
---
+++ packages/archetypes.multilingual/archetypes.multilingual-1.3.0.tar.gz/archetypes.multilingual-1.3.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+version = "1.3.0"
+
+setup(
+    name="archetypes.multilingual",
+    version=version,
+    description="Multilingual support for archetypes.",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    # Get more strings from https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 4.3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords="archetypes multilingual i18n translation",
+    author="awello",
+    author_email="awello@gmail.com",
+    url="https://github.com/plone/archetypes.multilingual",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["archetypes"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "Products.ATContentTypes",
+        "plone.multilingual",
+        "collective.monkeypatcher",
+        "Products.PloneLanguageTool",
+    ],
+    extras_require={"test": ["plone.app.testing", "archetypes.testcase"]},
+    entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
