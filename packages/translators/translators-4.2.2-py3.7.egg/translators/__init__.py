a05efb67bc084cc55254359853707c3096114246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:25 +0000

    Add package translators/4.2.2/translators-4.2.2-py3.7.egg

---
+++ packages/translators/translators-4.2.2-py3.7.egg/translators/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "4.2.2"
+__author__ = "UlionTse"
+
+from .google import google_api as google
+from .youdao import youdao_api as youdao
+from .tencent import tencent_api as tencent
+from .alibaba import alibaba_api as alibaba
