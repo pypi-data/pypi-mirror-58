4b051070cadb6d3ff0af4149b609f00121994006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:47 +0000

    Add package string-color/0.2.9/string_color-0.2.9-py3-none-any.whl

---
+++ packages/string-color/string_color-0.2.9-py3-none-any.whl/stringcolor/ops.py
@@ -0,0 +1,97 @@
+import json
+import os
+
+class String:
+    "A regular string"
+    def __init__(self, string):
+        self.string = string
+
+    def render(self):
+        return self.string
+
+    def bold(self):
+        return Bold(str(self))
+
+    def cs(self, color, bkg=None):
+        return Color(str(self), color, bkg)
+
+    def underline(self):
+        return Underline(str(self))
+
+    def __str__(self):
+        return self.render()
+
+    def __add__(self, other):
+        types = [int, float, type(open)]
+        if type(other) in types:
+            raise TypeError('Cannot concat ' + ', '.join([str(t) for t in types]))
+        try:
+            other = str(other)
+            return self.render() + other
+        except Exception:
+            raise TypeError(f"Cannot concat {type(other)}")
+
+    def __radd__(self, other):
+        types = [int, float, type(open)]
+        if type(other) in types:
+            raise TypeError('Cannot concat ' + ', '.join([str(t) for t in types]))
+        try:
+            other = str(other)
+            return other + self.render()
+        except Exception:
+            raise TypeError(f"Cannot concat {type(other)}")
+
+class Bold(String):
+    "Make string bold"
+    def render(self):
+        return f"\033[1m{self.string}\033[0m"
+
+
+def _load_colors():
+    colors_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'colors.json'
+    )
+    with open(colors_path) as f:
+        colors = json.loads(f.read())
+    return colors
+
+
+class Color(String):
+    "Color a string"
+    colors = _load_colors()
+
+    def __init__(self, string, color, bkg=None):
+        self.string = string
+        self.color = color
+        self.bkg = bkg
+
+    def render(self):
+        """color a string"""
+        ret = False
+        bkg_term = None
+        if self.bkg is not None:
+            for key, value in self.colors.items():
+                if (self.bkg.lower() == value["name"].lower() or
+                        self.bkg == value["hex"] or
+                        self.bkg == value["term"]):
+                    bkg_term = value["term"]
+                    break
+        for key, value in self.colors.items():
+            if (self.color.lower() == value["name"].lower() or
+                    self.color == value["hex"] or
+                    self.color == value["term"]):
+                ret = True
+                if bkg_term:
+                    return f"\033[38;5;{value['term']};48;5;{bkg_term}m{self.string}\033[0m"
+                else:
+                    return f"\033[38;5;{value['term']}m{self.string}\033[0m"
+                break
+        if not ret:
+            return self.string
+
+
+class Underline(String):
+    "Underline a string"
+    def render(self):
+        return f"\033[4m{self.string}\033[0m"
