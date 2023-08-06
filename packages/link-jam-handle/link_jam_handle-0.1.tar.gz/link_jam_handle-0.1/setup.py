2e0a53d71e1ea1b1ac4253bb295a9045892e8f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:02 +0000

    Add package link-jam-handle/0.1/link_jam_handle-0.1.tar.gz

---
+++ packages/link-jam-handle/link_jam_handle-0.1.tar.gz/link_jam_handle-0.1/setup.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from shutil import rmtree
+from setuptools import setup, Command
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class UploadCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+        self.status("Building Source distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("sudo twine upload dist/*")
+        sys.exit()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "link_jam_handle",
+    version="0.1",
+    author = "Kevin Hill",
+    author_email = "kah.kevin.hill@gmail.com",
+    description = ("A bunch of database management for linkkt software."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['link_jam_handle'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        'jamboree', 'mlfinlab', 'cytoolz', 'loguru', 'maya', 'crayons'
+    ],
+    cmdclass={"upload": UploadCommand},
+)
