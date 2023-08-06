1b5bec673847c21048dbc513955c7f3a5ede52d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:46 +0000

    Add package molywood/0.13/molywood-0.13.tar.gz

---
+++ packages/molywood/molywood-0.13.tar.gz/molywood-0.13/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='molywood',
+      version='0.13',
+      description='A script to automate the production of molecular movies in VMD',
+      url='https://gitlab.com/KomBioMol/molywood',
+      author='Milosz Wieczor',
+      author_email='milafternoon@gmail.com',
+      license='GNU GPLv3',
+      packages=['molywood'],
+      entry_points={'console_scripts': ['molywood = molywood.moly:molywood',
+                                        'molywood-gen-env = molywood.moly:gen_yml']},
+      python_requires='>=3.4',
+      zip_safe=False)
