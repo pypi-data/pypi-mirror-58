11cf656efbf648564116ebe5428893bd0a3a87af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:44 +0000

    Add package rpn-calc/0.1.0/rpn_calc-0.1.0.tar.gz

---
+++ packages/rpn-calc/rpn_calc-0.1.0.tar.gz/rpn_calc-0.1.0/setup.py
@@ -0,0 +1,38 @@
+"""Setup script for the package"""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    README = readme_file.read()
+
+
+setup(
+    name="rpn_calc",
+    version="0.1.0",
+    description="RPN calculator",
+    long_description=README,
+    author="Alain Girard",
+    author_email="alaingirardvd@gmail.com",
+    url="https://github.com/Allain18/pol",
+    license="MIT License",
+    keywords="rpn, calculator, reverse polish notation",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Environment :: Console",
+        "Operating System :: OS Independent"
+    ],
+    test_suite="test",
+    packages=find_packages(exclude=["test"]),
+    entry_points={
+        "console_scripts": [
+            "pol=rpn_calculator.calculator:main",
+        ]
+    },
+)
