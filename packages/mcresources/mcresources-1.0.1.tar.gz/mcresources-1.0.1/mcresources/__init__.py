8daa1c9ff7497156f6c9d11371ee389a211e4f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:46 +0000

    Add package mcresources/1.0.1/mcresources-1.0.1.tar.gz

---
+++ packages/mcresources/mcresources-1.0.1.tar.gz/mcresources-1.0.1/mcresources/__init__.py
@@ -0,0 +1,5 @@
+#  Part of mcresources by Alex O'Neill
+#  Work under copyright. Licensed under GPL-3.0
+#  For more information see the project LICENSE file
+
+from mcresources.resources import ResourceManager, clean_generated_resources
