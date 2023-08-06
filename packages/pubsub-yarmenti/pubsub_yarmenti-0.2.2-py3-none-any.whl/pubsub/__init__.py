056e7d627a8ee422df728393c7e67845a41922fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package pubsub-yarmenti/0.2.2/pubsub_yarmenti-0.2.2-py3-none-any.whl

---
+++ packages/pubsub-yarmenti/pubsub_yarmenti-0.2.2-py3-none-any.whl/pubsub/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Yannick ARMENTI"
+__version__ = "0.2.2"
+
+"""
+A basic implementation of the publisher / subscriber pattern.
+"""
+
+from .subscriber import Subscriber
+from .publisher import Publisher
