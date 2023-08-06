b63f624436e68559a9c525f6e45c57225e588728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:01 +0000

    Add package django-tiny-erp/0.1.2/django_tiny_erp-0.1.2-py3-none-any.whl

---
+++ packages/django-tiny-erp/django_tiny_erp-0.1.2-py3-none-any.whl/tiny_erp/__init__.py
@@ -0,0 +1,8 @@
+"""
+Main init file for tiny_erp
+"""
+VERSION = (0, 1, 2)
+
+__vega_admin_version__ = "0.0.16"
+__small_small_hr_version__ = "0.1.9"
+__version__ = ".".join(str(_) for _ in VERSION)
