6783cc1015eb5558d70a87469ac67d6f9176234f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:17 +0000

    Add package tiletanic/1.1.0/tiletanic-1.1.0.tar.gz

---
+++ packages/tiletanic/tiletanic-1.1.0.tar.gz/tiletanic-1.1.0/setup.py
@@ -0,0 +1,35 @@
+import sys
+from setuptools import setup, find_packages
+
+open_kwds = {}
+if sys.version_info > (3,):
+    open_kwds['encoding'] = 'utf-8'
+
+with open('README.rst', **open_kwds) as f:
+    readme = f.read()
+
+setup(name='tiletanic',
+      version='1.1.0',
+      description='Geospatial tiling utilities',
+      long_description=readme,
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      keywords='',
+      author='Patrick Young',
+      author_email='patrick.young@digitalglobe.com',
+      url='https://github.com/digitalglobe/tiletanic',
+      license='MIT',
+      packages=find_packages(exclude=['tests', 'docs']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['click',
+                        'geojson',
+                        'shapely>=1.6'],
+      entry_points='''
+         [console_scripts]
+         tiletanic=tiletanic.cli:cli
+      '''
+      )
