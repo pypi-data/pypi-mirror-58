d714f644de72508ca7e37b4f09b65256f8f8d8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:43 +0000

    Add package bwpy/0.1.8/bwpy-0.1.8-py3-none-any.whl

---
+++ packages/bwpy/bwpy-0.1.8-py3-none-any.whl/bwpy/bitwarden/org.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import json
+from dataclasses import dataclass
+from sh import bw
+
+
+@dataclass
+class BitwardenOrg:
+    org_name: str
+
+    def org_id(self):
+        orgs = json.loads(str(bw.list.organizations()))
+        for org in orgs:
+            if org["name"] == self.org_name:
+                return org["id"]
+        raise KeyError(f"organization not found: {org['name']}")
+
+    def org_collections(self):
+        return json.loads(
+            str(bw.list("org-collections", "--organizationid", self.org_id()))
+        )
