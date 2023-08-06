4ae3d57f8149d123079006596041fef822d4cef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:48 +0000

    Add package google-word-cloud-Chaz9578/0.0.4/google-word-cloud-Chaz9578-0.0.4.tar.gz

---
+++ packages/google-word-cloud-Chaz9578/google-word-cloud-Chaz9578-0.0.4.tar.gz/google-word-cloud-Chaz9578-0.0.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 18:41:58 2020
+
+@author: charlie
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="google-word-cloud-Chaz9578", # Replace with your own username
+    version="0.0.4",
+    author="Charlie Plumley",
+    author_email="charlie.plumley@gmail.com",
+    description="Create word clouds from a google search term",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/charlie9578/googleWordCloud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
