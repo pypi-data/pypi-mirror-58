da4443839bae4e8338d24b2342b9674b0859d920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:36 +0000

    Add package graph-generator-lib-py/0.1.1/graph_generator_lib_py-0.1.1.tar.gz

---
+++ packages/graph-generator-lib-py/graph_generator_lib_py-0.1.1.tar.gz/graph_generator_lib_py-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graph_generator_lib_py",
+    version="0.1.1",
+    description="Library for generating graphs for Grapls",
+    url="https://github.com/insanitybit/ggraph-generator-lib-py/",
+    author="insanitybit",
+    author_email="insanitybit@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
+        'graph_generator_lib_py': ["py.typed"],
+    },
+    include_package_data=True,
+    install_requires=['protobuf'],
+)
