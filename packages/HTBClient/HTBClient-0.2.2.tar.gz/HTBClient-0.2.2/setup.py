20a884eb4d1d3923b329635f572e34cec01e612e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package HTBClient/0.2.2/HTBClient-0.2.2.tar.gz

---
+++ packages/HTBClient/HTBClient-0.2.2.tar.gz/HTBClient-0.2.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HTBClient",
+    version="0.2.2",
+    author="Benjamin Grewell",
+    author_email="bgrewelldev@gmail.com",
+    description="A library to interact with the hackthebox.eu website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BGrewell/HTBClient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Intended Audience :: Information Technology",
+    ],
+    entry_points={
+        'console_scripts': [
+            'htb=HTBClient.__main__:main'
+        ]
+    },
+    python_requires='>=3.6',
+)
