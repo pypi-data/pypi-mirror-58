83620c0e45d2027b959997e06103c6661f10242d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:39 +0000

    Add package mlmodels/0.14.1/mlmodels-0.14.1.tar.gz

---
+++ packages/mlmodels/mlmodels-0.14.1.tar.gz/mlmodels-0.14.1/mlmodels/ztest.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import copy
+import math
+import os
+from collections import Counter, OrderedDict
+
+import numpy as np
+import pandas as pd
+import scipy as sci
+
+import sklearn as sk
+from sklearn import preprocessing
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+import tensorflow as tf
+print(tf, tf.__version__)
+
+
+print("os.getcwd", os.getcwd())
+
+
+
