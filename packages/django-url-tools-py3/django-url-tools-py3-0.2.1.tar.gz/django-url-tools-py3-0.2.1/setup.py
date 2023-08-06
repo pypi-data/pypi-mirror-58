718cc5634268d06aa0e72707140be8e38f514d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:26 +0000

    Add package django-url-tools-py3/0.2.1/django-url-tools-py3-0.2.1.tar.gz

---
+++ packages/django-url-tools-py3/django-url-tools-py3-0.2.1.tar.gz/django-url-tools-py3-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='django-url-tools-py3',
+    description='Django helpers for dealing with URLs in templates',
+    version='0.2.1',
+    packages=['url_tools', 'url_tools.templatetags'],
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    maintainer='Kyle Buzsaki',
+    maintainer_email='kbuzsaki@gmail.com',
+    url='https://github.com/kbuzsaki/django-url-tools-py3',
+    download_url='https://github.com/kbuzsaki/django-url-tools-py3/releases',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+
