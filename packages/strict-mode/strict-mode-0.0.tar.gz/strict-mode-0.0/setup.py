50d079cd3e23006e77fe7794f1f6f5e03abb9c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:57 +0000

    Add package strict-mode/0.0/strict-mode-0.0.tar.gz

---
+++ packages/strict-mode/strict-mode-0.0.tar.gz/strict-mode-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="strict-mode",
+      version="0.0",
+      description="""Python Strict Mode""",
+      url='https://github.com/pfalcon/strict',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['ullvm'])
