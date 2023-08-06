b157832ed6ebd325f209cb1be61fc6318bbd323f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:10 +0000

    Add package dghello/0.1.1/dghello-0.1.1.tar.gz

---
+++ packages/dghello/dghello-0.1.1.tar.gz/dghello-0.1.1/dghello/__init__.py
@@ -0,0 +1,1 @@
+from dghello.Hello import Hello
---
+++ packages/dghello/dghello-0.1.1.tar.gz/dghello-0.1.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name = 'dghello', # name of package folder
+  version = '0.1.1', # increase it with every release
+  author = 'Daniel Groner',
+  author_email = 'dgroner@fordham.edu',
+  license='MIT', # Chose a license from here: https://help.github.com/articles/li
+  description = 'Test package for github and PyPi', # a short description
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url = 'https://github.com/dgroner/dghello', # link to github
+  packages=setuptools.find_packages(),
+  keywords = ['DGHELLO', 'GRONER'], # Keywords for package
+  install_requires=[
+  ],
+classifiers=[
+  'Development Status :: 3 - Alpha', # "3 - Alpha", "4 - Beta" or "5
+  'Intended Audience :: Developers', # Define that audience as developers
+  'Topic :: Software Development :: Build Tools',   # TODO
+  'License :: OSI Approved :: MIT License', # Again, pick a license
+  'Programming Language :: Python :: 3', #Specify which python versions that you
+  'Operating System :: OS Independent',
+  ],
+python_requires='>=3.6',
+)
