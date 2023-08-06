875327f17d21bba7a1c1cc9f0a3f874a4df2f775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:04 +0000

    Add package fmztool/1.0/fmztool-1.0.tar.gz

---
+++ packages/fmztool/fmztool-1.0.tar.gz/fmztool-1.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='fmztool',
+      version='1.0',
+      author="ahrli",
+      author_email="ahrli_c@163.com",
+      url='https://github.com/Ahrli/fmz_tool',
+      packages=["fmztool"],
+       )
+
+
+
+
+
+
+
+
+
+
+
