63493b7ed20b6aec459a0bf090b2cdd0d70eac91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:19 +0000

    Add package nester-xyqiang/0.0.2/nester-xyqiang-0.0.2.tar.gz

---
+++ packages/nester-xyqiang/nester-xyqiang-0.0.2.tar.gz/nester-xyqiang-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nester-xyqiang", # Replace with your own username
+    version="0.0.2",
+    author="xyqiang",
+    author_email="xuyanqiangak@gmail.com",
+    description="A small example package",
+    long_description="Print list items iteratively",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7.4',
+)
