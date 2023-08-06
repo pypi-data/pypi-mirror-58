e1c53a7149c7551fc92f62ca6909e3bd75ab393e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:12 +0000

    Add package cleantoots/0.1.0/cleantoots-0.1.0.tar.gz

---
+++ packages/cleantoots/cleantoots-0.1.0.tar.gz/cleantoots-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cleantoots']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Mastodon.py>=1.5.0,<2.0.0',
+ 'black>=19.10b0,<20.0',
+ 'click>=7.0,<8.0',
+ 'pendulum>=2.0.5,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cleantoots',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Gabriel Augendre',
+    'author_email': 'gabriel@augendre.info',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
