bb8c3bd8d61114305cd3a21f18b7e47e3c8baa17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:04 +0000

    Add package spassgen/0.2/spassgen-0.2.tar.gz

---
+++ packages/spassgen/spassgen-0.2.tar.gz/spassgen-0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'spassgen',
+  packages = ['spassgen'],   
+  version = '0.2',      
+  license='MIT',
+  description = 'A Simple Secure Password Generator',
+  author = 'AlamarW',
+  author_email = 'alamardevel@gmail.com',
+  url = 'https://github.com/AlamarW/Secure-Password-Generator',   
+  download_url = 'https://github.com/AlamarW/Secure-Password-Generator/archive/V_02.tar.gzz',
+  keywords = ['PASSWORD','RANDOM','GENERATOR'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
