de52edc9d870a6c18ceb43e5d7bac9e751bcfed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:33 +0000

    Add package crystalball/0.2.4/crystalball-0.2.4.tar.gz

---
+++ packages/crystalball/crystalball-0.2.4.tar.gz/crystalball-0.2.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+requirements = [
+    "codex-africanus[dask] >= 0.2.0",
+    "dask-ms >= 0.2.3",
+    "regions",
+    "psutil"
+]
+
+extras_require = {
+  'testing': ['pytest', 'pytest-flake8']
+}
+
+PACKAGE_NAME = 'crystalball'
+__version__ = '0.2.4'
+
+setup(name=PACKAGE_NAME,
+      version=__version__,
+      description="Predicts visibilities from a parameterised sky model",
+      author="Paolo Serra",
+      author_email="paolo80serra@gmail.com",
+      entry_points={
+        'console_scripts': ['crystalball=Crystalball.crystalball:predict'],
+      },
+      extras_require=extras_require,
+      url="https://github.com/paoloserra/crystalball",
+      packages=["Crystalball"],
+      install_requires=requirements,
+      include_package_data=True,
+      python_requires=">=3.5",
+      license=["GNU GPL v2"],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Astronomy"
+      ])
