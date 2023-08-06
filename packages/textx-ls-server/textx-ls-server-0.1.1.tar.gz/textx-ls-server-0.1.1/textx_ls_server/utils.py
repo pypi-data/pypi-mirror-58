064ada4e65dd50627b29f40e8813b637b646161e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:32 +0000

    Add package textx-ls-server/0.1.1/textx-ls-server-0.1.1.tar.gz

---
+++ packages/textx-ls-server/textx-ls-server-0.1.1.tar.gz/textx-ls-server-0.1.1/textx_ls_server/utils.py
@@ -0,0 +1,27 @@
+import functools
+from typing import Callable
+
+
+def skip_not_supported_langs(func: Callable) -> Callable:
+    """Decorator which checks if document is in the workspace and call
+    decorated function.
+
+    Args:
+        func: callable that is being decorated
+    Returns:
+        Decorated callable
+    Raises:
+        None
+
+    """
+
+    @functools.wraps(func)
+    def decorator(ls, params, **kwargs):
+        try:
+            doc = ls.workspace.documents[params.textDocument.uri]
+            kwargs["doc"] = doc
+            return func(ls, params, **kwargs)
+        except KeyError:
+            pass
+
+    return decorator
