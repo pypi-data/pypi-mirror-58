5b8b6af85a00568bf9f8fdcdff5c9abeeeafda0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:08 +0000

    Add package galgebra/0.4.5/galgebra-0.4.5-py3-none-any.whl

---
+++ packages/galgebra/galgebra-0.4.5-py3-none-any.whl/galgebra/__init__.py
@@ -0,0 +1,25 @@
+"""
+.. currentmodule:: galgebra
+========================================
+galgebra (:mod:`galgebra`)
+========================================
+
+A module for geometric algebra.
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: generated
+
+    metric
+    ga
+    mv
+    lt
+    printer
+    utils
+"""
+
+from __future__ import absolute_import, division
+from __future__ import print_function
+from ._version import __version__
