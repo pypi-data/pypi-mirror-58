58eca2b18309a510256a362a00ca654fb8196aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:28 +0000

    Add package namopdf/1.0/namopdf-1.0.tar.gz

---
+++ packages/namopdf/namopdf-1.0.tar.gz/namopdf-1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(
+    name='namopdf',
+    version=1.0,
+    long_description=Path('README.md').read_text(),
+    package=setuptools.find_packages(exclude=['tests','data'])
+    
+    
+)
