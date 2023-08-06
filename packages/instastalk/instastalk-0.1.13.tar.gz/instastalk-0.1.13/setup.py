97d5eef7aa19d60fd69e91817c359b082e9dbfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package instastalk/0.1.13/instastalk-0.1.13.tar.gz

---
+++ packages/instastalk/instastalk-0.1.13.tar.gz/instastalk-0.1.13/setup.py
@@ -0,0 +1,31 @@
+import io
+from setuptools import setup
+
+with io.open("README.rst", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name='instastalk',
+    packages=['instastalk'],
+    version='0.1.13',
+    license='MIT',
+    description='Download your instacrush posts',
+    author='jjkoh95',
+    author_email='jjkoh95@gmail.com',
+    url='https://github.com/jjkoh95/instastalk',
+    keywords=['instagram', 'scrape'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+    ],
+    long_description=readme,
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
