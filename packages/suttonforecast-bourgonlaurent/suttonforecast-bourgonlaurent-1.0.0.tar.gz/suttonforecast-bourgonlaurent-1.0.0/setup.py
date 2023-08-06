4aaf336275959851335bfc38111c4dee9b2d5543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:08 +0000

    Add package suttonforecast-bourgonlaurent/1.0.0/suttonforecast-bourgonlaurent-1.0.0.tar.gz

---
+++ packages/suttonforecast-bourgonlaurent/suttonforecast-bourgonlaurent-1.0.0.tar.gz/suttonforecast-bourgonlaurent-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="suttonforecast-bourgonlaurent", # Replace with your own username
+    version="1.0.0",
+    author="Laurent Bourgon",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BourgonLaurent/TelegramSuttonForecast",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
