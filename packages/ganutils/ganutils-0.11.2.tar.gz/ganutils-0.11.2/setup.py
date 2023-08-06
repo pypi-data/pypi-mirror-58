af904c92cc04b9524882ecd3b27bfb9317bdf8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:46 +0000

    Add package ganutils/0.11.2/ganutils-0.11.2.tar.gz

---
+++ packages/ganutils/ganutils-0.11.2.tar.gz/ganutils-0.11.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+def load_readme():
+  return open("./README.md", "r").read()
+
+setup(
+  name = 'ganutils',
+  packages = find_packages(),
+  version = '0.11.2',
+  license='MIT',
+  description = 'This is an installable python package for scripts I wrote for myself.',
+  long_description=load_readme(),
+  author = 'Gan Tu',
+  author_email = 'tugan0329@gmail.com',
+  url = 'https://github.com/Michael-Tu/ganutils',
+  keywords = ['tugan', 'utils', 'tools'],
+  install_requires=[
+      'torch',
+      'numpy',
+      'sklearn',
+      'tqdm',
+      'requests',
+      'twilio',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
