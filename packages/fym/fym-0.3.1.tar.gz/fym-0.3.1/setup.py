e855b6630e391c4341da3f141ef8827b98f68b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:29 +0000

    Add package fym/0.3.1/fym-0.3.1.tar.gz

---
+++ packages/fym/fym-0.3.1.tar.gz/fym-0.3.1/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fym',
+    version='0.3.1',
+    url='https://github.com/fdcl-nrf/fym',
+    author='SNU FDCL',
+    description='SNU FDCL Fym: Flight simulator for various purpose',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'python>=3.7',
+        'gym',
+        'matplotlib',
+        'numdifftools',
+        'h5py',
+        'tqdm'
+    ]
+)
