984a8290f29fdef7981d70007ea855cd050ab7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:24 +0000

    Add package fakerlocationer/0.5/fakerlocationer-0.5.tar.gz

---
+++ packages/fakerlocationer/fakerlocationer-0.5.tar.gz/fakerlocationer-0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fakerlocationer',
+    version='0.5',
+    packages=['fakerlocationer'],
+    author="kenevil1",
+    author_email="no@email.com",
+    description="A simple class that can be used to fake Selenium Browser Geolocation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenevil1/fakerlocationer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
