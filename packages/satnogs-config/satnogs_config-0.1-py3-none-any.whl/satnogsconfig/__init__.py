2a715dffa240f6e2605a757eb8a032cb1b224f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:01 +0000

    Add package satnogs-config/0.1/satnogs_config-0.1-py3-none-any.whl

---
+++ packages/satnogs-config/satnogs_config-0.1-py3-none-any.whl/satnogsconfig/__init__.py
@@ -0,0 +1,34 @@
+"""
+SatNOGS Config module initialization
+"""
+import sys
+from pathlib import Path
+
+import pkg_resources
+
+import satnogsconfig.settings as settings
+from satnogsconfig.config import Config
+from satnogsconfig.menu import Menu
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+
+del get_versions
+
+MENU_FILE = 'menu.yml'
+
+
+def main():
+    """
+    SatNOGS Setup utility
+    """
+
+    config = Config(
+        Path(settings.CONFIG_FILE_DIR).joinpath(settings.CONFIG_FILE)
+    )
+    menu = Menu(pkg_resources.resource_stream(__name__, MENU_FILE), config)
+    try:
+        menu.show()
+    except KeyboardInterrupt:
+        sys.exit(0)
