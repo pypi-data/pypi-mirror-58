f48b9432e1b42211eb59e725276573bf3ce9630b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:15 +0000

    Add package wpkit/0.2.4.1/wpkit-0.2.4.1.tar.gz

---
+++ packages/wpkit/wpkit-0.2.4.1.tar.gz/wpkit-0.2.4.1/wpkit/projects/pjfiles/main.py
@@ -0,0 +1,11 @@
+# from wpkit.services.CloudOS import start_server
+from wpkit.linux import clean_port,get_local_ip
+# port1=80
+# port2=8002
+# clean_port(port1)
+# clean_port(port2)
+# start_server(__name__,host=get_local_ip() or "127.0.0.1",port1=port1,port2=port2)
+from wpkit.web.applications.demo import demo
+
+if __name__ == '__main__':
+    demo(host=get_local_ip(),port=80,import_name=__name__)
