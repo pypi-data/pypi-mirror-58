648628818d65a8f43a3539fe34bbebd59af0105e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:40 +0000

    Add package himawari8/0.6/himawari8-0.6.tar.gz

---
+++ packages/himawari8/himawari8-0.6.tar.gz/himawari8-0.6/himawari8/__init__.py
@@ -0,0 +1,3 @@
+from himawari8.himawari8 import GetImageEarth
+from himawari8.himawari8 import GetUrlTile
+from himawari8.himawari8 import GetLastImageDate
---
+++ packages/himawari8/himawari8-0.6.tar.gz/himawari8-0.6/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'himawari8',         
+  version = '0.6',      
+  packages = ['himawari8'],  
+  license='GPL-3.0',        
+  description = 'Assists in downloading images of planet earth from the japanese satellite himawari 8',   
+  author = 'Raphael Augusto Teixeira Silva',                 
+  author_email = 'raphaelaugustoteixeirasilva@gmail.com',      
+  url = 'https://github.com/Maharal/Himawari-8',  
+  download_url = 'https://github.com/Maharal/Himawari-8/archive/v0.6.tar.gz', 
+  keywords = ['earth', 'himawari 8', 'planet', 'photo'],   
+  install_requires=[           
+          'requests',
+          'Pillow',
+          'tqdm',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
