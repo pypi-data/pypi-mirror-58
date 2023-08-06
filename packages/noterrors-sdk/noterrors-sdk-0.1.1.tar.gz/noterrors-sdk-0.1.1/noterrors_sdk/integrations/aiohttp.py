c3de2bb26d81b73c4188893822a3df787a26cc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:04 +0000

    Add package noterrors-sdk/0.1.1/noterrors-sdk-0.1.1.tar.gz

---
+++ packages/noterrors-sdk/noterrors-sdk-0.1.1.tar.gz/noterrors-sdk-0.1.1/noterrors_sdk/integrations/aiohttp.py
@@ -0,0 +1,25 @@
+import asyncio
+import aiohttp
+
+from .base import ClientBase
+
+
+class AiohttpClient(ClientBase):
+
+    def prepare(self):
+        self.client = aiohttp.ClientSession()
+
+    def capture_message(self, message, type):
+        if self.client is None:
+            self.prepare()
+        asyncio.ensure_future(self._send_request(message, type))
+
+    async def _send_request(self, message, type):
+        copy_id = self.save_copy(message, type)
+        async with self.client.post(url=self.address + '/api/v1/events',
+                                    json=self.prepare_message(message, type),
+                                    headers=self.headers) as resp:
+            print(resp)
+            if resp.status == 200:
+                self.clean_copy(copy_id)
+
