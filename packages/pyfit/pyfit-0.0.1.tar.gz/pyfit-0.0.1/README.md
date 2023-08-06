b9dd5fd7c955b1b7b5bbad73e2625f3201289c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:05 +0000

    Add package pyfit/0.0.1/pyfit-0.0.1.tar.gz

---
+++ packages/pyfit/pyfit-0.0.1.tar.gz/pyfit-0.0.1/README.md
@@ -0,0 +1,23 @@
+[![Build Status](https://travis-ci.org/bpesquet/pyfit.svg?branch=master)](https://travis-ci.org/bpesquet/pyfit)
+
+# pyfit
+
+**pyfit** is a simple Machine Learning library built with Python and [NumPy](https://numpy.org/) for educational purposes.
+
+## Overview
+
+This project's main goal is to help ML students and enthusiasts get a deeper understanding of the Machine Learning workflow and main algorithms, by implementing them from scratch.
+
+As a Python package, it also strives to define a clean, pythonic API and follow good coding practices. It uses [type annotations](https://www.python.org/dev/peps/pep-0484/), [pylint](https://www.pylint.org/) and [mypy](http://mypy-lang.org/) for linting, and [pytest](https://pytest.org) for testing.
+
+## Status
+
+**pyfit** is currently under active development. See [Progress](https://github.com/bpesquet/pyfit/projects/1) for details.
+
+## Content
+
+- Data Preprocessing [ [Source](pyfit/preprocessing.py) | [Tests](tests/test_preprocessing.py) ]
+- Metrics [ [Source](pyfit/metrics/) | [Tests](tests/test_metrics.py) ]
+- K-Nearest Neighbors [ [Source](pyfit/neighbors.py) | [Tests](tests/test_neighbors.py) ]
+- Neural Networks [ [Source](pyfit/nn/) ]
+- ... More to come!
---
+++ packages/pyfit/pyfit-0.0.1.tar.gz/pyfit-0.0.1/tests/test_metrics.py
@@ -0,0 +1,48 @@
+"""
+Unit tests for metrics
+"""
+
+# Docstrings are superfluous for test functions
+# pylint: disable=missing-docstring
+
+from math import isclose
+import numpy as np
+from sklearn.metrics import mean_squared_error as mse_sk, accuracy_score
+from pyfit.metrics import mean_squared_error, euclidean_distance, accuracy
+
+
+def test_mse() -> None:
+    expected_list = (np.array([3, -0.5, 2, 7]),
+                     np.array([[0.5, 1], [-1, 1], [7, -6]]))
+    predicted_list = (np.array([2.5, 0.0, 2, 8]),
+                      np.array([[0, 2], [-1, 2], [8, -5]]))
+
+    for expected, predicted in zip(expected_list, predicted_list):
+        error = mean_squared_error(expected, predicted)
+        error_sk = mse_sk(expected, predicted)
+        assert error == error_sk
+
+
+def test_euclidean_distance() -> None:
+    # a = np.array([1, 1.5])
+    # b = np.array([3, 3])
+
+    # dist = euclidean_distance(a, b)
+    # dist_sk = eucl_dist_sk(a, b)
+    # assert dist == dist_sk
+
+    a = np.random.rand(3, 3)
+    b = np.random.rand(3, 3)
+
+    dist = euclidean_distance(a, b)
+    dist_sk = np.linalg.norm(a - b)
+    assert isclose(dist, dist_sk)
+
+
+def test_accuracy() -> None:
+    y_true = np.array([0, 1, 2, 3])
+    y_pred = np.array([0, 2, 1, 3])
+
+    acc = accuracy(y_true, y_pred)
+    acc_sk = accuracy_score(y_true, y_pred)
+    assert acc == acc_sk
