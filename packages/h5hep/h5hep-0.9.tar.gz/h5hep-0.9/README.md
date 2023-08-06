18357750fee47c57915b7cb8287298a8511abdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:13 +0000

    Add package h5hep/0.9/h5hep-0.9.tar.gz

---
+++ packages/h5hep/h5hep-0.9.tar.gz/h5hep-0.9/README.md
@@ -0,0 +1,16 @@
+# h5hep
+
+[![Build Status](https://travis-ci.org/mattbellis/h5hep.svg?branch=master)](https://travis-ci.org/mattbellis/h5hep)
+
+[![Documentation Status](https://readthedocs.org/projects/h5hep/badge/?version=latest)](https://h5hep.readthedocs.io/en/latest/?badge=latest)
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/mattbellis/h5hep/master)
+
+
+
+
+A file format built on HDF5 for use with Heterogeneous Entries in Parallel, and inspired by High Energy Physics datasets and the ROOT analysis toolkit.
+
+# Requirements
+* [h5py](https://www.h5py.org/)
+  * [Github](https://github.com/h5py/h5py)
---
+++ packages/h5hep/h5hep-0.9.tar.gz/h5hep-0.9/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+import sys
+
+requirements = ['h5py']
+
+if sys.version_info < (3, 3):
+    sys.stdout.write("At least Python 3.3 is required.\n")
+    sys.exit(1)
+elif sys.version_info < (3, 6):
+    # typing was added in 3.5, and we rely on critical features that were
+    # introduced in 3.5.2+, so for versions older than 3.6 we rely on
+    # a backport
+    requirements.append('backports.typing')
+
+#import versioneer
+
+setup(
+    name='h5hep',
+    version="0.9",
+    #cmdclass="0.9", # Is this right???
+    description='File format using HDF5 for heterogeneous length data entries.',
+    url='https://github.com/mattbellis/h5hep',
+    download_url='https://github.com/mattbellis/h5hep/archive/v0.9.tar.gz',
+    author='Matt Bellis',
+    author_email='mbellis@siena.edu',
+    license='MIT',
+    packages = ['h5hep'],
+    install_requires = ['numpy','h5py'],
+    #tests_require = ['pytest', 'pytest-cov'],
+    classifiers=[ # HAVE TO FIX ALL THIS
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
