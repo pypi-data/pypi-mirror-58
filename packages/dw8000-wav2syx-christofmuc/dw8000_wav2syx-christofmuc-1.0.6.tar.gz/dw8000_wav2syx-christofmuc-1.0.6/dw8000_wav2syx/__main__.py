0e6ca5fc1beae736741d701f67c5eb70ea7b03e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:43 +0000

    Add package dw8000-wav2syx-christofmuc/1.0.6/dw8000_wav2syx-christofmuc-1.0.6.tar.gz

---
+++ packages/dw8000-wav2syx-christofmuc/dw8000_wav2syx-christofmuc-1.0.6.tar.gz/dw8000_wav2syx-christofmuc-1.0.6/dw8000_wav2syx/__main__.py
@@ -0,0 +1,35 @@
+#
+#  Copyright (c) 2019 Christof Ruch. All rights reserved.
+#
+#  Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+#
+
+import argparse
+import tempfile
+
+from dw8000_wav2syx import dw8000_wav2bin
+from dw8000_wav2syx import dw8000_reverse_engineer
+
+
+def wav2syx():
+    parser = argparse.ArgumentParser(prog="dw8000_wav2syx", description='Convert a Korg DW8000 tape wav into syx format')
+    parser.add_argument('wavfile')
+    parser.add_argument('syxfile')
+    parser.add_argument('--verbose', type=bool, default=False)
+    parser.add_argument('--lowpass', type=bool, default=True)
+    parser.add_argument('--threshold', type=float, default=0.05)
+    parser.add_argument('--store', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    with tempfile.TemporaryFile() as bin_file:
+        worked = dw8000_wav2bin.transform_wav_to_bytes(args.wavfile, bin_file, hysteresis_threshold=args.threshold,
+                                                       lowpass=args.lowpass, verbose=args.verbose)
+        if worked:
+            # Rewind bin file so the next function will read from the start again
+            bin_file.seek(0)
+            dw8000_reverse_engineer.remap_tape_data_to_syx(tapefile=bin_file, syxfile= args.syxfile, verbose=args.verbose, store=args.store)
+
+
+if __name__ == '__main__':
+    wav2syx()
---
+++ packages/dw8000-wav2syx-christofmuc/dw8000_wav2syx-christofmuc-1.0.6.tar.gz/dw8000_wav2syx-christofmuc-1.0.6/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dw8000_wav2syx-christofmuc",  # Replace with your own username
+    version="1.0.6",
+    author="Christof",
+    author_email="christof.ruch@gmail.com",
+    description="Convert data for the Korg DW8000 stored as tape wav files into MIDI sysex format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/christofmuc/dw8000_wav2syx",
+    download_url="",  # This should point to github releases?
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "mido",
+        "scipy"
+    ],
+    entry_points={
+        'console_scripts': [
+            'dw8000_bin2syx= dw8000_wav2syx.dw8000_bin2syx:bin2syx',
+            'dw8000_wav2bin= dw8000_wav2syx.dw8000_wav2bin:wav2bin',
+            'dw8000_wav2syx= dw8000_wav2syx.dw8000_wav2syx:__main__',
+        ]
+    }
+)
