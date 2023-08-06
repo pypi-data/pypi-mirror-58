4ad4a4f60672207a5bd94d093fd93dfbc0c92205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:35 +0000

    Add package QGraphViz/0.0.15/QGraphViz-0.0.15.tar.gz

---
+++ packages/QGraphViz/QGraphViz-0.0.15.tar.gz/QGraphViz-0.0.15/QGraphViz/DotParser/Node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Saifeddine ALOUI
+Description:
+Dot perser implementation
+"""
+
+class Node():
+    """
+    The dot graphviz engine
+    """
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.pos=[0,0]
+        self.size=[1,1]
+        self.kwargs=kwargs
+        # Nodes
+        self.in_edges=[]
+        self.out_edges=[]
+        processed = 0
+
+    
