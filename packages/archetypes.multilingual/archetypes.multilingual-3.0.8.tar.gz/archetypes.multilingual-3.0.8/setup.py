1f55b1956acbe989b1df4454d985e9d8d8de7f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:08 +0000

    Add package archetypes.multilingual/3.0.8/archetypes.multilingual-3.0.8.tar.gz

---
+++ packages/archetypes.multilingual/archetypes.multilingual-3.0.8.tar.gz/archetypes.multilingual-3.0.8/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+version = '3.0.8'
+
+setup(
+    name='archetypes.multilingual',
+    version=version,
+    description='Multilingual support for archetypes.',
+    long_description=u'\n'.join([
+        open('README.rst').read(),
+        open('CHANGES.rst').read(),
+    ]),
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Framework :: Plone',
+        'Framework :: Plone :: Addon',
+        'Framework :: Plone :: 5.0',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='archetypes multilingual i18n translation',
+    author='awello',
+    author_email='awello@gmail.com',
+    url='https://github.com/plone/archetypes.multilingual',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['archetypes'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.ATContentTypes',
+        'plone.app.multilingual',
+        'collective.monkeypatcher',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.multilingual[test]',
+            'plone.app.contenttypes[archetypes]',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
