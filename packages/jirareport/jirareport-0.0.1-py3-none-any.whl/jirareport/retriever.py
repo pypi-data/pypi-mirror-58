8dfe6ab4803efdc5679e5162c868a2c4e706ce2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:40 +0000

    Add package jirareport/0.0.1/jirareport-0.0.1-py3-none-any.whl

---
+++ packages/jirareport/jirareport-0.0.1-py3-none-any.whl/jirareport/retriever.py
@@ -0,0 +1,55 @@
+import logging
+
+from jira import JIRA
+
+logger = logging.getLogger(__name__)
+
+
+class IssueIterator:
+    def __init__(self, get_more, get_detail=None):
+        self.data = None
+        self.get_more = get_more
+        self.get_detail = get_detail
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.data is None:
+            self.data = self.get_more(0)
+        if self.data.current + self.counter >= self.data.total:
+            raise StopIteration()
+        if self.counter == self.data.maxResults:
+            self.data = self.get_more(self.data.current + self.data.maxResults)
+            self.counter = 0
+
+        result = self.data[self.counter]
+        logger.info(
+            f"Processing issue {self.data.current + self.counter + 1}/{self.data.total}:"
+            f" {result.key}"
+        )
+        if self.get_detail:
+            result = self.get_detail(result.key)
+        self.counter += 1
+        return result
+
+
+class JiraRetriever:
+    def __init__(self, server):
+        self.jira = JIRA(server=server)
+
+    def retrieve_issues(self, date_from, date_to, projects=None):
+        str_from = date_from.strftime("%Y-%m-%d")
+        str_to = date_to.strftime("%Y-%m-%d")
+
+        def _internal(start_at):
+            logger.info(f"Retrieving more issues starting at {start_at}")
+            jql = f"(status != Closed OR (updated >= {str_from} AND updated <= {str_to}))"
+            if projects:
+                str_prj = ",".join(projects or [])
+                jql += f" AND project in ({str_prj})"
+
+            return self.jira.search_issues(jql, startAt=start_at)
+
+        return IssueIterator(_internal, self.jira.issue)
