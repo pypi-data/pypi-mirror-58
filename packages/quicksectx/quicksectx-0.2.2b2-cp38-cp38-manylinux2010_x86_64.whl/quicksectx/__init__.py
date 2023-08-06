0eb34e58e5128e806d121dcfaa627180ede2f7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:28 +0000

    Add package quicksectx/0.2.2b2/quicksectx-0.2.2b2-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/quicksectx/quicksectx-0.2.2b2-cp38-cp38-manylinux2010_x86_64.whl/quicksectx/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+from .quicksect1 import Interval, IntervalNode, IntervalTree, distance
+from .quicksect2 import IntervalX, IntervalNodeX, IntervalTreeX, distancex
+# from quicksectx.src import quicksect1 as quicksect
+# from quicksectx.src import quicksect2 as quicksectx
+from .version import __version__
