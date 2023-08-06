f9f58f5658f3eadf5411e9799438058c94bd8711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:47 +0000

    Add package auditorium/19.1.3/auditorium-19.1.3-py3-none-any.whl

---
+++ packages/auditorium/auditorium-19.1.3-py3-none-any.whl/auditorium/__main__.py
@@ -0,0 +1,50 @@
+# coding: utf8
+
+import fire
+import runpy
+import webbrowser
+
+from auditorium import Show
+
+
+class Auditorium:
+    @staticmethod
+    def run(
+        path,
+        host="127.0.0.1",
+        port=6789,
+        debug=False,
+        instance_name="show",
+        launch=True,
+    ):
+        "Runs a custom Python script as a slideshow."
+
+        show = Show.load(path, instance_name)
+        show.run(host=host, port=port, debug=debug, launch=launch)
+
+    @staticmethod
+    def demo(host="127.0.0.1", port=6789, debug=False, launch=True):
+        "Starts the demo slideshow."
+
+        from auditorium.demo import show
+
+        show.run(host, port, debug=debug, launch=launch)
+
+    @staticmethod
+    def render(path, theme="white", instance_name="show"):
+        "Renders a slideshow into a single HTML with all resources embedded."
+
+        show = Show.load(path, instance_name)
+        print(show.render(theme))
+
+    @staticmethod
+    def test():
+        return "It's OK!"
+
+
+def main():
+    fire.Fire(Auditorium, name="auditorium")
+
+
+if __name__ == "__main__":
+    main()
