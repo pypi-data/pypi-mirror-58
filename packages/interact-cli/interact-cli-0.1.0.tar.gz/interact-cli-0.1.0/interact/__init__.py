94cff38f2dd3e6b6c5dc1e97dab4ef83a3e92658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:44 +0000

    Add package interact-cli/0.1.0/interact-cli-0.1.0.tar.gz

---
+++ packages/interact-cli/interact-cli-0.1.0.tar.gz/interact-cli-0.1.0/interact/__init__.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# @Author: JanKinCai
+# @Date:   2019-12-12 22:41:53
+# @Last Modified by:   JanKinCai
+# @Last Modified time: 2019-12-24 23:01:41
+from __future__ import print_function
+import json
+
+from typing import (
+    Any
+)
+
+from interact.base import Interact
+
+
+__version__ = "0.1.0"
+__author__ = "jankincai"
+
+
+def interacts(iconfig: dict) -> Any:
+    """
+    Interacts
+
+    :param iconfig(dict): Interact cli config.
+
+    :return: any
+    """
+
+    return Interact(iconfig=iconfig)
+
+
+def interact(file: str) -> Any:
+    """
+    interact
+
+    :param file(str): xxxx.json file.
+
+    :return: Any
+    """
+
+    with open(file) as f:
+        return interacts(json.loads(f.read()))
+
+
+def loads(iconfig: dict) -> dict:
+    """
+    loads
+
+    :param iconfig(dict): Interact cli config.
+
+    :return: dict
+    """
+
+    return interacts(iconfig=iconfig).get_interact_data()
+
+
+def load(file: str) -> dict:
+    """
+    load
+
+    :param file(str): xxxx.json file.
+
+    :return: dict
+    """
+
+    with open(file) as f:
+        return loads(json.loads(f.read()))
---
+++ packages/interact-cli/interact-cli-0.1.0.tar.gz/interact-cli-0.1.0/interact/fields.py
@@ -0,0 +1,307 @@
+# -*- coding: utf-8 -*-
+# @Author: JanKinCai
+# @Date:   2019-12-23 12:37:34
+# @Last Modified by:   JanKinCai
+# @Last Modified time: 2019-12-24 21:05:24
+# import sys
+
+from typing import (
+    Any, Optional, List, Union
+)
+
+
+class BaseField(object):
+    """
+    BaseField
+
+    :param default(any): input default, default ``None``
+    :param description(str): prefix description, default ``""``
+    """
+
+    message = "Invalided `{value}`"
+    valid_type: Any = None
+
+    def __init__(self, default: Any=None, description: str="", *args, **kwargs):
+        """
+        init
+        """
+
+        self.default = default
+        self.descripiton = description
+        self.is_valid()
+
+    def is_valid(self) -> bool:
+        """
+        Valid default
+
+        :return: bool
+        """
+
+        assert self.valid_type is not None, "valid_type cannot be None."
+
+        if not isinstance(self.default, self.valid_type):
+            return False
+
+        return True
+
+    def print_message(self, **kwargs) -> None:
+        """
+        print message
+
+        :return: None
+        """
+
+        print(f"Error: {self.message.format(**kwargs)}")
+
+    def to_default(self, v: Any) -> Any:
+        """
+        To default
+
+        :param v(any): default
+        """
+
+        return v
+
+    def do_default(self) -> str:
+        """
+        Deal with default
+
+        :return: str
+        """
+
+        default = self.default
+
+        if isinstance(default, list):
+            default = ", ".join(default)
+
+        return f"[{self.to_default(default)}]" if default is not None else ""
+
+    def do(self) -> Any:
+        """
+        Deal with
+
+        :return: any
+        """
+
+        v = self.to_input() or self.default
+
+        # Reason isinstance(v, int)  v(bool) --> True
+        if not isinstance(v, str) and not (isinstance(v, int) and not isinstance(v, bool)):
+            return v
+
+        try:
+            return self.to_value(v)
+        except Exception as e:
+            # print("[+]:", e)
+            self.print_message(value=v)
+
+        return self.do()
+
+    def to_value(self, v: Any) -> Any:
+        """
+        To value
+
+        :param v(str): cmd data
+
+        :return: any
+        """
+
+        pass
+
+    def pre_input(self) -> str:
+        """
+        Pre input
+
+        :return: str
+        """
+
+        return ""
+
+    def post_input(self) -> str:
+        """
+        Post input
+
+        :return: str
+        """
+
+        return ""
+
+    def to_input(self) -> str:
+        """
+        To input
+
+        :return: str
+        """
+
+        v = f"{self.pre_input()}{self.descripiton} {self.do_default()}{self.post_input()}: "
+
+        return input(v)
+
+
+class BooleanField(BaseField):
+    """
+    BoleanField
+    """
+
+    mapping_boolean_true = [
+        "y", "Y",
+        "t", "T",
+        "true", "True",
+        True,
+    ]
+
+    mapping_boolean_false = [
+        "n", "N",
+        "f", "F",
+        "false", "False",
+        False,
+    ]
+
+    valid_type = bool
+
+    def to_default(self, v: bool) -> Any:
+        """
+        To default
+
+        :param v(bool): Default to input string
+
+        :return: any
+        """
+
+        return "y" if v else "n"
+
+    def to_value(self, v: str) -> bool:
+        """
+        To value
+
+        :param v(str): cmd data
+
+        :return: bool
+        """
+
+        if v in self.mapping_boolean_true:
+            return True
+
+        if v in self.mapping_boolean_false:
+            return False
+
+        raise ValueError()
+
+
+class StringField(BaseField):
+    """
+    StringField
+    """
+
+    valid_type = str
+
+    def to_value(self, v: str) -> str:
+        """
+        To value
+
+        :param v(str): cmd data
+
+        :return: str
+        """
+
+        return str(v)
+
+
+class IntField(BaseField):
+    """
+    IntField
+    """
+
+    valid_type = int
+
+    def to_value(self, v: str) -> int:
+        """
+        To value
+
+        :param v(str): cmd data
+
+        :return: int
+        """
+
+        return int(v)
+
+
+class ListField(BaseField):
+    """
+    ListField
+    """
+
+    valid_type = list
+
+    def to_value(self, v: Any) -> list:
+        """
+        To value
+
+        :param v(any): cmd data
+
+        :return: list
+        """
+
+        if isinstance(v, list):
+            return v
+
+        return v.replace(", ", ",").split(",")
+
+
+class ChoiceField(BaseField):
+    """
+    ChoiceField
+
+    :param choice(list): choice, default ``[]``
+    :param default(any): input default, default ``None``
+    :param description(str): prefix description, default ``""``
+    """
+
+    valid_type = int
+
+    def __init__(self, choice: list=[], default: Any=None, description: str="", *args, **kwargs):
+        """
+        init
+        """
+
+        self.choice = choice
+        super(ChoiceField, self).__init__(default=default or 1, description=description, *args, **kwargs)
+
+    def to_value(self, v: Any) -> Any:
+        """
+        To value
+
+        :param v(any): cmd data
+
+        :return: any
+        """
+
+        return self.choice[int(v) - 1]
+
+    def pre_input(self) -> str:
+        """
+        Pre input
+
+        :return: str
+        """
+
+        input_list = [
+            f"Select {self.descripiton.lower()}:",
+        ]
+
+        choice_list = [f"  {i} - {chi}" for i, chi in enumerate(self.choice, 1)]
+        choice_list.append(f"Choose from")
+        input_list.extend(choice_list)
+
+        return "\n".join(input_list)
+
+    def to_input(self) -> str:
+        """
+        To input
+
+        :return: str
+        """
+
+        v = f"{self.pre_input()} {self.do_default()}{self.post_input()}: "
+
+        return input(v)
