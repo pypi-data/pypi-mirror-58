e0dbca25bdc96b9d2f813f571034cf20590d483f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:16 +0000

    Add package BlackScholes-Simulations/0.0.1/BlackScholes-Simulations-0.0.1.tar.gz

---
+++ packages/BlackScholes-Simulations/BlackScholes-Simulations-0.0.1.tar.gz/BlackScholes-Simulations-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='BlackScholes-Simulations',
+    version='0.0.1',
+    desciption='Provide Greeks based on Black Scholes Model. Simulate option priced using Monte Carlo.',
+    author='Sapphire_mirror',
+    author_email='caowenjin0822@gmail.com',
+    url='https://github.com/wenjin-cao822/BlackScholes_Pricing_Simulations',
+    packages=setuptools.find_packages(),
+    zip_safe=True
+)
