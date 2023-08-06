8ebbf5e13eb8e5cae951c64eb1997747c4b8316e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:36 +0000

    Add package recursive-aronr/1.0.7/recursive_aronr-1.0.7.tar.gz

---
+++ packages/recursive-aronr/recursive_aronr-1.0.7.tar.gz/recursive_aronr-1.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name			= "recursive_aronr",
+    version			= "1.0.7",
+    py_modules		= ['recursive'],
+	url				= 'http://somethin.com',
+	author			= 'Roli',
+	author_email	= 'roli@gmail.com',
+    classifiers		=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
