ecdacf6830c4f4d680523757258e6459fb3603aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:04 +0000

    Add package fooster-web/0.3rc8/fooster-web-0.3rc8.tar.gz

---
+++ packages/fooster-web/fooster-web-0.3rc8.tar.gz/fooster-web-0.3rc8/fooster/web/json.py
@@ -0,0 +1,52 @@
+import json
+
+from fooster import web
+
+
+class JSONMixIn:
+    def encode(self, body):
+        if body is None:
+            return super().encode(''.encode(web.default_encoding))
+
+        self.response.headers.set('Content-Type', 'application/json')
+
+        return json.dumps(body).encode(web.default_encoding)
+
+    def decode(self, body):
+        content_type = self.request.headers.get('Content-Type')
+        if content_type is not None and content_type.lower().startswith('application/json'):
+            try:
+                return json.loads(body.decode(web.default_encoding))
+            except Exception:
+                raise web.HTTPError(400)
+
+        return super().decode(body)
+
+
+class JSONHandler(JSONMixIn, web.HTTPHandler):
+    pass
+
+
+class JSONErrorMixIn:
+    def respond(self):
+        self.response.headers.set('Content-Type', 'application/json')
+
+        if self.error.status_message:
+            status_message = self.error.status_message
+        else:
+            status_message = web.status_messages[self.error.code]
+
+        if self.error.message:
+            message = self.error.message
+        else:
+            message = {'error': self.error.code, 'status': status_message}
+
+        return self.error.code, status_message, json.dumps(message).encode(web.default_encoding)
+
+
+class JSONErrorHandler(JSONErrorMixIn, web.HTTPErrorHandler):
+    pass
+
+
+def new_error(error='[0-9]{3}', *, handler=JSONErrorHandler):
+    return {error: handler}
---
+++ packages/fooster-web/fooster-web-0.3rc8.tar.gz/fooster-web-0.3rc8/fooster/web/query.py
@@ -0,0 +1,33 @@
+import collections
+import urllib.parse
+
+from fooster import web
+
+
+regex = r'(?:\?(?P<query>[\w=&%.+]*))?'
+
+
+class QueryMixIn:
+    querystr = None
+
+    def respond(self):
+        if self.querystr is None and 'query' in self.groups:
+            self.querystr = self.groups['query']
+
+        if self.querystr is not None:
+            try:
+                self.request.query = collections.OrderedDict(urllib.parse.parse_qsl(self.querystr, True))
+            except Exception:
+                raise web.HTTPError(400)
+        else:
+            self.request.query = None
+
+        return super().respond()
+
+
+class QueryHandler(QueryMixIn, web.HTTPHandler):
+    pass
+
+
+def new(base, handler):
+    return {base + regex: handler}
