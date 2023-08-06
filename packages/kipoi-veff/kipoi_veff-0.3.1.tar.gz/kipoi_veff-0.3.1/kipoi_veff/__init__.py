41b2a2d13678af4a8159323a58e96876afad1e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:15 +0000

    Add package kipoi-veff/0.3.1/kipoi_veff-0.3.1.tar.gz

---
+++ packages/kipoi-veff/kipoi_veff-0.3.1.tar.gz/kipoi_veff-0.3.1/kipoi_veff/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+__author__ = 'Kipoi team'
+__email__ = 'avsec@in.tum.de'
+__version__ = '0.3.1'
+
+
+from . import scores
+from . import parsers
+from . import utils
+from . import snv_predict
+from . import specs
+# from .scores import Ref, Alt, Diff, LogitRef, LogitAlt, Logit, DeepSEA_effect
+# from .parsers import KipoiVCFParser
+
+from .snv_predict import predict_snvs, analyse_model_preds, score_variants
+from .utils import ModelInfoExtractor, SnvPosRestrictedRg, SnvCenteredRg, ensure_tabixed_vcf, VcfWriter, \
+    BedOverlappingRg
+
+from .mutation_map import MutationMap, MutationMapPlotter
+
+
+# Required by kipoi
+from .__main__ import cli_main
+from .specs import VarEffectModelArgs as ModelParser
+from .specs import VarEffectDataLoaderArgs as DataloaderParser
---
+++ packages/kipoi-veff/kipoi_veff-0.3.1.tar.gz/kipoi_veff-0.3.1/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+requirements = [
+    "kipoi>=0.6.12",
+    #$"git+ssh://git@github.com/DerThorsten/kipoi.git@niceparse",#egg=kipoi,
+    "kipoi-utils>=0.1.12",
+    # vep
+    "pyvcf",
+    "cyvcf2",
+    "pybedtools",
+    "pysam",  # required by pybedtools
+    "intervaltree<3",
+    "deepdish",
+    "matplotlib",
+    "seaborn",
+    "shapely",
+    "descartes",
+    # TODO - which requirements to we need?
+    "future",
+    "numpy",
+    "pandas",
+    "tqdm",
+    "related>=0.6.0",
+    "enum34",
+    "colorlog",
+    "cookiecutter",
+    # sometimes required
+    "h5py",
+    "urllib3>=1.21.1", #,<1.23",
+]
+
+test_requirements = [
+    "bumpversion",
+    "wheel",
+    "jedi",
+    "epc",
+    "pytest>=3.3.1",
+    "pytest-xdist",  # running tests in parallel
+    "pytest-pep8",  # see https://github.com/kipoi/kipoi/issues/91
+    "pytest-cov",
+    "coveralls",
+    "scikit-learn",
+    "cython",
+    # "genomelake",
+    "keras",
+    "tensorflow",
+    "kipoiseq"
+]
+
+setup(
+    name='kipoi_veff',
+    version='0.3.1',
+    description="kipoi_veff: variant effect prediction plugin for Kipoi",
+    author="Kipoi team",
+    author_email='avsec@in.tum.de',
+    url='https://github.com/kipoi/kipoi-veff',
+    long_description="kipoi_veff: variant effect prediction plugin for Kipoi",
+    packages=find_packages(),
+    install_requires=requirements,
+    extras_require={
+        "develop": test_requirements,
+    },
+    # entry_points={'console_scripts': ['kipoi_veff = kipoi_veff.__main__:main']},
+    license="MIT license",
+    zip_safe=False,
+    keywords=["variant effect prediction", "model zoo", "deep learning",
+              "computational biology", "bioinformatics", "genomics"],
+    test_suite='tests',
+    include_package_data=True,
+    tests_require=test_requirements
+)
