e930f921bd1959d525282008226eb4c8ba2b0c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:48 +0000

    Add package micropm4py/0.0.2/micropm4py-0.0.2-py3-none-any.whl

---
+++ packages/micropm4py/micropm4py-0.0.2-py3-none-any.whl/micropm4py/__init__.py
@@ -0,0 +1,8 @@
+from micropm4py import log, petrinet
+
+__version__ = '0.0.2'
+__doc__ = "MicroPM4Py - Process Mining for Micro-Controllers"
+__author__ = 'PADS'
+__author_email__ = 'a.berti@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "a.berti@pads.rwth-aachen.de"
