b0cb1d4073ec20956207160dc963959ff6b17c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:24 +0000

    Add package pathvalidate/1.1.1/pathvalidate-1.1.1.tar.gz

---
+++ packages/pathvalidate/pathvalidate-1.1.1.tar.gz/pathvalidate-1.1.1/pathvalidate/argparse.py
@@ -0,0 +1,56 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+
+from argparse import ArgumentTypeError
+
+from ._common import PathType
+from ._file import sanitize_filename, sanitize_filepath, validate_filename, validate_filepath
+from .error import ValidationError
+
+
+def validate_filename_arg(value: str) -> str:
+    try:
+        validate_filename(value)
+    except ValidationError as e:
+        raise ArgumentTypeError(e)
+
+    return value
+
+
+def validate_filepath_arg(value: str) -> str:
+    try:
+        validate_filepath(value)
+    except ValidationError as e:
+        raise ArgumentTypeError(e)
+
+    return value
+
+
+def sanitize_filename_arg(value: str) -> PathType:
+    return sanitize_filename(value)
+
+
+def sanitize_filepath_arg(value: str) -> PathType:
+    return sanitize_filepath(value)
+
+
+def filename(value: PathType) -> PathType:
+    # Deprecated
+    try:
+        validate_filename(value)
+    except ValidationError as e:
+        raise ArgumentTypeError(e)
+
+    return sanitize_filename(value)
+
+
+def filepath(value: PathType) -> PathType:
+    # Deprecated
+    try:
+        validate_filepath(value)
+    except ValidationError as e:
+        raise ArgumentTypeError(e)
+
+    return sanitize_filepath(value)
