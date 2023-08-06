5564b9c67536b0c53cfea5b94c4db1f2212d1fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package GBComments/0.0.6/GBComments-0.0.6.tar.gz

---
+++ packages/GBComments/GBComments-0.0.6.tar.gz/GBComments-0.0.6/GBComments.py
@@ -0,0 +1,19 @@
+import random
+
+from data import datas
+
+
+def produceComments(_type="好评", _class="鞋子", filename="data.json"):
+    """
+    :param _type: 好评 或者 差评
+    :param _class: 商品类目，例如：鞋子
+    """
+    try:
+        comments = datas[_type]
+        return (random.choice(comments["开始"]) + random.choice(comments["中间"]) + random.choice(comments["结束"])).replace("$", _class)
+    except Exception as e:
+        print("{} 文件找不到，请放在main的同目录下！".format(e))
+    
+
+if __name__ == "__main__":
+    produceComments(_type="好评", _class="手机")
