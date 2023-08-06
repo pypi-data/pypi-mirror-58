eea465a9c5a2e0d9c43d6ccae38b6ea8adc2a36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:57 +0000

    Add package GBComments/0.0.1/GBComments-0.0.1.tar.gz

---
+++ packages/GBComments/GBComments-0.0.1.tar.gz/GBComments-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='GBComments',
+    version='0.0.1',
+    py_modules=['GBComments'],
+    author="WRAllen",
+    author_email="1072274105@qq.com",
+    description="这是一个好评差评的包",
+)
