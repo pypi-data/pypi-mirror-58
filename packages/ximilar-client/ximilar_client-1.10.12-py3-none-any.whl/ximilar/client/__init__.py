df12d2ad977defc0e125e40141d89fa1dc048c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:59 +0000

    Add package ximilar-client/1.10.12/ximilar_client-1.10.12-py3-none-any.whl

---
+++ packages/ximilar-client/ximilar_client-1.10.12-py3-none-any.whl/ximilar/client/__init__.py
@@ -0,0 +1,7 @@
+from .client import RestClient
+from .recognition import RecognitionClient
+from .tagging import GenericTaggingClient, FashionTaggingClient
+from .colors import DominantColorProductClient, DominantColorGenericClient
+from .smart import SmartSearchClient
+from .detection import DetectionClient
+from .search import SimilarityPhotosClient, SimilarityProductsClient
