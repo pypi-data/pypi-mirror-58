af031f9769cd89202b3cb55f47753fb569a7c44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:04 +0000

    Add package openfizzbuzz/0.1.5/openfizzbuzz-0.1.5.tar.gz

---
+++ packages/openfizzbuzz/openfizzbuzz-0.1.5.tar.gz/openfizzbuzz-0.1.5/openfizzbuzz/__main__.py
@@ -0,0 +1,11 @@
+"""
+Entry point for module call of openfizzbuzz module.
+$ python -m openfizzbuzz
+
+Redirects to main.py run function.
+"""
+from openfizzbuzz import main
+
+
+if __name__ == '__main__':
+    main.run()
---
+++ packages/openfizzbuzz/openfizzbuzz-0.1.5.tar.gz/openfizzbuzz-0.1.5/tests/test_word.py
@@ -0,0 +1,8 @@
+import pytest
+
+from openfizzbuzz.core.word import Word, WordFactory
+
+
+def test_word_instantiation():
+    pass
+
