1ba9b2b6c10780787ca0457a8a9b7bd24291eb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:10 +0000

    Add package quantumlib/0.0.102/quantumlib-0.0.102.tar.gz

---
+++ packages/quantumlib/quantumlib-0.0.102.tar.gz/quantumlib-0.0.102/setup.py
@@ -0,0 +1,25 @@
+__version__ = '0.0.102'
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='quantumlib',
+    version=__version__,
+    author='Altertech',
+    author_email='div@altertech.com',
+    description='Quantum calculations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alttch/quantumlib',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+    ),
+)
