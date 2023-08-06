55d15cea69c6deaee3c3aaf75bcd01c1e457190e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package countries-utils/1.0.21/countries_utils-1.0.21-py2.py3-none-any.whl

---
+++ packages/countries-utils/countries_utils-1.0.21-py2.py3-none-any.whl/countries_utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) ALT-F1 SPRL, Abdelkrim Boujraf. All rights reserved.
+# Licensed under the EUPL License, Version 1.2. See LICENSE in the project root for license information.
+
+from .countries_utils import get_list_of_countries_in_text
+
+__all__ = ["get_list_of_countries_in_text"]
