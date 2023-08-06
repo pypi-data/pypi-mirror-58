bcd6474ce43c03c0a6724aef48aba58f344da777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package AioCacheBucket/0.0.7/AioCacheBucket-0.0.7.tar.gz

---
+++ packages/AioCacheBucket/AioCacheBucket-0.0.7.tar.gz/AioCacheBucket-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import sys
+from pathlib import Path
+ 
+setup(
+    name="AioCacheBucket",
+    version="0.0.7",
+    author="Chenwe-i-lin",
+    author_email="1846913566@qq.com",
+    description="cache for python, base on asyncio.",
+    long_description=Path("./readme.md").read_text("utf-8") if Path("./readme.md").exists() else "",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/NatriumLab/AioCacheBucket",
+    packages=['AioCacheBucket'],
+    install_requires=[
+        "infinity4py"
+    ]
+)
