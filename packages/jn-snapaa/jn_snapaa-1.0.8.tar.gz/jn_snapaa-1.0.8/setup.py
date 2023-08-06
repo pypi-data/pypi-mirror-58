a343aa703e1f0bb3423e1e29d3a5c61b76e31fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:19 +0000

    Add package jn-snapaa/1.0.8/jn_snapaa-1.0.8.tar.gz

---
+++ packages/jn-snapaa/jn_snapaa-1.0.8.tar.gz/jn_snapaa-1.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jn_snapaa',
+    version='1.0.8',
+    author='jnddd',
+    author_email='122296743@qq.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description='a easy snap js',
+    packages=['snaper'],
+    install_requires=[],
+    data_files=[('js', ['snap.js'])],
+)
