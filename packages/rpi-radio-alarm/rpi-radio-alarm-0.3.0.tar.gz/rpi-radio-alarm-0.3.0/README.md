34570e4d12567b0e2606dbb688449fb7810f2953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:06 +0000

    Add package rpi-radio-alarm/0.3.0/rpi-radio-alarm-0.3.0.tar.gz

---
+++ packages/rpi-radio-alarm/rpi-radio-alarm-0.3.0.tar.gz/rpi-radio-alarm-0.3.0/README.md
@@ -0,0 +1,9 @@
+# rpi-radio-alarm
+package for the rpi-radio-alarm development
+
+## Usage
+`pip install rpi-radio-alarm`
+
+### Return Values
+The `ApiHelper` object takes a `ResponseParser` as optional argument.
+This defines the way the different Responses are returned.
---
+++ packages/rpi-radio-alarm/rpi-radio-alarm-0.3.0.tar.gz/rpi-radio-alarm-0.3.0/rpiradioalarm/RpiResponseParser.py
@@ -0,0 +1,36 @@
+from rpiradioalarm import COMMANDS
+
+
+class ResponseParser(object):
+    GET_ALARM = 'get alarm'
+    GET_ALARMS = 'alarms'
+    CHANGE_ALARM = 'c alarm'
+    STOP_RADIO = 'st radio'
+    START_RADIO = 's radio'
+
+    def __init__(self):
+        self.parse_fun = {COMMANDS.GET_ALARMS: self.__get_alarms, COMMANDS.GET_ALARM: self.__get_alarm,
+                          COMMANDS.CHANGE_ALARM: self.__change_alarm, COMMANDS.START_RADIO: self.__start_radio,
+                          COMMANDS.STOP_RADIO: self.__stop_radio}
+
+    def parse_response(self, cmd, response, args):
+        return self.parse_fun.get(cmd)(response, args)
+
+    def __get_alarms(self, response, args):
+        return response
+
+    def __get_alarm(self, response, args):
+        return response
+
+    def __change_alarm(self, response, args):
+        return self.__get_alarm(response, args)
+
+    def __stop_radio(self, response, args):
+        return self.__radio_string(response, args)
+
+    def __start_radio(self, response, args):
+        return self.__radio_string(response, args)
+
+    @staticmethod
+    def __radio_string(response, args):
+        return response
