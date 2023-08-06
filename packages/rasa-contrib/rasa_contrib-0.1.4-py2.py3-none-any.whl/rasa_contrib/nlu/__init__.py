eaebf71ab04ad1bfc7c9fb599243218757551f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package rasa-contrib/0.1.4/rasa_contrib-0.1.4-py2.py3-none-any.whl

---
+++ packages/rasa-contrib/rasa_contrib-0.1.4-py2.py3-none-any.whl/rasa_contrib/nlu/__init__.py
@@ -0,0 +1,4 @@
+from rasa_contrib.nlu.featurizers import BertTextFeaturizer
+from rasa_contrib.nlu.extractors import BilstmCrfTensorFlowEntityExtractor
+from rasa_contrib.nlu.classifiers import TextCnnTensorFlowClassifier
+from rasa_contrib.nlu.utils import TensorflowNLP, PaddleNLP
