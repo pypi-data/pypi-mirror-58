b85009df7602b27cb099c58fc47a18cd8cdebc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:27 +0000

    Add package fileconfig/0.5.10/fileconfig-0.5.10-py2.py3-none-any.whl

---
+++ packages/fileconfig/fileconfig-0.5.10-py2.py3-none-any.whl/fileconfig/__init__.py
@@ -0,0 +1,13 @@
+# fileconfig - create class instances from config file section
+
+"""Parse config file and return class instances for each section."""
+
+from .bases import Config, Stacked
+
+__all__ = ['Config', 'Stacked']
+
+__title__ = 'fileconfig'
+__version__ = '0.5.10'
+__author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
+__license__ = 'MIT, see LICENSE.txt'
+__copyright__ = 'Copyright (c) 2014-2019 Sebastian Bank'
