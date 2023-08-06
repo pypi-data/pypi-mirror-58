c65fd1bd8b97dd44bf21e51a93bf712e7a2f881f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:29 +0000

    Add package guang/0.0.7.6.2/guang-0.0.7.6.2.tar.gz

---
+++ packages/guang/guang-0.0.7.6.2.tar.gz/guang-0.0.7.6.2/guang/__init__.py
@@ -0,0 +1,17 @@
+'''
+This is a universal function library.
+'''
+from . import *
+import guang.get_version as get_version
+
+
+__v = get_version.get_version(update=False)
+
+# '0.0.7.2.7'
+__version__ = '0.0.' + __v
+print('__version__:', __version__)
+__author__ = 'K.y'
+__copyright = 'Copyright 2019 K.y'
+
+__all__ = ["ML","Utils","DL", "Voice", "wechat"]
+
