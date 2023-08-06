6416fcbb2bab020d736d674c176285005d3d1b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:54 +0000

    Add package SLAZTA/0.3/SLAZTA-0.3.zip

---
+++ packages/SLAZTA/SLAZTA-0.3.zip/SLAZTA-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SLAZTA',
+    version='0.3',
+    author="Ash Munro",
+    author_email="anne.munro@canada.com",
+    packages=['SLAZTA',],
+    license='MIT License',
+    url='https://github.com/CSBP-CPSE/SLA-ZTA/',
+    description = 'SLAZTA is a tool to create self-contained areas from commuting data.',
+    long_description=open('README.txt').read(),
+)
