3a134cf0f017197195561ed218c8cf6d3073eb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package aws-mfa-util/0.0.3/aws-mfa-util-0.0.3.tar.gz

---
+++ packages/aws-mfa-util/aws-mfa-util-0.0.3.tar.gz/aws-mfa-util-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name='aws-mfa-util',
+    version='0.0.3',
+    author='hiroya akita',
+    author_email='akky.develop@gmail.com',
+    description='AWS CLIにMFA適用アカウントを作成・更新するためのツール',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['boto3', 'PyInquirer'],
+    url='https://github.com/papi-tokei/aws-mfa-util',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
