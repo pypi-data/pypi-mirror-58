1410db5af81424578810092cc5f1e9c522993464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:13 +0000

    Add package threefive/1.1.81/threefive-1.1.81-py3-none-any.whl

---
+++ packages/threefive/threefive-1.1.81-py3-none-any.whl/threefive/util.py
@@ -0,0 +1,26 @@
+import base64
+import bitstring
+
+
+def hex_decode(k):
+    try: return bytearray.fromhex(hex(k)[2:]).decode()
+    except: return k
+
+def kv_print(obj):
+    print(f'\t{vars(obj)}')
+ 
+def mk_bits(s):
+    if type(s) in [bitstring.BitStream,bitstring.ConstBitStream]: return s
+    try: return bitstring.ConstBitStream(bytes=base64.b64decode(s))
+    except: return bitstring.ConstBitStream(s)
+
+def reserved(bb,bst):
+    bb.bitpos+=bst
+
+def time_90k(k):
+    t= k/90000.0    
+    return f'{t :.6f}'
+
+
+
+
