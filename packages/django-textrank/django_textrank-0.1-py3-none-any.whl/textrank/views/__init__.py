734ba48b631188be9c145f87c0fc4e234bba954c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:50 +0000

    Add package django-textrank/0.1/django_textrank-0.1-py3-none-any.whl

---
+++ packages/django-textrank/django_textrank-0.1-py3-none-any.whl/textrank/views/__init__.py
@@ -0,0 +1,9 @@
+from textrank.views.pages import (
+    IndexView, GroupsView, KeywordsView, RankView, WeightFormView,
+)
+
+index = IndexView.as_view()
+groups = GroupsView.as_view()
+keywords = KeywordsView.as_view()
+rank = RankView.as_view()
+weightform = WeightFormView.as_view()
