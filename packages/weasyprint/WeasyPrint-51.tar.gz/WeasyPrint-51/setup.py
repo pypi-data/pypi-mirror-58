44a49ffebe0dd4bc13ef901b357e635872b8fe54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package weasyprint/51/WeasyPrint-51.tar.gz

---
+++ packages/weasyprint/WeasyPrint-51.tar.gz/WeasyPrint-51/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+    WeasyPrint
+    ==========
+
+    WeasyPrint converts web documents to PDF.
+
+    :copyright: Copyright 2011-2019 Simon Sapin and contributors, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+
+"""
+
+import sys
+
+from setuptools import setup
+
+if sys.version_info.major < 3:
+    raise RuntimeError(
+        'WeasyPrint does not support Python 2.x anymore. '
+        'Please use Python 3 or install an older version of WeasyPrint.')
+
+setup()
