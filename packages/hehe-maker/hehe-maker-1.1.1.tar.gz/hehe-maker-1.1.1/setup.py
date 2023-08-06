5a2137aee9fce4ec680e2ef760c82356f4169aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:03 +0000

    Add package hehe-maker/1.1.1/hehe-maker-1.1.1.tar.gz

---
+++ packages/hehe-maker/hehe-maker-1.1.1.tar.gz/hehe-maker-1.1.1/setup.py
@@ -0,0 +1,17 @@
+# Denna fil är endast till för att det ska gå att installera hehemaker.py.
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_descrtiption = fh.read()
+
+setup(
+    name='hehe-maker',
+    version='1.1.1',
+    author='Emil Jonathan Eriksson',
+    author_email='eje1999@gmail.com',
+    description='Ett enkelt kommandoverktyg för att sätta ihop PDF:er till HeHE',
+    long_descrtiption=long_descrtiption,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ginger51011/hehe-maker",
+    packages=find_packages(),
+)
