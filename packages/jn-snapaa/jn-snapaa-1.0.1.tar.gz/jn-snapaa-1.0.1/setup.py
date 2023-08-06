9387f173ab4ba623916a8d35d172fea6f50ca187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:15 +0000

    Add package jn-snapaa/1.0.1/jn-snapaa-1.0.1.tar.gz

---
+++ packages/jn-snapaa/jn-snapaa-1.0.1.tar.gz/jn-snapaa-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='jn-snapaa',
+    version='1.0.1',
+    description='a easy snap func',
+    author='szmgithubcode',
+    author_email='122296743@qq.com',
+    packages=find_packages(),
+    #install_requires=['requests'],
+)
