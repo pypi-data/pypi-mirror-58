1d1e84722943d38ac0143649b2ce16cb624c1bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:59 +0000

    Add package d3graph/0.1.2/d3graph-0.1.2.tar.gz

---
+++ packages/d3graph/d3graph-0.1.2.tar.gz/d3graph-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     install_requires=['pandas','numpy','seaborn','networkx>2'],
+     python_requires='>=3',
+     name='d3graph',  
+     version='0.1.2',
+#     version=versioneer.get_version(),    # VERSION CONTROL
+#     cmdclass=versioneer.get_cmdclass(),  # VERSION CONTROL
+     author="Erdogan Taskesen",
+     author_email="erdogant@gmail.com",
+     description="Interactive network in d3js",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/erdoganta/d3graph",
+	 download_url = 'https://github.com/erdoganta/d3graph/archive/0.1.2.tar.gz',
+     packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
+     include_package_data=True, # Must be true to include files depicted in MANIFEST.in
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+     ],
+ )
