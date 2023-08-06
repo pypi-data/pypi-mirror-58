6587f827f85f34ec30711a2076e070e5056c28a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:40 +0000

    Add package pyMetaClasses/1.0.0/pyMetaClasses-1.0.0.tar.gz

---
+++ packages/pyMetaClasses/pyMetaClasses-1.0.0.tar.gz/pyMetaClasses-1.0.0/README.md
@@ -0,0 +1,26 @@
+![PyPI - License](https://img.shields.io/pypi/l/pyMetaClasses)
+![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/Paebbels/pyMetaClasses) 
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/Paebbels/pyMetaClasses)
+[![Documentation Status](https://readthedocs.org/projects/pymetaclasses/badge/?version=latest)](https://pyMetaClasses.readthedocs.io/en/latest/?badge=latest)
+[![PyPI](https://img.shields.io/pypi/v/pyMetaClasses)](https://pypi.org/project/pyMetaClasses/)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pyMetaClasses)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/pyMetaClasses)
+![PyPI - Status](https://img.shields.io/pypi/status/pyMetaClasses)
+
+# pyMetaClasses
+
+A collection of MetaClasses for Python.
+
+
+## Contributors:
+
+* [Patrick Lehmann](https://github.com/Paebbels) (Maintainer)
+
+
+## License
+
+This library is licensed under [Apache License 2.0](LICENSE.md)
+
+-------------------------
+
+SPDX-License-Identifier: Apache-2.0
---
+++ packages/pyMetaClasses/pyMetaClasses-1.0.0.tar.gz/pyMetaClasses-1.0.0/pyMetaClasses/__init__.py
@@ -0,0 +1,51 @@
+# EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+# vim: tabstop=2:shiftwidth=2:noexpandtab
+# kate: tab-width 2; replace-tabs off; indent-width 2;
+# =============================================================================
+#               __  __      _         ____ _
+#   _ __  _   _|  \/  | ___| |_ __ _ / ___| | __ _ ___ ___  ___  ___
+#  | '_ \| | | | |\/| |/ _ \ __/ _` | |   | |/ _` / __/ __|/ _ \/ __|
+#  | |_) | |_| | |  | |  __/ || (_| | |___| | (_| \__ \__ \  __/\__ \
+#  | .__/ \__, |_|  |_|\___|\__\__,_|\____|_|\__,_|___/___/\___||___/
+#  |_|    |___/
+# =============================================================================
+# Authors:            Patrick Lehmann
+#
+# Python module:      A collection of MetaClasses for Python.
+#
+# Description:
+# ------------------------------------
+#		TODO
+#
+# License:
+# ============================================================================
+# Copyright 2017-2019 Patrick Lehmann - Bötzingen, Germany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============================================================================
+#
+__api__ = [
+	'Singleton',
+]
+__all__ = __api__
+
+
+class Singleton(type):
+	"""Implements a singleton pattern in form of a Python metaclass (a class constructing classes)."""
+	_instances = {}
+	def __call__(cls, *args, **kwargs):
+		if cls not in cls._instances:
+			cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+		return cls._instances[cls]
