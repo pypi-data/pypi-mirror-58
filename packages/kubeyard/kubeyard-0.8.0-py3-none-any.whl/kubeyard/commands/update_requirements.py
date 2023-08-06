0e42521cc6e7dd10c2ec0ef05543f29fbc819f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package kubeyard/0.8.0/kubeyard-0.8.0-py3-none-any.whl

---
+++ packages/kubeyard/kubeyard-0.8.0-py3-none-any.whl/kubeyard/commands/update_requirements.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+
+import sh
+
+from kubeyard.commands.devel import BaseDevelCommand
+
+logger = logging.getLogger(__name__)
+
+
+class UpdateRequirementsCommand(BaseDevelCommand):
+    """
+    Command can update requirements using `freeze_requirements` command in container.
+
+    Requirements: \n
+        - `freeze_requirements command` available in container \n
+        - volume with requirements files mounted \n
+
+    Can be overridden in <project_dir>/scripts/update_requirements.
+    """
+    custom_script_name = 'update_requirements'
+
+    def run_default(self):
+        logger.info('Updating requirements for "{}"...'.format(self.image))
+        sh.docker(
+            'run', '--rm', '-i',
+            '-u', '{}:{}'.format(self.uid, self.gid),
+            '-e', 'CUSTOM_COMPILE_COMMAND="kubeyard update_requirements"',
+            '-e', 'HOME=/tmp',
+            *self.volumes,
+            self.image,
+            'bash', '-c', 'freeze_requirements',
+            _err=sys.stdout.buffer,
+        )
+        logger.info('Requirements updated!')
