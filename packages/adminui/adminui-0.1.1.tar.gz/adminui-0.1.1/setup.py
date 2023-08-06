245c6683495081e7ceca67969f1eaf31499fd30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:35 +0000

    Add package adminui/0.1.1/adminui-0.1.1.tar.gz

---
+++ packages/adminui/adminui-0.1.1.tar.gz/adminui-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="adminui", # Replace with your own username
+    version="0.1.1",
+    author="Yu Wang (bigeyex)",
+    author_email="bigeyex@gmail.com",
+    description="Write professional web interface with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bigeyex/python-adminui",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'Flask'
+    ],
+    python_requires='>=3.6',
+)
