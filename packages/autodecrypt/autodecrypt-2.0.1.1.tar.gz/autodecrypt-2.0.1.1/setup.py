00428c849d531958a2effe3b54503bca5be0fde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:35 +0000

    Add package autodecrypt/2.0.1.1/autodecrypt-2.0.1.1.tar.gz

---
+++ packages/autodecrypt/autodecrypt-2.0.1.1.tar.gz/autodecrypt-2.0.1.1/setup.py
@@ -0,0 +1,34 @@
+#from distutils.core import setup
+from setuptools import find_packages, setup
+setup(
+    name = 'autodecrypt',
+    packages = ['autodecrypt'],
+    version = '2.0.1.1',
+    license='MIT',
+    description = 'Tool to decrypt iOS firmware images',
+    author = 'matteyeux',
+    author_email = 'mathieu.hautebas@gmail.com',
+    url = 'https://github.com/matteyeux/autodecrypt',
+    download_url = 'https://github.com/matteyeux/autodecrypt/archive/2.0.1.tar.gz',
+    keywords = ['autodecrypt', 'iOS', 'iBoot'],
+    install_requires = ['certifi', 'chardet', 'cssselect', 
+                        'idna', 'lxml', 'pyquery', 'remotezip', 
+                        'requests', 'tabulate', 'urllib3',
+    ],
+    classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Information Technology',
+      'Topic :: Utilities',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+    ],
+    entry_points = {
+        "console_scripts": [
+            "autodecrypt=autodecrypt.autodecrypt:main",
+        ]
+    }
+
+)
