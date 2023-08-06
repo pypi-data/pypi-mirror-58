4b8df7a779bdcce5eb6e6c21133394ef708c42cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:46 +0000

    Add package lifeomic-logging/0.2.6/lifeomic_logging-0.2.6.tar.gz

---
+++ packages/lifeomic-logging/lifeomic_logging-0.2.6.tar.gz/lifeomic_logging-0.2.6/lifeomic_logging/__init__.py
@@ -0,0 +1,9 @@
+import logging
+import threading
+from lifeomic_logging.logger import scoped_logger, get_request_context
+
+
+logging.getLogger("aws_xray_sdk").setLevel(logging.CRITICAL)
+logging.getLogger("aws_xray_sdk.core.models.entity").setLevel(logging.ERROR)
+
+__all__ = ["scoped_logger", "get_request_context"]
