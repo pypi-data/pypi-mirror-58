31451c0a603e42f93086a39a1670c73fe4173078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:50 +0000

    Add package python-messenger-bot/0.0.12/python-messenger-bot-0.0.12.tar.gz

---
+++ packages/python-messenger-bot/python-messenger-bot-0.0.12.tar.gz/python-messenger-bot-0.0.12/messenger/constant.py
@@ -0,0 +1,27 @@
+class Constant:
+    default_api_version = 5.0
+    send_api = 'https://graph.facebook.com/v%s/me/messages'%default_api_version
+    insights_api = 'https://graph.facebook.com/v%s/me/insights'%default_api_version
+    mps_threshold = 30
+    
+    class attachment_type:
+        audio = 'AUDIO'
+        video = 'VIDEO'
+        image = 'IMAGE'
+        file = 'FILE'
+        template = 'TEMPLATE'
+
+    class messaging_type:
+        response = 'RESPONSE'
+        update = 'UPDATE'
+        message_tag = 'MESSAGE_TAG'
+
+    class sender_action:
+        mark_seen = 'mark_seen'
+        typing_on = 'typing_on'
+        typing_off = 'typing_off'
+
+    class template_type:
+        receipt = 'receipt'
+        button = 'button'
+        generic = 'generic'
---
+++ packages/python-messenger-bot/python-messenger-bot-0.0.12.tar.gz/python-messenger-bot-0.0.12/messenger/helper/http/post.py
@@ -0,0 +1,20 @@
+﻿import requests
+
+class Post:
+    def __init__(self, page_access_token, **kwargs):
+        self.page_access_token = page_access_token
+        
+        return super().__init__(**kwargs)
+
+    def send(self, url, json):
+        try:
+            request_session = requests.Session()
+
+            params = {'access_token': self.page_access_token}
+            request = requests.Request('POST', url = url, params = params, json = json)
+            prepare = request.prepare()
+        
+            response = request_session.send(prepare)
+        finally:
+            request_session.close()
+            return response.text
