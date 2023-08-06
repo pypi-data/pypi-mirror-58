e4b26f1e77ca38b18b9f091520d8afe8345d5eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:41 +0000

    Add package finplot/0.3.6/finplot-0.3.6-py3.8.egg

---
+++ packages/finplot/finplot-0.3.6-py3.8.egg/finplot/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Financial data plotter with better defaults, api, behavior and performance than
+mpl_finance and plotly.
+
+Lines up your time-series with a shared X-axis; ideal for volume, RSI, etc.
+
+Zoom does something similar to what you'd normally expect for financial data,
+where the Y-axis is auto-scaled to highest high and lowest low in the active
+region.
+'''
+
+from .finplot import *
