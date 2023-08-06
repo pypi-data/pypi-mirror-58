c8844e3ef656c3e1f19013812dbb5e9182c50657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:11 +0000

    Add package managpu/1.2.4/managpu-1.2.4.tar.gz

---
+++ packages/managpu/managpu-1.2.4.tar.gz/managpu-1.2.4/managpu/_choice/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from ._gpu_manager import *
---
+++ packages/managpu/managpu-1.2.4.tar.gz/managpu-1.2.4/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+__author__ = "Perry"
+__version__ = "1.2.4"
+
+setup(
+      name='managpu',
+      version=__version__,
+      description='managpu: for choosing gpus',
+      author=__author__,
+      maintainer=__author__,
+      url='https://github.com/perryupan/managpu',
+      packages=find_packages(),
+      py_modules=[],
+      long_description="Make setting gpus more easy.",
+      license="GPLv3",
+      platforms=["any"],
+      install_requires = ["pynvml>=8.0.1"]
+)
