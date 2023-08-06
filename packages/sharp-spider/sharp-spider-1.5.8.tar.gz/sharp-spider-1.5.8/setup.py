a0e68d6503efc06ebd5e26f8733b7fe02765ff52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:06 +0000

    Add package sharp-spider/1.5.8/sharp-spider-1.5.8.tar.gz

---
+++ packages/sharp-spider/sharp-spider-1.5.8.tar.gz/sharp-spider-1.5.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sharp-spider',
+    version='1.5.8',
+    description='Distributed crawler based on async tornado redis',
+    license='Free',
+    author='wangzongzhe',
+    author_email='17184032534@163.com',
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    python_requires='>=3.6',
+    install_requires=[
+    'lxml',
+    'bs4',
+    'tornado',
+    'bs4',
+    'redis>=2.10.5',
+    'uuid',
+    'selenium',
+    'requests',
+    'six'
+    ],
+)
