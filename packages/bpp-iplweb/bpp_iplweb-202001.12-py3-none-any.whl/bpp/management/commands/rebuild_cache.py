f3fb81aac4635dbab242cb186af98275a0cbb79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:48 +0000

    Add package bpp-iplweb/202001.12/bpp_iplweb-202001.12-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202001.12-py3-none-any.whl/bpp/management/commands/rebuild_cache.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+import multiprocessing
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from bpp.models import Wydawnictwo_Zwarte, Wydawnictwo_Ciagle, rebuild_ciagle, rebuild_zwarte
+from bpp.util import partition_count, no_threads
+
+
+class Command(BaseCommand):
+    help = 'Odbudowuje cache'
+
+    def handle(self, *args, **options):
+        if not settings.TESTING:
+            from django import db
+            db.connections.close_all()
+
+        pool_size = no_threads(0.5)
+        pool = multiprocessing.Pool(processes=pool_size)
+
+        pool.starmap(rebuild_ciagle, partition_count(Wydawnictwo_Ciagle.objects, pool_size))
+        pool.starmap(rebuild_zwarte, partition_count(Wydawnictwo_Zwarte.objects, pool_size))
