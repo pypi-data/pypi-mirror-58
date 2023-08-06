e4507ad58e2106394742245cf7d10966608320da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:26 +0000

    Add package delnone/0.4/delnone-0.4.tar.gz

---
+++ packages/delnone/delnone-0.4.tar.gz/delnone-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='delnone',
+      version='0.4',
+      description='Function which removes "None"s from data structures.',
+      url='https://github.com/andrewguy9/delnone',
+      author='andrew thomson',
+      author_email='athomsonguy@gmail.com',
+      license='MIT',
+      packages=['delnone'],
+      entry_points = {
+      },
+      zip_safe=False)
