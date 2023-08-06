11fcedb14288f24b85ae20414baa078524caa35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:19 +0000

    Add package gcgc/0.12.0.dev2/gcgc-0.12.0.dev2-py3-none-any.whl

---
+++ packages/gcgc/gcgc-0.12.0.dev2-py3-none-any.whl/gcgc/__init__.py
@@ -0,0 +1,9 @@
+# (c) Copyright 2018 Trent Hauck
+# All Rights Reserved
+"""Top-level GCGC module."""
+
+__version__ = "0.12.0-dev.2"
+
+from gcgc.tokenizer import SequenceTokenizer
+from gcgc.tokenizer import SequenceTokenizerSpec
+from gcgc.cli import cli
---
+++ packages/gcgc/gcgc-0.12.0.dev2-py3-none-any.whl/gcgc/tokenizer/__init__.py
@@ -0,0 +1,8 @@
+# (c) Copyright 2018 Trent Hauck
+# All Rights Reserved
+"""Contains tokenizer level packages."""
+
+from gcgc.tokenizer.base import SequenceTokenizer
+from gcgc.tokenizer.base import SequenceTokenizerSpec
+
+__all__ = ["SequenceTokenizer", "SequenceTokenizerSpec"]
