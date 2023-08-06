b9f9d804facf72536f67af333daa8fbb2eea650d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:39 +0000

    Add package yahoo-quote-download/0.2.0/yahoo_quote_download-0.2.0.tar.gz

---
+++ packages/yahoo-quote-download/yahoo_quote_download-0.2.0.tar.gz/yahoo_quote_download-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='yahoo_quote_download',
+      version='0.2.0',
+      description='Yahoo Quote Downloader',
+      author='c0redumb',
+      url='https://github.com/c0redumb/yahoo_quote_download',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['yahoo_quote_download'],
+      install_requires=[
+          'six',
+      ],
+      classifiers=["Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: BSD License",
+                   "Operating System :: OS Independent",
+                   ],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'yqdownload=yahoo_quote_download.downloader:main',
+          ],
+      },
+      )
