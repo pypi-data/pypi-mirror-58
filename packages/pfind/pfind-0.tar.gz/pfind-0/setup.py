f5867213c5ac8287fce50afccd472eb60575a4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package pfind/0/pfind-0.tar.gz

---
+++ packages/pfind/pfind-0.tar.gz/pfind-0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='pfind',
+    version='0',
+    description='pFind Team',
+    url='http://pfind.org',
+    author='pFind Team',
+    author_email='pfind@ict.ac.cn',
+    license='',
+    packages=setuptools.find_packages(exclude=('test',)),
+)
