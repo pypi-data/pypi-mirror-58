f58cf186d1fb73d69b779d26b48cd9af9a413234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:27 +0000

    Add package webviz-config/0.0.39/webviz-config-0.0.39.tar.gz

---
+++ packages/webviz-config/webviz-config-0.0.39.tar.gz/webviz-config-0.0.39/webviz_config/__init__.py
@@ -0,0 +1,15 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+from ._localhost_token import LocalhostToken
+from ._localhost_open_browser import LocalhostOpenBrowser
+from ._localhost_certificate import LocalhostCertificate
+from ._is_reload_process import is_reload_process
+from ._container_abc import WebvizContainerABC
+from ._theme_class import WebvizConfigTheme
+from ._shared_settings_subscriptions import SHARED_SETTINGS_SUBSCRIPTIONS
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
---
+++ packages/webviz-config/webviz-config-0.0.39.tar.gz/webviz-config-0.0.39/webviz_config/containers/_example_portable.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from .. import WebvizContainerABC
+from ..webviz_store import webvizstore
+from ..common_cache import CACHE
+
+
+class ExamplePortable(WebvizContainerABC):
+    def __init__(self, some_number: int):
+        self.some_number = some_number
+
+    def add_webvizstore(self):
+        return [(input_data_function, [{"some_number": self.some_number}])]
+
+    @property
+    def layout(self):
+        return str(input_data_function(self.some_number))
+
+
+@CACHE.memoize(timeout=CACHE.TIMEOUT)
+@webvizstore
+def input_data_function(some_number) -> pd.DataFrame:
+    print("This time I'm actually doing the calculation...")
+    return pd.DataFrame(
+        data={
+            "col1": [some_number, some_number * 2],
+            "col2": [some_number * 3, some_number * 4],
+        }
+    )
