1ee48b5f808bcdc4464fad5cca6afc183662d9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:14 +0000

    Add package femos/0.0.6/femos-0.0.6.tar.gz

---
+++ packages/femos/femos-0.0.6.tar.gz/femos-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="femos",
+    version="0.0.6",
+    author="estissy",
+    author_email="estissy@gmail.com",
+    description="My small library for neuroevolution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/estissy/femos",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requirements=">=3.0",
+)
