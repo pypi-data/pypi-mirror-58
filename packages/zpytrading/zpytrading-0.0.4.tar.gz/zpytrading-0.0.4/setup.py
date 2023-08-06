271f87ba41683458ef46859f2c705c049f81b2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:02 +0000

    Add package zpytrading/0.0.4/zpytrading-0.0.4.tar.gz

---
+++ packages/zpytrading/zpytrading-0.0.4.tar.gz/zpytrading-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'zpytrading',
+  packages = ['zpytrading'],
+  version = '0.0.4',
+  license='MIT',
+  description = 'Zinnion Streaming / Trading SDK',
+  long_description=long_description,
+  long_description_content_type="text/markdown",  
+  author = 'Mauro Delazeri',
+  author_email = 'mauro@zinnion.com',
+  url = 'https://github.com/Zinnion/zpytrading',
+  download_url = 'https://github.com/Zinnion/zpytrading/archive/v0.0.4.tar.gz',
+  keywords = ['zpytrading','zinnion','sdk','api'],
+  install_requires=[
+          'pyzmq',
+      ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],  
+)
