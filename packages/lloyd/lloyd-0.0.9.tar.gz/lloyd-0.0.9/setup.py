9b0881dd62b7cbf49ec5fdf150e5360cadc3767c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:26 +0000

    Add package lloyd/0.0.9/lloyd-0.0.9.tar.gz

---
+++ packages/lloyd/lloyd-0.0.9.tar.gz/lloyd-0.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup (
+  name='lloyd',
+  version='0.0.9',
+  packages=['lloyd'],
+  keywords = ['data-visualization', 'geometry', 'voronoi', 'lloyd-iteration'],
+  description='Lloyd iteration for distributing points in two-dimensional space.',
+  url='https://github.com/duhaime/lloyd',
+  author='Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT',
+  install_requires=[
+    'matplotlib>=2.0.0',
+    'numpy>=1.15.2',
+    'scikit-learn>=0.18.1',
+    'scipy>=1.1.0',
+    'umap-learn>=0.2.3',
+  ],
+)
