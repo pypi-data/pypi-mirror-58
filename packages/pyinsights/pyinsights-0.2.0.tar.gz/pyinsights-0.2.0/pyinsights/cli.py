3a425097ff6efef8e5d98d90781ae814c626c784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:15 +0000

    Add package pyinsights/0.2.0/pyinsights-0.2.0.tar.gz

---
+++ packages/pyinsights/pyinsights-0.2.0.tar.gz/pyinsights-0.2.0/pyinsights/cli.py
@@ -0,0 +1,50 @@
+import argparse
+from typing import Any, Dict
+import sys
+
+from pyinsights import __version__
+from pyinsights.pyinsights import run
+
+
+def parse_args() -> Dict[str, Any]:
+    """Parse arguments
+
+    Returns:
+        Dict[str, Any]
+    """
+
+    parser = argparse.ArgumentParser(
+        prog='pyinsights',
+        description='AWS CloudWatch Logs Insights is wrapped by Python',
+    )
+
+    parser.add_argument(
+        '-c',
+        '--config',
+        required=True,
+        default='pyinsights.yml',
+        help='PyInsights config file path',
+    )
+
+    parser.add_argument(
+        '-f',
+        '--format',
+        choices=['json', 'table'],
+        default='json',
+        help='Output format "json" or "table"'
+    )
+
+    parser.add_argument('-p', '--profile', help='AWS profile name')
+
+    parser.add_argument('-r', '--region', help='AWS region')
+
+    parser.add_argument(
+        '-v', '--version', action='version', version=__version__
+    )
+
+    return vars(parser.parse_args())
+
+
+def cli() -> bool:
+    args = parse_args()
+    sys.exit(run(args))
