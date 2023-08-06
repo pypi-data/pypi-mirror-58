a57ba192cd26efaf75d5c3229db2dbeaec0374be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:49 +0000

    Add package py-News-key/0.0.6/py_News_key-0.0.6-py3-none-any.whl

---
+++ packages/py-News-key/py_News_key-0.0.6-py3-none-any.whl/py_News_key/__init__.py
@@ -0,0 +1,13 @@
+#-*-coding:utf 8-*-
+
+version = "0.0.6"
+data = {
+    "LXP": [
+        "LXP",
+        "\u5218\u5148\u5e73"
+    ],
+    "WWK": [
+        "WWK",
+        "\u738b\u6587\u5eb71"
+    ]
+}
