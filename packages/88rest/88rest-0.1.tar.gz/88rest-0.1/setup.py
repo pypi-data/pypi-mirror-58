cf2ad2377b5a35fe6184523eb4c3031f51159dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:21 +0000

    Add package 88rest/0.1/88rest-0.1.tar.gz

---
+++ packages/88rest/88rest-0.1.tar.gz/88rest-0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="88rest",
+    version="0.1",
+    author="Rimba Prayoga",
+    author_email="rimba47prayoga@gmail.com",
+    description="88 Rest Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://pypi.python.org/pypi/88rest/",
+    packages=["rest88"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "Django >= 2.2.8, < 3",
+        "django-rest-framework",
+        "requests",
+        "orm88"
+    ]
+)
