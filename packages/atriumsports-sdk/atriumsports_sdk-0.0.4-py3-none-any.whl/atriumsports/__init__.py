7481de31723c8af1416594fb012023085217e2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package atriumsports-sdk/0.0.4/atriumsports_sdk-0.0.4-py3-none-any.whl

---
+++ packages/atriumsports-sdk/atriumsports_sdk-0.0.4-py3-none-any.whl/atriumsports/__init__.py
@@ -0,0 +1,32 @@
+"""
+    Hold AtriumSports class
+"""
+from atriumsports.datacore.datacore import DatacoreAPI
+
+
+class AtriumSports:
+    """
+    A class that handles getting the correct client objects
+    """
+
+
+    def __init__(self, options):
+        """ initialise the class """
+
+        self._options = {
+            'sport': options.get('sport', 'basketball'),
+            'credential_id': options.get('credential_id'),
+            'credential_secret': options.get('credential_secret'),
+            'organizations': options.get('organizations', []),
+            'org_group': options.get('org_group'),
+            'environment': options.get('environment', 'production'),
+            'headers': options.get('headers', {}),
+            'version': 1, # version
+        }
+
+    def client(self, client_type):
+        """ return a client object """
+
+        if client_type == 'datacore':
+            return DatacoreAPI(self._options)
+        return None
