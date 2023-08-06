7e7c18c4b565614841b2fbec36fa1b3cf00ca476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:17 +0000

    Add package maubot/0.1.0.dev29/maubot-0.1.0.dev29-py3-none-any.whl

---
+++ packages/maubot/maubot-0.1.0.dev29-py3-none-any.whl/maubot/management/frontend/build/precache-manifest.3402bf6b5086c5ed6e3b91be53b55cb3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a4befcd86b15ad5d2bd6093786e2d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a2e58bdf26237a7489c",
+    "url": "./static/css/main.d263a6d4.chunk.css"
+  },
+  {
+    "revision": "a93cef0c364f1ca94354",
+    "url": "./static/js/2.1a8d0502.chunk.js"
+  },
+  {
+    "revision": "9a2e58bdf26237a7489c",
+    "url": "./static/js/main.d3deb7ad.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "33906f184b9fdf0533c579c3caf02765",
+    "url": "./static/media/bot.33906f18.svg"
+  },
+  {
+    "revision": "7e77da69cc71663ae51ce4b08b52e0d3",
+    "url": "./static/media/chevron-left.7e77da69.svg"
+  },
+  {
+    "revision": "b6b6c4db699010301b02593710377f70",
+    "url": "./static/media/chevron-right.b6b6c4db.svg"
+  },
+  {
+    "revision": "9ecee2ce95b0de731b1498b48e71c737",
+    "url": "./static/media/firacode-bold.9ecee2ce.woff2"
+  },
+  {
+    "revision": "24310bd06e2537b92a92f33dabdc038b",
+    "url": "./static/media/firacode-regular.24310bd0.woff2"
+  },
+  {
+    "revision": "1851431378ea0e1c6a63c7261ee65d9e",
+    "url": "./static/media/plus.18514313.svg"
+  },
+  {
+    "revision": "77d77f36bed0a452984832f6b5f22e3f",
+    "url": "./static/media/raleway-bold.77d77f36.woff2"
+  },
+  {
+    "revision": "d724dad2e61905f488d048e51d45ae3e",
+    "url": "./static/media/raleway-light.d724dad2.woff2"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "./static/media/raleway-regular.43c849ea.woff2"
+  },
+  {
+    "revision": "ebe1c3a69ddb3b046edbb1f11f689d78",
+    "url": "./static/media/sort-down.ebe1c3a6.svg"
+  },
+  {
+    "revision": "4bff71677abd84948fb3335d9ecf4388",
+    "url": "./static/media/sort-up.4bff7167.svg"
+  },
+  {
+    "revision": "a57b766207fe6c113be29df03f171a6d",
+    "url": "./static/media/upload.a57b7662.svg"
+  }
+]);
