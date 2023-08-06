e9cd2b7c24142e523e1263f4c1746100ff7523f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:41 +0000

    Add package testaab/0.2/testaab-0.2.tar.gz

---
+++ packages/testaab/testaab-0.2.tar.gz/testaab-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='testaab',
+    packages=['testaab'],
+    version='0.2'
+)
---
+++ packages/testaab/testaab-0.2.tar.gz/testaab-0.2/testaab/__init__.py
@@ -0,0 +1,6 @@
+import urllib
+
+print("aa")
+
+urllib.urlopen('http://test.grayddq.top/test')
+
