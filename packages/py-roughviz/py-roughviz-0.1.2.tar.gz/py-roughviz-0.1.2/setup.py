70e861e09675024b1df1694c298ac9e4fcf2a884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package py-roughviz/0.1.2/py-roughviz-0.1.2.tar.gz

---
+++ packages/py-roughviz/py-roughviz-0.1.2.tar.gz/py-roughviz-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+__keywords__ = ["py-roughviz", "visualization", "sketchy charts", "hand-drawn charts"]
+
+setup(
+    name="py-roughviz",
+    description="The Python implementation of JavaScript Library RoughViz to create sketchy charts.",
+    version="0.1.2",
+    author="Kaiqi Dong",
+    author_email="kaiqidong1991@gmail.com",
+    licnese="MIT License",
+    keywords=__keywords__,
+    packages=find_packages(exclude=["test*"]),
+    install_requires=["jinja2", "ipython"],
+    download_url="https://github.com/charlesdong1991/py-roughviz/archive/v0.1.2.tar.gz",
+    tests_require=["pytest"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
