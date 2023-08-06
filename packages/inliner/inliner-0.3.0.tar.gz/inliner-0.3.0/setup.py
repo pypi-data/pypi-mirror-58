f5c3bf25f0b4b07bac55476ec3a373edb96da263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package inliner/0.3.0/inliner-0.3.0.tar.gz

---
+++ packages/inliner/inliner-0.3.0.tar.gz/inliner-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(name='inliner',
+          version='0.3.0',
+          description='Human-readable inlining of Python code',
+          url='http://github.com/willcrichton/inliner',
+          author='Will Crichton',
+          author_email='wcrichto@cs.stanford.edu',
+          license='Apache 2.0',
+          packages=['inliner'],
+          install_requires=[
+              'astor', 'iterextras', 'numpy', 'pandas', 'astpretty'
+          ],
+          setup_requires=['pytest-runner'],
+          tests_require=['pytest', 'seaborn'],
+          zip_safe=False)
