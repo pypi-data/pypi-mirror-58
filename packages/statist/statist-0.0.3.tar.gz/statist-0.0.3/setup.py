9d7dba0f56c2e07794dbc163a74e243ccd2c266a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package statist/0.0.3/statist-0.0.3.tar.gz

---
+++ packages/statist/statist-0.0.3.tar.gz/statist-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "statist",
+    packages = ["statist"],
+    entry_points = {
+        "console_scripts": ['statist = statist.statist:statist']
+        },
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version = "0.0.3",
+    description = "Get Todoist user stats in CLI",
+    author = "Yoginth",
+    author_email = "yoginth@protonmail.com",
+    url = "https://yoginth.com",
+    classifiers=(
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+    ),
+    project_urls={
+        'Source': 'https://gitlab.com/yo/statist'
+    },
+    install_requires=[
+        'colorama',
+        'todoist-python',
+    ],
+)
---
+++ packages/statist/statist-0.0.3.tar.gz/statist-0.0.3/statist/graph.py
@@ -0,0 +1,60 @@
+import colorama
+
+colorama.init()
+
+def karmaTrend(data):
+    max_value = max(count for _, count in data)
+    increment = max_value / 25
+    longest_label_length = max(len(label) for label, _ in data)
+    
+    for label, count in data:
+        bar_chunks, remainder = divmod(int(count * 7 / increment), 7)
+        bar = '█' * bar_chunks
+        if remainder > 0:
+            bar += chr(ord('█') + (8 - remainder))
+        bar = bar or  '▏'
+        print(f'{label.rjust(longest_label_length)} ▏ {count:#4d} {bar}')
+
+def daily(data, goal):
+    max_value = max(count for _, count in data)
+    increment = max_value / 25
+    longest_label_length = max(len(label) for label, _ in data)
+    
+    for label, count in data:
+        bar_chunks, remainder = divmod(int(count * 7 / increment), 7)
+        bar = '█' * bar_chunks
+        if remainder > 0:
+            bar += chr(ord('█') + (8 - remainder))
+        bar = bar or  ' ▏'
+        if count < goal:
+            print(label.rjust(longest_label_length), end="")
+            print(' ▏ ' + str(count).rjust(3) + ' ', end="")
+            print(colorama.Fore.RED + '✔️ ' + bar)
+            print(colorama.Style.RESET_ALL, end="")
+        else:
+            print(label.rjust(longest_label_length), end="")
+            print(' ▏ ' + str(count).rjust(3) + ' ', end="")
+            print(colorama.Fore.GREEN + '✔️ ' + bar)
+            print(colorama.Style.RESET_ALL, end="")
+
+def weekly(data, goal):
+    max_value = max(count for _, count in data)
+    increment = max_value / 25
+    longest_label_length = max(len(label) for label, _ in data)
+    
+    for label, count in data:
+        bar_chunks, remainder = divmod(int(count * 7 / increment), 7)
+        bar = '█' * bar_chunks
+        if remainder > 0:
+            bar += chr(ord('█') + (8 - remainder))
+        bar = bar or  '▏'
+        if count < goal:
+            print(label.rjust(longest_label_length), end="")
+            print(' ▏ ' + str(count).rjust(3) + ' ', end="")
+            print(colorama.Fore.RED + '✔️ ' + bar)
+            print(colorama.Style.RESET_ALL, end="")
+        else:
+            print(label.rjust(longest_label_length), end="")
+            print(' ▏ ' + str(count).rjust(3) + ' ', end="")
+            print(colorama.Fore.GREEN + '✔️ ' + bar)
+            print(colorama.Style.RESET_ALL, end="")
