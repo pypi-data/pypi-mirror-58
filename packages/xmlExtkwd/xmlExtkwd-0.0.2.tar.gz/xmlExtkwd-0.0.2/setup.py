ee0dd5a5faacaba279bd80befafa21dc5796fb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package xmlExtkwd/0.0.2/xmlExtkwd-0.0.2.tar.gz

---
+++ packages/xmlExtkwd/xmlExtkwd-0.0.2.tar.gz/xmlExtkwd-0.0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name="xmlExtkwd",
+    version="0.0.2",
+    author="Clare",
+    description="hw is handsome",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
