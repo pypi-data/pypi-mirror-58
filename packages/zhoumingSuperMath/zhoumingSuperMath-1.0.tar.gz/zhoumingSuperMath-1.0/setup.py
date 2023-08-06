d7d227330f59b959246831c096c59e4fc08a3018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package zhoumingSuperMath/1.0/zhoumingSuperMath-1.0.tar.gz

---
+++ packages/zhoumingSuperMath/zhoumingSuperMath-1.0.tar.gz/zhoumingSuperMath-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name="zhoumingSuperMath",
+    version="1.0",   #版本号
+    description="这是第一个对外发布的模块，用于测试！",
+    author="liupei",
+    author_email="liupei@unilumin.cn",
+    py_modules=["zhoumingSuperMath.demo1", "zhoumingSuperMath.demo2"]
+)
