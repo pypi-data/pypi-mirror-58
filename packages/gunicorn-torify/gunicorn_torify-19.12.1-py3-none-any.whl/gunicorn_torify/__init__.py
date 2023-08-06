6ee25001336eea209ec56cbbad359a8ccc9294cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:39 +0000

    Add package gunicorn-torify/19.12.1/gunicorn_torify-19.12.1-py3-none-any.whl

---
+++ packages/gunicorn-torify/gunicorn_torify-19.12.1-py3-none-any.whl/gunicorn_torify/__init__.py
@@ -0,0 +1,2 @@
+from gunicorn_torify.api import on_starting, on_exit
+from gunicorn_torify._version import __version__
---
+++ packages/gunicorn-torify/gunicorn_torify-19.12.1-py3-none-any.whl/gunicorn_torify/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides gunicorn_torify version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update gunicorn_torify` to change this file.
+
+from incremental import Version
+
+__version__ = Version("gunicorn_torify", 19, 12, 1)
+__all__ = ["__version__"]
