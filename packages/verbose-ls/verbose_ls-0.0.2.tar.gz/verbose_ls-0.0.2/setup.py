c2bf816b5c4e9beae1017a7b6e2079aaf37e326f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package verbose-ls/0.0.2/verbose_ls-0.0.2.tar.gz

---
+++ packages/verbose-ls/verbose_ls-0.0.2.tar.gz/verbose_ls-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+VERSION = "0.0.2"
+
+setup(
+    name="verbose_ls",
+    version=VERSION,
+    description="Format 'ls' output into verbose, human readable output",
+    url="https://github.com/NelsonScott/verbose_ls",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["verbose_ls = verbose_ls:main"]},
+    include_package_data=True,
+    python_requires=">=3.7",
+)
