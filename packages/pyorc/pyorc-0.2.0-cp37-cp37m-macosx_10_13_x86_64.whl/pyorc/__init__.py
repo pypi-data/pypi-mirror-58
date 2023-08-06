ee5a49e08ceec505102f291090ce8db5cc250bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:43 +0000

    Add package pyorc/0.2.0/pyorc-0.2.0-cp37-cp37m-macosx_10_13_x86_64.whl

---
+++ packages/pyorc/pyorc-0.2.0-cp37-cp37m-macosx_10_13_x86_64.whl/pyorc/__init__.py
@@ -0,0 +1,9 @@
+from pyorc._pyorc import typedescription
+
+from .enums import *
+from .errors import *
+from .reader import Column, Reader, Stripe
+from .writer import Writer
+
+
+__version__ = "0.2.0"
