f59c4b4e7f3372f5b4203cdb5090b2d1b41200e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:34 +0000

    Add package pyGenealogicalTools/0.4.2/pyGenealogicalTools-0.4.2-py3-none-any.whl

---
+++ packages/pyGenealogicalTools/pyGenealogicalTools-0.4.2-py3-none-any.whl/analyzefamily/__init__.py
@@ -0,0 +1,7 @@
+from tests.FIXTURES import FATHER_PROFILE_SANDBOX
+__all__ = ["ancerstors_climb", "matcher_geni_profile"]
+
+FATHER = "father"
+MOTHER = "mother"
+CHILD = "child"
+PARTNER = "partner"
