11da7007cca7b1ceceaa52535611c25b9ecd49d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:40 +0000

    Add package automap/0.0.1/automap-0.0.1.tar.gz

---
+++ packages/automap/automap-0.0.1.tar.gz/automap-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import Extension, setup
+
+
+with open("README.md") as file:
+    LONG_DESCRIPTION = file.read()
+
+
+setup(
+    name="automap",
+    version="0.0.1",
+    description="High-performance autoincremented integer-valued mappings.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.5.0",
+    url="https://github.com/brandtbucher/automap",
+    author="Brandt Bucher",
+    author_email="brandtbucher@gmail.com",
+    license="MIT",
+    package_data={"": ["*.h"]},
+    ext_modules=[Extension("automap", ["automap.c"])],
+)
