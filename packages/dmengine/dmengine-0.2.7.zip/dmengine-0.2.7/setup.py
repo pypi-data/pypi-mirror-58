b0fd3d6b1537a8a367b164fc7f29fc35ae3a54ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:29 +0000

    Add package dmengine/0.2.7/dmengine-0.2.7.zip

---
+++ packages/dmengine/dmengine-0.2.7.zip/dmengine-0.2.7/setup.py
@@ -0,0 +1,45 @@
+# setup.py
+
+import io
+from setuptools import setup, find_packages
+
+setup(
+    name='dmengine',
+    version='0.2.7',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Distributed Morphology calculator',
+    keywords='DM Halle Marantz impoverishment fission linguistics',
+    license='MIT',
+    url='https://github.com/xflr6/dmengine',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['dmengine=dmengine.__main__:main']},
+    package_data={'dmengine': ['reporting/template.tex']},
+    zip_safe=False,
+    platforms='any',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    install_requires=[
+        'oset',
+        'PyYAML',
+    ],
+    extras_require={
+        'dev': ['flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['pytest>=3.4,!=3.10.0', 'pytest-cov'],
+    },
+    long_description=io.open('README.rst', encoding='utf-8').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
