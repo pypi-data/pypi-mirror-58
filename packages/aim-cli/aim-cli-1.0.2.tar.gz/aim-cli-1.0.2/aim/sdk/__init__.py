e57113d2b7ea73206126a1d9d55162bea420711b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:14 +0000

    Add package aim-cli/1.0.2/aim-cli-1.0.2.tar.gz

---
+++ packages/aim-cli/aim-cli-1.0.2.tar.gz/aim-cli-1.0.2/aim/sdk/__init__.py
@@ -0,0 +1,15 @@
+from aim.sdk.track import *
+
+
+# Serializable classes references
+loss = 'Loss'
+accuracy = 'Accuracy'
+image = 'Image'
+misclassification = 'Misclassification'
+checkpoint = 'Checkpoint'
+metric = 'Metric'
+correlation = 'Correlation'
+label_correlation = 'LabelCorrelation'
+weights = 'WeightsDistribution'
+gradients = 'GradientsDistribution'
+hyperparams = 'HyperParameters'
