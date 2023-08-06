88289477784a2d8e1d9d33824dbea89ca3181506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:53 +0000

    Add package mythe-micros/0.0.0/mythe_micros-0.0.0.tar.gz

---
+++ packages/mythe-micros/mythe_micros-0.0.0.tar.gz/mythe_micros-0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= "mythe_micros",
+    version = "0.0.0",
+    keywords=("pip","micro","service"),
+    description=("Easy Micro Service Library"),
+    long_description=("To be fill."),
+    license="MIT Licence",
+
+    url="",
+    author="Mythezone",
+    author_email="125616515@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+
+)
