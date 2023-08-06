eb68045a62c056a8298773f54ce074811013b213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:16 +0000

    Add package stalker/0.2.24/stalker-0.2.24-py2.py3-none-any.whl

---
+++ packages/stalker/stalker-0.2.24-py2.py3-none-any.whl/stalker/testing.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+# Stalker a Production Asset Management System
+# Copyright (C) 2009-2018 Erkan Ozgur Yilmaz
+#
+# This file is part of Stalker.
+#
+# Stalker is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# Stalker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with Stalker.  If not, see <http://www.gnu.org/licenses/>
+"""Helper classes for testing
+"""
+
+import unittest
+import logging
+from stalker import log
+logger = logging.getLogger(__name__)
+logger.setLevel(log.logging_level)
+
+
+def create_db(database_name):
+    """creates new PostgreSQL database
+    """
+    logger.debug('creating database: %s' % database_name)
+    import subprocess
+
+    # fallback to check_call for Python 2.6
+    try:
+        process_caller = subprocess.check_output
+    except AttributeError:
+        process_caller = subprocess.check_call
+
+    process_caller(
+        'psql -c "CREATE DATABASE %s;" -U postgres' % database_name,
+        shell=True
+    )
+
+
+def create_random_db():
+    """creates a random named PostgreSQL database
+
+    :returns (str, str): db_url, db_name
+    """
+    # create a new database for this test only
+    import uuid
+    database_name = 'stalker_test_%s' % uuid.uuid4().hex[:4]
+    database_url = \
+        'postgresql://stalker_admin:stalker@localhost/%s' % database_name
+    create_db(database_name)
+    return database_url, database_name
+
+
+def drop_db(database_name):
+    """drops a PostgreSQL database
+    """
+    import subprocess
+    import time
+
+    # fallback to check_call for Python 2.6
+    try:
+        process_caller = subprocess.check_output
+    except AttributeError:
+        process_caller = subprocess.check_call
+
+    while True:
+        output = ''
+        try:
+            output = process_caller(
+                'psql -c "DROP DATABASE %s;" -U postgres' % database_name,
+                # stderr=subprocess.PIPE,
+                shell=True
+            )
+        except subprocess.CalledProcessError as e:
+            # sleep for 1 seconds and run again
+            logger.debug(output)
+            logger.debug(str(e))
+            time.sleep(1)
+        else:
+            return
+
+
+class UnitTestDBBase(unittest.TestCase):
+    """the base for Stalker Pyramid Views unit tests
+    """
+
+    config = {}
+    database_url = None
+    database_name = None
+
+    @classmethod
+    def setUpClass(cls):
+        """setup once
+        """
+        # create a new database for this test only
+        cls.database_url, cls.database_name = create_random_db()
+
+        # update the config
+        cls.config['sqlalchemy.url'] = cls.database_url
+        from sqlalchemy.pool import NullPool
+        cls.config['sqlalchemy.poolclass'] = NullPool
+
+    @classmethod
+    def tearDownClass(cls):
+        """tear down once
+        """
+        from stalker.db.session import DBSession
+        DBSession.close_all()
+        drop_db(cls.database_name)
+
+    def setUp(self):
+        """setup test
+        """
+        import os
+        from stalker.config import Config
+        try:
+            os.environ.pop(Config.env_key)
+        except KeyError:
+            # already removed
+            pass
+
+        # regenerate the defaults
+        import stalker
+        stalker.defaults = Config()
+
+        import datetime
+        stalker.defaults.timing_resolution = datetime.timedelta(hours=1)
+
+        # init database
+        from stalker import db
+        # remove anything beforehand
+        db.setup(self.config)
+        db.init()
+
+    def tearDown(self):
+        """clean up the test
+        """
+        import datetime
+        from stalker import defaults
+        from stalker.db.declarative import Base
+        from stalker.db.session import DBSession
+
+        # clean up test database
+        DBSession.rollback()
+        connection = DBSession.connection()
+        engine = connection.engine
+        connection.close()
+
+        Base.metadata.drop_all(engine, checkfirst=True)
+        DBSession.remove()
+
+        defaults.timing_resolution = datetime.timedelta(hours=1)
+
+    @property
+    def admin(self):
+        """returns the admin user
+        """
+        from stalker import defaults, User
+        from stalker.db.session import DBSession
+        with DBSession.no_autoflush:
+            return User.query\
+                .filter(User.login == defaults.admin_login)\
+                .first()
+
+
+class PlatformPatcher(object):
+    """patches given callable
+    """
+
+    def __init__(self):
+        self.callable = None
+        self.original = None
+
+    def patch(self, desired_result):
+        """
+        """
+        import platform
+        self.original = platform.system
+
+        def f():
+            return desired_result
+
+        platform.system = f
+
+    def restore(self):
+        """restores the given callable_
+        """
+        if self.original:
+            import platform
+            platform.system = self.original
---
+++ packages/stalker/stalker-0.2.24-py2.py3-none-any.whl/tests/models/test_schedulers.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# Stalker a Production Asset Management System
+# Copyright (C) 2009-2018 Erkan Ozgur Yilmaz
+#
+# This file is part of Stalker.
+#
+# Stalker is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# Stalker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with Stalker.  If not, see <http://www.gnu.org/licenses/>
+
+import unittest
+import pytest
+from stalker import SchedulerBase
+
+
+class SchedulerBaseTester(unittest.TestCase):
+    """tests the stalker.models.scheduler.SchedulerBase
+    """
+
+    def setUp(self):
+        """set up the test
+        """
+        super(SchedulerBaseTester, self).setUp()
+
+        from stalker import Studio
+        self.test_studio = Studio(name='Test Studio')
+        self.kwargs = {
+            'studio': self.test_studio
+        }
+        self.test_scheduler_base = SchedulerBase(**self.kwargs)
+
+    def test_studio_argument_is_skipped(self):
+        """testing if the studio attribute will be None if the studio argument
+        is skipped
+        """
+        self.kwargs.pop('studio')
+        new_scheduler_base = SchedulerBase(**self.kwargs)
+        assert new_scheduler_base.studio is None
+
+    def test_studio_argument_is_None(self):
+        """testing if the studio attribute will be None if the studio argument
+        is None
+        """
+        self.kwargs['studio'] = None
+        new_scheduler_base = SchedulerBase(**self.kwargs)
+        assert new_scheduler_base.studio is None
+
+    def test_studio_attribute_is_None(self):
+        """testing if the studio argument can be set to None
+        """
+        self.test_scheduler_base.studio = None
+        assert self.test_scheduler_base.studio is None
+
+    def test_studio_argument_is_not_a_Studio_instance(self):
+        """testing if a TypeError will be raised when the studio argument is
+        not stalker.models.studio.Studio instance
+        """
+        self.kwargs['studio'] = 'not a studio instance'
+        with pytest.raises(TypeError) as cm:
+            SchedulerBase(**self.kwargs)
+
+        assert str(cm.value) == \
+            'SchedulerBase.studio should be an instance of ' \
+            'stalker.models.studio.Studio, not str'
+
+    def test_studio_attribute_is_not_a_Studio_instance(self):
+        """testing if a TypeError will be raised when the studio attribute is
+        set to a value which is not a stalker.models.studio.Studio instance
+        """
+        with pytest.raises(TypeError) as cm:
+            self.test_scheduler_base.studio = 'not a studio instance'
+
+        assert str(cm.value) == \
+            'SchedulerBase.studio should be an instance of ' \
+            'stalker.models.studio.Studio, not str'
+
+    def test_studio_argument_is_working_properly(self):
+        """testing if the studio argument value is correctly passed to the
+        studio attribute
+        """
+        assert self.test_scheduler_base.studio == self.kwargs['studio']
+
+    def test_studio_attribute_is_working_properly(self):
+        """testing if the studio attribute is working properly
+        """
+        from stalker import Studio
+        new_studio = Studio(name='Test Studio 2')
+        self.test_scheduler_base.studio = new_studio
+        assert self.test_scheduler_base.studio == new_studio
+
+    def test_schedule_method_will_raise_not_implemented_error(self):
+        """testing if the schedule() method will raise a NotImplementedError
+        """
+        base = SchedulerBase()
+        with pytest.raises(NotImplementedError) as cm:
+            base.schedule()
+
+        assert str(cm.value) == ''
