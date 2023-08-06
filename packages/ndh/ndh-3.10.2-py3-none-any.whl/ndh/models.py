b84322f5e6c9d89959043acdf6f62a04eec31186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:15 +0000

    Add package ndh/3.10.2/ndh-3.10.2-py3-none-any.whl

---
+++ packages/ndh/ndh-3.10.2-py3-none-any.whl/ndh/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from autoslug import AutoSlugField  # type: ignore
+
+from .querysets import NameOrderedQuerySet
+from .utils import full_url
+
+
+class Links(object):
+    absolute_url_detail = True
+
+    def get_absolute_url(self):
+        app, model = self._meta.app_label, self._meta.model_name
+        if self.absolute_url_detail:
+            return reverse(f'{app}:{model}', kwargs={'slug': self.slug} if hasattr(self, 'slug') else {'pk': self.pk})
+        else:
+            return reverse(f'{app}:{model}s')
+
+    def get_full_url(self) -> str:
+        return full_url(self.get_absolute_url())
+
+    def get_admin_url(self):
+        return reverse(f'admin:{self._meta.app_label}_{self._meta.model_name}_change', args=[self.id])
+
+    def get_link(self) -> str:
+        return mark_safe(f'<a href="{self.get_absolute_url()}">{self}</a>')
+
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class NamedModel(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    objects = NameOrderedQuerySet.as_manager()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.name
