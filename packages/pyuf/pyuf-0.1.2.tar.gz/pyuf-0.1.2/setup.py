9bce52076274a0cc6bb319dba1ba55984780304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:39 +0000

    Add package pyuf/0.1.2/pyuf-0.1.2.tar.gz

---
+++ packages/pyuf/pyuf-0.1.2.tar.gz/pyuf-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.rst", "r") as longdesc:
+    long_description = longdesc.read()
+
+
+setup(
+    name="pyuf",
+    description="Union-Find data structure implementation in Python",
+    long_description=long_description,
+    author="Mariano Anaya",
+    version="0.1.2",
+    license="MIT",
+    author_email="marianoanaya@gmail.com",
+    packages=find_packages(where="src/"),
+    package_dir={"": "src"},
+    extras_require={"dev": ["pytest", "black", "flake8", "mypy"]},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
