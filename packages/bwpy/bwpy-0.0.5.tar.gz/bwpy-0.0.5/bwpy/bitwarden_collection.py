f37dcb2b079ad0db41cdb28d013ae6ab9fa45994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:56 +0000

    Add package bwpy/0.0.5/bwpy-0.0.5.tar.gz

---
+++ packages/bwpy/bwpy-0.0.5.tar.gz/bwpy-0.0.5/bwpy/bitwarden_collection.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import json
+from dataclasses import dataclass
+from sh import bw
+from .bitwarden_org import BitwardenOrg
+
+
+@dataclass
+class BitwardenCollection(BitwardenOrg):
+    collection_name: str
+
+    def collection_id(self):
+        for collection in self.org_collections():
+            if collection["externalId"] == self.collection_name:
+                return collection["id"]
+        return None
+
+    def collection_items(self):
+        return json.loads(str(bw.list.items("--collectionid", self.collection_id())))
+
+    def __str__(self):
+        lines = ""
+        for item in self.collection_items():
+            if not item["login"]["username"]:
+                raise KeyError(f"missing 'username' key for item: {item['name']}")
+
+            if not item["login"]["password"]:
+                raise KeyError(f"missing 'password' key for item: {item['name']}")
+
+            lines += f"{item['login']['username']} = \"{item['login']['password']}\"\n"
+        return lines.rstrip()
