dd0305ca91c86e822b3bdf82176fab0005ac072b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package distrib-pkg/0.1/distrib_pkg-0.1.tar.gz

---
+++ packages/distrib-pkg/distrib_pkg-0.1.tar.gz/distrib_pkg-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distrib_pkg',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distrib_pkg'],
+      author = 'Nati Aluk',
+      zip_safe=False)
