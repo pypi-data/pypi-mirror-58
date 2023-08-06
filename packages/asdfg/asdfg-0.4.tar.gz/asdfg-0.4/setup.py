e00ae9d1680f8e14cb668868603bfc3475a21d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package asdfg/0.4/asdfg-0.4.tar.gz

---
+++ packages/asdfg/asdfg-0.4.tar.gz/asdfg-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='asdfg',
+    version='0.4',
+    packages=find_packages(exclude=['documentation.txt']),
+    # package_dir={'':''},
+    url="http://www.example.com",
+    license='',
+    author='veera',
+    author_email='veerabhadrarao.uppu@nexiilabs.com',
+    description='asdfg_description',
+    download_url="https://github.com/uvbr/package_creation/archive/0.4.tar.gz"
+)
