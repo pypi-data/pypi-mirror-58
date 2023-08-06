d9aec3c4cb806c84211aee4025f857224c819004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package simple-proxy-pool/0.0.9/simple-proxy-pool-0.0.9.tar.gz

---
+++ packages/simple-proxy-pool/simple-proxy-pool-0.0.9.tar.gz/simple-proxy-pool-0.0.9/simple_pp/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8
+
+''' simple proxy pool '''
+from .simple_pp import simple_pp  # , PROXYPOOL
+from .limited_as_completed import limited_as_completed
+# from .jhao.ProxyGetter import ProxyGetter
+
+# date__ = '2020.1.1'
+
+__version__ = '0.0.9'
+__date__ = '2020.1.3'
+VERSION = tuple(__version__.split('.'))
