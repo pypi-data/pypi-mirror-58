f51a633677f36326ae44a80dd3e0254b8988eddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:05 +0000

    Add package telegram-task-bot/0.0.8/telegram-task-bot-0.0.8.tar.gz

---
+++ packages/telegram-task-bot/telegram-task-bot-0.0.8.tar.gz/telegram-task-bot-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='telegram-task-bot',
+    version='0.0.8',
+    license='BSD-3',
+    description='rpi-radio-alarm library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='bb4L',
+    author_email='39266013+bb4L@users.noreply.github.com',
+    url='https://github.com/bb4L/telegram-task-bot-pip',
+    packages=['telegramtaskbot'],
+    keywords=['Telegram', 'Bot'],
+    install_requires=[
+        'python-telegram-bot==12.2.0',
+        'python-dotenv==0.10.3',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True,
+)
+# python setup.py sdist && twine upload dist/*
