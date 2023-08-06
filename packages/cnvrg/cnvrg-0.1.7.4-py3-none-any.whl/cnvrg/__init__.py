061f71f877521494f10aa6faba25579c470fe75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:10 +0000

    Add package cnvrg/0.1.7.4/cnvrg-0.1.7.4-py3-none-any.whl

---
+++ packages/cnvrg/cnvrg-0.1.7.4-py3-none-any.whl/cnvrg/__init__.py
@@ -0,0 +1,11 @@
+from cnvrg.modules.library import Library
+from cnvrg.modules.library import Library as AILibrary
+from cnvrg.modules.experiment import Experiment
+from cnvrg.modules.endpoint import Endpoint
+from cnvrg.modules.dataset import Dataset
+from cnvrg.modules.cnvrg_me import Cnvrg
+from cnvrg.modules.data_connector import *
+from cnvrg.version import VERSION
+
+
+__version__ = VERSION
