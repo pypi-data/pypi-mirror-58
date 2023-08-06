ac86386f135f2d4485f360d70c3011f35345deec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:09 +0000

    Add package bottle-tools/2019.12.22/bottle_tools-2019.12.22.tar.gz

---
+++ packages/bottle-tools/bottle_tools-2019.12.22.tar.gz/bottle_tools-2019.12.22/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+__version__ = "2019.12.22"
+
+with open("README.md", "r") as fl:
+    long_desc = fl.read()
+
+
+setup(
+    name="bottle_tools",
+    author="Arjoonn Sharma",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    version=__version__,
+    packages=["bottle_tools"],
+    install_requires=["bottle"],
+    python_requires=">=3.6",
+    project_urls={"Source": "https://github.com/thesage21/bottle_tools"},
+    zip_safe=False,
+)
