c848edc7fe32d4e130703b52a5d38e78219fb86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:39 +0000

    Add package django-background-tasks/1.2.5/django-background-tasks-1.2.5.tar.gz

---
+++ packages/django-background-tasks/django-background-tasks-1.2.5.tar.gz/django-background-tasks-1.2.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import codecs
+
+version = __import__('background_task').__version__
+
+classifiers = [c for c in open('classifiers').read().splitlines() if '#' not in c]
+
+setup(
+    name='django-background-tasks',
+    version=version,
+    description='Database backed asynchronous task queue',
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    author='arteria GmbH, John Montgomery',
+    author_email='admin@arteria.ch',
+    url='http://github.com/arteria/django-background-tasks',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    install_requires=open('requirements.txt').read().splitlines(),
+    zip_safe=True,
+    classifiers=classifiers,
+)
