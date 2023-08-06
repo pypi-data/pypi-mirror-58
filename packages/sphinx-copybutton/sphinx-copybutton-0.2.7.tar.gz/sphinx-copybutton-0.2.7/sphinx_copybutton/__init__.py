eddb73a3ce8d3a74e3fb18342a0cba455fca7dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:01 +0000

    Add package sphinx-copybutton/0.2.7/sphinx-copybutton-0.2.7.tar.gz

---
+++ packages/sphinx-copybutton/sphinx-copybutton-0.2.7.tar.gz/sphinx-copybutton-0.2.7/sphinx_copybutton/__init__.py
@@ -0,0 +1,21 @@
+"""A small sphinx extension to add "copy" buttons to code blocks."""
+import os
+
+__version__ = "0.2.7"
+
+def scb_static_path(app):
+    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))
+    app.config.html_static_path.append(static_path)
+
+def setup(app):
+    print('Adding copy buttons to code blocks...')
+    # Add our static path
+    app.connect('builder-inited', scb_static_path)
+
+    # Add relevant code to headers
+    app.add_stylesheet('copybutton.css')
+    app.add_javascript('clipboard.min.js')
+    app.add_javascript("copybutton.js")
+    return {"version": __version__,
+            "parallel_read_safe": True,
+            "parallel_write_safe": True}
