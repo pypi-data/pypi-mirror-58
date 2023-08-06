5f2fda173d4a20a7af33ad2b2fe831eef7b2bbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:09 +0000

    Add package pyhelpers/1.0.20/pyhelpers-1.0.20.tar.gz

---
+++ packages/pyhelpers/pyhelpers-1.0.20.tar.gz/pyhelpers-1.0.20/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+
+    name='pyhelpers',
+    version='1.0.20',
+
+    author='Qian Fu',
+    author_email='qian.fu@outlook.com',
+
+    description="A toolkit of helper functions to facilitate data manipulation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/mikeqfu/pyhelpers',
+
+    install_requires=[
+        # 'gdal',
+        # 'feather-format',
+        'fuzzywuzzy',
+        # 'matplotlib',
+        # 'nltk',
+        'numpy',
+        'openpyxl',
+        # 'pandas',
+        # 'pdfkit',
+        'python-rapidjson',
+        # 'pyproj',
+        'requests',
+        'tqdm',
+        'xlrd',
+        'xlwt',
+        'XlsxWriter'
+    ],
+
+    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux'
+    ],
+)
