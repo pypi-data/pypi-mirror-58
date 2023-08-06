4ddeab6632f8ac81186373bfb15ef2fd43256743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:11 +0000

    Add package cspy/0.0.9/cspy-0.0.9.tar.gz

---
+++ packages/cspy/cspy-0.0.9.tar.gz/cspy-0.0.9/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="cspy",
+    version="0.0.9",
+    description=
+    "A collection of algorithms for the (Resource) Constrained Shortest Path Problem",
+    license="MIT",
+    author="David Torres",
+    author_email="d.torressanchez@lancs.ac.uk",
+    keywords=[
+        "shortest path", "resource constrained shortest path",
+        "bidirectional algorithm", "tabu search"
+    ],
+    long_description_content_type="text/markdown",
+    long_description=open("README.rst", "r").read(),
+    url="https://github.com/torressa/cspy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
