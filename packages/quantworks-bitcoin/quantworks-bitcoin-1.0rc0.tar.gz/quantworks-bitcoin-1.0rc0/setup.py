201fa1d8aed6898c96696f75ac629fc7e3d2907b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:45 +0000

    Add package quantworks-bitcoin/1.0rc0/quantworks-bitcoin-1.0rc0.tar.gz

---
+++ packages/quantworks-bitcoin/quantworks-bitcoin-1.0rc0.tar.gz/quantworks-bitcoin-1.0rc0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quantworks',
+ 'quantworks.ext.bitcoincharts',
+ 'quantworks.ext.bitstamp',
+ 'quantworks.ext.testcases']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['quantworks==0.21rc2', 'requests>=2.22.0,<3.0.0', 'six>=1.13.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'quantworks-bitcoin',
+    'version': '1.0rc0',
+    'description': 'Bitcoin integrations for quantworks',
+    'long_description': None,
+    'author': 'ttymck',
+    'author_email': 'tyler@tylerkontra.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
