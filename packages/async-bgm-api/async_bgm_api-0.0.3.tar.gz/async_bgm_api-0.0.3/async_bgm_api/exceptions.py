4aa73b1aa890661a585c1b6931798d6f00f81736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:45 +0000

    Add package async-bgm-api/0.0.3/async_bgm_api-0.0.3.tar.gz

---
+++ packages/async-bgm-api/async_bgm_api-0.0.3.tar.gz/async_bgm_api-0.0.3/async_bgm_api/exceptions.py
@@ -0,0 +1,25 @@
+import httpx
+
+
+class ServerConnectionError(Exception):
+    def __init__(
+        self,
+        request: httpx.Request = None,
+        response: httpx.Response = None,
+        raw_exception: Exception = None,
+    ):
+        self.request = request
+        self.response = response
+        self.raw = raw_exception
+
+
+class RecordNotFound(Exception):
+    def __init__(
+        self,
+        request: httpx.Request = None,
+        response: httpx.Response = None,
+        raw_exception: Exception = None,
+    ):
+        self.request = request
+        self.response = response
+        self.raw = raw_exception
