0cb4a077961c5276e7b1ac5cb30d0fbed36cd158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:57 +0000

    Add package tap-pivotal-tracker/0.1.1/tap_pivotal_tracker-0.1.1.tar.gz

---
+++ packages/tap-pivotal-tracker/tap_pivotal_tracker-0.1.1.tar.gz/tap_pivotal_tracker-0.1.1/tap_pivotal_tracker/__init__.py
@@ -0,0 +1,96 @@
+import json
+import os
+
+import rollbar
+import singer
+
+from .client import (AccountsStream, LabelsStream, ProjectMembershipsStream,
+                     ProjectsStream, StoriesStream)
+
+LOGGER = singer.get_logger()
+
+try:
+    ROLLBAR_ACCESS_TOKEN = os.environ["ROLLBAR_ACCESS_TOKEN"]
+    ROLLBAR_ENVIRONMENT = os.environ["ROLLBAR_ENVIRONMENT"]
+except KeyError:
+    LOGGER.info("No Rollbar environment variables found. Rollbar logging disabled..")
+    log_to_rollbar = False
+else:
+    rollbar.init(ROLLBAR_ACCESS_TOKEN, ROLLBAR_ENVIRONMENT)
+    log_to_rollbar = True
+
+AVAILABLE_STREAMS = {
+    AccountsStream,
+    ProjectsStream,
+    ProjectMembershipsStream,
+    LabelsStream,
+    StoriesStream
+}
+
+REQUIRED_CONFIG_KEYS = [
+    "api_token"
+]
+
+
+def discover(args, select_all=False):
+    LOGGER.info('Starting discovery..')
+
+    catalog = {"streams": []}
+    for available_stream in AVAILABLE_STREAMS:
+        stream = available_stream.from_args(args)
+        catalog_entry = {
+            "stream": stream.tap_stream_id,
+            "tap_stream_id": stream.tap_stream_id,
+            "schema": stream.schema,
+            "metadata": singer.metadata.get_standard_metadata(schema=stream.schema,
+                                                              key_properties=stream.key_properties,
+                                                              valid_replication_keys=stream.bookmark_properties,
+                                                              replication_method=stream.replication_method)
+        }
+
+        if select_all is True:
+            catalog_entry["metadata"][0]["metadata"]["selected"] = True
+        catalog["streams"].append(catalog_entry)
+
+    print(json.dumps(catalog, indent=2))
+    LOGGER.info("Finished discovery..")
+
+
+def sync(args):
+    LOGGER.info('Starting sync..')
+
+    if not args.state:
+        args.state = {"bookmarks": {}}
+
+    selected_streams = {catalog_entry.stream for catalog_entry in args.catalog.get_selected_streams(args.state)}
+
+    for available_stream in AVAILABLE_STREAMS:
+        if available_stream.tap_stream_id in selected_streams:
+            stream = available_stream.from_args(args)
+            LOGGER.info(f"Starting sync for stream {stream.tap_stream_id}..")
+            singer.bookmarks.set_currently_syncing(state=args.state, tap_stream_id=stream.tap_stream_id)
+            stream.write_state_message()
+            stream.write_schema_message()
+            stream.sync()
+            singer.bookmarks.set_currently_syncing(state=args.state, tap_stream_id=None)
+            stream.write_state_message()
+
+
+def main():
+    args = singer.parse_args(required_config_keys=REQUIRED_CONFIG_KEYS)
+    if args.discover:
+        try:
+            discover(args, select_all=True)
+        except:
+            LOGGER.exception('Caught exception during Discovery..')
+            rollbar.report_exc_info()
+    else:
+        try:
+            sync(args)
+        except:
+            LOGGER.exception('Caught exception during Sync..')
+            rollbar.report_exc_info()
+
+
+if __name__ == "__main__":
+    main()
