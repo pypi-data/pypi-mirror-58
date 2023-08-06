12d0c57b4d2a756c474add5cd7503380ef36d2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:42 +0000

    Add package simple-proxy-pool/0.0.4/simple-proxy-pool-0.0.4.tar.gz

---
+++ packages/simple-proxy-pool/simple-proxy-pool-0.0.4.tar.gz/simple-proxy-pool-0.0.4/simple_pp/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8
+
+''' simple proxy pool '''
+from .simple_pp import simple_pp  # , PROXYPOOL
+from .limited_as_completed import limited_as_completed
+
+__version__ = '0.0.4'
+__date__ = '2020.1.1'
+VERSION = __version__
