2bbb20f6c3d4d4568d57313f31a6d8fc16abce2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:18 +0000

    Add package icsdscraper/0.0.1/icsdscraper-0.0.1.tar.gz

---
+++ packages/icsdscraper/icsdscraper-0.0.1.tar.gz/icsdscraper-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="icsdscraper",
+    version="0.0.1",
+    author="Achilleas Papakonstantinou",
+    author_email="achipap@hotmail.com",
+    description="ICSD website scraper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CheatModeON/icsd-scraper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
