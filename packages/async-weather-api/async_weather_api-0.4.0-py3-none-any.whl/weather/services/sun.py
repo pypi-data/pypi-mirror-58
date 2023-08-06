86dde7527b57f71101adea5aa44232a8a324d9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:06 +0000

    Add package async-weather-api/0.4.0/async_weather_api-0.4.0-py3-none-any.whl

---
+++ packages/async-weather-api/async_weather_api-0.4.0-py3-none-any.whl/weather/services/sun.py
@@ -0,0 +1,27 @@
+import datetime
+import time
+from typing import Dict
+import aiohttp
+
+
+def _utc_to_local(date: str) -> datetime.datetime:
+    """Converts utl to local datetime."""
+    now_timestamp = time.time()
+    return datetime.datetime.strptime(date, "%I:%M:%S %p") + (
+        datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
+    )
+
+
+async def today(latitude: float, longitude: float) -> Dict[str, str]:
+    """Returns sunrise/sunset for today."""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}") as response:
+            response.raise_for_status()
+            data = await response.json()
+
+    sun_data = data.get("results", {})
+    for key, value in tuple(sun_data.items()):  # type: str, str
+        if "AM" not in value and "PM" not in value:
+            continue
+        sun_data[key] = datetime.datetime.strftime(_utc_to_local(value), "%I:%M:%S %p")
+    return sun_data
