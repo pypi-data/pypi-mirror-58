11825465009b7dec7192acb9062c273dc8f8e0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:28 +0000

    Add package plone.app.locales/5.1.21/plone.app.locales-5.1.21.tar.gz

---
+++ packages/plone.app.locales/plone.app.locales-5.1.21.tar.gz/plone.app.locales-5.1.21/README.rst
@@ -0,0 +1,31 @@
+Introduction
+============
+
+This package contains the translation files for Plone Core
+
+.. note:: A `Contributor’s Agreement <https://plone.org/foundation/contributors-agreement>`_ is not needed to contribute to this repository.
+
+- atcontenttypes-manual.pot
+- atcontenttypes.pot
+- atreferencebrowserwidget-manual.pot
+- atreferencebrowserwidget.pot
+- cmfeditions-manual.pot
+- cmfeditions.pot
+- cmfplacefulworkflow-manual.pot
+- cmfplacefulworkflow.pot
+- plone-generated.pot
+- plone-manual.pot
+- plone.pot
+- plonefrontpage.pot
+- plonelocales.pot
+- widgets.pot
+
+and the following non core add-ons at ``locales-addons`` directory:
+
+- plone.app.ldap.pot
+- plone.app.ldap-manual.pot
+- plone.app.caching.pot
+- plone.app.caching-manual.pot
+
+This version is compatible with Plone 5.1.x and Plone 5.2.x
+
---
+++ packages/plone.app.locales/plone.app.locales-5.1.21.tar.gz/plone.app.locales-5.1.21/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+version = '5.1.21'
+
+setup(
+    name='plone.app.locales',
+    version=version,
+    description='Translation files for Plone',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
+        'Framework :: Zope2',
+        'Framework :: Zope :: 4',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='plone i18n locale translation',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/collective/plone.app.locales',
+    license='GPL version 2',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+)
