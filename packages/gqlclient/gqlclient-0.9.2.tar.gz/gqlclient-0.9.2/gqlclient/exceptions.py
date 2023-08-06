9de5d08162f2784fd87f3e096327bd3d701377f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package gqlclient/0.9.2/gqlclient-0.9.2.tar.gz

---
+++ packages/gqlclient/gqlclient-0.9.2.tar.gz/gqlclient-0.9.2/gqlclient/exceptions.py
@@ -0,0 +1,58 @@
+"""
+Exceptions raised by the gql client
+"""
+from dataclasses import dataclass
+
+
+__all__ = [
+    "EncoderResponseException",
+    "GQLClientException",
+    "GraphQLException",
+    "ModelException",
+    "ServerConnectionException",
+    "ServerResponseException",
+]
+
+
+class GQLClientException(Exception):
+    """
+    Base Exception for all exceptions raised by the gql client
+    """
+
+
+class ModelException(GQLClientException):
+    """
+    Exception raised when the input model (parameter or response)
+    can't be used
+    """
+
+
+class ServerConnectionException(GQLClientException):
+    """
+    Exception raised when the graphql server can't be reached
+    """
+
+
+class ServerResponseException(GQLClientException):
+    """
+    Exception raised when the graphql server returns a HTTP status > 299
+    """
+
+
+class EncoderResponseException(ServerResponseException):
+    """
+    Exception raised when the response encoder encounters a
+    response it cannot encode
+    """
+
+
+@dataclass
+class GraphQLException(GQLClientException):
+    """
+    Exception raised when there is are graphql error(s) returned by the server
+    """
+
+    errors: list = None
+
+    def __str__(self):
+        return f"Errors were raised during graphql processing: details={self.errors}"
