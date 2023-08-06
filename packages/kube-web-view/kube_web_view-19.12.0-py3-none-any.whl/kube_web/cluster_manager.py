5f7ffd0be8868ce2d7090eebc429328a732db138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:09 +0000

    Add package kube-web-view/19.12.0/kube_web_view-19.12.0-py3-none-any.whl

---
+++ packages/kube-web-view/kube_web_view-19.12.0-py3-none-any.whl/kube_web/cluster_manager.py
@@ -0,0 +1,69 @@
+from .resource_registry import ResourceRegistry
+from .selector import selector_matches
+
+from .cluster_discovery import OAuth2BearerTokenAuth
+
+from typing import Dict, List
+
+from pathlib import Path
+
+
+class Cluster:
+    def __init__(
+        self, name: str, api, labels: dict, spec: dict, preferred_api_versions: dict
+    ):
+        self.name = name
+        self.api = api
+        self.labels = labels or {}
+        self.spec = spec or {}
+        self.resource_registry = ResourceRegistry(api, preferred_api_versions)
+
+
+class ClusterNotFound(Exception):
+    def __init__(self, cluster):
+        self.cluster = cluster
+
+
+class ClusterManager:
+    def __init__(
+        self,
+        discoverer,
+        selector: dict,
+        cluster_auth_token_path: Path,
+        preferred_api_versions: dict,
+    ):
+        self._clusters: Dict[str, Cluster] = {}
+        self.discoverer = discoverer
+        self.selector = selector
+        self.cluster_auth_token_path = cluster_auth_token_path
+        self.preferred_api_versions = preferred_api_versions
+        self.reload()
+
+    def reload(self):
+        _clusters = {}
+        for cluster in self.discoverer.get_clusters():
+            if selector_matches(self.selector, cluster.labels):
+                if self.cluster_auth_token_path:
+                    # overwrite auth mechanism with dynamic access token (loaded from file)
+                    cluster.api.session.auth = OAuth2BearerTokenAuth(
+                        self.cluster_auth_token_path
+                    )
+                _clusters[cluster.name] = Cluster(
+                    cluster.name,
+                    cluster.api,
+                    cluster.labels,
+                    cluster.spec,
+                    self.preferred_api_versions,
+                )
+
+        self._clusters = _clusters
+
+    @property
+    def clusters(self) -> List[Cluster]:
+        return list(self._clusters.values())
+
+    def get(self, cluster: str) -> Cluster:
+        obj = self._clusters.get(cluster)
+        if not obj:
+            raise ClusterNotFound(cluster)
+        return obj
