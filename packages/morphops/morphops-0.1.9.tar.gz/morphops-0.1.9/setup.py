4dd975fa6c61ed8c35255f0026a60a1a44de1719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:13 +0000

    Add package morphops/0.1.9/morphops-0.1.9.tar.gz

---
+++ packages/morphops/morphops-0.1.9.tar.gz/morphops-0.1.9/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+version = {}
+with open("morphops/_version.py") as version_file:
+    exec(version_file.read(), version)
+
+setup(
+    name='morphops',
+    version=version['__version__'],
+    description='Geometric morphometrics operations in python',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Vai Patel',
+    author_email='vai.patel@gmail.com',
+    url='https://github.com/vaipatel/morphops',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers = [
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering'
+    ],
+    setup_requires= ['numpy >= 1.10.0'],
+    install_requires= ['numpy >= 1.10.0'],
+    python_requires='>=3.5',
+    include_package_data=True
+)
