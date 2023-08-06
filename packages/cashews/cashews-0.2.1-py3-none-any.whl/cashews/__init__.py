33bfcf6993413f004e108ffac95e9245a4595ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:17 +0000

    Add package cashews/0.2.1/cashews-0.2.1-py3-none-any.whl

---
+++ packages/cashews/cashews-0.2.1-py3-none-any.whl/cashews/__init__.py
@@ -0,0 +1,12 @@
+from .cache_utils.locked import LockedException  # noqa
+from .cache_utils.rate import RateLimitException  # noqa
+from .wrapper import Cache
+
+# pylint: disable=invalid-name
+cache = Cache()
+rate_limit = cache.rate_limit
+fail = cache.fail
+early = cache.early
+hit = cache.hit
+perf = cache.perf
+locked = cache.locked
