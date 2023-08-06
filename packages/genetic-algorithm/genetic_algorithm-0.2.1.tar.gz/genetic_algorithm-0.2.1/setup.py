4c1085b7147828ec8c1c291e3b25b647527e1151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:27 +0000

    Add package genetic-algorithm/0.2.1/genetic_algorithm-0.2.1.tar.gz

---
+++ packages/genetic-algorithm/genetic_algorithm-0.2.1.tar.gz/genetic_algorithm-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+  name = 'genetic_algorithm',
+  packages = ['genetic_algorithm'],
+  version = '0.2.1',
+  license='MIT',
+  description = 'A python package implementing the genetic algorithm',
+  author = 'He Yingxu',
+  author_email = 'yingxu.he1998@gmail.com',
+  url = 'https://github.com/YingxuH/genetic_algorithm',
+  download_url = 'https://github.com/YingxuH/genetic_algorithm/archive/0.2.1.tar.gz',
+  keywords = ['genetic algorithm'],
+  install_requires = [
+    "numpy",
+    "pandas"
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
