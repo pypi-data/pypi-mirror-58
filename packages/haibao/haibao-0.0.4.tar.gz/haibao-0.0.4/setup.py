0dddbc6ac60b367fd96724a69cfa279e08986452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:25 +0000

    Add package haibao/0.0.4/haibao-0.0.4.tar.gz

---
+++ packages/haibao/haibao-0.0.4.tar.gz/haibao-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="haibao",
+    version="0.0.4",
+    author="seal",
+    author_email="seal.xu@qq.com",
+    description="seal haibao tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
