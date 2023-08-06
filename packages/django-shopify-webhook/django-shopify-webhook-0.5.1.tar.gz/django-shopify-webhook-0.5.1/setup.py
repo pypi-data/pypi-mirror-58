c181ee1485f64ffeff0586c3b1b569eb3b2abade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package django-shopify-webhook/0.5.1/django-shopify-webhook-0.5.1.tar.gz

---
+++ packages/django-shopify-webhook/django-shopify-webhook-0.5.1.tar.gz/django-shopify-webhook-0.5.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = __import__('shopify_webhook').__version__
+
+setup(
+    name = 'django-shopify-webhook',
+    version = version,
+    description = 'A package for the creation of Shopify Apps using the Embedded App SDK.',
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author = 'Gavin Ballard',
+    author_email = 'gavin@discolabs.com',
+    url = 'https://github.com/discolabs/django-shopify-webhook',
+    license = 'None',
+
+    packages = find_packages(),
+
+    install_requires = [
+        'django >=1.7',
+    ],
+
+    zip_safe = True,
+    classifiers = [],
+)
