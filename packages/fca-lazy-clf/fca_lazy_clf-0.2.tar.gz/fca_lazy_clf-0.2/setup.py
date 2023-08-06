506f1db945146a03b954d5932ff165c3cc4a3907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:47 +0000

    Add package fca-lazy-clf/0.2/fca_lazy_clf-0.2.tar.gz

---
+++ packages/fca-lazy-clf/fca_lazy_clf-0.2.tar.gz/fca_lazy_clf-0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name='fca_lazy_clf',
+  packages=['fca_lazy_clf'],
+  version='0.2',
+  license='MIT',
+  description='Lazy binary classifier based on Formal Concept Analysis',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author='Vitaliy Pozdnyakov',
+  author_email='pozdnyakov.vitaliy@yandex.ru',
+  url='https://github.com/vpozdnyakov/fca_lazy_clf',
+  keywords=['fca', 'formal-concept-analysis', 'lazy-learning', 'binary-classification'],
+  install_requires=[
+          'pandas',
+          'numpy',
+          'sklearn',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
