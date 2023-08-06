1c4a1c51a1184815ddcde0dc983ed99ab382bc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:08 +0000

    Add package torchservant/0.2.1/torchservant-0.2.1-py3-none-any.whl

---
+++ packages/torchservant/torchservant-0.2.1-py3-none-any.whl/torchservant/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/09/15 19:34
+# @Author  : LQX
+# @Email   : qxsoftware@163.com
+# @File    : __init__.py
+# @Software: PyCharm
+
+from torchservant import modelkeeper
+from torchservant import board
+from torchservant import stats
+from torchservant import classicmodels
+from torchservant import observer
+from torchservant import weightransfer
