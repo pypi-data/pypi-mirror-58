e624f0f5162c9be70e6a8eccadb84d8c695f3baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:24 +0000

    Add package tinvest/1.0.10/tinvest-1.0.10-py3-none-any.whl

---
+++ packages/tinvest/tinvest-1.0.10-py3-none-any.whl/tinvest/base_client.py
@@ -0,0 +1,20 @@
+from typing import Generic, Optional, TypeVar
+
+from .constants import PRODUCTION
+
+T = TypeVar('T')
+
+
+class BaseClient(Generic[T]):
+    def __init__(self, token: str, *, session: Optional[T] = None):
+        if not token:
+            raise ValueError('Token cannot be empty')
+        self._base_url: str = PRODUCTION
+        self._token: str = token
+        self._session = session
+
+    @property
+    def session(self) -> T:
+        if self._session:
+            return self._session
+        raise AttributeError
