0fd0ad1cd4ab3de1865f9493aa1d9f80eff3c93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:09 +0000

    Add package django-page/0.15.0/django-page-0.15.0.tar.gz

---
+++ packages/django-page/django-page-0.15.0.tar.gz/django-page-0.15.0/page/admin.py
@@ -0,0 +1,91 @@
+from django.contrib import admin
+from django.core.cache import cache
+from reversion.admin import VersionAdmin
+
+from page.models import Page, Pagelet, DownloadableFile, Post, PostCategory, Slide, Popup
+
+
+class SlideInline(admin.StackedInline):
+    sortable = "order"
+    model = Slide
+    extra = 0
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.clear()
+
+
+class PopupInline(admin.StackedInline):
+    model = Popup
+    extra = 0
+
+
+@admin.register(Page)
+class PageAdmin(VersionAdmin):
+    list_display = ['title', 'code', 'id', 'updated', 'created']
+    inlines = [SlideInline, PopupInline]
+
+    date_hierarchy = 'created'
+    fieldsets = (
+        (None, {
+            'fields': ('code', 'page_type', 'title', 'subtitle', 'featured')
+        }),
+        ('PC 환경 컨텐츠', {
+            'fields': ('content', 'style'),
+        }),
+        ('모바일 환경 컨텐츠', {
+            'classes': ('collapse',),
+            'fields': ('mobile_content', 'mobile_style'),
+        }),
+        ('커스터마이징', {
+            'classes': ('collapse',),
+            'fields': ('template','script'),
+        }),
+        ('기록', {
+            'fields': ('updated', 'created'),
+        }),
+    )
+    readonly_fields = ('updated', 'created')
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.clear()
+
+
+@admin.register(Pagelet)
+class PageletAdmin(VersionAdmin):
+    list_display = ['title', 'code', 'id', 'updated', 'created']
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.clear()
+
+
+@admin.register(DownloadableFile)
+class DownloadableFileAdmin(VersionAdmin):
+    list_display = ['name', 'file', ]
+    date_hierarchy = 'created'
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.clear()
+
+
+@admin.register(Post)
+class PostAdmin(VersionAdmin):
+    list_display = ['title', 'active', 'topmost', 'created', 'cate']
+    list_editable = ['active', 'topmost']
+    list_filter = ['cate', 'active', 'topmost']
+    date_hierarchy = 'created'
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.clear()
+
+
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.clear()
---
+++ packages/django-page/django-page-0.15.0.tar.gz/django-page-0.15.0/page/templatetags/page.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+from django import template
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+import os
+
+from page.models import Page, Pagelet, DownloadableFile
+from page.shortcode import unpack_content_with_request
+
+register = template.Library()
+
+
+@register.simple_tag
+def main_url():
+    try:
+        return Page.objects.get(page_type='main').get_absolute_url()
+    except Page.DoesNotExist:
+        try:
+            return Page.objects.get(code='main').get_absolute_url()
+        except Page.DoesNotExist:
+            return '/'
+
+
+@register.simple_tag(takes_context=True)
+def page(context, page_code):
+    if not page_code:
+        return ""
+
+    page_obj, _ = Page.objects.get_or_create(code=page_code, defaults={'title': page_code, 'content': page_code})
+    return unpack_content_with_request(context['request'], page_obj.content, {
+        'title': page_obj.title,
+        'edit': reverse('admin:page_page_change', args=[page_obj.id])
+    })
+
+
+@register.simple_tag(takes_context=True)
+def pagelet(context, pagelet_code):
+    if not pagelet_code:
+        return ""
+
+    try:
+        pagelet_obj = Pagelet.objects.get(title=pagelet_code)
+    except Pagelet.DoesNotExist:
+        pagelet_obj, _ = Pagelet.objects.get_or_create(code=pagelet_code,
+                                                       defaults={'title': pagelet_code, 'content': pagelet_code})
+
+    return unpack_content_with_request(context['request'], pagelet_obj.content, {
+        'title': pagelet_obj.title,
+        'edit': reverse('admin:page_page_change', args=[pagelet_obj.id])
+    })
+
+
+@register.simple_tag(takes_context=True)
+def pagelet_title(context, pagelet_code):
+    if not pagelet_code:
+        return ""
+
+    try:
+        pagelet_obj = Pagelet.objects.get(title=pagelet_code)
+    except Pagelet.DoesNotExist:
+        pagelet_obj, _ = Pagelet.objects.get_or_create(code=pagelet_code,
+                                                       defaults={'title': pagelet_code, 'content': pagelet_code})
+
+    return pagelet_obj.title
+
+
+@register.simple_tag
+def download(value, color='button-border white'):
+    try:
+        int_value = int(value)
+        target = DownloadableFile.objects.get(pk=int_value)
+    except (ValueError, DownloadableFile.DoesNotExist):
+        target = None
+
+    if not target:
+        try:
+            target = DownloadableFile.objects.get(name=value)
+        except DownloadableFile.DoesNotExist:
+            return ""
+
+    if not target.file or not target.file.name:
+        return mark_safe("""<a class="button %s" href="#" 
+            data-togglerfe"modal" data-target="#error-dialog" data-message="죄송합니다! %s 다운로드가 아직 준비되지 않았습니다."> 
+            <span> %s 다운받기</span>
+            <i class="fa fa-download"></i>
+            </a>""" % (color, target.name, target.name))
+
+    url = reverse("page:download", args=(target.pk, os.path.basename(target.file.name)))
+
+    return mark_safe("""<a class="button %s" href="%s">
+                        <span>%s 다운받기</span>
+                        <i class="fa fa-download"></i>
+                    </a>""" % (color, url, target.name))
+
+
+@register.simple_tag
+def view(value):
+    try:
+        int_value = int(value)
+        target = DownloadableFile.objects.get(pk=int_value)
+    except (ValueError, DownloadableFile.DoesNotExist):
+        target = None
+
+    if not target:
+        try:
+            target = DownloadableFile.objects.get(name=value)
+        except DownloadableFile.DoesNotExist:
+            return ""
+
+    return mark_safe(
+        '<a class="btn btn-primary" href="%s"><i class="fa fa-eye"></i> "%s" 보기</a>' % (target.file.url, target.name))
+
+
+@register.simple_tag
+def page_button(value, label=None, color='btn-info'):
+    try:
+        int_value = int(value)
+        target = Page.objects.get(pk=int_value)
+    except (ValueError, Page.DoesNotExist):
+        target = None
+
+    if not target:
+        try:
+            target = Page.objects.get(code=value)
+        except Page.DoesNotExist:
+            return ""
+
+    url = reverse("page:page_by_id", args=(target.pk,))
+    if not label:
+        label = target.title
+
+    return mark_safe("""<a class="btn %s" href="%s">%s</a>""" % (color, url, label))
