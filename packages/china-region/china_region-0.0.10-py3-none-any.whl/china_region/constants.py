0c2ccdb0cb0385f5ba8b6f895dc5c94d15992e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:46 +0000

    Add package china-region/0.0.10/china_region-0.0.10-py3-none-any.whl

---
+++ packages/china-region/china_region-0.0.10-py3-none-any.whl/china_region/constants.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+csv_name = os.path.join(current_dir, 'region.csv')
+
+CITY_DF = pd.read_csv(csv_name, engine='python', encoding='utf-8')
+
+
+
