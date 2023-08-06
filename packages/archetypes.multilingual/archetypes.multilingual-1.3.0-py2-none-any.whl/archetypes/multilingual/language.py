88eb704bdd16749a1948a4fc14f6db60f731db38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package archetypes.multilingual/1.3.0/archetypes.multilingual-1.3.0-py2-none-any.whl

---
+++ packages/archetypes.multilingual/archetypes.multilingual-1.3.0-py2-none-any.whl/archetypes/multilingual/language.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4:
+from plone.multilingual.interfaces import (
+    ILanguage,
+    LANGUAGE_INDEPENDENT,
+)
+from zope import interface
+
+
+class ATLanguage(object):
+
+    interface.implements(ILanguage)
+
+    def __init__(self, context):
+        self.context = context
+
+    def get_language(self):
+        language = self.context.Language()
+        if not language:
+            language = LANGUAGE_INDEPENDENT
+        return language
+
+    def set_language(self, language):
+        # Override the setLanguage method imposed by LP
+        # and access to the direct mutator of the object
+        self.context.getField("language").set(self.context, language)
