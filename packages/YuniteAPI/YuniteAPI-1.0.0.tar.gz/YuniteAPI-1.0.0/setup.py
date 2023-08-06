af9b5b802f96cb9cb09107822265b06016129eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:09 +0000

    Add package YuniteAPI/1.0.0/YuniteAPI-1.0.0.tar.gz

---
+++ packages/YuniteAPI/YuniteAPI-1.0.0.tar.gz/YuniteAPI-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import re
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('YuniteAPI/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+with open('README.md') as f:
+    readme = f.read()
+setup(name='YuniteAPI',
+      author='SylteA',
+      version=version,
+      packages=['YuniteAPI'],
+      license='MIT',
+      description='A asynchronous python wrapper for the Yunite API',
+      url='https://github.com/SylteA/YuniteAPI',
+      long_description_content_type='text/markdown',
+      long_description=readme,
+      install_requires=requirements,
+      python_requires='>=3.7.5',
+      keywords=['wrapper', 'Yunite', 'api'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+      ])
+
