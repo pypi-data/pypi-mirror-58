c9b2158a390b8e9afa29fab18d30e02f112b9188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:11 +0000

    Add package fileloghelper/0.0.5/fileloghelper-0.0.5.tar.gz

---
+++ packages/fileloghelper/fileloghelper-0.0.5.tar.gz/fileloghelper-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fileloghelper",
+    version="0.0.5",
+    author="mithem",
+    author_email="miguel.themann@gmail.com",
+    description="Helper for logging to files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mithem/filelog",
+    packages=["fileloghelper"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_dir={'': 'src'}
+)
