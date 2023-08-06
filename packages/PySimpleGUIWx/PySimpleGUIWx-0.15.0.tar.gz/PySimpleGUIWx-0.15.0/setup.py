7b99660b49bf66c923f9c34fcb768b8c8921afe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:28 +0000

    Add package PySimpleGUIWx/0.15.0/PySimpleGUIWx-0.15.0.tar.gz

---
+++ packages/PySimpleGUIWx/PySimpleGUIWx-0.15.0.tar.gz/PySimpleGUIWx-0.15.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setuptools.setup(
+    name="PySimpleGUIWx",
+    version="0.15.0",
+    author="MikeTheWatchGuy",
+    author_email="mike@PySimpleGUI.org",
+    description="The Engineering Pre-Release WxPython version of PySimpleGUI. PySimpleGUI SDK Launched in 2018 Actively developed and supported. Super-simple to create custom GUI's.  Now supports tkinter, Qt, WxPython, and Remi",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="GUI UI tkinter wrapper simple easy beginner novice student graphics progressbar progressmeter",
+    url="https://github.com/PySimpleGUI/PySimpleGUI",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: Multimedia :: Graphics",
+        "Operating System :: OS Independent"
+    ),
+)
