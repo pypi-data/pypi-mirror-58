b2a7c97b28015a4260faff3b25ea3978b09c8730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:34 +0000

    Add package saya/0.0.2/saya-0.0.2.tar.gz

---
+++ packages/saya/saya-0.0.2.tar.gz/saya-0.0.2/saya/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .VK.Vk import Vk
+from .VK.Event import Event
+
+__copyright__ = "2020"
+__version__ = "0.0.2"
+__authors__ = ["Ethosa"]
+
+if __name__ == '__main__':
+    print(Vk, Event)
