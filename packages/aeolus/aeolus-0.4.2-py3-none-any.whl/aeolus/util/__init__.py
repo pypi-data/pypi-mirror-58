8c2d875ab79f6e01d974ed4f30c5d956de6a6418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:58 +0000

    Add package aeolus/0.4.2/aeolus-0.4.2-py3-none-any.whl

---
+++ packages/aeolus/aeolus-0.4.2-py3-none-any.whl/aeolus/util/__init__.py
@@ -0,0 +1,5 @@
+"""Submodule with various utility functions used elsewhere in aeolus."""
+from .text import fmt_lonlat, subplot_label_generator
+
+
+__all__ = ("fmt_lonlat", "subplot_label_generator")
