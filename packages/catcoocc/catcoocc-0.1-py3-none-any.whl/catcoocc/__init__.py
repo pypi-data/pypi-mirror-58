540d89a3c7b8ded10f98436347115c884acf9ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:16 +0000

    Add package catcoocc/0.1/catcoocc-0.1-py3-none-any.whl

---
+++ packages/catcoocc/catcoocc-0.1-py3-none-any.whl/catcoocc/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+# Informatin on the package
+__version__ = "0.1"
+__author__ = "Tiago Tresoldi"
+__email__ = "tresoldi@shh.mpg.de"
+
+# Build the namespace
+from catcoocc.utils import *
+from catcoocc import scorer
