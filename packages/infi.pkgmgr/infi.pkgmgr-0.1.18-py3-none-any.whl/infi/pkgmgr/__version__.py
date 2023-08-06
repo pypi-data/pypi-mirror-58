1c59f58cd57e4caf3af1da24399d8d8592421f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:03 +0000

    Add package infi.pkgmgr/0.1.18/infi.pkgmgr-0.1.18-py3-none-any.whl

---
+++ packages/infi.pkgmgr/infi.pkgmgr-0.1.18-py3-none-any.whl/infi/pkgmgr/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.18"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.18)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'bfb2f28d28214ae1206795327449636af8768e66'
+__git_head_subject__ = 'Finished Release v0.1.18'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2019-12-23 15:10:28'
