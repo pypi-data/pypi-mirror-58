47617ebdaeea0e810b945548ef58674091f447da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:22 +0000

    Add package datafeeds/0.1.0/datafeeds-0.1.0.tar.gz

---
+++ packages/datafeeds/datafeeds-0.1.0.tar.gz/datafeeds-0.1.0/datafeeds/client/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2019-12-27 16:42
+# @Author  : liuqiuxi
+# @Email   : liuqiuxi1990@gmail.com
+# @File    : __init__.py
+# @Project : datafeeds
+# @Software: PyCharm
+# @Remark  : This is datafeeds client, the most important class
+
+from datafeeds.utils import BarFeedConfig
---
+++ packages/datafeeds/datafeeds-0.1.0.tar.gz/datafeeds-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# datafeeds
+#
+# Copyright 2011-2019 liuqiuxi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="datafeeds",
+    url="https://github.com/liuqiuxi/datafeeds",
+    version="0.1.0",
+    description="Python Data Source",
+    author="liuqiuxi",
+    author_email="liuqiuxi1990@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"],
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=[]
+)
