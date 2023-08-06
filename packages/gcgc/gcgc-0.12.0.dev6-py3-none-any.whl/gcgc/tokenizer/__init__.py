a0d1a170f1d9aed601a81545de50cc376e49fc9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:28 +0000

    Add package gcgc/0.12.0.dev6/gcgc-0.12.0.dev6-py3-none-any.whl

---
+++ packages/gcgc/gcgc-0.12.0.dev6-py3-none-any.whl/gcgc/tokenizer/__init__.py
@@ -0,0 +1,8 @@
+# (c) Copyright 2018 Trent Hauck
+# All Rights Reserved
+"""Contains tokenizer level packages."""
+
+from gcgc.tokenizer.kmer_tokenzier import KmerTokenizer
+from gcgc.tokenizer.kmer_tokenzier import KmerTokenizerSettings
+
+__all__ = ["KmerTokenizer", "KmerTokenizerSettings"]
