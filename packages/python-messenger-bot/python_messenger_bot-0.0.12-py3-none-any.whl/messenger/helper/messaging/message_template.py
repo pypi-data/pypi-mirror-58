694602578f169253003631108db778af437d1aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:50 +0000

    Add package python-messenger-bot/0.0.12/python_messenger_bot-0.0.12-py3-none-any.whl

---
+++ packages/python-messenger-bot/python_messenger_bot-0.0.12-py3-none-any.whl/messenger/helper/messaging/message_template.py
@@ -0,0 +1,9 @@
+﻿class MessageTemplate:
+
+    def __init__(self, **kwargs):
+        self.text=''
+        
+        return super().__init__(**kwargs)
+
+    def json_to_message(self, json):
+        self.text = json['text']
