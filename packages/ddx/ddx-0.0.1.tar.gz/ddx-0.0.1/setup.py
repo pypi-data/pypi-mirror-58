e60ef416864d93521a5a8600efa5adf9322ddaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:37 +0000

    Add package ddx/0.0.1/ddx-0.0.1.tar.gz

---
+++ packages/ddx/ddx-0.0.1.tar.gz/ddx-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ddx",
+    version="0.0.1",
+    author="ChanMo",
+    author_email="chan.mo@outlook.com",
+    description="快速搭建基于Docker的Django项目",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChanMo/ddx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
