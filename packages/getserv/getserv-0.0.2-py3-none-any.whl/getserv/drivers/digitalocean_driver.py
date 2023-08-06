f596bf586dd99fde801ac0cf812c8288a8886931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package getserv/0.0.2/getserv-0.0.2-py3-none-any.whl

---
+++ packages/getserv/getserv-0.0.2-py3-none-any.whl/getserv/drivers/digitalocean_driver.py
@@ -0,0 +1,80 @@
+import argparse
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+import json
+import os
+import sys
+
+from .base_driver import BaseDriver
+
+
+class DigitaloceanDriver(BaseDriver):
+  api_base_url = 'https://api.digitalocean.com/v2/'
+
+  def get_name():
+    return 'digitalocean'
+
+  def create_parser(sub_parsers: argparse._SubParsersAction):
+    return sub_parsers.add_parser(
+      DigitaloceanDriver.get_name(),
+      help='Retrieves ips from DigitalOcean droplets by tags',
+      description='Retrieve node ips by tags'
+    )
+
+  def init_parser(parser: argparse.ArgumentParser):
+    parser.add_argument(
+      '--digitalocean-access-token',
+      default=os.environ.get('DIGITALOCEAN_ACCESS_TOKEN', ''),
+      help='\
+        If not set, the "DIGITALOCEAN_ACCESS_TOKEN" \
+        environment variable will be used instead\
+      '
+    )
+
+    parser.add_argument(
+      'droplet_tags',
+      nargs=1,
+      type=str,
+      help='A comma separated list of tags'
+    )
+
+    return parser
+
+  def run(self, args):
+    return self.fetch(
+      ','.join(args.droplet_tags),
+      args.digitalocean_access_token
+    )
+
+  def fetch(self, tagSeq: str, token: str):
+    tags = [tag.strip() for tag in tagSeq.split(',')]
+    droplet_ips = set()
+    results = []
+
+    for tag in tags:
+      try:
+        request = Request(self.api_base_url + 'droplets?tag_name=' + tag)
+
+        request.add_header('Content-Type', 'application/json')
+        request.add_header('Authorization', 'Bearer ' + token)
+        results.append(json.loads(urlopen(request).read()))
+      except HTTPError as e:
+        if (e.code == 401):
+          print(
+            'Invalid access token for Digital ocean given: 401 Unauthorized'
+          )
+        else:
+          print('Something went wrong while accessing the DigitalOcean API')
+
+        sys.exit(1)
+
+    for result in results:
+      for droplet in result['droplets']:
+        droplet_tags = droplet['tags']
+
+        if set(tags).issubset(droplet_tags):
+          for network in droplet['networks']['v4']:
+            if network['type'] == 'public':
+              droplet_ips.add(network['ip_address'])
+
+    return list(droplet_ips)
