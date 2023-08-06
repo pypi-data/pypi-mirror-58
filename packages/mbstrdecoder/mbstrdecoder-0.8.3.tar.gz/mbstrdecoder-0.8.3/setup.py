f503b9fc8c7eeddeb2e567c5a6637075927f9459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:31 +0000

    Add package mbstrdecoder/0.8.3/mbstrdecoder-0.8.3.tar.gz

---
+++ packages/mbstrdecoder/mbstrdecoder-0.8.3.tar.gz/mbstrdecoder-0.8.3/setup.py
@@ -0,0 +1,96 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import io
+import os.path
+import sys
+
+import setuptools
+
+
+MODULE_NAME = "mbstrdecoder"
+REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
+REQUIREMENT_DIR = "requirements"
+ENCODING = "utf8"
+
+pkg_info = {}
+
+
+def need_pytest():
+    return set(["pytest", "test", "ptr"]).intersection(sys.argv)
+
+
+def get_release_command_class():
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with io.open("README.rst", encoding=ENCODING) as f:
+    long_description = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_require = [line.strip() for line in f if line.strip()]
+
+SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
+PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=pkg_info["__version__"],
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description="multi-byte character string decoder",
+    include_package_data=True,
+    keywords=["multi-byte character", "unicode", "decoder"],
+    license=pkg_info["__license__"],
+    long_description=long_description,
+    packages=setuptools.find_packages(exclude=["test*"]),
+    project_urls={"Source": REPOSITORY_URL, "Tracker": "{:s}/issues".format(REPOSITORY_URL),},
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=SETUPTOOLS_REQUIRES + install_requires,
+    setup_requires=SETUPTOOLS_REQUIRES + PYTEST_RUNNER_REQUIRES,
+    tests_require=tests_require,
+    extras_require={
+        "all": [],  # TODO: remove in the future release
+        "dev": ["releasecmd>=0.1.0,<1", "twine", "wheel"]
+        + ["autoflake", "black", "isort"]
+        + ["codespell", "pylama"]
+        + tests_require,
+        "test": tests_require,
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+    ],
+    cmdclass=get_release_command_class(),
+)
