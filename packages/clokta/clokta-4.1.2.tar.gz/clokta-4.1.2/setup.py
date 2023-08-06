2c91c77746f0766c141bc75067b5ff37d90e6eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:50 +0000

    Add package clokta/4.1.2/clokta-4.1.2.tar.gz

---
+++ packages/clokta/clokta-4.1.2.tar.gz/clokta-4.1.2/setup.py
@@ -0,0 +1,33 @@
+''' Define executable setup '''
+import os
+from setuptools import setup, find_packages
+import warnings
+
+setup(
+    name='clokta',
+    version='4.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['clokta'],
+    install_requires=[
+        'beautifulsoup4',
+        'boto3',
+        'click>=7.0',
+        'configparser',
+        'enum-compat',
+        'keyring',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'clokta=clokta.cloktacli:assume_role'
+        ]
+    },
+    namespace_packages=['clokta'],
+    author="Robert Antonucci and the WaPo platform tools team",
+    author_email="opensource@washingtonpost.com",
+    url="https://github.com/washingtonpost/clokta",
+    download_url="https://github.com/washingtonpost/clokta/tarball/4.1.2",
+    keywords=['okta', 'clokta', 'aws', 'cli'],
+    classifiers=[]
+)
