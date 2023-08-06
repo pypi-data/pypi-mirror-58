657193643faa4f43f141a92cbb1690d23d815875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:37 +0000

    Add package mlc-tools/0.4.640/mlc_tools-0.4.640-py3-none-any.whl

---
+++ packages/mlc-tools/mlc_tools-0.4.640-py3-none-any.whl/mlc_tools/module_cpp/generator.py
@@ -0,0 +1,22 @@
+from mlc_tools.module_cpp.generator_ref_counter import GeneratorRefCounter
+from ..base.generator import GeneratorBase
+from .generator_data_storage import GeneratorDataStorage
+from ..base.generator_visitor import GeneratorVisitor
+from .generator_predefined_files import GeneratorPredefinedFiles
+from .generator_operator_equals import GeneratorOperatorEquals
+from .generator_observer import GeneratorObserver
+
+
+class Generator(GeneratorBase):
+
+    def __init__(self):
+        GeneratorBase.__init__(self)
+
+    def generate(self, model):
+        GeneratorBase.generate(self, model)
+        GeneratorDataStorage().generate(model)
+        GeneratorVisitor().generate(model, True)
+        GeneratorPredefinedFiles().generate(model)
+        GeneratorOperatorEquals().generate(model)
+        GeneratorObserver().generate(model)
+        GeneratorRefCounter().generate(model)
