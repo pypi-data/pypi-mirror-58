867ced65ff3d3870cbe79bfa541c8c596ff53e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:43 +0000

    Add package kkt/0.1.4/kkt-0.1.4.tar.gz

---
+++ packages/kkt/kkt-0.1.4.tar.gz/kkt-0.1.4/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['kkt', 'kkt.builders', 'kkt.builders.kernels', 'kkt.commands', 'kkt.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0',
+ 'gitpython>=3.0,<4.0',
+ 'kaggle>=1.5,<2.0',
+ 'lockfile>=0.12.2,<0.13.0',
+ 'poetry==1.0.0',
+ 'tomlkit>=0.5.8,<0.6.0']
+
+entry_points = \
+{'console_scripts': ['kkt = kkt.cli:main']}
+
+setup_kwargs = {
+    'name': 'kkt',
+    'version': '0.1.4',
+    'description': 'A tool for kaggle kernel',
+    'long_description': None,
+    'author': 'Masahiro Wada',
+    'author_email': 'argon.argon.argon@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
