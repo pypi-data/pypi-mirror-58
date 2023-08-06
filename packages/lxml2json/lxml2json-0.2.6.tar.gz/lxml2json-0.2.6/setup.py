9a4e0fbf8e4f10012c8a5902ab9f20d1f67fdbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:49 +0000

    Add package lxml2json/0.2.6/lxml2json-0.2.6.tar.gz

---
+++ packages/lxml2json/lxml2json-0.2.6.tar.gz/lxml2json-0.2.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lxml2json",
+    version="0.2.6",
+    author="Robert Parelius",
+    author_email="rparelius@gmail.com",
+    description="converts XML elements into their JSON equivalent or vice versa",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rparelius/lxml2json",
+    packages=setuptools.find_packages(),
+    install_requires=['lxml'],    
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
