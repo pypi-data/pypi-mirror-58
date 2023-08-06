6e9aec603a1ff34e6ffec4204552251a6089d0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:04 +0000

    Add package napoleontoolbox/0.5.2/napoleontoolbox-0.5.2.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-0.5.2.tar.gz/napoleontoolbox-0.5.2/napoleontoolbox/movie/movie_maker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+def createCorrelationBarMovie(rolled_sharpe, saving_path):
+    FFMpegWriter = manimation.writers['ffmpeg']
+    metadata = dict(title='Movie Test', artist='Matplotlib',
+                    comment='Movie support!')
+    writer = FFMpegWriter(fps=15, metadata=metadata)
+
+    fig = plt.figure()
+    ax = fig.gca()
+
+    print('saving file')
+    with writer.saving(fig, saving_path, 100):
+        for row in rolled_sharpe.iterrows():
+            ax.clear()
+            plt.ylim(-1, 1)
+            histo_values = row[1].to_frame()
+            histo_values.columns = ['CORR']
+            histo_values['STRAT'] = histo_values.index
+            histo_values.plot.bar(x='STRAT', y='CORR', rot=90, ax = ax, title = str(row[0]), legend = None)
+            writer.grab_frame()
+
+def createCorrelationMovoe(stacked_rolled_sharpe, saving_path):
+    def createCorrelationBarMovie(rolled_sharpe, saving_path):
+        FFMpegWriter = manimation.writers['ffmpeg']
+        metadata = dict(title='Movie Test', artist='Matplotlib',
+                        comment='Movie support!')
+        writer = FFMpegWriter(fps=15, metadata=metadata)
+
+        fig = plt.figure()
+        ax = fig.gca()
+
+        print('saving file')
+        with writer.saving(fig, saving_path, 100):
+            for row in rolled_sharpe.iterrows():
+                ax.clear()
+                plt.ylim(-1, 1)
+                histo_values = row[1].to_frame()
+                histo_values.columns = ['CORR']
+                histo_values['STRAT'] = histo_values.index
+                histo_values.plot.bar(x='STRAT', y='CORR', rot=90, ax=ax, title=str(row[0]), legend=None)
+                writer.grab_frame()
