90840d97ee86555bf47b6b07ab29e17368dabcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:39 +0000

    Add package libfwps-python/20191221/libfwps-python-20191221.tar.gz

---
+++ packages/libfwps-python/libfwps-python-20191221.tar.gz/libfwps-20191221/include/libfwps.h
@@ -0,0 +1,147 @@
+/*
+ * Library to access the Windows Property Store format
+ *
+ * Copyright (C) 2013-2019, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _LIBFWPS_H )
+#define _LIBFWPS_H
+
+#include <libfwps/codepage.h>
+#include <libfwps/definitions.h>
+#include <libfwps/error.h>
+#include <libfwps/extern.h>
+#include <libfwps/features.h>
+#include <libfwps/types.h>
+
+#include <stdio.h>
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+/* -------------------------------------------------------------------------
+ * Support functions
+ * ------------------------------------------------------------------------- */
+
+/* Returns the library version
+ */
+LIBFWPS_EXTERN \
+const char *libfwps_get_version(
+             void );
+
+/* -------------------------------------------------------------------------
+ * Error functions
+ * ------------------------------------------------------------------------- */
+
+/* Frees an error
+ */
+LIBFWPS_EXTERN \
+void libfwps_error_free(
+      libfwps_error_t **error );
+
+/* Prints a descriptive string of the error to the stream
+ * Returns the amount of printed characters if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_error_fprint(
+     libfwps_error_t *error,
+     FILE *stream );
+
+/* Prints a descriptive string of the error to the string
+ * The end-of-string character is not included in the return value
+ * Returns the amount of printed characters if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_error_sprint(
+     libfwps_error_t *error,
+     char *string,
+     size_t size );
+
+/* Prints a backtrace of the error to the stream
+ * Returns the amount of printed characters if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_error_backtrace_fprint(
+     libfwps_error_t *error,
+     FILE *stream );
+
+/* Prints a backtrace of the error to the string
+ * The end-of-string character is not included in the return value
+ * Returns the amount of printed characters if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_error_backtrace_sprint(
+     libfwps_error_t *error,
+     char *string,
+     size_t size );
+
+/* -------------------------------------------------------------------------
+ * Format class identifier functions
+ * ------------------------------------------------------------------------- */
+
+/* Retrieves a string containing the identifier of the folder identifier
+ */
+LIBFWPS_EXTERN \
+const char *libfwps_format_class_identifier_get_identifier(
+             const uint8_t *format_class_identifier );
+
+/* Retrieves a string containing the description of the folder identifier
+ */
+LIBFWPS_EXTERN \
+const char *libfwps_format_class_identifier_get_description(
+             const uint8_t *format_class_identifier );
+
+/* -------------------------------------------------------------------------
+ * Storage functions
+ * ------------------------------------------------------------------------- */
+
+/* Creates a storage
+ * Make sure the value storage is referencing, is set to NULL
+ * Returns 1 if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_storage_initialize(
+     libfwps_storage_t **storage,
+     libfwps_error_t **error );
+
+/* Frees a storage
+ * Returns 1 if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_storage_free(
+     libfwps_storage_t **storage,
+     libfwps_error_t **error );
+
+/* Copies a serialized property storage from a byte stream
+ * Returns 1 if successful or -1 on error
+ */
+LIBFWPS_EXTERN \
+int libfwps_storage_copy_from_byte_stream(
+     libfwps_storage_t *storage,
+     const uint8_t *byte_stream,
+     size_t byte_stream_size,
+     int ascii_codepage,
+     libfwps_error_t **error );
+
+#if defined( __cplusplus )
+}
+#endif
+
+#endif /* !defined( _LIBFWPS_H ) */
+
---
+++ packages/libfwps-python/libfwps-python-20191221.tar.gz/libfwps-20191221/pyfwps/pyfwps.c
@@ -0,0 +1,197 @@
+/*
+ * Python bindings module for libfwps (pyfwps)
+ *
+ * Copyright (C) 2013-2019, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <narrow_string.h>
+#include <types.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( HAVE_WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "pyfwps.h"
+#include "pyfwps_error.h"
+#include "pyfwps_libcerror.h"
+#include "pyfwps_libfwps.h"
+#include "pyfwps_python.h"
+#include "pyfwps_storage.h"
+#include "pyfwps_unused.h"
+
+/* The pyfwps module methods
+ */
+PyMethodDef pyfwps_module_methods[] = {
+	{ "get_version",
+	  (PyCFunction) pyfwps_get_version,
+	  METH_NOARGS,
+	  "get_version() -> String\n"
+	  "\n"
+	  "Retrieves the version." },
+
+	/* Sentinel */
+	{ NULL,
+	  NULL,
+	  0,
+	  NULL}
+};
+
+/* Retrieves the pyfwps/libfwps version
+ * Returns a Python object if successful or NULL on error
+ */
+PyObject *pyfwps_get_version(
+           PyObject *self PYFWPS_ATTRIBUTE_UNUSED,
+           PyObject *arguments PYFWPS_ATTRIBUTE_UNUSED )
+{
+	const char *errors           = NULL;
+	const char *version_string   = NULL;
+	size_t version_string_length = 0;
+
+	PYFWPS_UNREFERENCED_PARAMETER( self )
+	PYFWPS_UNREFERENCED_PARAMETER( arguments )
+
+	Py_BEGIN_ALLOW_THREADS
+
+	version_string = libfwps_get_version();
+
+	Py_END_ALLOW_THREADS
+
+	version_string_length = narrow_string_length(
+	                         version_string );
+
+	/* Pass the string length to PyUnicode_DecodeUTF8
+	 * otherwise it makes the end of string character is part
+	 * of the string
+	 */
+	return( PyUnicode_DecodeUTF8(
+	         version_string,
+	         (Py_ssize_t) version_string_length,
+	         errors ) );
+}
+
+#if PY_MAJOR_VERSION >= 3
+
+/* The pyfwps module definition
+ */
+PyModuleDef pyfwps_module_definition = {
+	PyModuleDef_HEAD_INIT,
+
+	/* m_name */
+	"pyfwps",
+	/* m_doc */
+	"Python libfwps module (pyfwps).",
+	/* m_size */
+	-1,
+	/* m_methods */
+	pyfwps_module_methods,
+	/* m_reload */
+	NULL,
+	/* m_traverse */
+	NULL,
+	/* m_clear */
+	NULL,
+	/* m_free */
+	NULL,
+};
+
+#endif /* PY_MAJOR_VERSION >= 3 */
+
+/* Initializes the pyfwps module
+ */
+#if PY_MAJOR_VERSION >= 3
+PyMODINIT_FUNC PyInit_pyfwps(
+                void )
+#else
+PyMODINIT_FUNC initpyfwps(
+                void )
+#endif
+{
+	PyObject *module           = NULL;
+	PyGILState_STATE gil_state = 0;
+
+#if defined( HAVE_DEBUG_OUTPUT )
+	libfwps_notify_set_stream(
+	 stderr,
+	 NULL );
+	libfwps_notify_set_verbose(
+	 1 );
+#endif
+
+	/* Create the module
+	 * This function must be called before grabbing the GIL
+	 * otherwise the module will segfault on a version mismatch
+	 */
+#if PY_MAJOR_VERSION >= 3
+	module = PyModule_Create(
+	          &pyfwps_module_definition );
+#else
+	module = Py_InitModule3(
+	          "pyfwps",
+	          pyfwps_module_methods,
+	          "Python libfwps module (pyfwps)." );
+#endif
+	if( module == NULL )
+	{
+#if PY_MAJOR_VERSION >= 3
+		return( NULL );
+#else
+		return;
+#endif
+	}
+	PyEval_InitThreads();
+
+	gil_state = PyGILState_Ensure();
+
+	/* Setup the storage type object
+	 */
+	pyfwps_storage_type_object.tp_new = PyType_GenericNew;
+
+	if( PyType_Ready(
+	     &pyfwps_storage_type_object ) < 0 )
+	{
+		goto on_error;
+	}
+	Py_IncRef(
+	 (PyObject *) &pyfwps_storage_type_object );
+
+	PyModule_AddObject(
+	 module,
+	 "storage",
+	 (PyObject *) &pyfwps_storage_type_object );
+
+	PyGILState_Release(
+	 gil_state );
+
+#if PY_MAJOR_VERSION >= 3
+	return( module );
+#else
+	return;
+#endif
+
+on_error:
+	PyGILState_Release(
+	 gil_state );
+
+#if PY_MAJOR_VERSION >= 3
+	return( NULL );
+#else
+	return;
+#endif
+}
+
