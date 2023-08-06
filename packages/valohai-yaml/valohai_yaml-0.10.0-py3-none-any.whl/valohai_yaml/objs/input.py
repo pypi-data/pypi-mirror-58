16c28def5523d382a482d0f6345576942bdfd682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:01 +0000

    Add package valohai-yaml/0.10.0/valohai_yaml-0.10.0-py3-none-any.whl

---
+++ packages/valohai-yaml/valohai_yaml-0.10.0-py3-none-any.whl/valohai_yaml/objs/input.py
@@ -0,0 +1,17 @@
+from .base import Item
+
+
+class Input(Item):
+
+    def __init__(
+        self,
+        *,
+        name,
+        default=None,
+        optional=False,
+        description=None
+    ) -> None:
+        self.name = name
+        self.default = default  # may be None, a string or a list of strings
+        self.optional = bool(optional)
+        self.description = description
