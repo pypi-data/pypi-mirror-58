88b9d4215d986f523a3679040e34eb537880180e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:24 +0000

    Add package bwpy/0.1.6/bwpy-0.1.6-py3-none-any.whl

---
+++ packages/bwpy/bwpy-0.1.6-py3-none-any.whl/bwpy/bwpy.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import json
+from sh import bw
+from pathlib import Path
+from .bitwarden.item import BitwardenItem
+from .bitwarden.collection import BitwardenCollection
+from .version import version
+
+
+def app_version():
+    return version
+
+
+def pull(args):
+    bw.sync()
+
+    collection = BitwardenCollection(
+        org_name=args.org, collection_name=args.collection,
+    )
+
+    print(collection.json(filter=args.item))
+
+
+def push(args):
+    item = BitwardenItem(
+        org_name=args.org, collection_name=args.collection, item_name=args.item
+    )
+
+    json_data = json.loads(args.json)
+
+    bw.sync()
+
+    if args.force:
+        result = item.upsert(json_data)
+    else:
+        result = item.insert(json_data)
+
+    if not args.silent:
+        print(f"successfully pushed item: {args.item} ({result['id']})")
+
+    bw.sync()
