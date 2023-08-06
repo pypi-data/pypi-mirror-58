92c5b5dd6e7937b0745e2a04c46169bc3dc86d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package fastly/0.3.1/fastly-0.3.1-py3-none-any.whl

---
+++ packages/fastly/fastly-0.3.1-py3-none-any.whl/fastly/auth.py
@@ -0,0 +1,23 @@
+"""
+"""
+from __future__ import absolute_import
+
+from six.moves.urllib.parse import urlencode
+
+
+class KeyAuthenticator(object):
+    def __init__(self, key):
+        self.key = key
+
+    def add_auth(self, headers):
+        headers['Fastly-Key'] = self.key
+
+
+class SessionAuthenticator(object):
+    def __init__(self, conn, login, password):
+        body = urlencode({'user': login, 'password': password})
+        resp, data = conn.request('POST', '/login', body)
+        self.session_key = resp.getheader('Set-Cookie')
+
+    def add_auth(self, headers):
+        headers['Cookie'] = self.session_key
