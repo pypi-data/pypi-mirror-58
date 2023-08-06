fb9db316a67a045d4009632b7d69e9a026e1f411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:16 +0000

    Add package cmdline-commentdater/0.1.0/cmdline_commentdater-0.1.0-py3.6.egg

---
+++ packages/cmdline-commentdater/cmdline_commentdater-0.1.0-py3.6.egg/commentdater/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 15:03:28 2019
+
+@author: lavanyasingh
+"""
+
+"""commentdater.__main__: executed when commentdater directory is called as script."""
+
+from .src import main
+main()
