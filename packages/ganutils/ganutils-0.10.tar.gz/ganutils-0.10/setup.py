cfb6958d671b3e8902ba278e6baedd52448e67d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:20 +0000

    Add package ganutils/0.10/ganutils-0.10.tar.gz

---
+++ packages/ganutils/ganutils-0.10.tar.gz/ganutils-0.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'ganutils',
+  packages = find_packages(),
+  version = '0.10',
+  license='MIT',
+  description = 'This is an installable python package for scripts I wrote for myself.',
+  author = 'Gan Tu',
+  author_email = 'tugan0329@gmail.com',
+  url = 'https://github.com/Michael-Tu/ganutils',
+  keywords = ['tugan', 'utils', 'tools'],
+  install_requires=[
+      'torch',
+      'numpy',
+      'sklearn',
+      'tqdm',
+      'requests'
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
