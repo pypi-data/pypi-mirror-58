671da52647034020f6e768035029dad04c08f5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:09 +0000

    Add package xiaobaiinstaller/1.0/xiaobaiinstaller-1.0-py3-none-any.whl

---
+++ packages/xiaobaiinstaller/xiaobaiinstaller-1.0-py3-none-any.whl/xiaobaiinstaller/xiaobaiinstaller.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding=utf-8 -*-
+"""
+    author:     Tser
+    time:       2019/12/30 17:50
+    project:    xiaobai
+    file:       installer.py
+"""
+from xiaobaiInstaller import main as xiaobai_main
+
+def main():
+    xiaobai_main()
+if __name__ == '__main__':
+    main()
