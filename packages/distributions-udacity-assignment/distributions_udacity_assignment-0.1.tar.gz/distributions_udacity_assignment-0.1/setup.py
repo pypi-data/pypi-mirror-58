ef73bf7e870dd22faae7f5a0a1ef418e415d6b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package distributions-udacity-assignment/0.1/distributions_udacity_assignment-0.1.tar.gz

---
+++ packages/distributions-udacity-assignment/distributions_udacity_assignment-0.1.tar.gz/distributions_udacity_assignment-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_assignment',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_assignment'],
+      zip_safe=False)
