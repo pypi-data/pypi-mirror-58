052305882e90f45bd612dd421d052b50c0e5a975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:35 +0000

    Add package djangorestframework-emailuser/0.2.0.dev0/djangorestframework_emailuser-0.2.0.dev0-py3-none-any.whl

---
+++ packages/djangorestframework-emailuser/djangorestframework_emailuser-0.2.0.dev0-py3-none-any.whl/emailuser/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('users/register/', views.RegistrationView.as_view(), name='register'),
+    path('users/<int:pk>', views.RetrieveUpdateUserView.as_view(), name='retrieve_update'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
