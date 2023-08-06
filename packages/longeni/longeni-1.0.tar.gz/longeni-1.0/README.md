fb8863e56c5b5cd9c5ab3f18a8b96a8e3381214d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package longeni/1.0/longeni-1.0.tar.gz

---
+++ packages/longeni/longeni-1.0.tar.gz/longeni-1.0/README.md
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+
+<h2>Web Robot</h2>
+
+<h1></h1>
+
+
+<p>You can use this framework to create digital robots for webApps.</p>
+
+<h1></h1>
+
+
+<p>You can contact: david.johnnes@gmail.com  'for support'.</p>
+
+<h1></h1>
+
+
---
+++ packages/longeni/longeni-1.0.tar.gz/longeni-1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='longeni',
+    version='1.0',
+    url="https://johnnes-smarts.ch",
+    license='License :: OSI Approved :: MIT License',
+    author='David Johnnes',
+    author_email='david.johnnes@gmail.com',
+    description='Framework for web automation and testing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='DevOps, Audit Automation, Audit Orchestration, Vendor-Neutral configurations Audit',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
