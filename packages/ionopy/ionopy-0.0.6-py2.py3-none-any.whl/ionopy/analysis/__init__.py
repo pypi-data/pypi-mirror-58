bdc4f1f59349246d8af3f50b5ec9376261f63a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:36 +0000

    Add package ionopy/0.0.6/ionopy-0.0.6-py2.py3-none-any.whl

---
+++ packages/ionopy/ionopy-0.0.6-py2.py3-none-any.whl/ionopy/analysis/__init__.py
@@ -0,0 +1,10 @@
+# let init-time option registration happen
+#from ionopy.process import *
+#from ionopy.analysis import *
+#import analysis
+#from .analysis import *
+from ionopy.analysis import analysis
+#from pandas.core.api import *
+#from ._version import get_versions
+
+
