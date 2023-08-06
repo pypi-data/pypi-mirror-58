eb70d30ee98c6ddabfeb4db67897ce9422b4bc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:09 +0000

    Add package keepercommander/4.24/keepercommander-4.24.tar.gz

---
+++ packages/keepercommander/keepercommander-4.24.tar.gz/keepercommander-4.24/keepercommander/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Commander
+# Copyright 2019 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+
+__version__ = '4.24'
