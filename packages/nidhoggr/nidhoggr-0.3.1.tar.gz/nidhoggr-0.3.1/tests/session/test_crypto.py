c42a1ce423adc4c306ffe8de8f9e201e6108a755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:37 +0000

    Add package nidhoggr/0.3.1/nidhoggr-0.3.1.tar.gz

---
+++ packages/nidhoggr/nidhoggr-0.3.1.tar.gz/nidhoggr-0.3.1/tests/session/test_crypto.py
@@ -0,0 +1,12 @@
+from nidhoggr.bl import BLConfig
+from nidhoggr.models.session import UserProperty, SignedUserProperty
+from nidhoggr.utils.crypto import sign_property, verify_property
+
+
+def test_crypto(config: BLConfig):
+    unsigned_prop = UserProperty(name="twilight", value="sparkle")
+    signed_prop = sign_property(private_key=config.key_pair.private, prop=unsigned_prop)
+    assert signed_prop is not None
+    assert isinstance(signed_prop, SignedUserProperty)
+    assert isinstance(signed_prop.signature, str)
+    assert verify_property(public_key=config.key_pair.public, prop=signed_prop)
