daf6f6be2c9d423eb5659fba0c06ff0d3f260516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:25 +0000

    Add package drf-writable-nested/0.5.4/drf-writable-nested-0.5.4.tar.gz

---
+++ packages/drf-writable-nested/drf-writable-nested-0.5.4.tar.gz/drf-writable-nested-0.5.4/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+from io import open
+
+from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('drf_writable_nested')
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='drf-writable-nested',
+    version=version,
+    url='http://github.com/beda-software/drf-writable-nested',
+    license='BSD',
+    description=(
+        'Writable nested helpers for django-rest-framework\'s serializers'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=('drf restframework rest_framework django_rest_framework'
+              ' serializers drf_writable_nested'),
+    author='beda.software',
+    author_email='drfwritablenested@beda.software',
+    packages=['drf_writable_nested'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
