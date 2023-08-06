e091c592d3b2f8c61a39536ef89d28004351e4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:20 +0000

    Add package eveliver/1.2.0/eveliver-1.2.0.tar.gz

---
+++ packages/eveliver/eveliver-1.2.0.tar.gz/eveliver-1.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    # To upload this package to pypi:
+    # python setup.py sdist
+    # python -m twine upload dist/*
+    setup(
+        name='eveliver',
+        version='1.2.0',
+        description='Some pytorch utilities for NLP',
+        author='Jiaju Du',
+        author_email='i@dujiaju.me',
+        url='https://github.com/jiajudu/evelivers',
+        packages=['eveliver'],
+        install_requires=[
+            'torch>=1.2.0',
+            'numpy>=1.16.4'
+        ]
+    )
