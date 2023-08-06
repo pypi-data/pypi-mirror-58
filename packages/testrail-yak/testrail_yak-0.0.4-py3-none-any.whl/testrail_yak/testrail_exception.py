2625c11991c300973d01ee858a010feb79f1ebef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:35 +0000

    Add package testrail-yak/0.0.4/testrail_yak-0.0.4-py3-none-any.whl

---
+++ packages/testrail-yak/testrail_yak-0.0.4-py3-none-any.whl/testrail_yak/testrail_exception.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .testrail import APIError
+
+
+# TestRail
+class TestRailException(APIError):
+    pass
+
+
+class TestRailUserException(TestRailException):
+    pass
+
+
+class TestRailProjectException(TestRailException):
+    pass
+
+
+class TestRailSectionException(TestRailException):
+    pass
+
+
+class TestRailTestSuiteException(TestRailException):
+    pass
+
+
+class TestRailTestCaseException(TestRailException):
+    pass
+
+
+class TestRailTestRunException(TestRailException):
+    pass
+
+
+class TestRailTestPlanException(TestRailException):
+    pass
+
+
+class TestRailTestException(TestRailException):
+    pass
+
+
+class TestRailNewEntityException(TestRailException):
+    pass
+
+
+class TestRailUpdateException(TestRailException):
+    pass
+
+
+class TestRailDeleteException(TestRailException):
+    pass
+
+
+class TestRailSuiteModeException(TestRailException):
+    pass
