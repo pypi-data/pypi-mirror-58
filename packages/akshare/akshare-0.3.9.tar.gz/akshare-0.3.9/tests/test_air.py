8ced8f2dd6f7f1fabf7fdee5dc153a237a801f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:39 +0000

    Add package akshare/0.3.9/akshare-0.3.9.tar.gz

---
+++ packages/akshare/akshare-0.3.9.tar.gz/akshare-0.3.9/tests/test_air.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2019/12/12 18:16
+contact: jindaxiang@163.com
+desc: 测试文件
+"""
+from akshare.air.aqi_study import air_hourly
+from akshare.index.index_weibo import weibo_index
+
+
+def test_air_hourly():
+    df_hourly = air_hourly('成都', '2019-12-10')
+    assert len(df_hourly) >= 10
+
+
+def test_weibo_index():
+    weibo_index_df = weibo_index(word="python", time_type="3month")
+    assert len(weibo_index_df) >= 10
+
+
+if __name__ == "__main__":
+    test_air_hourly()
+    test_weibo_index()
