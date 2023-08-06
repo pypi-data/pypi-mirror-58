7d098e09322d1e3f3c75d4dca3606f7feab8573c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:06 +0000

    Add package solstice/0.0.22/solstice-0.0.22.tar.gz

---
+++ packages/solstice/solstice-0.0.22.tar.gz/solstice-0.0.22/solstice/core/project.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains implementation for Solstice Artella project
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Tomas Poveda"
+__license__ = "MIT"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpovedatd@gmail.com"
+
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib2 import quote
+
+from artellapipe.core import project as artella_project
+
+from solstice.core import asset, node
+
+
+class Solstice(artella_project.ArtellaProject, object):
+
+    ASSET_CLASS = asset.SolsticeAsset
+    ASSET_NODE_CLASS = node.SolsticeAssetNode
+
+    def __init__(self):
+        super(Solstice, self).__init__(name='Solstice')
