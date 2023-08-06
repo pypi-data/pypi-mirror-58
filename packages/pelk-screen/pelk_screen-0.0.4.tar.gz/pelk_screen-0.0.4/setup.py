3555acbeb85a6869dabee8ba202ba5bfc4cfc7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:59 +0000

    Add package pelk-screen/0.0.4/pelk_screen-0.0.4.tar.gz

---
+++ packages/pelk-screen/pelk_screen-0.0.4.tar.gz/pelk_screen-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pelk_screen",
+    version="0.0.4",
+    author="PerfectELK",
+    author_email="perfect-elk@perfect-elk.ru",
+    description="Screenshot lib",
+    long_description=long_description,
+    url="https://github.com/PerfectELK/pelk_screen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
