e26d334eaf7fc36a4c34154f21b71b23f0806c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:56 +0000

    Add package test-jx/0.0.2/test_jx-0.0.2.tar.gz

---
+++ packages/test-jx/test_jx-0.0.2.tar.gz/test_jx-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 15:24:56 2019
+
+@author: lvjingzhe
+"""
+
+from setuptools import setup
+
+
+setup(
+      name="test_jx",
+      version="0.0.2",
+      packages=["test_jx"],
+      author="Xuan Ji",
+      url="https://github.com/XuanLyu/SLE",
+      author_email="jixuan_1021@163.com",
+      description="used to describe uncertainty in financial data",
+      license='MIT',
+      python_requires='>=3.6',
+      )
