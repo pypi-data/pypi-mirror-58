cd13a6126e564d4c8aedd59a9ef9d39bae3757d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:46 +0000

    Add package bcml/2.7.0.post1/bcml-2.7.0.post1.tar.gz

---
+++ packages/bcml/bcml-2.7.0.post1.tar.gz/bcml-2.7.0.post1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from pathlib import Path
+
+with open("docs/README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name='bcml',
+    version='2.7.0.post1',
+    author='NiceneNerd',
+    author_email='macadamiadaze@gmail.com',
+    description='A mod manager for The Legend of Zelda: Breath of the Wild on Cemu',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/NiceneNerd/BCML',
+    include_package_data=True,
+    packages=['bcml'],
+    entry_points={
+        'gui_scripts': [
+            'bcml = bcml.__init__:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'aamp>=1.4.0',
+        'byml>=2.3.0.post1',
+        'syaz0>=1.0.0rc4',
+        'PySide2>=5.13.0',
+        'pyYaml>=5.1.1',
+        'sarc>=2.0.1',
+        'rstb>=1.1.2',
+        'xxhash>=1.3.0'
+    ]
+)
