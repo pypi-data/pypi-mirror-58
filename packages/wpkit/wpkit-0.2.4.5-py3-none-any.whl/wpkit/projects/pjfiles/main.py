2c3ce2ffe47062405d667e69bda6e78917c5e868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:33 +0000

    Add package wpkit/0.2.4.5/wpkit-0.2.4.5-py3-none-any.whl

---
+++ packages/wpkit/wpkit-0.2.4.5-py3-none-any.whl/wpkit/projects/pjfiles/main.py
@@ -0,0 +1,23 @@
+# from wpkit.services.CloudOS import start_server
+from wpkit.linux import clean_port,get_local_ip
+# port1=80
+# port2=8002
+# clean_port(port1)
+# clean_port(port2)
+# start_server(__name__,host=get_local_ip() or "127.0.0.1",port1=port1,port2=port2)
+from wpkit.web.applications.demo import demo,DemoApp,LocalFSServer
+
+def main():
+    app=DemoApp(__name__)
+    app.sitemap['Download']='http://%s:%s'%(get_local_ip(),8001)
+    # app.register_blueprint(LocalFSServer(nickname="ManageDownloads",url_prefix="/manage_download",path="/var/www/html"))
+    app.run(host=get_local_ip(),port=80)
+def test():
+    app=DemoApp(__name__)
+    app.sitemap['Download']='http://%s:%s'%(get_local_ip(),8001)
+    app.register_blueprint(LocalFSServer(nickname="ManageDownloads",url_prefix="/manage_download",path="D:/"))
+    app.run(host=get_local_ip(),port=80)
+if __name__ == '__main__':
+    main()
+    # test()
+    # demo(host=get_local_ip(),port=80,import_name=__name__)
