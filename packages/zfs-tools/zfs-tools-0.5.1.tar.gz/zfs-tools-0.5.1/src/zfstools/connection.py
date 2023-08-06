c34895a796c7d27e2b681c3c4377446148d15523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:04 +0000

    Add package zfs-tools/0.5.1/zfs-tools-0.5.1.tar.gz

---
+++ packages/zfs-tools/zfs-tools-0.5.1.tar.gz/zfs-tools-0.5.1/src/zfstools/connection.py
@@ -0,0 +1,145 @@
+'''
+ZFS connection classes
+'''
+
+import subprocess
+import os
+from zfstools.models import PoolSet
+from zfstools.util import progressbar, SpecialPopen
+try:
+    from Queue import Queue
+except ImportError:
+    from multiprocessing import Queue
+from threading import Thread
+
+
+# Work-around for check_output not existing on Python 2.6, as per
+# http://stackoverflow.com/questions/4814970/subprocess-check-output-doesnt-seem-to-exist-python-2-6-5
+# The implementation is lifted from
+# http://hg.python.org/cpython/file/d37f963394aa/Lib/subprocess.py#l544
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd) # , output=output)
+        return output
+    subprocess.check_output = f
+
+class ZFSConnection:
+    host = None
+    _poolset = None
+    _dirty = True
+    _trust = False
+    _properties = None
+    def __init__(self,host="localhost", trust=False, sshcipher=None, properties=None, identityfile=None, knownhostsfile=None):
+        self.host = host
+        self._trust = trust
+        self._properties = properties if properties else []
+        self._poolset= PoolSet()
+        if host in ['localhost','127.0.0.1']:
+            self.command = ["zfs"]
+        else:
+            self.command = ["ssh","-o","BatchMode yes","-a","-x"]
+            if self._trust:
+                self.command.extend(["-o","CheckHostIP no"])
+                self.command.extend(["-o","StrictHostKeyChecking no"])
+            if sshcipher != None:
+                self.command.extend(["-c",sshcipher])
+            if identityfile != None:
+                self.command.extend(["-i",identityfile])
+            if knownhostsfile != None:
+                self.command.extend(["-o","UserKnownHostsFile %s" % knownhostsfile])
+            self.command.extend([self.host,"zfs"])
+
+    def _get_poolset(self):
+        if self._dirty:
+            properties = [ 'creation' ] + self._properties
+            stdout2 = subprocess.check_output(self.command + ["list", "-Hpr", "-o", ",".join( ['name'] + properties ), "-t", "all"])
+            self._poolset.parse_zfs_r_output(stdout2,properties)
+            self._dirty = False
+        return self._poolset
+    pools = property(_get_poolset)
+
+    def create_dataset(self,name):
+        subprocess.check_call(self.command + ["create", name])
+        self._dirty = True
+        return self.pools.lookup(name)
+
+    def destroy_dataset(self, name):
+        subprocess.check_call(self.command + ["destroy", name])
+        self._dirty = True
+
+    def destroy_recursively(self, name):
+        subprocess.check_call(self.command + ["destroy", '-r', name])
+        self._dirty = True
+
+    def snapshot_recursively(self,name,snapshotname,properties={}):
+        plist = sum( map( lambda x: ['-o', '%s=%s' % x ], properties.items() ), [] )
+        subprocess.check_call(self.command + ["snapshot", "-r" ] + plist + [ "%s@%s" % (name, snapshotname)])
+        self._dirty = True
+    
+    def send(self,name,opts=None,bufsize=-1,compression=False):
+        if not opts: opts = []
+        cmd = list(self.command)
+        if compression and cmd[0] == 'ssh': cmd.insert(1,"-C")
+        cmd = cmd + ["send"] + opts + [name]
+        p = SpecialPopen(cmd,stdin=open(os.devnull),stdout=subprocess.PIPE,bufsize=bufsize)
+        return p
+
+    def receive(self,name,pipe,opts=None,bufsize=-1,compression=False):
+        if not opts: opts = []
+        cmd = list(self.command)
+        if compression and cmd[0] == 'ssh': cmd.insert(1,"-C")
+        cmd = cmd + ["receive"] + opts + [name]
+        p = SpecialPopen(cmd,stdin=pipe,bufsize=bufsize)
+        return p
+
+    def transfer(self, dst_conn, s, d, fromsnapshot=None, showprogress=False, bufsize=-1, send_opts=None, receive_opts=None, ratelimit=-1, compression=False):
+        if send_opts is None: send_opts = []
+        if receive_opts is None: receive_opts = []
+        
+        queue_of_killables = Queue()
+
+        if fromsnapshot: fromsnapshot=["-i",fromsnapshot]
+        else: fromsnapshot = []
+        sndprg = self.send(s, opts=[] + fromsnapshot + send_opts, bufsize=bufsize, compression=compression)
+        sndprg_supervisor = Thread(target=lambda: queue_of_killables.put((sndprg, sndprg.wait())))
+        sndprg_supervisor.start()
+
+        if showprogress:
+            try:
+                        barprg = progressbar(pipe=sndprg.stdout,bufsize=bufsize,ratelimit=ratelimit)
+                        barprg_supervisor = Thread(target=lambda: queue_of_killables.put((barprg, barprg.wait())))
+                        barprg_supervisor.start()
+                        sndprg.stdout.close()
+            except OSError:
+                        os.kill(sndprg.pid,15)
+                        raise
+        else:
+            barprg = sndprg
+
+        try:
+                        rcvprg = dst_conn.receive(d,pipe=barprg.stdout,opts=["-Fu"]+receive_opts,bufsize=bufsize,compression=compression)
+                        rcvprg_supervisor = Thread(target=lambda: queue_of_killables.put((rcvprg, rcvprg.wait())))
+                        rcvprg_supervisor.start()
+                        barprg.stdout.close()
+        except OSError:
+                os.kill(sndprg.pid, 15)
+                if sndprg.pid != barprg.pid: os.kill(barprg.pid, 15)
+                raise
+
+        dst_conn._dirty = True
+        allprocesses = set([rcvprg, sndprg]) | ( set([barprg]) if showprogress else set() )
+        while allprocesses:
+            diedprocess, retcode = queue_of_killables.get()
+            allprocesses = allprocesses - set([diedprocess])
+            if retcode != 0:
+                [ p.kill() for p in allprocesses ]
+                raise subprocess.CalledProcessError(retcode, diedprocess._saved_args)
---
+++ packages/zfs-tools/zfs-tools-0.5.1.tar.gz/zfs-tools-0.5.1/src/zfstools/util.py
@@ -0,0 +1,111 @@
+'''
+Miscellaneous utility functions
+'''
+
+import sys
+import os
+import subprocess
+
+def simplify(x):
+    '''Take a list of tuples where each tuple is in form [v1,v2,...vn]
+    and then coalesce all tuples tx and ty where tx[v1] equals ty[v2],
+    preserving v3...vn of tx and discarding v3...vn of ty.
+
+    m = [
+    (1,2,"one"),
+    (2,3,"two"),
+    (3,4,"three"),
+    (8,9,"three"),
+    (4,5,"four"),
+    (6,8,"blah"),
+    ]
+    simplify(x) -> [[1, 5, 'one'], [6, 9, 'blah']]
+    '''
+    y = list(x)
+    if len(x) < 2: return y
+    for idx,o in enumerate(list(y)):
+        for idx2,p in enumerate(list(y)):
+            if idx == idx2: continue
+            if o and p and o[0] == p[1]:
+                y[idx] = None
+                y[idx2] = list(p)
+                y[idx2][0] = p[0]
+                y[idx2][1] = o[1]
+    return [ n for n in y if n is not None ]
+
+def uniq(seq, idfun=None):
+    '''Makes a sequence 'unique' in the style of UNIX command uniq'''
+    # order preserving
+    if idfun is None:
+        def idfun(x): return x
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        # in old Python versions:
+        # if seen.has_key(marker)
+        # but in new ones:
+        if marker in seen: continue
+        seen[marker] = 1
+        result.append(item)
+    return result
+
+
+class SpecialPopen(subprocess.Popen):
+    def __init__(self, *a, **kw):
+        self._saved_args = a[0] if kw.get("args") is None else kw.get("args")
+        subprocess.Popen.__init__(self, *a, **kw)
+
+
+def progressbar(pipe, bufsize=-1, ratelimit=-1):
+
+    def clpbar(cmdname):
+        barargs = []
+        if bufsize != -1:
+            barargs = ["-bs", str(bufsize)]
+        if ratelimit != -1:
+            barargs = barargs + ['-th', str(ratelimit)]
+        barprg = SpecialPopen(
+            [cmdname, "-dan"] + barargs,
+            stdin=pipe, stdout=subprocess.PIPE, bufsize=bufsize)
+        return barprg
+
+    def pv(cmdname):
+        barargs = []
+        if bufsize != -1:
+            barargs = ["-B", str(bufsize)]
+        if ratelimit != -1:
+            barargs = barargs + ['-L', str(ratelimit)]
+        barprg = SpecialPopen(
+            [cmdname, "-ptrb"] + barargs,
+            stdin=pipe, stdout=subprocess.PIPE, bufsize=bufsize)
+        return barprg
+
+    barprograms = [
+        ("bar", clpbar),
+        ("clpbar", clpbar),
+        ("pv", pv),
+    ]
+
+    for name, func in barprograms:
+        try:
+            subprocess.call([name, '-h'], stdout=open(os.devnull, "w"), stderr=open(os.devnull, "w"), stdin=open(os.devnull, "r"))
+        except OSError as e:
+            if e.errno == 2: continue
+            assert 0, "not reached while searching for clpbar or pv"
+        return func(name)
+    raise OSError(2, "no such file or directory searching for clpbar or pv")
+
+def stderr(text):
+    """print out something to standard error, followed by an ENTER"""
+    sys.stderr.write(text)
+    sys.stderr.write("\n")
+
+__verbose = False
+def verbose_stderr(*args, **kwargs):
+    global __verbose
+    if __verbose: stderr(*args, **kwargs)
+
+def set_verbose(boolean):
+    global __verbose
+    __verbose = boolean
