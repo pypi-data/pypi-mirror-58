552425e9368ee4cc1e09738b8106675cab39d3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:49 +0000

    Add package django-simple-metatags/1.0.1/django_simple_metatags-1.0.1-py3-none-any.whl

---
+++ packages/django-simple-metatags/django_simple_metatags-1.0.1-py3-none-any.whl/metatags/admin.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericStackedInline
+
+from .forms import InlineMetaTagForm, MetaTagForm
+from .models import MetaTag
+
+
+class MetaTagInlineMeta(forms.MediaDefiningClass):
+
+    def __new__(mcs, name, bases, attrs):
+        if 'modeltranslation' in settings.INSTALLED_APPS:
+            from modeltranslation.admin import TranslationGenericStackedInline
+            bases = (TranslationGenericStackedInline,)
+        return super(MetaTagInlineMeta, mcs).__new__(mcs, name, bases, attrs)
+
+
+class MetaTagAdminMeta(forms.MediaDefiningClass):
+
+    def __new__(mcs, name, bases, attrs):
+        if 'modeltranslation' in settings.INSTALLED_APPS:
+            from modeltranslation.admin import TranslationAdmin
+            bases = (TranslationAdmin,)
+        return super(MetaTagAdminMeta, mcs).__new__(mcs, name, bases, attrs)
+
+
+class MetaTagInline(GenericStackedInline, metaclass=MetaTagInlineMeta):
+    model = MetaTag
+    extra = 1
+    max_num = 1
+    can_delete = False
+    form = InlineMetaTagForm
+    template = 'metatags/admin/edit_inline/stacked.html'
+
+
+@admin.register(MetaTag)
+class MetaTagAdmin(admin.ModelAdmin, metaclass=MetaTagAdminMeta):
+    form = MetaTagForm
+    list_display = ('url',)
+    search_fields = ('url', 'title', 'keywords', 'description')
+
+    def get_queryset(self, request):
+        return super(MetaTagAdmin, self).get_queryset(request).filter(
+            content_type__isnull=True,
+            object_id__isnull=True,
+        )
