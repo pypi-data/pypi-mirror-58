a396f6c5a02ffaf49c0d323fc209c3753c776cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:18 +0000

    Add package django-nplusone/0.0.5/django_nplusone-0.0.5.tar.gz

---
+++ packages/django-nplusone/django_nplusone-0.0.5.tar.gz/django_nplusone-0.0.5/nplusone/__init__.py
@@ -0,0 +1,1 @@
+from .base import show_nplusones
---
+++ packages/django-nplusone/django_nplusone-0.0.5.tar.gz/django_nplusone-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_nplusone",
+    version="0.0.5",
+    author="Shafiq Ur Rahman",
+    author_email="shafiq.tnoli@gmail.com",
+    description="Discover possible N+1 queries in your code base during development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shaffooo/django_nplusone",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        "Programming Language :: Python :: 2",
+    ],
+    install_requires=[
+          'Django>=1.11.20',
+    ],
+    python_requires='>=2.7',
+)
