be9418ad148d47036e6193f941e43cf5ab0cd2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:58 +0000

    Add package testpoetrypypi/0.1.11/testpoetrypypi-0.1.11.tar.gz

---
+++ packages/testpoetrypypi/testpoetrypypi-0.1.11.tar.gz/testpoetrypypi-0.1.11/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testpoetrypypi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry-version>=0.1.5,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['runtest = runtest:main',
+                     'testpoetrypypi = testpoetrypypi:main']}
+
+setup_kwargs = {
+    'name': 'testpoetrypypi',
+    'version': '0.1.11',
+    'description': '',
+    'long_description': '# testpoetrypypi\nA reference-ish implementation of a Python project that uses Poetry (and pyproject.toml), CircleCI, Git Flow (i.e. `master` + `develop` + feature branches) and semantic-release to compute the next version and publish to PyPI upon merging `develop` into `master`.\n\n\n## Some useful Poetry commands for me to remember:\n`poetry install`\n\n`poetry run pytest`\n\n`poetry run testpoetrypypi`\n\n`poetry run semantic-release version --noop`\n',
+    'author': 'Jeff',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
