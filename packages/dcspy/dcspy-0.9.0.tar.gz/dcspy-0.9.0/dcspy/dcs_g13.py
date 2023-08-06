3cf7bd858d3ed657158453fb596d58cf6c8e42c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package dcspy/0.9.0/dcspy-0.9.0.tar.gz

---
+++ packages/dcspy/dcspy-0.9.0.tar.gz/dcspy-0.9.0/dcspy/dcs_g13.py
@@ -0,0 +1,88 @@
+import socket
+from logging import basicConfig, DEBUG, info, debug, warning, error
+from time import sleep
+
+from packaging import version
+from requests import get
+
+from dcspy.dcsbios import ProtocolParser
+from dcspy.logitech import G13
+
+__version__ = '0.9.0'
+basicConfig(format='%(asctime)s | %(levelname)-7s | %(message)s / %(filename)s:%(lineno)d', level=DEBUG)
+
+
+def attempt_connect(sock: socket.socket) -> None:
+    """
+    Attempt to connect to localhost.
+
+    :param sock: socket
+    """
+    connected = False
+    info('Waiting for DCS connection...')
+    while not connected:
+        try:
+            sock.connect(('127.0.0.1', 7778))
+            info('Connected')
+            connected = True
+        except socket.error:
+            sleep(2)
+
+
+def check_current_version() -> None:
+    """Check if version is current."""
+    try:
+        url = 'https://api.github.com/repos/emcek/dcspy/releases'
+        response = get(url)
+        if response.status_code == 200:
+            json_response = response.json()
+            online_version = json_response['tag_name']
+            if version.parse(online_version) > version.parse(__version__):
+                info(f'There is new version of dcspy: {online_version}')
+            elif version.parse(online_version) == version.parse(__version__):
+                info('This is up-to-date version')
+            else:
+                debug(f'Something goes wrong: local version: {__version__} a online_version: {online_version}')
+        else:
+            warning(f'Unable to check version online. Try again later. Status={response.status_code}')
+    except Exception as e:
+        warning(f'Unable to check version online: {e}')
+
+
+def run() -> None:
+    """Main of running function."""
+    info(f'dcspy {__version__} https://github.com/emcek/dcspy')
+    check_current_version()
+    while True:
+        parser = ProtocolParser()
+        g13 = G13(parser)
+        g13.info_display(('G13 initialised OK', 'Waiting for DCS', '', f'dcspy: {__version__}'))
+
+        sock = socket.socket()
+        sock.settimeout(None)
+
+        attempt_connect(sock)
+        while True:
+            try:
+                c = sock.recv(1)
+                parser.process_byte(c)
+                if g13.shouldActivateNewAC:
+                    g13.activate_new_ac()
+                g13.button_handle(sock)
+            except socket.error as exp:
+                debug(f'Main loop socket error: {exp}')
+                sleep(2)
+            except KeyboardInterrupt:
+                info('Exit due to Ctrl-C')
+                exit(0)
+            except Exception as exp:
+                error(f'Unexpected error: resetting... {exp.__class__.__name__}', exc_info=True)
+                sleep(2)
+                break
+        del sock
+        del g13
+        del parser
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/dcspy/dcspy-0.9.0.tar.gz/dcspy-0.9.0/setup.py
@@ -0,0 +1,46 @@
+import io
+from os.path import abspath, dirname, join
+
+from setuptools import setup, find_packages
+
+from dcspy.dcs_g13 import __version__
+
+here = abspath(dirname(__file__))
+
+with io.open(join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with io.open(join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.read().splitlines()
+
+setup(name='dcspy',  # Required
+      version=__version__,  # Required
+      description='Software for integrating DCS: F/A-18C, F-16C and Ka-50 with Logitech G13',  # Required
+      long_description=long_description,  # Optional
+      long_description_content_type='text/markdown',  # Optional (see note above)
+      url='https://github.com/emcek/dcspy',  # Optional
+      author='Michal Plichta',  # Optional
+      license='MIT License',
+      entry_points={'console_scripts': ['dcs_g13 = dcspy.dcs_g13:run']},
+      data_files=[('dcspy_data', ['images/dcspy.ico'])],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: Microsoft :: Windows :: Windows 10',
+                   'Topic :: Games/Entertainment',
+                   'Topic :: Games/Entertainment :: Simulation',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: System :: Hardware',
+                   'Topic :: Utilities'],
+      keywords='logitech logitech-sdk logitech-keyboards logitech-gaming logitech-gaming-keyboard dcs-world dcs g13',
+      # packages=find_packages(exclude=['tests']),  # Required
+      packages=find_packages(),  # Required
+      install_requires=requires,  # Optional
+      platforms=['win32', 'nt', 'Windows'],
+      # extras_require={'testing': ['pytest']},
+      project_urls={'Bug Reports': 'https://github.com/emcek/dcspy/issues',
+                    'Source': 'https://github.com/emcek/dcspy'})
