1f5dc03ac616762571d4b52c23e3dbf0c26adf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:31 +0000

    Add package collective.z3cform.datagridfield/1.5.2/collective.z3cform.datagridfield-1.5.2.tar.gz

---
+++ packages/collective.z3cform.datagridfield/collective.z3cform.datagridfield-1.5.2.tar.gz/collective.z3cform.datagridfield-1.5.2/src/collective/z3cform/datagridfield/static/init_field.js
@@ -0,0 +1,8 @@
+require([
+    'jquery',
+    '++resource++collective.z3cform.datagridfield/datagridfield',
+], function ($) {
+    $(document).ready(function() {
+        window.dataGridField2Functions.init();
+    });
+})
