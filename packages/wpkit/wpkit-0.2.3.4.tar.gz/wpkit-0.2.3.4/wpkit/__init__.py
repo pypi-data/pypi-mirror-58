050078b5d1fd36c39179a4c079298196b53dd903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:20 +0000

    Add package wpkit/0.2.3.4/wpkit-0.2.3.4.tar.gz

---
+++ packages/wpkit/wpkit-0.2.3.4.tar.gz/wpkit-0.2.3.4/wpkit/__init__.py
@@ -0,0 +1,6 @@
+import logging
+try:
+    import jinja2,flask,gitpython
+except:
+    logging.warning('This package requires jinja2,flask,gitpython as dependencies. you need to install them!')
+from . import heart,pjtools,gen_scripts,linux,piu,web,data,examples,basic
