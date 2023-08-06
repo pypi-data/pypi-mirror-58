59d0b58a1e46162a7388bd36ffd0664d4814562a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:42 +0000

    Add package ogpparser/1.0.2/ogpparser-1.0.2.tar.gz

---
+++ packages/ogpparser/ogpparser-1.0.2.tar.gz/ogpparser-1.0.2/ogpparser/ogpparser.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+def ogpparser(url):
+    '''
+    url : input URL to parse
+    '''
+    response = requests.get(url)
+    response.encoding = response.apparent_encoding
+ 
+    bs = BeautifulSoup(response.text, 'html.parser')
+    
+    result = {}
+    for header in bs.find_all('head'):
+        meta_list = header.find_all('meta')
+        for meta in meta_list:
+            if meta.has_attr('property'):
+                if meta['property'][:3]=='og:':
+                    tag = meta['property'].strip('og:')
+                    content = meta['content'].strip()
+                    if tag not in result.keys():
+                        result[tag] = content
+                    else:
+                        if type(result[tag])==str:
+                            result[tag] = [result[tag]]
+                            result[tag].append(content)
+                        else:
+                            result[tag].append(content)
+            elif meta.has_attr('name'):
+                if meta['name'][:3]=='og:':
+                    tag = meta['name'].strip('og:')
+                    content = meta['content'].strip()
+                    if tag not in result.keys():
+                        result[tag] = content
+                    else:
+                        if type(result[tag])==str:
+                            result[tag] = [result[tag]]
+                            result[tag].append(content)
+                        else:
+                            result[tag].append(content)
+    
+    return result
---
+++ packages/ogpparser/ogpparser-1.0.2.tar.gz/ogpparser-1.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ogpparser',
+    packages=find_packages(),
+
+    version='1.0.2',
+
+    license='MIT',
+
+    install_requires=['beautifulsoup4'],
+
+    author='reeve0930',
+    author_email='reeve0930@gmail.com',
+
+    url='https://github.com/reeve0930/ogpparser',
+
+    description='Open Graph Protocolのタグを取得',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='OGP OpenGraphProtocol parser', 
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
