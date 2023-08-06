94db171e17eb3fbbb8bbd63abd9159917d27d327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:50 +0000

    Add package popcon/0.0.3/popcon-0.0.3.tar.gz

---
+++ packages/popcon/popcon-0.0.3.tar.gz/popcon-0.0.3/popcon/datasets/io.py
@@ -0,0 +1,60 @@
+from os.path import abspath, dirname, join
+
+import numpy as np
+import pandas as pd
+from sklearn.utils import Bunch
+
+from ..utils import Multigraph, parse_participants, load_edgelists
+
+
+def _get_datapath(path):
+    """Get the absolute path to dataset."""
+    root = abspath(dirname(__file__))
+    return join(root, "data", path)
+
+
+def _load_dataset(data_path, extension):
+    """
+    Generage `Multigraph` object for a given dataset.
+
+    Parameters
+    ----------
+    data_path : Path
+        Path to BIDS-formatted dataset
+    extension : string
+        File extension for all edgelists in the dataset
+
+    Returns
+    -------
+    Multigraph : Multigraph
+        Multigraph object for the dataset
+    """
+
+    # Read the participant descriptor
+    participants, param_dict = parse_participants(data_path)
+
+    # Read the graphs
+    sample_size = int(param_dict["sample_size"])
+    n_vertices = int(param_dict["n_vertices"])
+    graphs = load_edgelists(data_path, participants, sample_size, n_vertices, extension)
+
+    return Multigraph(participants, graphs)
+
+
+def load_mice():
+    """Load the Duke 2018 mouse data."""
+
+    # Load the graphs
+    data_path = _get_datapath(path="duke")
+    multigraph = _load_dataset(data_path, extension="ssv")
+
+    # Read the atlas
+    atlas = pd.read_csv(join(data_path, "atlas.csv"))
+    inner_hier_labels = atlas["Macrostructure"].tolist() * 2
+    outer_hier_labels = np.array(166 * ["Right"] + 166 * ["Left"])
+
+    return Bunch(
+        multigraph=multigraph,
+        structures=inner_hier_labels,
+        hemisphere=outer_hier_labels,
+    )
