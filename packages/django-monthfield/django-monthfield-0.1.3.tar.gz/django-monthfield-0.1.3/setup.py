f838598146d41606d93dae76fc1735fe46f4dccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:50 +0000

    Add package django-monthfield/0.1.3/django-monthfield-0.1.3.tar.gz

---
+++ packages/django-monthfield/django-monthfield-0.1.3.tar.gz/django-monthfield-0.1.3/setup.py
@@ -0,0 +1,17 @@
+'''Still needs some work to properly include the test_site and sqlite db'''
+
+from setuptools import setup
+
+setup(
+    name='django-monthfield',
+    version='0.1.3',
+    author=u'Matthys Kroon',
+    author_email='matthysk@clearspark.co.za',
+    packages=['month'],
+    include_package_data=True,
+    url='https://github.com/clearspark/django-monthfield',
+    license='BSD licence, see LICENCE',
+    description='Provides a field for storing months (YYYY-MM) on django models.',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+)
