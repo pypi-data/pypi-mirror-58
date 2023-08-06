cf2444681fd7846f4df4fa94ce5f52d21e85f1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:27 +0000

    Add package afniRTI/2.7.0/afniRTI-2.7.0.tar.gz

---
+++ packages/afniRTI/afniRTI-2.7.0.tar.gz/afniRTI-2.7.0/setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="afniRTI",
+    version="2.7.0",
+    author="V. R-singh",
+    author_email="for_usenet@yahoo.com",
+    description="AFNI Real-Time Interface in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roopchansinghv/afni-real-time-interface-in-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7,<3.0',
+    # python_requires='>=3.0',
+)
+
