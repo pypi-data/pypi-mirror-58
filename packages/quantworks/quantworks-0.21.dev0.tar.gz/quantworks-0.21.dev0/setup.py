6ffb2c856868d35e4efdca0b5f11d3e2320189e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:53 +0000

    Add package quantworks/0.21.dev0/quantworks-0.21.dev0.tar.gz

---
+++ packages/quantworks/quantworks-0.21.dev0.tar.gz/quantworks-0.21.dev0/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quantworks',
+ 'quantworks.barfeed',
+ 'quantworks.broker',
+ 'quantworks.dataseries',
+ 'quantworks.feed',
+ 'quantworks.optimizer',
+ 'quantworks.stratanalyzer',
+ 'quantworks.strategy',
+ 'quantworks.talibext',
+ 'quantworks.technical',
+ 'quantworks.tools',
+ 'quantworks.utils',
+ 'quantworks.websocket']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['coveralls>=1.9,<2.0',
+ 'matplotlib==3.1.2',
+ 'numpy==1.18.0',
+ 'python-dateutil==2.8.1',
+ 'pytz==2019.3',
+ 'requests==2.22.0',
+ 'retrying==1.3.3',
+ 'scipy==1.4.1',
+ 'six==1.13.0',
+ 'tornado==6.0.3',
+ 'ws4py==0.3.4']
+
+extras_require = \
+{'TALib': ['TA-Lib>=0.4.17,<0.5.0']}
+
+setup_kwargs = {
+    'name': 'quantworks',
+    'version': '0.21.dev0',
+    'description': 'Python Algorithmic Trading Framework',
+    'long_description': None,
+    'author': 'Gabriel Martin Becedillas Ruiz',
+    'author_email': 'pyalgotrade@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
