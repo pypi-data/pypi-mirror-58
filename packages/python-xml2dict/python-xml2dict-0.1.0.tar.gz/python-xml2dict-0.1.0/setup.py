857bec93b8cd8b55c655c41c65c82b255c4fa926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:43 +0000

    Add package python-xml2dict/0.1.0/python-xml2dict-0.1.0.tar.gz

---
+++ packages/python-xml2dict/python-xml2dict-0.1.0.tar.gz/python-xml2dict-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# python3 setup.py build
+# python3 setup.py sdist upload
+import setuptools
+
+setuptools.setup(
+    name='python-xml2dict',
+    version='0.1.0',
+    url='https://github.com/gotthardp/python-xml2dict.git',
+    author='Petr Gotthard',
+    author_email='petr.gotthard@centrum.cz',
+    description='Flexible XML to dict Converter',
+    install_requires=[],
+)
+
+# end of file
