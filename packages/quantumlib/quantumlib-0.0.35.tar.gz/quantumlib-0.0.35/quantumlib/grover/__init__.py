6369d9c4cb9121a43ef5912ba77c06e5f07b2bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:05 +0000

    Add package quantumlib/0.0.35/quantumlib-0.0.35.tar.gz

---
+++ packages/quantumlib/quantumlib-0.0.35.tar.gz/quantumlib-0.0.35/quantumlib/grover/__init__.py
@@ -0,0 +1,10 @@
+def find_collision(f, result, max_collisions=1):
+    """
+    Finds collisions for a given function
+
+    Args:
+        f: function to process
+        result: function result
+        max_collisions: max collissions to find (default: 1)
+    """
+    raise RuntimeError('Quantum FPU not found (8192 qubits required)')
