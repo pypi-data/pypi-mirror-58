f849093dc4fb1d7df914b03c98dbd91e2c0a94fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:54 +0000

    Add package circleguard/3.1.0/circleguard-3.1.0-py3-none-any.whl

---
+++ packages/circleguard/circleguard-3.1.0-py3-none-any.whl/circleguard/comparer.py
@@ -0,0 +1,207 @@
+import itertools
+import sys
+import logging
+import math
+
+import numpy as np
+
+from circleguard.loadable import Replay
+from circleguard.enums import Mod
+from circleguard.exceptions import InvalidArgumentsException, CircleguardException
+import circleguard.utils as utils
+from circleguard.result import ReplayStealingResult
+
+class Comparer:
+    """
+    Manages comparing :class:`~.replay.Replay`\s for replay stealing.
+
+    Parameters
+    ----------
+    threshold: int
+        If a comparison scores below this value, one of the
+        :class:`~.replay.Replay` in the comparison is considered cheated.
+    replays1: list[:class:`~circleguard.loadable.Replay`]
+        The replays to compare against either ``replays2`` if ``replays`` is
+        not ``None``, or against other replays in ``replays1``.
+    replays2: list[:class:`~circleguard.loadable.Replay`]
+        The replays to compare against ``replays1``.
+
+    Notes
+    -----
+    If ``replays2`` is passed, each replay in ``replays1`` is compared against
+    each replay in ``replays2``. Otherwise, each replay in ``replays1`` is
+    compared against each other replay in ``replays1``
+    (``len(replays1) choose 2`` comparisons).
+
+    The order of ``replays1`` and ``replays2`` has no effect; comparing 1 to 2
+    is the same as comparing 2 to 1.
+
+    See Also
+    --------
+    :class:`~circleguard.investigator.Investigator`, for investigating single
+    replays.
+    """
+
+    def __init__(self, threshold, replays1, replays2=None):
+        self.log = logging.getLogger(__name__)
+        self.threshold = threshold
+
+        # filter beatmaps we had no data for
+        self.replays1 = [replay for replay in replays1 if replay.replay_data is not None]
+        self.replays2 = [replay for replay in replays2 if replay.replay_data is not None] if replays2 else None
+        self.mode = "double" if self.replays2 else "single"
+        self.log.debug("Comparer initialized: %r", self)
+
+    def compare(self):
+        """
+        If ``replays2`` is not ``None``, compares all replays in replays1
+        against all replays in replays2. Otherwise, compares all replays in
+        ``replays1`` against all other replays in ``replays1``
+        (``len(replays1) choose 2`` comparisons).
+
+        Yields
+        ------
+        :class:`~.result.ComparisonResult`
+            Results representing the comparison of two replays.
+        """
+
+        self.log.info("Comparing replays with mode: %s", self.mode)
+        self.log.debug("replays1: %r", self.replays1)
+        self.log.debug("replays2: %r", self.replays2)
+
+        #TODO: a little bit hacky and I don't think works 100% correctly, if mode is double but replays2 is None
+        if not self.replays1 or self.replays2 == []:
+            return
+
+        if self.mode == "double":
+            iterator = itertools.product(self.replays1, self.replays2)
+        elif self.mode == "single":
+            iterator = itertools.combinations(self.replays1, 2)
+        else:
+            raise InvalidArgumentsException("'mode' must be one of 'double' or 'single'")
+
+        for replay1, replay2 in iterator:
+            if replay1.replay_id == replay2.replay_id:
+                self.log.debug("Not comparing %r and %r with the same id", replay1, replay2)
+                continue
+            yield self._result(replay1, replay2)
+
+
+    def _result(self, replay1, replay2):
+        """
+        Compares two :class:`~.replay.Replay`\s.
+
+        Parameters
+        ----------
+        replay1: :class:`~.replay.Replay`
+            The first replay to compare.
+        replay2: :class:`~.replay.Replay`
+            The second replay to compare.
+
+        Returns
+        -------
+        :class:`~.result.ComparisonResult`
+            The result of comparing ``replay1`` to ``replay2``.
+        """
+        self.log.log(utils.TRACE, "comparing %r and %r", replay1, replay2)
+        result = Comparer._compare_two_replays(replay1, replay2)
+        mean = result[0]
+        sigma = result[1]
+        ischeat = False
+        if(mean < self.threshold):
+            ischeat = True
+
+        return ReplayStealingResult(replay1, replay2, mean, ischeat)
+
+    @staticmethod
+    def _compare_two_replays(replay1, replay2):
+        """
+        Calculates the average cursor distance between two
+        :class:`~.replay.Replay`\s, and the standard deviation of the distance.
+
+        Parameters
+        ----------
+        replay1: :class:`~.replay.Replay`
+            The first replay to compare.
+        replay2: :class:`~.replay.Replay`
+            The second replay to compare.
+
+        Returns
+        -------
+        tuple[float, float]
+            (average distance, stddev) of the cursors of the two replays.
+        """
+
+        # get all coordinates in numpy arrays so that they're arranged like:
+        # [ x_1 x_2 ... x_n
+        #   y_1 y_2 ... y_n ]
+        # indexed by columns first.
+        data1 = replay1.as_list_with_timestamps()
+        data2 = replay2.as_list_with_timestamps()
+
+        # interpolate
+        (data1, data2) = utils.interpolate(data1, data2)
+
+        # remove time and keys from each tuple
+        data1 = [d[1:3] for d in data1]
+        data2 = [d[1:3] for d in data2]
+        if (Mod.HR in replay1.mods) ^ (Mod.HR in replay2.mods): # xor, if one has hr but not the other
+            for d in data1:
+                d[1] = 384 - d[1]
+
+        (mu, sigma) = Comparer._compute_data_similarity(data1, data2)
+        return (mu, sigma)
+
+    @staticmethod
+    def _compute_data_similarity(data1, data2):
+        """
+        Calculates the average cursor distance between two lists of cursor data,
+        and the standard deviation of the distance.
+
+        Parameters
+        ----------
+        data1: list[tuple(int, int)]
+            The first set of cursor data, containing the x and y positions
+            of the cursor at each datapoint.
+        data2: list[tuple(int, int)]
+            The first set of cursor data, containing the x and y positions
+            of the cursor at each datapoint.
+
+        Returns
+        -------
+        tuple[float, float]
+            (average distance, stddev) between the two datasets.
+
+        Notes
+        -----
+        The two data lists must have previously been interpolated to each other.
+        This is why we can get away with only lists of [x,y] and not [x,y,t].
+        """
+
+        data1 = np.array(data1)
+        data2 = np.array(data2)
+
+        # switch if the second is longer, so that data1 is always the longest.
+        if len(data2) > len(data1):
+            (data1, data2) = (data2, data1)
+
+        shortest = len(data2)
+
+        distance = data1[:shortest] - data2
+        # square all numbers and sum over the second axis (add row 2 to row 1),
+        # finally take the square root of each number to get all distances.
+        # [ x_1 x_2 ... x_n   => [ x_1 ** 2 ... x_n ** 2
+        #   y_1 y_2 ... y_n ] =>   y_1 ** 2 ... y_n ** 2 ]
+        # => [ x_1 ** 2 + y_1 ** 2 ... x_n ** 2 + y_n ** 2 ]
+        # => [ d_1 ... d_2 ]
+        distance = (distance ** 2).sum(axis=1) ** 0.5
+
+        mu, sigma = distance.mean(), distance.std()
+
+        return (mu, sigma)
+
+    def __repr__(self):
+        return f"Comparer(threshold={self.threshold},replays1={self.replays1},replays2={self.replays2})"
+
+    def __str__(self):
+        return f"Comparer with thresh {self.threshold}"
---
+++ packages/circleguard/circleguard-3.1.0-py3-none-any.whl/circleguard/utils.py
@@ -0,0 +1,273 @@
+from logging import Formatter
+from copy import copy
+
+from circleguard.enums import Mod
+
+
+########### LOGGING ##############
+
+TRACE = 5
+
+class ColoredFormatter(Formatter):
+    """
+    A subclass of :class:`logging.Formatter` that uses ANSI escape codes
+    to color different parts of the :class:`logging.LogRecord` when printed to the
+    console.
+
+    Notes
+    -----
+    Adapted from https://stackoverflow.com/a/46482050.
+    """
+
+    COLOR_PREFIX = '\033['
+    COLOR_SUFFIX = '\033[0m'
+    COLOR_MAPPING = {
+        "TRACE"    : 90, # bright black
+        "DEBUG"    : 94, # bright blue
+        "INFO"     : 95, # bright magenta
+        "WARNING"  : 31, # red
+        "ERROR"    : 91, # bright red
+        "CRITICAL" : 41, # white on red bg
+
+        "NAME"     : 32,  # green
+        "MESSAGE"  : 93,  # bright yellow
+        "FILENAME" : 92,  # bright green
+        "LINENO"   : 91   # bright red
+    }
+
+    def __init__(self, patern):
+        Formatter.__init__(self, patern)
+        self.colored_log = "{prefix}{{color}}m{{msg}}{suffix}".format(
+                    prefix=self.COLOR_PREFIX, suffix=self.COLOR_SUFFIX)
+
+    def format(self, record):
+        # c as in colored, not as in copy
+        c_record = copy(record)
+
+        # logging's choice of camelCase, not mine
+        threadName = c_record.threadName
+        color = self.COLOR_MAPPING["NAME"]
+        c_threadName = self.colored_log.format(color=color, msg=threadName)
+
+        levelname = c_record.levelname
+        color = self.COLOR_MAPPING.get(levelname, 37) # default to white
+        c_levelname = self.colored_log.format(color=color, msg=levelname)
+
+        name = c_record.name
+        color = self.COLOR_MAPPING["NAME"]
+        c_name = self.colored_log.format(color=color, msg=name)
+
+        message = c_record.msg # why is this msg, but we format it as %(message)s in the formatter? blame logging
+        color = self.COLOR_MAPPING["MESSAGE"]
+        c_msg = self.colored_log.format(color=color, msg=message)
+
+        filename = c_record.filename
+        color = self.COLOR_MAPPING["FILENAME"]
+        c_filename = self.colored_log.format(color=color, msg=filename)
+
+        lineno = c_record.lineno
+        color = self.COLOR_MAPPING["LINENO"]
+        c_lineno = self.colored_log.format(color=color, msg=lineno)
+
+        c_record.threadName = c_threadName
+        c_record.levelname = c_levelname
+        c_record.name = c_name
+        c_record.msg = c_msg
+        c_record.filename = c_filename
+        c_record.lineno = c_lineno
+
+        return Formatter.format(self, c_record)
+
+######### UTIL METHODS ###########
+
+def span_to_list(span):
+    """
+    Converts a span to the set of numbers covered by that span.
+
+    Parameters
+    ----------
+    span: str
+        The span of numbers to convert to a set. A number may occur more than
+        once - whether explicitly or in a range - in the span, but will
+        only occur once in the returned set.
+
+    Returns
+    -------
+    set
+        The set of numbers described by the `span`.
+
+    Examples
+    --------
+    >>> span_to_list("1-3,6,2-4")
+    {1, 2, 3, 4, 6}
+    """
+    ret = set()
+    for s in span.split(","):
+        if "-" in s:
+            p = s.split("-")
+            l = list(range(int(p[0]), int(p[1])+1))
+            ret.update(l)
+        else:
+            ret.add(int(s))
+    return ret
+
+
+
+# https://github.com/kszlim/osu-replay-parser/blob/master/osrparse/replay.py#L64
+def bits(n):
+    if n == 0:
+        yield 0
+    while n:
+        b = n & (~n+1)
+        yield b
+        n ^= b
+
+
+class Interpolation:
+    """A utility class containing coordinate interpolations."""
+
+    @staticmethod
+    def linear(x1, x2, r):
+        """
+        Linearly interpolates coordinate tuples x1 and x2 with ratio r.
+
+        Args:
+            Float x1: The startpoint of the interpolation.
+            Float x2: The endpoint of the interpolation.
+            Float r: The ratio of the points to interpolate to.
+        """
+
+        return ((1 - r) * x1[0] + r * x2[0], (1 - r) * x1[1] + r * x2[1])
+
+    @staticmethod
+    def before(x1, x2, r):
+        """
+        Returns the startpoint of the range.
+
+        Args:
+            Float x1: The startpoint of the interpolation.
+            Float x2: The endpoint of the interpolation.
+            Float r: Ignored.
+        """
+
+        return x2
+
+def interpolate(data1, data2, interpolation=Interpolation.linear, unflip=False):
+    """
+    Interpolates the longer of the datas to match the timestamps of the shorter.
+
+    Args:
+        List data1: A list of tuples of (t, x, y).
+        List data2: A list of tuples of (t, x, y).
+        Boolean unflip: Preserves input order of data1 and data2 if True.
+
+    Returns:
+        If unflip:
+            The tuple (data1, data2), where one is interpolated to the other
+            and said other without uninterpolatable points.
+        Else:
+            The tuple (clean, inter), respectively the shortest of
+            the datasets without uninterpolatable points and the longest
+            interpolated to the timestamps of shortest.
+
+    """
+
+    flipped = False
+
+    # if the first timestamp in data2 is before the first in data1 switch
+    # so data1 always has some timestamps before data2.
+    if data1[0][0] > data2[0][0]:
+        flipped = not flipped
+        (data1, data2) = (data2, data1)
+
+    # get the smallest index of the timestamps after the first timestamp in data2.
+    i = next((i for (i, p) in enumerate(data1) if p[0] > data2[0][0]))
+
+    # remove all earlier timestamps, if data1 is longer than data2 keep one more
+    # so that the longest always starts before the shorter dataset.
+    data1 = data1[i:] if len(data1) < len(data2) else data1[i - 1:]
+
+    if len(data1) > len(data2):
+        flipped = not flipped
+        (data1, data2) = (data2, data1)
+
+    # for each point in data1 interpolate the points around the timestamp in data2.
+    j = 0
+    inter = []
+    clean = []
+    for between in data1:
+        # keep a clean version with only values that can be interpolated properly.
+        clean.append(between)
+
+        # move up to the last timestamp in data2 before the current timestamp.
+        while j < len(data2) - 1 and data2[j][0] < between[0]:
+            j += 1
+
+        if j == len(data2) - 1:
+            break
+
+        before = data2[j]
+        after = data2[j + 1]
+
+        # calculate time differences
+        # dt1 =  ---2       , data1
+        # dt2 = 1-------3   , data2
+        dt1 = between[0] - before[0]
+        dt2 = after[0] - before[0]
+
+        # skip trying to interpolate to this event
+        # if its surrounding events are not set apart in time
+        # and replace it with the event before it
+        if dt2 == 0:
+            inter.append((between[0], *before[1:]))
+            continue
+
+        # interpolate the coordinates in data2
+        # according to the ratios of the time differences
+        x_inter = interpolation(before[1:], after[1:], dt1 / dt2)
+
+        # filter out interpolation artifacts which send outliers even further away
+        if abs(x_inter[0]) > 600 or abs(x_inter[1]) > 600:
+            inter.append(between)
+            continue
+
+        t_inter = between[0]
+
+        inter.append((t_inter, *x_inter))
+
+    if unflip and flipped:
+        (clean, inter) = (inter, clean)
+
+    return (clean, inter)
+
+def resample(timestamped, frequency):
+    """
+    Resample timestamped data at the given frequency.
+
+    Args:
+        List timestamped: A list of tuples of (t, x, y).
+        Float frequency: The frequency to resample data to in Hz.
+
+    Returns
+        A list of tuples of (t, x, y) with constant time interval 1 / frequency.
+    """
+
+    i = 0
+    t = timestamped[0][0]
+    t_max = timestamped[-1][0]
+
+    resampled = []
+
+    while t < t_max:
+        while timestamped[i][0] < t:
+            i += 1
+
+        dt1 = t - timestamped[i - 1][0]
+        dt2 = timestamped[i][0] - timestamped[i - 1][0]
+
+        inter = Interpolation.linear(timestamped[i - 1][1:], timestamped[i][1:], dt1 / dt2)
+
+        resampled.append((t, *inter))
+        t += 1000 / frequency
+
+    return resampled
