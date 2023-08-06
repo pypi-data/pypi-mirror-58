433013262e6ded3ce7b2fabd8bbc038a4428ef78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:10 +0000

    Add package rgkpdf/1.1/rgkpdf-1.1.tar.gz

---
+++ packages/rgkpdf/rgkpdf-1.1.tar.gz/rgkpdf-1.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="rgkpdf",
+    version=1.1,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+
+)
