5f16fc75630da7ac1344ede521649647cc73f0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:42 +0000

    Add package fiduswriter-phplist/3.7.2/fiduswriter-phplist-3.7.2.tar.gz

---
+++ packages/fiduswriter-phplist/fiduswriter-phplist-3.7.2.tar.gz/fiduswriter-phplist-3.7.2/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_namespace_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='fiduswriter-phplist',
+    version='3.7.2',
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    license='AGPL License',
+    description='A Fidus Writer plugin to integrate with an emaillist using PHPList.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://www.github.com/fiduswriter/fiduswriter-phplist',
+    author='Johannes Wilm',
+    author_email='johannes@fiduswriter.org',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
