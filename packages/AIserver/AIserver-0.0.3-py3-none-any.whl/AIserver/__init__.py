0bf10e076a2b4a0f5e5fb0251007cce16d492b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:10 +0000

    Add package AIserver/0.0.3/AIserver-0.0.3-py3-none-any.whl

---
+++ packages/AIserver/AIserver-0.0.3-py3-none-any.whl/AIserver/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '0.0.3'
+"""Extensions to the 'distutils' for large or complex distributions"""
+NAME = "AIserver"
+from AIserver import server as s
+from AIserver import client as c
+__metaclass__ = type
+__all__ = [
+    'client', 'server'
+]
+
+
+class Server(object):
+
+    @staticmethod
+    def left(step):
+        s.left(step)
+        # print("向左转")
+
+
+class Client(object):
+
+    @staticmethod
+    def left(step):
+        c.left(step)
