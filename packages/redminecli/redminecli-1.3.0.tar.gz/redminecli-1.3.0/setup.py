024051b74d163cdbf5f9b4c005013b05ae47cd44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:49 +0000

    Add package redminecli/1.3.0/redminecli-1.3.0.tar.gz

---
+++ packages/redminecli/redminecli-1.3.0.tar.gz/redminecli-1.3.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="redminecli",
+    version="1.3.0",
+    description="Command line interface for Redmine",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Ege Güneş",
+    author_email="egegunes@gmail.com",
+    url="https://github.com/egegunes/redmine-cli",
+    license="GPLv3",
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=["requests>=2.22.0", "click==7.0", "colorama==0.4.1"],
+    entry_points="""
+        [console_scripts]
+        redmine=redmine.cli.main:cli
+    """,
+)
