b719f34c0d8443681ed601797089df91695ea0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package fiases/1.8.84/fiases-1.8.84.tar.gz

---
+++ packages/fiases/fiases-1.8.84.tar.gz/fiases-1.8.84/fiases/fias_update.py
@@ -0,0 +1,44 @@
+import fiases.fias_data
+from fiases.address import import_address
+from fiases.houses import import_houses
+from fiases.fias_delta_update import update
+from fiases.fias_data import ES
+from fiases.snapshot import createFullSnapshot
+from elasticsearch.client import IndicesClient
+from elasticsearch.client import SnapshotClient
+
+ADDR_UPDATE_CNT=0
+
+def importFull():
+    houses = fiases.fias_data.Houses()
+    import_houses(houses=houses)
+    refreshIndex()
+
+    address = fiases.fias_data.Address()
+    import_address(address=address)
+    refreshIndex()
+
+def getSnapshotStatus():
+    sn =  SnapshotClient(ES); 
+    status = sn.get(repository="fias")
+    return status;
+
+
+
+def updateFias():
+    if not ES.indices.exists(fiases.fias_data.ADDRESS_INDEX):
+        print("No ADDR index found. Start import full...")
+        importFull()
+    else:
+        ADDR_UPDATE_CNT=update(isDebug=True)
+    createFullSnapshot()
+    refreshIndex()
+    fiases.fias_data.createTmpDir();
+    return ADDR_UPDATE_CNT
+
+
+def refreshIndex():
+    IndicesClient(ES).refresh()
+    IndicesClient(ES).flush()
+    IndicesClient(ES).forcemerge()
+# updateFias()
