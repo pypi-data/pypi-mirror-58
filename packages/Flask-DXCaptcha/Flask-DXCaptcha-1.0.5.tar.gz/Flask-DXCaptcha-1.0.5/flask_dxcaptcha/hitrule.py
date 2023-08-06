ee12438441f4f58f9a9155150640547d759d801c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:36 +0000

    Add package Flask-DXCaptcha/1.0.5/Flask-DXCaptcha-1.0.5.tar.gz

---
+++ packages/Flask-DXCaptcha/Flask-DXCaptcha-1.0.5.tar.gz/Flask-DXCaptcha-1.0.5/flask_dxcaptcha/hitrule.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: durban.zhang
+# @Date:   2019-12-25 16:42:46
+# @Last Modified by:   durban.zhang
+# @Last Modified time: 2019-12-25 17:02:43
+
+
+class HitRule:
+
+    def __init__(self, id, leftValue):
+        self.id = id
+        self.leftValue = leftValue
