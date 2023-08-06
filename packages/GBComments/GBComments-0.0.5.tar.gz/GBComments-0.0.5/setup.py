4546e87c852b7514706bfbbba371ce64d55af538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:13 +0000

    Add package GBComments/0.0.5/GBComments-0.0.5.tar.gz

---
+++ packages/GBComments/GBComments-0.0.5.tar.gz/GBComments-0.0.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='GBComments',
+    version='0.0.5',
+    py_modules=['GBComments'],
+    author="WRAllen",
+    author_email="1072274105@qq.com",
+    # 这里会在他人搜索时显示
+    description="用于生成好评差评的库",
+    # github地址
+    url='https://github.com/WRAllen/GBComments'
+)
