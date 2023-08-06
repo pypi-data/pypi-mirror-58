a3d6911011cb94a42d45ff8466e3288848f03234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:59 +0000

    Add package wmata/1.0.0/wmata-1.0.0-py3-none-any.whl

---
+++ packages/wmata/wmata-1.0.0-py3-none-any.whl/wmata/rail/line.py
@@ -0,0 +1,14 @@
+"""MetroRail Line related structures
+"""
+from enum import Enum
+
+class Line(Enum):
+    """A MetroRail Line
+    """
+    RD = "RD"
+    BL = "BL"
+    YL = "YL"
+    YLRP = "YLRP"
+    OR = "OR"
+    GR = "GR"
+    SV = "SV"
