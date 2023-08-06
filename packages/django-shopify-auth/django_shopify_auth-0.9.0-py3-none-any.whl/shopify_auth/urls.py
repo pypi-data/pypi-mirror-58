b6d3ff00d27257b3e960a7f50804d4455ff124ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:09 +0000

    Add package django-shopify-auth/0.9.0/django_shopify_auth-0.9.0-py3-none-any.whl

---
+++ packages/django-shopify-auth/django_shopify_auth-0.9.0-py3-none-any.whl/shopify_auth/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+  url(r'^finalize/$',     views.finalize),
+  url(r'^authenticate/$', views.authenticate),
+  url(r'^$',              views.login),
+]
