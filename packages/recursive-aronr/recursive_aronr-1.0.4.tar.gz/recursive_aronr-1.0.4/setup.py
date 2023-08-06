03cc4cee56f9ca0eddc83ebc4139905de76f2325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:34 +0000

    Add package recursive-aronr/1.0.4/recursive_aronr-1.0.4.tar.gz

---
+++ packages/recursive-aronr/recursive_aronr-1.0.4.tar.gz/recursive_aronr-1.0.4/setup.py
@@ -0,0 +1,12 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+	name			= 'recursive_aronr',
+	version			= '1.0.4',
+	py_modules		= ['recursive_aronr'],
+	author			= 'Roli',
+	author_email	= 'roli@email.com',
+	url				= 'http://www.aronr.net.com',
+	description		= 'This is a recursive printer of lists!',
+)
