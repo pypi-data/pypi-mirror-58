ec7f13a545fa65912971abf8bb289ee3a0ccfabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package pervane/0.0.16/pervane-0.0.16.tar.gz

---
+++ packages/pervane/pervane-0.0.16.tar.gz/pervane-0.0.16/pervane/cli.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from subprocess import Popen
+import serve
+
+
+def main(as_module=False):
+    print('sys.argv', sys.argv)
+    # Popen('python3 serve.py')
+    serve.cli_main()
+
+
+# if __name__ == "__main__":
+#     main(as_module=True)
