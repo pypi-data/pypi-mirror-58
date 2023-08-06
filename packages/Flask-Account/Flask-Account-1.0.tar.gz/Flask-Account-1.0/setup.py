0969955a45f0b9278680b0dcc39bfa6ab6648356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:01 +0000

    Add package Flask-Account/1.0/Flask-Account-1.0.tar.gz

---
+++ packages/Flask-Account/Flask-Account-1.0.tar.gz/Flask-Account-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Account',
+    version='1.0',
+    packages=['flask_account'],
+    url='https://github.com/gerelorant/Flask-Account',
+    license='MIT',
+    author='Gere Lóránt',
+    author_email='gerelorant@gmail.com',
+    description='User account boilerplate for Flask'
+)
