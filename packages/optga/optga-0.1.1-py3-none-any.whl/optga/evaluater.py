20a78a8e60ec546986dd3836a8e954089303a7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:47 +0000

    Add package optga/0.1.1/optga-0.1.1-py3-none-any.whl

---
+++ packages/optga/optga-0.1.1-py3-none-any.whl/optga/evaluater.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class Evaluator:
+
+    def __init__(self, config, models):
+
+        self.config = config
+
+        self.objective_names = (list(self.config.objectives)
+                                if self.config.objectives else [])
+
+        self.models = models
+
+    def evaluate(self, population):
+        fitness = np.zeros((population.shape[0], len(self.objective_names)))
+        for i, oname in enumerate(self.objective_names):
+            fitness[:, i] = self.models[oname].predict(population)
+        return fitness
