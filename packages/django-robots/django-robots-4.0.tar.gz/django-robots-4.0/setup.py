ecf780474f3f120d626830d8a6e4789ffd01ba68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package django-robots/4.0/django-robots-4.0.tar.gz

---
+++ packages/django-robots/django-robots-4.0.tar.gz/django-robots-4.0/setup.py
@@ -0,0 +1,51 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+
+def read(*parts):
+    return open(path.join(path.dirname(__file__), *parts)).read()
+
+
+setup(
+    name='django-robots',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    long_description=read("README.rst"),
+    long_description_content_type='text/x-rst',
+    description='Robots exclusion application for Django, complementing Sitemaps.',
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    python_requires='>1.1.1',
+    url='https://github.com/jazzband/django-robots/',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    package_data={
+        'robots': [
+            'locale/*/LC_MESSAGES/*',
+            'templates/robots/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+    ]
+)
