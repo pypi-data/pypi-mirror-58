58467382a9ac3690d03247677441ba4a4da05b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:36 +0000

    Add package freenit/0.0.10/freenit-0.0.10-py3-none-any.whl

---
+++ packages/freenit/freenit-0.0.10-py3-none-any.whl/freenit/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask, send_file
+from flask_collect import Collect
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_security import PeeweeUserDatastore, Security
+
+from . import cli
+from .api import create_api
+from .db import db
+from .utils import sendmail as sm
+
+
+def create_app(config, app_name, app=None):
+    if app is None:
+        app = Flask(__name__)
+        app.config.from_object(config)
+    app.models = f'{app_name}.models'
+
+    @app.route('/media/<path:path>')
+    def send_media(path):
+        fullPath = f"../{app.config['MEDIA_PATH']}/{path}"
+        try:
+            return send_file(fullPath)
+        except FileNotFoundError:
+            return 'No such file', 404
+
+    app.sendmail = lambda to, message: sm(app.config, to, message)
+
+    app.collect = Collect(app)
+    db.init_app(app)
+    app.db = db
+
+    from .models.user import User
+    from .models.role import Role, UserRoles
+    app.user_datastore = PeeweeUserDatastore(
+        app.db,
+        User,
+        Role,
+        UserRoles,
+    )
+    app.security = Security(app, app.user_datastore)
+    app.jwt = JWTManager(app)
+    create_api(app)
+    app.cors = CORS(app, supports_credentials=True)
+    cli.register(app)
+
+    return app
