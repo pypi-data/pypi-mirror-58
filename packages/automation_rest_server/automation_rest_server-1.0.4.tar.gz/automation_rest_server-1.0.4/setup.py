3725a0fea911a428a00cfb5d2ae7edb7701567d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:42 +0000

    Add package automation_rest_server/1.0.4/automation_rest_server-1.0.4.tar.gz

---
+++ packages/automation_rest_server/automation_rest_server-1.0.4.tar.gz/automation_rest_server-1.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+packages = ["automation_rest_server"]
+
+setup(
+    name = 'automation_rest_server',
+    version = '1.0.4',
+    keywords = ['runner', 'server'],
+    description = 'NVMe production server',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+      ],
+    license = 'MIT License',
+	url = 'https://pypi.org/project/automation_rest_server',
+    install_requires = ['flask',
+						'flask-restful',
+						'requests',
+						'PyMySQL',
+						'pyftpdlib'],
+    include_package_data=True, 
+    author = 'yuwen123441',
+    author_email = 'yuwen123441@qq.com',
+    packages = packages,
+    platforms = 'any',
+    entry_points={'console_scripts': [
+        'redis_run = automation_rest_server.run',
+    ]},
+)
