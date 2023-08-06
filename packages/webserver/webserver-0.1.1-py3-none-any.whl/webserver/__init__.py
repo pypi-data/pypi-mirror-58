bc63811af991e803937f6907ac4844cca7d0bc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:18 +0000

    Add package webserver/0.1.1/webserver-0.1.1-py3-none-any.whl

---
+++ packages/webserver/webserver-0.1.1-py3-none-any.whl/webserver/__init__.py
@@ -0,0 +1,24 @@
+from os import path
+from subprocess import run as run_command
+
+import click
+
+from .run import run
+
+if not path.exists("/etc/webserver"):
+    run_command(["sudo", "mkdir", "-p", "/etc/webserver/dhparams"])
+    run_command(["sudo", "mkdir", "-p", "/etc/webserver/letsencrypt"])
+    run_command(["sudo", "mkdir", "-p", "/etc/webserver/logs"])
+    run_command(["sudo", "mkdir", "-p", "/etc/webserver/static"])
+    run_command(["sudo", "mkdir", "-p", "/etc/webserver/conf.d"])
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+cli.add_command(run)
+
+if __name__ == "__main__":
+    cli()
