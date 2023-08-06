084724eeec1b2137d7a686c9cee373697dce0ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package django-markdownfield/0.6.1/django_markdownfield-0.6.1-py3-none-any.whl

---
+++ packages/django-markdownfield/django_markdownfield-0.6.1-py3-none-any.whl/markdownfield/util.py
@@ -0,0 +1,28 @@
+from typing import Dict
+from urllib.parse import urlparse
+
+from django.conf import settings
+
+
+def format_link(attrs: Dict[tuple, str], new: bool = False):
+    """
+    This is really weird and ugly, but that's how bleach linkify filters work.
+    """
+    try:
+        p = urlparse(attrs[(None, 'href')])
+    except KeyError:
+        # no href, probably an anchor
+        return attrs
+
+    if not any([p.scheme, p.netloc, p.path]) and p.fragment:
+        # the link isn't going anywhere, probably a fragment link
+        return attrs
+
+    c = urlparse(settings.SITE_URL)
+    if p.netloc != c.netloc:
+        # link is external - secure and mark
+        attrs[(None, 'target')] = '_blank'
+        attrs[(None, 'class')] = attrs.get((None, 'class'), '') + ' external'
+        attrs[(None, 'rel')] = 'nofollow noopener noreferrer'
+
+    return attrs
