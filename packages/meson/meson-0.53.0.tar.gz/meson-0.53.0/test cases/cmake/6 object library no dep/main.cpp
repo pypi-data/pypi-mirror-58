f85780ef7ac6606754c6143a1ac9cf3677e65079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:14 +0000

    Add package meson/0.53.0/meson-0.53.0.tar.gz

---
+++ packages/meson/meson-0.53.0.tar.gz/meson-0.53.0/test cases/cmake/6 object library no dep/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "libA.hpp"
+#include "libB.hpp"
+
+using namespace std;
+
+int main(void) {
+  cout << getLibStr() << " -- " << getZlibVers() << endl;
+  return EXIT_SUCCESS;
+}
