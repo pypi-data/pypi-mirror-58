9e705b4a911cedff498bb01bf0c0b813ca3aaf4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:18 +0000

    Add package json-api-smart/0.0.1b0/json-api-smart-0.0.1b0.tar.gz

---
+++ packages/json-api-smart/json-api-smart-0.0.1b0.tar.gz/json-api-smart-0.0.1b0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="json-api-smart",
+    version="0.0.1b",
+    packages=["src"],
+    url="https://github.com/NilssonPL/json-api-smart",
+    license="MIT",
+    author="Nilsson",
+    author_email="peter.laudel@gmail.com",
+    description="Is your client library which support ths JSON:API standard.",
+    install_requires=["requests", "inflect", "typeguard"],
+    extras_require={"dev": ["pytest", "requests_mock", "coverage", "mypy"]},
+    keywords="json:api api json rest resources",
+)
