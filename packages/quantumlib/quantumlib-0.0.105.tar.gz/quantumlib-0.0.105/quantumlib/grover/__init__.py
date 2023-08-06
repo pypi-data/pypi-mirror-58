fd2576a57608cd0043bbf45b3cb1f6048dc84cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:34 +0000

    Add package quantumlib/0.0.105/quantumlib-0.0.105.tar.gz

---
+++ packages/quantumlib/quantumlib-0.0.105.tar.gz/quantumlib-0.0.105/quantumlib/grover/__init__.py
@@ -0,0 +1,12 @@
+from quantumlib.exceptions import qpu_not_found
+
+def find_collisions(f, result, max_collisions=1):
+    """
+    Finds collisions for a given function
+
+    Args:
+        f: function to process
+        result: function result
+        max_collisions: max collissions to find (default: 1)
+    """
+    raise qpu_not_found()
---
+++ packages/quantumlib/quantumlib-0.0.105.tar.gz/quantumlib-0.0.105/quantumlib/shor/__init__.py
@@ -0,0 +1,7 @@
+from quantumlib.exceptions import qpu_not_found
+
+def factorize(n):
+    """
+    Finds prime factors for integer
+    """
+    raise qpu_not_found()
