7c6577b76cafbf6d76dbdf1dbaf57b5e3e4637b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:28 +0000

    Add package lins-asana/1.3.0.12/lins_asana-1.3.0.12.tar.gz

---
+++ packages/lins-asana/lins_asana-1.3.0.12.tar.gz/lins_asana-1.3.0.12/lins_asana/api_asana.py
@@ -0,0 +1,81 @@
+import json
+import requests
+
+
+class ApiAsana:
+    def __init__(self, base_url, token):
+        self.base_url = base_url
+        self.headers = {
+            'Accept': '/',
+            'Accept-Encoding': 'gzip, deflate',
+            'Authorization': token,
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json'
+        }
+
+    def get_workspaces(self) -> list:
+        url = f'{self.base_url}/workspaces'
+        response = requests.get(url=url, headers=self.headers)
+        return json.loads(response.text).get('data', [])
+
+    def get_teams(self, workspace) -> list:
+        url = f'{self.base_url}/organizations/{workspace}/teams'
+        response = requests.get(url=url, headers=self.headers)
+        return json.loads(response.text).get('data', [])
+
+    def get_projects(self, worspace, team) -> list:
+        url = f'{self.base_url}/projects'
+        params = {
+            'workspace': worspace,
+            'team': team
+        }
+        response = requests.get(url=url, params=params, headers=self.headers)
+        return json.loads(response.text).get('data', [])
+
+    def get_tasks(self, project='') -> list:
+        url = f'{self.base_url}/tasks'
+        params = {
+            'project': project
+        }
+        response = requests.get(url=url, params=params, headers=self.headers)
+        return json.loads(response.text).get('data', [])
+
+    def get_task_by_id(self, task_gid) -> dict:
+        url = f'{self.base_url}/tasks/{task_gid}'
+        response = requests.get(url=url, headers=self.headers)
+        return json.loads(response.text).get('data', {})
+
+    def get_events(self, resource, sync='') -> tuple:
+        url = f'{self.base_url}/events'
+        params = {
+            'resource': resource,
+            'sync': sync
+        }
+        response = requests.get(url, params=params, headers=self.headers)
+        obj = json.loads(response.text)
+        return obj.get('data', []), obj.get('sync', '')
+
+    def patch_task(self, task_gid: int, data: dict) -> tuple:
+        url = f'{self.base_url}/tasks/{task_gid}'
+        response = requests.put(url=url, data=json.dumps({'data': data}), headers=self.headers)
+        return response.ok, json.loads(response.text).get('data', {})
+
+    def get_webhooks(self, workspace: int) -> list:
+        url = f'{self.base_url}/webhooks'
+        response = requests.get(url=url, params={'workspace': workspace}, headers=self.headers)
+        return json.loads(response.text).get('data', [])
+
+    def post_webhook(self, resource: int, target: str) -> bool:
+        url = f'{self.base_url}/webhooks'
+        response = requests.post(
+            url=url,
+            headers=self.headers,
+            data=json.dumps({
+                'data': {
+                    'resource': resource,
+                    'target': target
+                }
+            })
+        )
+        return response.ok
