4ab7fcebb741511a4ffa3d52dbd777d3f5e78f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package SpeedTorch/0.1.6/SpeedTorch-0.1.6.tar.gz

---
+++ packages/SpeedTorch/SpeedTorch-0.1.6.tar.gz/SpeedTorch-0.1.6/SpeedTorch/__init__.py
@@ -0,0 +1,31 @@
+
+from SpeedTorch.CUPYLive import PMemory
+from SpeedTorch.CUPYLive import my_pinned_allocator
+from SpeedTorch.CUPYLive import _Common
+from SpeedTorch.CUPYLive import ModelFactory
+from SpeedTorch.CUPYLive import OptimizerFactory
+from SpeedTorch.CUPYLive import COM
+from SpeedTorch.CUPYLive import DataGadget
+
+from SpeedTorch.CPUCupyPinned import PMemoryMM
+from SpeedTorch.CPUCupyPinned import my_pinned_allocatorMM
+from SpeedTorch.CPUCupyPinned import _CommonMM
+from SpeedTorch.CPUCupyPinned import ModelFactoryMM
+from SpeedTorch.CPUCupyPinned import OptimizerFactoryMM
+from SpeedTorch.CPUCupyPinned import COMMM
+from SpeedTorch.CPUCupyPinned import DataGadgetMM
+
+from SpeedTorch.GPUTorch import _GPUPytorchCommon
+from SpeedTorch.GPUTorch import GPUPytorchModelFactory
+from SpeedTorch.GPUTorch import GPUPytorchOptimizerFactory
+from SpeedTorch.GPUTorch import GPUPytorchCOM
+
+from SpeedTorch.CPUTorchPinned import _CPUPytorchCommon
+from SpeedTorch.CPUTorchPinned import CPUPytorchModelFactory
+from SpeedTorch.CPUTorchPinned import CPUPytorchOptimizerFactory
+from SpeedTorch.CPUTorchPinned import CPUPytorchCOM
+
+from SpeedTorch.GenTorch import _PytorchCommon
+from SpeedTorch.GenTorch import PytorchModelFactory
+from SpeedTorch.GenTorch import PytorchOptimizerFactory
+from SpeedTorch.GenTorch import PytorchCOM
