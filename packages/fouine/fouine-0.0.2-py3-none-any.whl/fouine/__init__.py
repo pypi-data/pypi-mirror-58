56e47ec750f552251e4f2152e39eb49ca1cb6258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:31 +0000

    Add package fouine/0.0.2/fouine-0.0.2-py3-none-any.whl

---
+++ packages/fouine/fouine-0.0.2-py3-none-any.whl/fouine/__init__.py
@@ -0,0 +1,7 @@
+"""fouine - A tiny download function that does the maximum"""
+
+from fouine._download import download
+
+__version__ = "0.0.2"
+__author__ = "Loïc Grobol <loic.grobol@gmail.com>"
+__all__ = [download]
