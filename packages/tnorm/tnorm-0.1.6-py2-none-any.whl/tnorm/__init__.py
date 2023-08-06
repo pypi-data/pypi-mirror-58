3f032a6f80f5dfdcf1643f6449e1f72eff38f30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:04 +0000

    Add package tnorm/0.1.6/tnorm-0.1.6-py2-none-any.whl

---
+++ packages/tnorm/tnorm-0.1.6-py2-none-any.whl/tnorm/__init__.py
@@ -0,0 +1,16 @@
+name = "tnorm"
+
+import sys
+try:
+    sys.path.remove('/Applications/Regina.app/Contents/MacOS/python')
+except ValueError:
+    pass
+
+from tnorm.version import __version__
+
+import tnorm.kernel
+import tnorm.GUI
+
+from tnorm.TN_wrapper import TN_wrapper as load
+
+#IN_SAGE=False
