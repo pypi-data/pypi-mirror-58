84771ba1579b78109ffd19cdd5750575a932046f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:43 +0000

    Add package jinja2-error/0.1.0/jinja2_error-0.1.0-py3-none-any.whl

---
+++ packages/jinja2-error/jinja2_error-0.1.0-py3-none-any.whl/jinja2_error/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .jinja2_error import ErrorExtension
+
+__author__ = 'Robin Hu'
+__email__ = 'given.hubin@gmail.com'
+__version__ = '0.1.0'
+
+
+__all__ = ['ErrorExtension']
