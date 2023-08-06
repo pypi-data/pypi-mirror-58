16187023aa1d8378a503c3a7e49a9bcbbc83654d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:14 +0000

    Add package distributions-jk/0.1/distributions-jk-0.1.tar.gz

---
+++ packages/distributions-jk/distributions-jk-0.1.tar.gz/distributions-jk-0.1/README.md
@@ -0,0 +1,11 @@
+# dsnd-distributions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Author
+
+Jiyang Kang (jiyang71@gmail.com)
---
+++ packages/distributions-jk/distributions-jk-0.1.tar.gz/distributions-jk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-jk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-jk'],
+      zip_safe=False)
