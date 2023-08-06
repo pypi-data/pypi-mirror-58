3290d39baaacd477f08bfe4925aa15efc174221b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:20 +0000

    Add package distributions-tl/0.1/distributions_tl-0.1.tar.gz

---
+++ packages/distributions-tl/distributions_tl-0.1.tar.gz/distributions_tl-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_tl',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['distributions_tl'],
+      author='Teanlouise',
+      zip_safe=False)
