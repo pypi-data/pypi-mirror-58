726904216c7ac6ab848a008f12c161e19ee3bf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:51 +0000

    Add package mapsbr/0.0.1/mapsbr-0.0.1.tar.gz

---
+++ packages/mapsbr/mapsbr-0.0.1.tar.gz/mapsbr-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import distutils
+
+long_description = """
+MapsBR - Getting brazilian geospatial data with Python
+
+This package helps you get brazilian geospatial data from IBGE sources directly into a GeoPandas structure. It also has functionalities to retrieve data from an ArcGIS host.
+
+Learn more about it in our [repository](https://github.com/phelipetls/mapsbr) or [documentation](https://mapsbr.readthedocs.io).
+"""
+
+setuptools.setup(
+    name="mapsbr",
+    version="0.0.1",
+    author="Phelipe Teles",
+    author_email="phelipe_teles@hotmail.com",
+    description="Getting brazilian geospatial data with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phelipetls/mapsbr",
+    packages=setuptools.find_packages(),
+    install_requires=["geopandas", "requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    project_urls={
+        "Documentation": "https://mapsbr.readthedocs.io/",
+    },
+)
