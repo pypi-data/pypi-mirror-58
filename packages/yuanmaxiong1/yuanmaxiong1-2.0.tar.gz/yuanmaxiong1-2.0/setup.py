880dba0786d7d4f65dd70b7d815cbffc8629044a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:53 +0000

    Add package yuanmaxiong1/2.0/yuanmaxiong1-2.0.tar.gz

---
+++ packages/yuanmaxiong1/yuanmaxiong1-2.0.tar.gz/yuanmaxiong1-2.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open('README.md', 'r', encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='yuanmaxiong1',
+                 version='2.0',
+                 description='markdown editor',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 author='Wangzhe',
+                 author_email='markdown@felinae.net',
+                 url='https://markdown.felinae.net',
+                 keywords='django markdown editor editormd',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False,
+                 include_package_data=True,
+                 classifiers=(
+                     "Programming Language :: Python",
+                     "Development Status :: 4 - Beta",
+                     "Operating System :: OS Independent",
+                     "License :: OSI Approved :: Apache Software License",
+                     "Framework :: Django"
+                 ))
