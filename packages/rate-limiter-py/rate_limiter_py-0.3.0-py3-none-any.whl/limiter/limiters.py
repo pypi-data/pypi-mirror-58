51f67b9ee852a1de0162f8985e7fcee912491e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:24 +0000

    Add package rate-limiter-py/0.3.0/rate_limiter_py-0.3.0-py3-none-any.whl

---
+++ packages/rate-limiter-py/rate_limiter_py-0.3.0-py3-none-any.whl/limiter/limiters.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+from limiter.utils import validate_table_env_fallback
+from limiter.managers import FungibleTokenManager, NonFungibleTokenManager
+
+class BaseTokenLimiter:
+    """
+    Base class for both fungible and non-fungible token limiters.
+
+    Args:
+        resource_name (str): Name of the resource being rate-limited.
+        default_limit (str): Number of tokens to use if no explicit limit is defined in the limits table.
+    """
+    def __init__(self, resource_name, default_limit):
+        self.resource_name = resource_name
+        self.default_limit = default_limit
+        self._manager = None
+
+class BaseFungibleTokenLimiter(BaseTokenLimiter):
+    """
+    Extensions of this class provide the most convient usages for rate limiting implementations, e.g. a decorator
+
+    Args:
+        resource_name (str): Name of the resource being rate-limited.
+        default_limit (str): Number of tokens to use if no explicit limit is defined in the limits table.
+        default_window (str): Sliding window of time, in sec, if no explicit limit is defined in the limits table.
+        token_table (str): Name of the DynamoDB table containing tokens.
+                           Can be set via environment variable `FUNGIBLE_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+        limit_table (str): Name of the DynamoDB table containing limits.
+                           Can be set via environment variable `LIMIT_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+    """
+    def __init__(self, resource_name, default_limit, default_window, token_table=None, limit_table=None):
+        super(BaseFungibleTokenLimiter, self).__init__(resource_name, default_limit)
+
+        self.token_table = validate_table_env_fallback(token_table, 'FUNGIBLE_TABLE', 'fungible-tokens')
+        self.limit_table = validate_table_env_fallback(limit_table, 'LIMIT_TABLE', 'limits')
+        self.default_window = default_window
+
+    @property
+    def manager(self):
+        """ Fungible token manager """
+        if not self._manager:
+            self._manager = FungibleTokenManager(self.token_table,
+                                                 self.limit_table,
+                                                 self.resource_name,
+                                                 self.default_limit,
+                                                 self.default_window)
+        return self._manager
+
+class FungibleTokenContextManager(BaseFungibleTokenLimiter):
+    """
+    Fungible token rate-limiter implemented as a context manager.
+
+    Args:
+        resource_name (str): Name of the resource being rate-limited.
+        account_id (str): The account to retrieve a token on behalf of.
+        default_limit (str): Number of tokens to use if no explicit limit is defined in the limits table.
+        default_window (str): Sliding window of time, in sec, if no explicit limit is defined in the limits table.
+        token_table (str): Name of the DynamoDB table containing tokens.
+                           Can be set via environment variable `FUNGIBLE_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+        limit_table (str): Name of the DynamoDB table containing limits.
+                           Can be set via environment variable `LIMIT_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+
+    Note:
+        This class is exported with the more succinct name `fungible_limiter`
+
+    Examples:
+        This example assumes `token_table` and `limit_table` have been set via environment variables.
+
+        >>> from limiter import fungible_limiter
+        >>> with fungible_limiter('my-resource', 'my-account', 10, 1):
+        ...   my_rate_limited_func()
+        ...   print 'Done!'
+        Done!
+    """
+    def __init__(self,
+                 resource_name,
+                 account_id,
+                 default_limit,
+                 default_window,
+                 token_table=None,
+                 limit_table=None):
+        super(FungibleTokenContextManager, self).__init__(resource_name, default_limit, default_window, token_table,
+                                                          limit_table)
+        self.account_id = account_id
+
+    def __enter__(self):
+        self.get_token()
+
+    def __exit__(self, *args):
+        pass
+
+    def get_token(self):
+        """ Check the limit and claim a token """
+        self.manager.get_token(self.account_id)
+
+class FungibleTokenLimiterDecorator(BaseFungibleTokenLimiter):
+    """
+    Fungible token rate-limiter implemented as a decorator.
+
+    This decorator requires the account id to be an arguement (positional or keyword) of the function being
+    decorated. The location of the account id arguement is set either with `account_id_pos` or `account_id_key`.
+
+    Note:
+        This class is exported with the more succinct name `rate_limit`.
+
+    Args:
+        resource_name (str): Name of the resource being rate-limited.
+        default_limit (str): Number of tokens to use if no explicit limit is defined in the limits table.
+        default_window (str): Sliding window of time, in sec, if no explicit limit is defined in the limits table.
+        token_table (str): Name of the DynamoDB table containing tokens.
+                           Can be set via environment variable `FUNGIBLE_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+        limit_table (str): Name of the DynamoDB table containing limits.
+                           Can be set via environment variable `LIMIT_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+        account_id_pos (int): Index of the account id in the args of the function being decorated. Defaults to None.
+        account_id_key (str): Key of the account id in the kwargs of the function being decorated.
+                              Defaults to `account_id`.
+
+    Examples:
+        These examples assume `token_table` and `limit_table` have been set via environment variables.
+
+        >>> from limiter import rate_limit
+        >>>
+        >>> @rate_limit('my-resource', 10, 1, account_id_pos=1)
+        ... def first_func(arg_1, account_id)
+        ...   print 'In first_func'
+        >>>
+        >>> @rate_limit('my-resource', 10, 1, account_id_key=my_account_id)
+        ... def second_func(arg_1, my_account_id='my-account')
+        ...   print 'In second_func'
+        >>>
+        >>> first_func('foo', 'my-account')
+        In first_func
+        >>> second_func('bar', 'my-account')
+        In second_func
+    """
+    def __init__(self,
+                 resource_name,
+                 default_limit,
+                 default_window,
+                 token_table=None,
+                 limit_table=None,
+                 account_id_pos=None,
+                 account_id_key='account_id'):
+        super(FungibleTokenLimiterDecorator, self).__init__(resource_name, default_limit, default_window, token_table,
+                                                            limit_table)
+        self.is_account_id_kwarg = account_id_pos is None
+        self.account_id_index = account_id_key if self.is_account_id_kwarg else account_id_pos
+
+    def __call__(self, func_to_limit):
+        def rate_limited_func(*args, **kwargs):
+            """
+            Extract the account id from the decorated function arguments, fetch a token and call the decorated function.
+
+            Args:
+              *args (list): Positional arguments for the decorated function.
+              **kwargs (dict): Keyword arguments for the decorated function.
+
+            Returns:
+                function: The decorated function wrapped in the account id extraction and token retrieval steps.
+            """
+            account_id = kwargs[self.account_id_index] if self.is_account_id_kwarg else args[self.account_id_index]
+            self.manager.get_token(account_id)
+            return func_to_limit(*args, **kwargs)
+        return rate_limited_func
+
+class NonFungibleTokenLimiterContextManager(BaseTokenLimiter):
+    """
+    Non-fungible token rate-limiter implemented as a context manager.
+
+    This class does not create tokens. Rather, it creates instances of TokenReservation which are able to create
+    a single token. If the context is exited due to an exception, the token reservation will be deleted and the
+    exception propogated.
+
+    Args:
+        resource_name (str): Name of the resource being rate-limited.
+        default_limit (str): Number of tokens to use if no explicit limit is defined in the limits table.
+        token_table (str): Name of the DynamoDB table containing tokens.
+                           Can be set via environment variable `FUNGIBLE_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+        limit_table (str): Name of the DynamoDB table containing limits.
+                           Can be set via environment variable `LIMIT_TABLE`
+                           or synthesized using the `LIMITER_TABLES_BASE_NAME` environment variable.
+
+    Note:
+        This class is exported with the more succinct name `non_fungible_limiter`
+
+    Examples:
+        This example assumes `token_table` and `limit_table` have been set via environment variables.
+
+        >>> from limiter import non_fungible_limiter
+        >>> with non_fungible_limiter('my-resource', 'my-account', 10) as reservation:
+        ...   emr_cluster_id = create_emr_cluster()
+        ...   reservation.create_token(emr_cluster_id)
+        ...   print 'Done!'
+        Done!
+    """
+    def __init__(self,
+                 resource_name,
+                 account_id,
+                 default_limit,
+                 token_table=None,
+                 limit_table=None):
+        super(NonFungibleTokenLimiterContextManager, self).__init__(resource_name, default_limit)
+
+        self.account_id = account_id
+        self.token_table = validate_table_env_fallback(token_table, 'NON_FUNGIBLE_TABLE', 'non-fungible-tokens')
+        self.limit_table = validate_table_env_fallback(limit_table, 'LIMIT_TABLE', 'limits')
+
+        self.reservation = None
+
+    @property
+    def manager(self):
+        """ Non-fungible token manager """
+        if not self._manager:
+            self._manager = NonFungibleTokenManager(self.token_table, self.limit_table, self.resource_name,
+                                                    self.default_limit)
+        return self._manager
+
+    def __enter__(self):
+        self.reservation = self.get_reservation()
+        return self.reservation
+
+    def __exit__(self, *args):
+        if any(args):
+            print(str(args))
+            self.reservation.delete()
+
+    def get_reservation(self):
+        """ Check the limit and create a reservation """
+        return self.manager.get_reservation(self.account_id)
+
+non_fungible_context_manager = NonFungibleTokenLimiterContextManager
+fungible_context_manager = FungibleTokenContextManager
+decorator = FungibleTokenLimiterDecorator
---
+++ packages/rate-limiter-py/rate_limiter_py-0.3.0-py3-none-any.whl/test/test_managers.py
@@ -0,0 +1,364 @@
+#!/usr/bin/env python
+import uuid
+import random
+from unittest import TestCase
+from test.utils import random_string, now_utc_sec, now_utc_ms, create_non_fung_table, create_limit_table
+from moto import mock_dynamodb2
+from mock import Mock, MagicMock
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+from limiter.managers import FungibleTokenManager, NonFungibleTokenManager, TokenReservation, _compute_refill_amount
+from limiter.exceptions import CapacityExhaustedException
+
+class FungibleTokenManagerTest(TestCase):
+    def setUp(self):
+        self.token_table = random_string()
+        self.limit_table = random_string()
+        self.resource_name = random_string()
+
+        self.limit = 10
+        self.window = 100
+        self.window_ms = self.window * 1000
+        self.token_ms = float(self.limit) / self.window_ms
+        self.ms_token = float(self.window_ms) / self.limit
+
+        self.manager = FungibleTokenManager(self.token_table, self.limit_table, self.resource_name, self.limit,
+                                            self.window)
+
+    def test_compute_refill_amount(self):
+        current_tokens = 5
+        time_since_refill = 30000
+
+        expected = 8
+        actual = _compute_refill_amount(current_tokens, time_since_refill, self.limit, self.token_ms)
+
+        self.assertEqual(expected, actual)
+
+    def test_compute_refill_amount_negative_balance(self):
+        current_tokens = -7
+        time_since_refill = 30000
+
+        expected = 3
+        actual = _compute_refill_amount(current_tokens, time_since_refill, self.limit, self.token_ms)
+
+        self.assertEqual(expected, actual)
+
+    def test_compute_refill_amount_refill_lag(self):
+        current_tokens = 0
+        time_since_refill = 11500000
+
+        expected = self.limit - 1
+        actual = _compute_refill_amount(current_tokens, time_since_refill, self.limit, self.token_ms)
+        self.assertEqual(expected, actual)
+
+    def test_get_bucket_token(self):
+        account_id = random_string()
+        exec_time = now_utc_ms()
+
+        expected = {'tokens': 5, 'last_refill': now_utc_sec()}
+        response = {'Attributes': expected}
+        mock_token_table = Mock()
+        mock_token_table.update_item = MagicMock(return_value=response)
+        self.manager._token_table = mock_token_table
+
+        actual = self.manager._get_bucket_token(account_id, exec_time, self.ms_token)
+        self.assertEqual(expected, actual)
+
+        actual_args = mock_token_table.update_item.call_args_list
+        self.assertEqual(1, len(actual_args))
+
+        expected_args = [
+            ({
+                'Key': {
+                    'resourceName': self.resource_name,
+                    'accountId': account_id
+                },
+                'UpdateExpression': 'add tokens :dec set lastToken = :exec_time',
+                'ConditionExpression': 'tokens > :min OR lastToken < :failsafe OR attribute_not_exists(tokens)',
+                'ExpressionAttributeValues': {
+                    ':dec': -1,
+                    ':min': 0,
+                    ':failsafe': exec_time - self.ms_token,
+                    ':exec_time': exec_time
+                },
+                'ReturnValues': 'ALL_NEW'
+            })
+        ]
+
+        self.assertEqual(expected_args, actual_args[0])
+
+    def test_get_bucket_token_exhausted(self):
+        exec_time = now_utc_ms()
+        account_id = random_string()
+        error_response = {
+            'Error': {
+                'Code': 'ConditionalCheckFailedException'
+            }
+        }
+
+        mock_token_table = Mock()
+        mock_token_table.update_item = MagicMock(side_effect=ClientError(error_response, None))
+        self.manager._token_table = mock_token_table
+
+        self.assertRaises(CapacityExhaustedException,
+                          self.manager._get_bucket_token, account_id, exec_time, self.ms_token)
+
+    def test_refill_bucket_tokens(self):
+        account_id = random_string()
+        tokens = 8
+        refill_time = now_utc_ms()
+
+        mock_token_table = Mock()
+        mock_token_table.update_item = Mock()
+        self.manager._token_table = mock_token_table
+
+        self.manager._refill_bucket_tokens(account_id, tokens, refill_time)
+
+        actual_args = mock_token_table.update_item.call_args_list
+        self.assertEqual(1, len(actual_args))
+
+        expected_args = [
+            ({
+                'Key': {
+                    'resourceName': self.resource_name,
+                    'accountId': account_id
+                },
+                'UpdateExpression': 'set tokens = :tokens, lastRefill = :refill_time',
+                'ConditionExpression': 'lastRefill < :refill_time OR attribute_not_exists(lastRefill)',
+                'ExpressionAttributeValues': {
+                    ':tokens': tokens,
+                    ':refill_time': refill_time
+                },
+                'ReturnValues': 'NONE'
+            })
+        ]
+
+        self.assertEqual(expected_args, actual_args[0])
+
+    @mock_dynamodb2
+    def test_account_resource_limit(self):
+        limit = random.randint(0, 100)
+        window = random.randint(0, 100)
+        account_id = random_string()
+
+        mock_limit_table = create_limit_table(self.limit_table)
+        _insert_limit(mock_limit_table, self.resource_name, account_id, limit, window)
+
+        self.manager._limit_table = mock_limit_table
+        result = self.manager._get_account_resource_limit(account_id)
+
+        self.assertEqual(limit, result['limit'])
+        self.assertEqual(window, result['windowSec'])
+
+    @mock_dynamodb2
+    def test_account_resource_limit_defaults(self):
+        account_id = random_string()
+
+        mock_limit_table = create_limit_table(self.limit_table)
+        self.manager._limit_table = mock_limit_table
+
+        result = self.manager._get_account_resource_limit(account_id)
+        self.assertEqual(self.limit, result['limit'])
+        self.assertEqual(self.window, result['windowSec'])
+
+    @mock_dynamodb2
+    def test_account_resource_limit_blacklist(self):
+        limit = 0
+        window = random.randint(0, 100)
+        account_id = random_string()
+
+        mock_limit_table = create_limit_table(self.limit_table)
+        _insert_limit(mock_limit_table, self.resource_name, account_id, limit, window)
+        self.manager._limit_table = mock_limit_table
+
+        self.assertRaises(CapacityExhaustedException, self.manager._get_account_resource_limit, account_id)
+
+class NonFungibleTokenManagerTest(TestCase):
+    def setUp(self):
+        self.token_table = random_string()
+        self.limit_table = random_string()
+        self.resource_name = random_string()
+
+        self.limit = 5
+        self.manager = NonFungibleTokenManager(self.token_table, self.limit_table, self.resource_name, self.limit)
+
+    @mock_dynamodb2
+    def test_get_reservation(self):
+        now = now_utc_sec()
+        account_id = random_string()
+        coordinate = '{}:{}'.format(self.resource_name, account_id)
+
+        mock_token_table = create_non_fung_table(self.token_table)
+        mock_limit_table = create_limit_table(self.limit_table)
+        _insert_limit(mock_limit_table, self.resource_name, account_id, self.limit)
+
+        self.manager._token_table = mock_token_table
+        self.manager._limit_table = mock_limit_table
+
+        self.manager.get_reservation(account_id)
+
+        response = mock_token_table.query(KeyConditionExpression=Key('resourceCoordinate').eq(coordinate))
+        self.assertEqual(1, response['Count'])
+
+        items = dict(pair for item in response['Items'] for pair in item.items())
+        self.assertEqual(self.resource_name, items['resourceName'])
+        self.assertEqual(account_id, items['accountId'])
+        self.assertTrue(items['expirationTime'] > now)
+        self.assertIn('resourceId', items)
+
+    @mock_dynamodb2
+    def test_get_reservation_exhausted(self):
+        now = now_utc_sec()
+        account_id = random_string()
+        coordinate = '{}:{}'.format(self.resource_name, account_id)
+
+        mock_token_table = create_non_fung_table(self.token_table)
+        mock_limit_table = create_limit_table(self.limit_table)
+        _insert_limit(mock_limit_table, self.resource_name, account_id, self.limit)
+
+        self.manager._token_table = mock_token_table
+        self.manager._limit_table = mock_limit_table
+
+        # Insert enough tokens to reach the limit
+        for i in range(0, self.limit):
+            token = {
+                'resourceCoordinate': coordinate,
+                'resourceName': self.resource_name,
+                'accountId': account_id,
+                'resourceId': 'resource-' + str(i),
+                'expirationTime': now + 10000,
+                'reservationId': random_string()
+            }
+            mock_token_table.put_item(Item=token)
+
+        self.assertRaises(CapacityExhaustedException, self.manager.get_reservation, account_id)
+
+    @mock_dynamodb2
+    def test_get_token_count(self):
+        now = now_utc_sec()
+        account_id = random_string()
+        coordinate = '{}:{}'.format(self.resource_name, account_id)
+
+        mock_token_table = create_non_fung_table(self.token_table)
+        mock_limit_table = create_limit_table(self.limit_table)
+        _insert_limit(mock_limit_table, self.resource_name, account_id, self.limit)
+
+
+        expected_count = 1
+
+        # Insert 2 expired and 1 valid
+        expired_token_1 = {
+            'resourceCoordinate': coordinate,
+            'resourceName': self.resource_name,
+            'accountId': account_id,
+            'resourceId': random_string(),
+            'expirationTime': now,
+            'reservationId': random_string()
+        }
+
+        expired_token_2 = {
+            'resourceCoordinate': coordinate,
+            'resourceName': self.resource_name,
+            'accountId': account_id,
+            'resourceId': random_string(),
+            'expirationTime': now - 1000,
+            'reservationId': random_string()
+        }
+
+        valid_token = {
+            'resourceCoordinate': coordinate,
+            'resourceName': self.resource_name,
+            'accountId': account_id,
+            'resourceId': random_string(),
+            'expirationTime': now + 300,
+            'reservationId': random_string()
+        }
+
+        mock_token_table.put_item(Item=expired_token_1)
+        mock_token_table.put_item(Item=expired_token_2)
+        mock_token_table.put_item(Item=valid_token)
+
+        self.manager._token_table = mock_token_table
+        self.manager._limit_table = mock_limit_table
+        actual_count = self.manager._get_token_count(account_id, now)
+
+        self.assertEqual(expected_count, actual_count)
+
+    @mock_dynamodb2
+    def test_get_token_count_no_tokens(self):
+        now = now_utc_sec()
+        account_id = random_string()
+
+        mock_token_table = create_non_fung_table(self.token_table)
+        mock_limit_table = create_limit_table(self.limit_table)
+        _insert_limit(mock_limit_table, self.resource_name, account_id, self.limit)
+
+        self.manager._token_table = mock_token_table
+        self.manager._limit_table = mock_limit_table
+
+        expected_count = 0
+        actual_count = self.manager._get_token_count(account_id, now)
+
+        self.assertEqual(expected_count, actual_count)
+
+class TokenReservationTest(TestCase):
+    def setUp(self):
+        self.table_name = random_string()
+        self.res_id = str(uuid.uuid4())
+        self.resource_name = random_string()
+        self.account_id = random_string()
+        self.coordinate = '{}:{}'.format(self.resource_name, self.account_id)
+
+    @mock_dynamodb2
+    def test_delete_reservation(self):
+        mock_token_table = create_non_fung_table(self.table_name)
+
+        reserve = TokenReservation(self.res_id, mock_token_table, self.resource_name, self.account_id, self.coordinate)
+        _insert_reservation(mock_token_table, reserve)
+
+        response = mock_token_table.query(KeyConditionExpression=Key('resourceCoordinate').eq(self.coordinate))
+        self.assertEqual(1, response['Count'])
+
+        reserve.delete()
+
+        response = mock_token_table.query(KeyConditionExpression=Key('resourceCoordinate').eq(self.coordinate))
+        self.assertEqual(0, response['Count'])
+
+    def test_create_after_delete(self):
+        mock_token_table = Mock()
+        mock_token_table.delete_item = Mock()
+        mock_token_table.update_item = Mock()
+        reserve = TokenReservation(self.res_id, mock_token_table, self.resource_name, self.account_id, self.coordinate)
+
+        reserve.delete()
+        self.assertRaises(ValueError, reserve.create_token, random_string())
+
+    def test_double_create(self):
+        mock_token_table = Mock()
+        mock_token_table.delete_item = Mock()
+        mock_token_table.update_item = Mock()
+        reserve = TokenReservation(self.res_id, mock_token_table, self.resource_name, self.account_id, self.coordinate)
+
+        reserve.create_token(random_string())
+        self.assertRaises(ValueError, reserve.create_token, random_string())
+
+def _insert_reservation(mock_token_table, reservation):
+    reservation_item = {
+        'resourceCoordinate': reservation.coordinate,
+        'resourceName': reservation.resource_name,
+        'accountId': reservation.account_id,
+        'resourceId': reservation.id,
+        'reservationId': reservation.id,
+        'expirationTime': now_utc_sec() + 300
+    }
+    mock_token_table.put_item(Item=reservation_item)
+
+def _insert_limit(mock_limit_table, resource_name, account_id, limit, window_sec=0):
+    limit_item = {
+        'resourceName': resource_name,
+        'accountId': account_id,
+        'limit': limit,
+        'windowSec': window_sec,
+        'serviceName': random_string()
+    }
+    mock_limit_table.put_item(Item=limit_item)
