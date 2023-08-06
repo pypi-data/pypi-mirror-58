550f8e65f11b24ef78f965e5491dd6797a6cb091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package ullvm/0.0/ullvm-0.0.tar.gz

---
+++ packages/ullvm/ullvm-0.0.tar.gz/ullvm-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="ullvm",
+      version="0.0",
+      description="""Lightweight bindings for LLVM C API""",
+      url='https://github.com/pfalcon/ullvm',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['ullvm'])
