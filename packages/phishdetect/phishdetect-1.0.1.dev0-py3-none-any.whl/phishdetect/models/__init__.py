87d35b01bb7c08c3ea49ccafde93f6a5cf0da71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:40 +0000

    Add package phishdetect/1.0.1.dev0/phishdetect-1.0.1.dev0-py3-none-any.whl

---
+++ packages/phishdetect/phishdetect-1.0.1.dev0-py3-none-any.whl/phishdetect/models/__init__.py
@@ -0,0 +1,8 @@
+# This file is part of phishdetect-python:
+# https://github.com/phishdetect/phishdetect-python
+# See the file 'LICENSE' for copying permission.
+
+from .events import Events
+from .indicators import Indicators
+from .reports import Reports
+from .users import Users
