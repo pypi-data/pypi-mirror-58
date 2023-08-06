5c076eecc3212a23e8b1ecdf6c6a1fa65117da71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:23 +0000

    Add package dev-env/0.6.2/dev_env-0.6.2-py3-none-any.whl

---
+++ packages/dev-env/dev_env-0.6.2-py3-none-any.whl/dev_env-0.6.2.data/scripts/build-readme-dot-rst.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#
+# see the README.md in the same directory as this script for a
+# description of why this script exists what this script does
+#
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0")" >&2
+    exit 1
+fi
+
+REPO_ROOT_DIR=$("$SCRIPT_DIR_NAME/repo-root-dir.sh")
+
+DOCKER_CONTAINER_NAME=$(python3 -c "import uuid; print(uuid.uuid4().hex)")
+
+docker run \
+    --name "$DOCKER_CONTAINER_NAME" \
+    --volume "$REPO_ROOT_DIR:/app" \
+    "$DEV_ENV_DOCKER_IMAGE" \
+    pandoc "/app/README.md" -o "/tmp/README.rst"
+
+rm -f "$REPO_ROOT_DIR/README.rst"
+docker container cp "$DOCKER_CONTAINER_NAME:/tmp/README.rst" "$REPO_ROOT_DIR/README.rst"
+
+docker container rm "$DOCKER_CONTAINER_NAME"
+
+exit 0
