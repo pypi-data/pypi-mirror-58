c79fb71df481d68105cc31fcc6cacc940d756098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:05 +0000

    Add package spider3/1.0.2/spider3-1.0.2.tar.gz

---
+++ packages/spider3/spider3-1.0.2.tar.gz/spider3-1.0.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name='spider3',
+  version='1.0.2',
+  author='hao.liu',
+  author_email='l.hao.2012@qq.com',
+  description='opener',
+  url='',
+  # packages=['spider']
+  packages=setuptools.find_packages(),
+  python_requires='>=3.7'
+)
---
+++ packages/spider3/spider3-1.0.2.tar.gz/spider3-1.0.2/spider3/__init__.py
@@ -0,0 +1,4 @@
+
+from spider3.proxy import handle_opener
+
+__ALL__ = ['handle_opener']
