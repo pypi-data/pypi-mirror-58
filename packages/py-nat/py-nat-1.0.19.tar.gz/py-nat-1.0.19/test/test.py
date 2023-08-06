a814ed793029ea1eb423af5f4fc490de3619fcef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:17 +0000

    Add package py-nat/1.0.19/py-nat-1.0.19.tar.gz

---
+++ packages/py-nat/py-nat-1.0.19.tar.gz/py-nat-1.0.19/test/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+#
+# @author: natloc_developer
+# @date: 2020/1/2
+# @time: 15:03
+# @doc:
+# Copyright © 2020 natloc_developer. All rights reserved.
+#
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(sys.path[0]))
+print(sys.path[0])
+
+import nat.datetime
+
