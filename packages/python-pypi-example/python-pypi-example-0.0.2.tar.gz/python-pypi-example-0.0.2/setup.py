0ced6f471c0a174680f8c848c7b189607059560a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:54 +0000

    Add package python-pypi-example/0.0.2/python-pypi-example-0.0.2.tar.gz

---
+++ packages/python-pypi-example/python-pypi-example-0.0.2.tar.gz/python-pypi-example-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+package_name = 'python-pypi-example'
+version = '0.0.2'
+classifiers = [
+    'Development Status :: 1 - Planning',
+
+    'Intended Audience :: Developers',
+
+    'Programming Language :: Python :: 3.6',
+]
+
+setup(
+    name=package_name,
+    version=version,
+    classifiers=classifiers,
+    author='Guy King',
+    author_email='grking8@gmail.com',
+    license='MIT',
+    url='https://github.com/family-guy/python-pypi-example.git',
+)
