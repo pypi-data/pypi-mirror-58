aa644627aae42cfb2bba443d0da6e9df20c519f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:33 +0000

    Add package features-better-logger/0.1/features_better_logger-0.1.tar.gz

---
+++ packages/features-better-logger/features_better_logger-0.1.tar.gz/features_better_logger-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name="features_better_logger",
+    packages=["features_better_logger"],
+    version="0.1",
+    license="MIT",
+    description="Find Features Better Logging Package",
+    author="Calum Webb",
+    author_email="calumpeterwebb@icloud.com",
+    url="https://github.com/findfeatures/better-logger",
+    download_url="https://github.com/findfeatures/better-logger/archive/v0.1.tar.gz",
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
