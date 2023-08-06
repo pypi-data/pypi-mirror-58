ad35f664a19cd92233812e6035233da712c19d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:37 +0000

    Add package pyleap/0.2.17/pyleap-0.2.17.tar.gz

---
+++ packages/pyleap/pyleap-0.2.17.tar.gz/pyleap-0.2.17/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# pip install twine wheel
+# python setup.py sdist bdist_wheel
+# twine upload dist/* -u VicWang -p PYPI******
+
+setuptools.setup(
+    name="pyleap",
+    version="0.2.17",
+    author="Vic Wang",
+    author_email="305880887@qq.com",
+    description='A simple 2D game engine base on pyglet',
+    long_description='A simple 2D game engine base on pyglet',
+    long_description_content_type="text/markdown",
+    url='http://github.com/XRDX/pyleap',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyglet==1.3.2',
+        'leapserver>=0.0.6',
+        'leaptask>=0.0.20'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
