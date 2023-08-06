7b972db2c62df52f9281a5ef72041e18801f5af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:08 +0000

    Add package bpp-iplweb/202001.14/bpp_iplweb-202001.14-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202001.14-py3-none-any.whl/django_bpp/version.py
@@ -0,0 +1,12 @@
+VERSION = "202001.14"
+
+if __name__ == "__main__":
+    import sys
+
+    OUTPUT = VERSION
+
+    if "--json" in sys.argv:
+        import json
+        OUTPUT = json.dumps({"VERSION": VERSION})
+
+    sys.stdout.write(OUTPUT)
