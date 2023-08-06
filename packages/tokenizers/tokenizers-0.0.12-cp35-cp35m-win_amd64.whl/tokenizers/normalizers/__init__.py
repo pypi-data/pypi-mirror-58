f0392466b07c85a6dce4383f2ea4286e2b03cfcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:22 +0000

    Add package tokenizers/0.0.12/tokenizers-0.0.12-cp35-cp35m-win_amd64.whl

---
+++ packages/tokenizers/tokenizers-0.0.12-cp35-cp35m-win_amd64.whl/tokenizers/normalizers/__init__.py
@@ -0,0 +1,9 @@
+from .. import normalizers
+
+Normalizer = normalizers.Normalizer
+BertNormalizer = normalizers.BertNormalizer
+NFD = normalizers.NFD
+NFKD = normalizers.NFKD
+NFC = normalizers.NFC
+NFKC = normalizers.NFKC
+Sequence = normalizers.Sequence
