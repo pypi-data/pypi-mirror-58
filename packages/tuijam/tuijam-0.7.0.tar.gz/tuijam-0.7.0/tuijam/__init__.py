4ad867fa2c02c0ced4f4607f48d5ca1deeb322dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:13 +0000

    Add package tuijam/0.7.0/tuijam-0.7.0.tar.gz

---
+++ packages/tuijam/tuijam-0.7.0.tar.gz/tuijam-0.7.0/tuijam/__init__.py
@@ -0,0 +1,12 @@
+from os.path import join, expanduser
+import gettext
+_ = gettext.gettext
+
+__version__ = "0.7.0"
+CONFIG_DIR = join(expanduser("~"), ".config", "tuijam")
+CONFIG_FILE = join(CONFIG_DIR, "config.yaml")
+LOG_FILE = join(CONFIG_DIR, "log.txt")
+HISTORY_FILE = join(CONFIG_DIR, "hist.json")
+QUEUE_FILE = join(CONFIG_DIR, "queue.json")
+CRED_FILE = join(CONFIG_DIR, "google_oauth.cred")
+LOCALE_DIR = join(CONFIG_DIR, "lang")
