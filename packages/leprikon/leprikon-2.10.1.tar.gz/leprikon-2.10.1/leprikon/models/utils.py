42f8394f6e41d8057fff85307c9b43b56789faa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:20 +0000

    Add package leprikon/2.10.1/leprikon-2.10.1.tar.gz

---
+++ packages/leprikon/leprikon-2.10.1.tar.gz/leprikon-2.10.1/leprikon/models/utils.py
@@ -0,0 +1,114 @@
+from collections import namedtuple
+
+from django.utils.functional import cached_property, lazy
+from django.utils.translation import ugettext_lazy as _
+
+from ..conf import settings
+from ..utils import currency, paragraph
+
+
+class PaymentStatus(namedtuple('_PaymentsStatus', ('price', 'discount', 'paid'))):
+
+    @property
+    def receivable(self):
+        return self.price - self.discount
+
+    @property
+    def balance(self):
+        return self.paid - self.receivable
+
+    @property
+    def color(self):
+        if self.balance == 0:
+            return settings.LEPRIKON_COLOR_PAID
+        elif self.balance < 0:
+            return settings.LEPRIKON_COLOR_NOTPAID
+        else:
+            return settings.LEPRIKON_COLOR_OVERPAID
+
+    @property
+    def title(self):
+        if self.balance == 0:
+            return _('paid')
+        elif self.balance < 0:
+            return _('{} let to pay').format(currency(-self.balance))
+        else:
+            return _('{} overpaid').format(currency(self.balance))
+
+    def __repr__(self):
+        return 'PaymentStatus(price={price}, discount={discount}, paid={paid}, balance={balance})'.format(
+            price=self.price,
+            discount=self.discount,
+            paid=self.paid,
+            balance=self.balance,
+        )
+
+    def __str__(self):
+        return '<strong title="{title}" style="color: {color}">{balance}</strong>'.format(
+            color=self.color,
+            balance=currency(self.balance),
+            title=self.title,
+        )
+
+    def __add__(self, other):
+        if other == 0:
+            return self
+        return PaymentStatus(
+            price=self.price + other.price,
+            discount=self.discount + other.discount,
+            paid=self.paid + other.paid,
+        )
+
+    __radd__ = __add__
+
+
+class BankAccount:
+    def __init__(self, iban):
+        self.iban = iban
+
+    @cached_property
+    def country_code(self):
+        return self.iban[:2]
+
+    @cached_property
+    def bank_code(self):
+        return self.iban[4:8]
+
+    @cached_property
+    def account_prefix(self):
+        return self.iban[8:14].lstrip('0')
+
+    @cached_property
+    def account_number(self):
+        return self.iban[14:].lstrip('0')
+
+    def __str__(self):
+        return '%s%s%s/%s' % (
+            self.account_prefix,
+            self.account_prefix and '-',
+            self.account_number,
+            self.bank_code,
+        )
+
+
+def generate_variable_symbol(registration):
+    # get base variable symol from the configured expression
+    variable_symbol = eval(settings.LEPRIKON_VARIABLE_SYMBOL_EXPRESSION, {'reg': registration})
+
+    # add check digit
+    odd_sum = 0
+    even_sum = 0
+    for i, char in enumerate(str(variable_symbol)):
+        if i % 2:
+            even_sum += int(char)
+        else:
+            odd_sum += int(char)
+    check_digit = (odd_sum * 3 + even_sum) % 10
+    return variable_symbol * 10 + check_digit
+
+
+def html_help_text_with_default(default):
+    return paragraph(_('Keep empty to use default value:\n{}').format(default))
+
+
+lazy_html_help_text_with_default = lazy(html_help_text_with_default, str)
