67927ef9e2b9356490937b0e9cc257fb58221bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:41 +0000

    Add package ward/0.21.0a0/ward-0.21.0a0.tar.gz

---
+++ packages/ward/ward-0.21.0a0.tar.gz/ward-0.21.0a0/setup.py
@@ -0,0 +1,32 @@
+import platform
+
+from setuptools import setup
+
+version = "0.21.0a0"
+description = "A modern Python 3 test framework for finding and fixing flaws faster."
+with open("README.md", "r") as fh:
+    if platform.system() != "Windows":
+        long_description = fh.read()
+    else:
+        long_description = description
+
+setup(
+    name="ward",
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/darrenburns/ward",
+    author="Darren Burns",
+    author_email="darrenb900@gmail.com",
+    license="MIT",
+    packages=["ward"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["ward=ward.run:run"]},
+    install_requires=[
+        "colorama==0.4.1",
+        "termcolor==1.1.0",
+        "dataclasses==0.6",
+        "click==7.0",
+    ],
+)
