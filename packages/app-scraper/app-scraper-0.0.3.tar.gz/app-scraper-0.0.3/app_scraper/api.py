482038f11ef18de34c2812257e0cad09967be537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:35 +0000

    Add package app-scraper/0.0.3/app-scraper-0.0.3.tar.gz

---
+++ packages/app-scraper/app-scraper-0.0.3.tar.gz/app-scraper-0.0.3/app_scraper/api.py
@@ -0,0 +1,15 @@
+# from app_scraper import scraper
+import scraper
+
+def details(app_id, hl="en", gl="us"):
+    """Sends a GET request to the app's info page, parses the app's details, and
+    returns them as a dict.
+    :param app_id: the app to retrieve details from, e.g. 'com.nintendo.zaaa'
+    :return: a dictionary of app details
+    """
+    s = scraper.AppScraper(hl, gl)
+    return s.details(app_id)
+
+if __name__ == "__main__":
+    crawler = scraper.AppScraper(website='xpose repo')
+    crawler.details('com.devadvance.rootcloak2')
