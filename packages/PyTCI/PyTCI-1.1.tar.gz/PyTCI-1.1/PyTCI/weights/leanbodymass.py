155d7bfcaaf14463dc357216241de9b3dbd61cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:17 +0000

    Add package PyTCI/1.1/PyTCI-1.1.tar.gz

---
+++ packages/PyTCI/PyTCI-1.1.tar.gz/PyTCI-1.1/PyTCI/weights/leanbodymass.py
@@ -0,0 +1,126 @@
+def james(height: float, weight: float, sex: str) -> float:
+    """ returns lean body mass as per james equations """
+    """James, W. "Research on obesity: a report of the DHSS/MRC group" HM Stationery Office 1976"""
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        return 1.1 * weight - 128 * ((weight / height) * (weight / height))
+    else:
+        return 1.07 * weight - 148 * ((weight / height) * (weight / height))
+
+
+def boer(height: float, weight: float, sex: str) -> float:
+    """ returns lean body mass as per Boer equation """
+    """ Boer P. "Estimated lean body mass as an index for normalization of body fluid volumes in man." Am J Physiol 1984; 247: F632-5"""
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        lbm = (0.407 * weight) + (0.267 * height) - 19.2
+    else:
+        lbm = (0.252 * weight) + (0.473 * height) - 48.3
+
+    return round(lbm, 1)
+
+
+def hume66(height: float, weight: float, sex: str) -> float:
+    """ returns lean body mass as per the 1966 Hume paper """
+    """ Hume, R "Prediction of lean body mass from height and weight.". J Clin Pathol. 1966 Jul; 19(4):389-91"""
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        lbm = (0.32810 * weight) + (0.33929 * height) - 29.5336
+    else:
+        lbm = (0.29569 * weight) + (0.41813 * height) - 43.2933
+
+    return round(lbm, 1)
+
+
+def hume71(height: float, weight: float, sex: str) -> float:
+    """ returns lean body mass from Hume & Weyers(1971) """
+    """ Relationship between total body water and surface area in normal and obese subjects. Hume R, Weyers E J Clin Pathol 24 p234-8 (1971 Apr) """
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        lbm = (0.4066 * weight) + (0.2668 * height) - 19.19
+    else:
+        lbm = (0.2518 * weight) + (0.4720 * height) - 48.32
+
+    return round(lbm, 1)
+
+
+def janmahasation(height: float, weight: float, sex: str) -> float:
+    """ lean body mass as per Janmahasation / Han 2005 """
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+    bodymass = bmi(height, weight)
+
+    if sex == "m":
+        lbm = (9270 * weight) / (6680 + 216 * bodymass)
+    else:
+        lbm = (9270 * weight) / (8780 + 244 * bodymass)
+
+    return round(lbm, 1)
+
+
+def bmi(height: float, weight: float) -> float:
+    """calculates BMI"""
+    bmi = weight / ((height / 100) ** 2)
+    return round(bmi, 1)
+
+
+def idealbodyweight(height: float, sex: str) -> float:
+    """ ideal body weight as per ARDSnet/Devine  """
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        ibm = 50.0 + 0.91 * (height - 152.4)
+    else:
+        ibm = 45.5 + 0.91 * (height - 152.4)
+
+    return round(ibm, 1)
+
+
+def adjustedbodyweight(height: float, weight: float, sex: str) -> float:
+    """ adjusted body weight for obese patients """
+
+    ibw = idealbodyweight(height, sex)
+    abw = ibw + 0.4 * (weight - ibw)
+
+    return round(abw, 1)
+
+
+def alsallami(age: float, height: float, weight: float, sex: str) -> float:
+
+    if sex == "m":
+        alsal = (0.88 + (
+            0.12 / (1 + (age / 13.4) ** -12.7)
+        ) )* janmahasation(height, weight, sex)
+    else:
+        alsal = (1.11 + (
+            (1 - 1.11) / (1 + (age / 7.1) ** -1.1)
+        )) * janmahasation(height, weight, sex)
+
+    return alsal
---
+++ packages/PyTCI/PyTCI-1.1.tar.gz/PyTCI-1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTCI",
+    version="1.1",
+    author="Jakob Mathiszig-Lee",
+    author_email="jakob@mathisziglee.co.uk",
+    description="A package for target controlled infusions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JMathiszig-Lee/PyTCI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
