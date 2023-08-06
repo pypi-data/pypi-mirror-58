646eab056ec1cd3d84ff611427c9592f325d08a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:45 +0000

    Add package pydatacleaner/0.1.2.8/pydatacleaner-0.1.2.8.tar.gz

---
+++ packages/pydatacleaner/pydatacleaner-0.1.2.8.tar.gz/pydatacleaner-0.1.2.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("VERSION.txt", "r") as fh:
+    version = fh.read().strip()
+
+
+setuptools.setup(
+     name='pydatacleaner',
+     version=version,
+     author="Kevin Crouse",
+     author_email="krcrouse@gmail.com",
+     description="A utility designed to process/parse/clean scalars, especially text. (note: in active development)",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://gitlab.com/krcrouse/datacleaner",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+         "Development Status :: 3 - Alpha"
+     ],
+ )
