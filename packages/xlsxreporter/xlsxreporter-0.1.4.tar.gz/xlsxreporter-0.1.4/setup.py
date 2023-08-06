71ad12d4f01ae4252bde0a85d01f4f8a9cc1d384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:09 +0000

    Add package xlsxreporter/0.1.4/xlsxreporter-0.1.4.tar.gz

---
+++ packages/xlsxreporter/xlsxreporter-0.1.4.tar.gz/xlsxreporter-0.1.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['xlsxreporter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['xlsxwriter>=1.1,<2.0']
+
+setup_kwargs = {
+    'name': 'xlsxreporter',
+    'version': '0.1.4',
+    'description': 'A thin wrapper around xlsxwriter, to aid in generating row-by-row XLSX reports.',
+    'long_description': None,
+    'author': 'Jonas Myrlund',
+    'author_email': 'myrlund@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
