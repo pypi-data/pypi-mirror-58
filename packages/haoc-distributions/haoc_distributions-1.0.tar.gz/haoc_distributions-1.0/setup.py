5440cea81c4f4de88f86eef6d981bd2b7f4ddd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:50 +0000

    Add package haoc-distributions/1.0/haoc_distributions-1.0.tar.gz

---
+++ packages/haoc-distributions/haoc_distributions-1.0.tar.gz/haoc_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'haoc_distributions',
+      version = '1.0',
+      description = 'Gaussian distributions',
+      packages = ['haoc_distributions'],
+      author = 'Hao Cui',
+      author_email= 'chraggedbear@hotmail.com',
+    zip_safe = False)
