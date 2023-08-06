723b8a1bc5f41d1aa4de2ff3a0122b94ef325647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package cvopt/0.4.2/cvopt-0.4.2.tar.gz

---
+++ packages/cvopt/cvopt-0.4.2.tar.gz/cvopt-0.4.2/cvopt/model_selection/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+from ._search import SimpleoptCV, RandomoptCV, HyperoptCV, BayesoptCV, GAoptCV
+
+__all__ = ("SimpleoptCV", "RandomoptCV", "HyperoptCV", "BayesoptCV", "GAoptCV")
---
+++ packages/cvopt/cvopt-0.4.2.tar.gz/cvopt-0.4.2/setup.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+        name             = "cvopt",
+        version          = "0.4.2",
+        description      = "Parameter search and feature selection's class, Integrated visualization and archive log.",
+        license          = "BSD-2-Clause",
+        author           = "gen/5",
+        author_email     = "gen_fifth@outlook.jp",
+        url              = "https://github.com/genfifth/cvopt.git",
+        packages         = find_packages(),
+        install_requires = ["numpy>=1.14", 
+                            "pandas>=0.22.0", 
+                            "scikit-learn>=0.19.1", 
+                            "hyperopt>=0.1", 
+                            "networkx==1.11", 
+                            "GPy>=1.9.2", 
+                            "gpyopt>=1.2.1", 
+                            "bokeh>=0.12.14"],
+        )
