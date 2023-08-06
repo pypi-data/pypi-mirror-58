2922863b344c073a94f941ba04b89bc9a6dacd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package fastly/0.3.1/fastly-0.3.1.tar.gz

---
+++ packages/fastly/fastly-0.3.1.tar.gz/fastly-0.3.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+exec(open('fastly/_version.py').read())
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fastly",
+    version=__version__,
+    author="Fastly",
+    author_email="support@fastly.com",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities"
+    ],
+    description="Fastly python API",
+    keywords="fastly api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['fastly'],
+    install_requires=[
+        'six',
+    ],
+    scripts=[
+        "bin/purge_service",
+        "bin/purge_key",
+        "bin/purge_url"
+    ],
+    url="https://github.com/fastly/fastly-py",
+)
