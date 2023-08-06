5277c4838f0958daa71243b4c830a4ecdefa62f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:19 +0000

    Add package deepmdbenchmark/0.0.1/deepmdbenchmark-0.0.1.tar.gz

---
+++ packages/deepmdbenchmark/deepmdbenchmark-0.0.1.tar.gz/deepmdbenchmark-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name="deepmdbenchmark",
+    install_requires=['numpy', 'py-cpuinfo', 'leancloud', 'nvgpu'],
+    packages=['deepmdbenchmark'],
+    entry_points={
+        'console_scripts': ['deepmdbenchmark = deepmdbenchmark.benchmark:run']
+    },
+    package_data={
+        'deepmdbenchmark': ['*.json','data/type.raw','data/set.*/*.npy'],
+    },
+    version="0.0.1",
+)
