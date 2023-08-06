62164d69c028f15d3fb992b567705e86e2539fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package androidautotest/0.0.2.26/androidautotest-0.0.2.26-py3-none-any.whl

---
+++ packages/androidautotest/androidautotest-0.0.2.26-py3-none-any.whl/androidautotest/errors.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+class Error(Exception):
+    pass
+
+class NoDeviceError(Error):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class PictureNotFoundError(Error):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class PictureNotExistError(Error):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class PathNotExistError(Error):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class CaseNotFoundError(Error):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class CaseHasExistError(Error):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class ADBNotFoundError(Error):
+    def __init__(self):
+        self.message = r'ADB is not install in your computer, visit https://developer.android.google.cn/studio/releases/platform-tools'
+    def __str__(self):
+        return repr(self.message)
+
+class TesseractNotFoundError(Error):
+    def __init__(self):
+        self.message = r'Tesseract is not install in your computer, visit https://github.com/tesseract-ocr/tesseract'
+    def __str__(self):
+        return repr(self.message)
