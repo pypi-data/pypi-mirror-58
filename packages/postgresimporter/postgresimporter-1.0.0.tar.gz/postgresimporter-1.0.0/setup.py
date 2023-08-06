b99cbc2e2c8158f4720354369153e5144f64a00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:39 +0000

    Add package postgresimporter/1.0.0/postgresimporter-1.0.0.tar.gz

---
+++ packages/postgresimporter/postgresimporter-1.0.0.tar.gz/postgresimporter-1.0.0/setup.py
@@ -0,0 +1,51 @@
+from distutils.core import setup
+from pathlib import Path
+
+short_description = (
+    "A simple python wrapper script based on pgfutter "
+    "to load multiple dumped csv files into a postgres database."
+)
+
+try:
+    import m2r
+
+    long_description = m2r.parse_from_file(Path().parent / "README.md")
+except ImportError:
+    long_description = short_description
+
+setup(
+    name="postgresimporter",
+    packages=["postgresimporter"],
+    version="1.0.0",
+    license="MIT",
+    description=short_description,
+    long_description=long_description,
+    author="romnn",
+    author_email="contact@romnn.com",
+    url="https://github.com/romnnn/postgresimporter",
+    keywords=["postgres", "PostgreSQL", "database", "import", "load", "dump", "CSV"],
+    python_requires=">=3.6",
+    install_requires=[
+        "jinja2",
+        "dataclasses",
+        "progressbar2",
+        "chardet",
+        "prettytable",
+    ],
+    extras_require=dict(dev=["blessings", "pygments", "m2r", "pyfakefs"]),
+    package_data={"postgresimporter": ["hooks"]},
+    classifiers=[
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={"console_scripts": ["postgresimporter=postgresimporter:main"]},
+)
