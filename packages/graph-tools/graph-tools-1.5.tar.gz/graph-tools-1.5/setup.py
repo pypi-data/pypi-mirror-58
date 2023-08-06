c6694b2a79e3509e5fae627f963dd414f5d06068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:03 +0000

    Add package graph-tools/1.5/graph-tools-1.5.tar.gz

---
+++ packages/graph-tools/graph-tools-1.5.tar.gz/graph-tools-1.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graph-tools",
+    version="1.5",
+    author="Hiroyuki Ohsaki",
+    author_email="ohsaki@lsnl.jp",
+    description=
+    "tools for graph theory and network science with many generation models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h-ohsaki/graph-tools",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'perlcompat', 'pytess'],
+    scripts=['bin/graphgen'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
