50a846631c355a05196df796a648dc3e81248318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:25 +0000

    Add package naydikota1337/0.0.1/naydikota1337-0.0.1.tar.gz

---
+++ packages/naydikota1337/naydikota1337-0.0.1.tar.gz/naydikota1337-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="naydikota1337", # Replace with your own username
+    version="0.0.1",
+    author="maud",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aye",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
