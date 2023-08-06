c0f2ca87913cda15313ddfd7af285428aa7c2a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:49 +0000

    Add package springheel/5.2.3/springheel-5.2.3.tar.gz

---
+++ packages/springheel/springheel-5.2.3.tar.gz/springheel-5.2.3/springheel/command_line.py
@@ -0,0 +1,13 @@
+import springheel
+
+def build():
+    """Generate a Springheel site."""
+    springheel.build()
+
+def init():
+    """Initialize a Springheel project."""
+    springheel.init()
+
+def version():
+    """Print version information."""
+    springheel.version()
