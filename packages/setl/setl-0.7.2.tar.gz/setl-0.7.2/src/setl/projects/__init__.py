b7810aa9a290f87b70255034086f3c28b39e3730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:32 +0000

    Add package setl/0.7.2/setl-0.7.2.tar.gz

---
+++ packages/setl/setl-0.7.2.tar.gz/setl-0.7.2/src/setl/projects/__init__.py
@@ -0,0 +1,20 @@
+__all__ = ["Project"]
+
+from .base import BaseProject
+from .build import ProjectBuildManagementMixin
+from .dev import ProjectDevelopMixin
+from .hook import ProjectPEP517HookCallerMixin
+from .meta import ProjectMetadataMixin
+from .setup import ProjectSetupMixin
+
+
+# Order is important here for Mypy; more derived mixins need to come first.
+class Project(
+    ProjectDevelopMixin,
+    ProjectPEP517HookCallerMixin,
+    ProjectBuildManagementMixin,
+    ProjectMetadataMixin,
+    ProjectSetupMixin,
+    BaseProject,
+):
+    pass
