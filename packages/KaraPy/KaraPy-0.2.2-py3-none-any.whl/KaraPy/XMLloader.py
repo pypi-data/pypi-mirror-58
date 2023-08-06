5cbfa0bb011da91b8fa54d7b354a037d35bd654b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:37 +0000

    Add package KaraPy/0.2.2/KaraPy-0.2.2-py3-none-any.whl

---
+++ packages/KaraPy/KaraPy-0.2.2-py3-none-any.whl/KaraPy/XMLloader.py
@@ -0,0 +1,42 @@
+import xml.etree.ElementTree as ET 
+from KaraPy.KaraWorld import World
+from KaraPy.Enums import Type, Move
+
+def LoadWorldFile(source):
+    data = open(source)
+    tree = ET.parse(data)
+    root = tree.getroot()
+
+    sizex = int(root.attrib["sizex"]) 
+    sizey = int(root.attrib["sizey"]) 
+
+    size = 1
+    W = World(meshSize=size,size=(sizex,sizey))
+    W.build()
+    
+    T = root.find("XmlWallPoints")
+    for tree in T.findall("XmlPoint"):
+        x =  int(tree.attrib["x"])
+        y = sizey - int(tree.attrib["y"]) -1
+        W.Place(Type.Tree, (x,y))
+    L = root.find("XmlPaintedfieldPoints")
+    for leaf in L.findall("XmlPoint"):
+        x = int(leaf.attrib["x"])
+        y = sizey - int(leaf.attrib["y"]) -1
+        W.Place(Type.Leaf, (x,y))
+    K = root.find("XmlKaraList")
+    k = K.find("XmlKara")
+    x = int(k.attrib["x"])
+    y = sizey - int(k.attrib["y"]) -1
+    f = k.attrib["direction"]
+    
+    W.Place(Type.Kara, (x,y))
+
+    if f == "0":
+        W.Move(Move.TurnLeft)
+    if f == "1":
+        W.Move(Move.TurnLeft)
+        W.Move(Move.TurnLeft)
+    if f == "2":
+        W.Move(Move.TurnRight)
+    return W
