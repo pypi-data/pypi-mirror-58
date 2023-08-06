715269308e8225c4eef7251baade8dfd61630c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:57 +0000

    Add package appear/1.2/appear-1.2.tar.gz

---
+++ packages/appear/appear-1.2.tar.gz/appear-1.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="appear",
+    version="1.2",
+    description="Make program internals appear in the browser",
+    author="Andrew Houghton",
+    author_email="houghtonandrew0@gmail.com",
+    url="https://github.com/andrew-houghton/appear",
+    packages=["appear"],
+    install_requires=['Flask-SocketIO', 'grequests'],
+)
