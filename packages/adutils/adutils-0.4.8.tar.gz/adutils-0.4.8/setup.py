0f513b1e3619cd4efc454f0bf06709a84db9074c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:09 +0000

    Add package adutils/0.4.8/adutils-0.4.8.tar.gz

---
+++ packages/adutils/adutils-0.4.8.tar.gz/adutils-0.4.8/setup.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name='adutils',
+      version='0.4.8',
+      license='MIT',
+      url='http://github.com/benleb/adutils',
+      author='Ben Lebherz',
+      author_email='git@benleb.de',
+      description='Helper and utilities for AppDaemon apps',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      packages=['adutils'],
+      zip_safe=False,
+      platforms='any',
+      classifiers=[
+          "Topic :: Software Development",
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ]
+      )
