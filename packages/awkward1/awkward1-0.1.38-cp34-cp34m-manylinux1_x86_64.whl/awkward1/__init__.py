b4c7c2317975816357f1bb6df2aee0d3b491fd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:49 +0000

    Add package awkward1/0.1.38/awkward1-0.1.38-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/awkward1/awkward1-0.1.38-cp34-cp34m-manylinux1_x86_64.whl/awkward1/__init__.py
@@ -0,0 +1,28 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+namespace = {}
+
+import awkward1.layout
+from awkward1.layout import Type
+from awkward1.layout import UnknownType
+from awkward1.layout import PrimitiveType
+from awkward1.layout import ListType
+from awkward1.layout import RegularType
+from awkward1.layout import RecordType
+from awkward1.layout import OptionType
+from awkward1.layout import UnionType
+from awkward1.layout import ArrayType
+
+import awkward1._numba
+
+import awkward1.highlevel
+from awkward1.highlevel import Array
+from awkward1.highlevel import Record
+from awkward1.highlevel import FillableArray
+
+from awkward1.operations.convert import *
+from awkward1.operations.describe import *
+
+from awkward1.behavior.string import *
+
+__version__ = awkward1.layout.__version__
