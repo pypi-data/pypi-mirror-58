6868e7bcd1191242ba2e0099d9b59892bb4dccce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:21 +0000

    Add package KaraPy/0.4.7/KaraPy-0.4.7-py3-none-any.whl

---
+++ packages/KaraPy/KaraPy-0.4.7-py3-none-any.whl/KaraPy/KaraBrain.py
@@ -0,0 +1,55 @@
+from KaraPy.Enums import Sensor, Move
+
+class State(object):
+    global Transitions
+    global Name
+    global isStart
+    Transitions = []
+    Name = "Stanadrt"
+    isStart = False
+
+    def __init__(self, _Name, _isStart=False):
+        global Name, isStart
+        Name = _Name
+        isStart = _isStart
+
+    def AddTransition(self, t):
+        global Transitions
+        Transitions.append(t)
+        
+
+    def getStart(self):
+        global isStart
+        return isStart
+
+    def getTransitions(self):
+        global Transitions
+        return Transitions
+
+
+class Transition(object):
+    global condition
+    global output
+    global nextState
+    condition = [[Sensor.L, True]]
+    output = [Move.Down]
+    nextState = None
+
+    def __init__(self, _condition, _output=[], _nextState=None):
+        global condition, output, nextState
+        condition = _condition
+        output = _output
+        nextState = _nextState
+
+    def getConditions(self):
+        global condition
+        return condition
+
+    def getOutput(self):
+        global output
+        return output
+
+    def getNextState(self):
+        global nextState
+        return nextState
+
