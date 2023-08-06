b9840cec744c0c06123f786652d110c4e1bec6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:50 +0000

    Add package sagas/0.1.1/sagas-0.1.1-py3-none-any.whl

---
+++ packages/sagas/sagas-0.1.1-py3-none-any.whl/sagas/api/info_stack.py
@@ -0,0 +1,24 @@
+from sanic import Sanic
+
+from .ner_mod import ner_mod
+from .root_mod import bp
+from .info_mod import info
+
+app = Sanic(__name__)
+app.blueprint(bp)
+app.blueprint(info)
+app.blueprint(ner_mod)
+
+class InfoStack(object):
+    def run(self, port=1700, debug=True):
+        """
+        $ python -m sagas.api.info_stack run 1700 False
+        $ curl localhost:1700
+        """
+        app.run(host='0.0.0.0', port=1700, debug=debug)
+
+if __name__ == '__main__':
+    import fire
+    fire.Fire(InfoStack)
+
+
