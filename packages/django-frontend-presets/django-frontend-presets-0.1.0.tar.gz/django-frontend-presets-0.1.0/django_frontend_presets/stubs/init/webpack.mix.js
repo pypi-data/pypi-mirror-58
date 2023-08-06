22863994a06ba099c3771c6d3efd0274b9502651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package django-frontend-presets/0.1.0/django-frontend-presets-0.1.0.tar.gz

---
+++ packages/django-frontend-presets/django-frontend-presets-0.1.0.tar.gz/django-frontend-presets-0.1.0/django_frontend_presets/stubs/init/webpack.mix.js
@@ -0,0 +1,15 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/static/js/app.js', 'resources/static/js/dist')
+    .sass('resources/static/sass/app.scss', 'resources/static/css');
