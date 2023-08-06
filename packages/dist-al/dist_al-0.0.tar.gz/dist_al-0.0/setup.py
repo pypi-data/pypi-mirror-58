38241778c71c56ec6355de9ed7e723aa9e587c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:03 +0000

    Add package dist-al/0.0/dist_al-0.0.tar.gz

---
+++ packages/dist-al/dist_al-0.0.tar.gz/dist_al-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_al',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_al'],
+      author_email='mihajlovic.aleksa@gmail.com',
+      zip_safe=False)
