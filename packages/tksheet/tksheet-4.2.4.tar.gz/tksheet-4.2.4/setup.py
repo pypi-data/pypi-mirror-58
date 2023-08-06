e325e125ac03b993985fdd13c78ad3c143482206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:40 +0000

    Add package tksheet/4.2.4/tksheet-4.2.4.tar.gz

---
+++ packages/tksheet/tksheet-4.2.4.tar.gz/tksheet-4.2.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'tksheet',
+  packages = ['tksheet'],
+  version = '4.2.4',
+  license='MIT',
+  description = 'Tkinter table / sheet widget',
+  author = 'ragardner',
+  author_email = 'ragardner@protonmail.com',
+  url = 'https://github.com/ragardner/tksheet',
+  download_url = 'https://github.com/ragardner/tksheet/archive/4.2.4.tar.gz',
+  keywords = ['tkinter', 'table', 'widget'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3 :: Only'
+  ],
+)
